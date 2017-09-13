f8a40c6a67b3ebfbf3330019c63f6727d5e30ba8
Clemens Weninger <weninc@slac.stanford.edu>
2017-09-13 14:56:38 -0700

    cleanup and optimize the spscqueue

--- drp/spscqueue.hh
+++ drp/spscqueue.hh
@@ -2,81 +2,106 @@
 #define SPSCQUEUE_H
 
 #include <atomic>
-#include <condition_variable>
+#include <cassert>
 #include <mutex>
 #include <vector>
+#include <condition_variable>
 
-template<typename T>
+template <typename T>
 class SPSCQueue
 {
 public:
-    SPSCQueue(int capacity) : write_index(0), read_index(0), terminate(false)
+    SPSCQueue(int capacity) : m_terminate(false), m_write_index(0), m_read_index(0)
     {
-        buffer.resize(capacity);
-        buffer_mask = capacity - 1;
+        assert((capacity & (capacity - 1)) == 0);
+        m_ring_buffer.resize(capacity);
+        m_capacity = capacity;
+        m_buffer_mask = capacity - 1;
     }
+
+    SPSCQueue(const SPSCQueue&) = delete;
+    void operator=(const SPSCQueue&) = delete;
+
+    SPSCQueue(SPSCQueue&& d)
+    {
+        m_terminate.store(false);
+        m_write_index.store(d.m_write_index.load());
+        m_read_index.store(d.m_read_index.load());
+        m_ring_buffer = std::move(d.m_ring_buffer);
+        m_capacity = d.m_capacity;
+        m_buffer_mask = d.m_buffer_mask;
+    }
+
     void push(T value)
     {
-        int64_t index = write_index.load();
-        buffer[index & buffer_mask] = value;
+        int64_t index = m_write_index.load(std::memory_order_relaxed);
+        m_ring_buffer[index & m_buffer_mask] = value;
         int64_t next = index + 1;
-        write_index.store(next); // std::memory_order_release
+        m_write_index.store(next, std::memory_order_release);
+        // avoid reordering of the write_index store and the read_index load
+        asm volatile("mfence" ::: "memory");
         // signal consumer
-        if (index == read_index.load(std::memory_order_acquire)) {
-            std::unique_lock<std::mutex> lock(_mutex);
-            _condition.notify_one();
+        if (index == m_read_index.load(std::memory_order_acquire)) {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_condition.notify_one();
         }
     }
+
     bool pop(T& value)
     {
-        int64_t index = read_index.load();
+        int64_t index = m_read_index.load(std::memory_order_relaxed);
 
         // Queue is empty
-        if (index == write_index.load()) { // std::memory_order_acquire
-            std::unique_lock<std::mutex> lock(_mutex);
-            _condition.wait(lock, [this] { return !is_empty() || terminate.load(); });
-            if (terminate && is_empty()) {
+        if (index == m_write_index.load(std::memory_order_acquire)) {
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_condition.wait(lock, [this] {
+                return !is_empty() || m_terminate.load(std::memory_order_acquire);
+            });
+            if (m_terminate && is_empty()) {
                 return false;
             }
         }
 
-        value = buffer[index & buffer_mask];
+        value = m_ring_buffer[index & m_buffer_mask];
         int64_t next = index + 1;
-        read_index.store(next, std::memory_order_release);
+        m_read_index.store(next, std::memory_order_release);
         return true;
     }
+
     bool is_empty()
     {
-        return read_index.load() == write_index.load();
+        return m_read_index.load(std::memory_order_acquire) ==
+               m_write_index.load(std::memory_order_acquire);
     }
+
     int guess_size()
     {
-        int ret = write_index.load(std::memory_order_acquire) - read_index.load(std::memory_order_acquire);
-        return std::max(0, ret);
+        int ret = m_write_index.load(std::memory_order_acquire) -
+                  m_read_index.load(std::memory_order_acquire);
+        if (ret < 0) {
+            ret += m_capacity;
+        }
+        return ret;
     }
+
     void shutdown()
     {
         {
-            std::unique_lock<std::mutex> lock(_mutex);
-            terminate.store(true);
+            std::unique_lock<std::mutex> lock(m_mutex);
+            m_terminate.store(true, std::memory_order_release);
         }
-        _condition.notify_one();
+        m_condition.notify_one();
     }
 
 private:
-    std::mutex _mutex;
-    char _pad1[64 - sizeof(std::mutex)];
-
-    std::atomic<int64_t> write_index;
-    char _pad2[64 - sizeof(std::atomic<int64_t>)];
-
-    std::atomic<int64_t> read_index;
-    char _pad3[64 - sizeof(std::atomic<int64_t>)];
-
-    std::condition_variable _condition;
-    std::atomic<bool> terminate;
-    int64_t buffer_mask;
-    std::vector<T> buffer;
+    std::mutex m_mutex;
+    std::condition_variable m_condition;
+    std::atomic<bool> m_terminate;
+    int64_t m_buffer_mask, m_capacity;
+    std::vector<T> m_ring_buffer;
+    alignas(64) std::atomic<int64_t> m_write_index;
+    alignas(64) std::atomic<int64_t> m_read_index;
+    char _pad[64 - sizeof(std::atomic<int64_t>)];
 };
 
 #endif // SPSCQUEUE_H
