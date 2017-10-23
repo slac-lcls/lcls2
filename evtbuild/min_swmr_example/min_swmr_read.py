a1b8317feacf950e419e76cd736d90c4e7eeaabd
egmb <eliseo@slac.stanford.edu>
2017-10-23 10:00:23 -0700

    Added minimal working example of SWMR

---
+++ evtbuild/min_swmr_example/min_swmr_read.py
@@ -0,0 +1,63 @@
+import h5py, glob
+import numpy,time
+
+# Each client reads a single file.
+# It checks the size and, if it is greater than the number
+# of batches already read, reads the next chunk. 
+# Otherwise it waits for new data to be written  
+
+# Number of events to read at once
+batches = 10
+
+
+def client(file_list):
+
+    file_name = 'swmr_file.py'
+
+
+    with h5py.File(file_name, 'r', swmr=True) as f:
+        dset = f["data"]
+        file_cap = dset.shape[0]
+        written_batches = file_cap/batch_size 
+
+        print('Reading %s' % file_name)
+        batch_num = 0
+
+        # Main loop over the events in dset
+        while True:
+            # Check the current batch_num against the size of the dset
+            # if batch_num is equal to the current size of the dset
+            # refresh it
+            # Otherwise break
+
+            while True:
+                start = time.time()
+                if batch_num == written_batches:
+                    dset.id.refresh()
+                    file_cap = dset.shape[0]
+                    written_batches = file_cap/batch_size 
+                if batch_num == written_batches:
+                    time.sleep(0.05)
+                elif batch_num < written_batches:
+                    break
+            # while True:
+            #     start = time.time()
+            #     dset.id.refresh()
+            #     file_cap = dset.shape[0]
+            #     written_batches = file_cap/batch_size 
+            #     if written_batches > batch_num:
+            #         break
+
+            # This line reads the dset
+            crcio = dset[batch_num*batch_size:(batch_num+1)*batch_size][:]
+            cr_mbytes = crcio.nbytes/10**6
+            end = time.time()
+#            sleep_time = 0.0*(end-start)
+#            time.sleep(sleep_time)
+            cr_speed = cr_mbytes/((end-start)+sleep_time)
+            
+            print('Read data batch %i/%i at %i MB/s' %(batch_num, written_batches, cr_speed))
+            batch_num += 1
+
+            
+client()
---
+++ evtbuild/min_swmr_example/min_swmr_write.py
@@ -0,0 +1,57 @@
+import h5py
+import numpy as np
+import time
+
+# Prepare all the files for writing and reading
+
+file_name = 'swmr_file.py'
+batch_size = 10
+
+# Function for creating random image arrays
+# Some SSD controllers (SandForce) compress data before writing
+# which could lead to erroneous results if the data were patterned
+mb_per_img = 16
+arr_size = mb_per_img*500000
+
+def create_image(mb_per_img):
+    image = np.random.randint(0,2**16,size=(250,250,4), dtype='uint16')
+    arr = np.tile(image,2*mb_per_img)
+    arr = arr.ravel()
+    return arr
+
+
+# Create the HDF file in SWMR mode
+f = h5py.File(file_name, 'w', libver='latest')
+dset = f.create_dataset("data", shape = (1,arr_size), chunks = (batch_size, arr_size), maxshape=(None,arr_size), dtype = 'uint16')
+f.swmr_mode = True
+
+
+# Main loop to write out the data
+try:
+    while True:
+        start = time.time()
+        shape = f["data"].shape
+        batch_num = (shape[0]/batch_size) +1
+
+
+        f['data'].resize((shape[0]+batch_size, shape[1]))
+
+        out_img = np.array([create_image() for x in range(batch_size)])
+
+
+        f['data'][-batch_size:] = out_img
+        f.flush()
+
+        end = time.time()
+        cr_speed = out_img.nbytes/(10**6*(end-start))
+        print('Data batch %i written at %i MB/s' % (batch_num, cr_speed))
+
+finally:
+    f.close()
+
+
+
+
+
+
+
