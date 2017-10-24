60c6770f01c08ce72dffe7b3a07a3161e9745ff6
Clemens Weninger <clemens@m-info.de>
2017-10-24 16:23:31 -0700

    Update TimeStamp.cc

--- xtcdata/xtc/src/TimeStamp.cc
+++ xtcdata/xtc/src/TimeStamp.cc
@@ -9,84 +9,68 @@
 
 namespace XtcData
 {
-enum { v_ticks = 0, k_ticks = 24 };
-enum { v_cntrl = 24, k_cntrl = 8 };
-enum { m_ticks = ((1 << k_ticks) - 1), s_ticks = (m_ticks << v_ticks) };
-enum { m_cntrl = ((1 << k_cntrl) - 1), s_cntrl = (m_cntrl << v_cntrl) };
-enum { v_fiduc = 0, k_fiduc = TimeStamp::NumFiducialBits };
-enum { v_vecto = TimeStamp::NumFiducialBits, k_vecto = 32 - TimeStamp::NumFiducialBits };
-enum { m_fiduc = ((1 << k_fiduc) - 1), s_fiduc = (m_fiduc << v_fiduc) };
-enum { m_vecto = ((1 << k_vecto) - 1), s_vecto = (m_vecto << v_vecto) };
+    enum { v_pulseId = 0, k_pulseId = 56 };
+    enum { v_cntrl = 56, k_cntrl = 8 };
+    enum { m_pulseId = ((1 << k_pulseId) - 1), s_pulseId = (m_pulseId << v_pulseId) };
+    enum { m_cntrl = ((1 << k_cntrl) - 1), s_cntrl = (m_cntrl << v_cntrl) };
 }
 
-XtcData::TimeStamp::TimeStamp() : _low(0), _high(0)
+XtcData::TimeStamp::TimeStamp() : _value(0)
 {
 }
 
 XtcData::TimeStamp::TimeStamp(const XtcData::TimeStamp& input)
-: _low(input._low), _high(input._high)
+: _value(input._value)
 {
 }
 
 XtcData::TimeStamp::TimeStamp(const XtcData::TimeStamp& input, unsigned control)
-: _low((input._low & s_ticks) | ((control & m_cntrl) << v_cntrl)), _high(input._high)
+: _value((input._value & s_ticks) | ((control & m_cntrl) << v_cntrl))
 {
 }
 
-XtcData::TimeStamp::TimeStamp(unsigned low, unsigned high, unsigned vector, unsigned control)
-: _low((low & s_ticks) | ((control & m_cntrl) << v_cntrl)),
-  _high((high & s_fiduc) | ((vector & m_vecto) << v_vecto))
+unsigned XtcData::TimeStamp::pulseId() const
 {
-}
-
-unsigned XtcData::TimeStamp::ticks() const
-{
-    return (_low & s_ticks) >> v_ticks;
-}
-
-unsigned XtcData::TimeStamp::fiducials() const
-{
-    return (_high & s_fiduc) >> v_fiduc;
+    return (_value & s_pulseId) >> v_pulseId;
 }
 
 unsigned XtcData::TimeStamp::control() const
 {
-    return (_low & s_cntrl) >> v_cntrl;
-}
-
-unsigned XtcData::TimeStamp::vector() const
-{
-    return (_high & s_vecto) >> v_vecto;
+    return (_value & s_cntrl) >> v_cntrl;
 }
 
 XtcData::TimeStamp& XtcData::TimeStamp::operator=(const XtcData::TimeStamp& input)
 {
-    _low = input._low;
-    _high = input._high;
+    _value = input._value;
     return *this;
 }
 
 bool XtcData::TimeStamp::operator==(const XtcData::TimeStamp& ref) const
 {
-    return fiducials() == ref.fiducials();
+    return pulseId() == ref.pulseId();
+}
+
+bool XtcData::TimeStamp::operator!=(const XtcData::TimeStamp& ref) const
+{
+    return pulseId() != ref.pulseId();
 }
 
 bool XtcData::TimeStamp::operator>=(const XtcData::TimeStamp& ref) const
 {
-    return fiducials() >= ref.fiducials();
+    return pulseId() >= ref.pulseId();
 }
 
 bool XtcData::TimeStamp::operator<=(const XtcData::TimeStamp& ref) const
 {
-    return fiducials() <= ref.fiducials();
+    return pulseId() <= ref.pulseId();
 }
 
 bool XtcData::TimeStamp::operator>(const XtcData::TimeStamp& ref) const
 {
-    return fiducials() > ref.fiducials();
+    return pulseId() > ref.pulseId();
 }
 
 bool XtcData::TimeStamp::operator<(const XtcData::TimeStamp& ref) const
 {
-    return fiducials() < ref.fiducials();
+    return pulseId() < ref.pulseId();
 }
