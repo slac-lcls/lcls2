b399b959827356d452dfb75d0d989180f690f7b3
Clemens Weninger <clemens@m-info.de>
2017-10-24 16:15:32 -0700

    Update TimeStamp.hh

--- xtcdata/xtc/TimeStamp.hh
+++ xtcdata/xtc/TimeStamp.hh
@@ -8,35 +8,29 @@ namespace XtcData
 class TimeStamp
 {
 public:
-    enum { NumFiducialBits = 17 };
-    enum { MaxFiducials = (1 << 17) - 32 };
-    enum { ErrFiducial = (1 << 17) - 1 };
-
+    enum { NumPulseIdBits = 56 };
+   
 public:
     TimeStamp();
     TimeStamp(const TimeStamp&);
     TimeStamp(const TimeStamp&, unsigned control);
-    TimeStamp(unsigned ticks, unsigned fiducials, unsigned vector, unsigned control = 0);
+    TimeStamp(unsigned pulseId, unsigned control = 0);
 
 public:
-    unsigned ticks() const; // 119MHz counter within the fiducial for
-    //   eventcode which initiated the readout
-    unsigned fiducials() const; // 360Hz pulse ID
+    unsigned pulseId() const; // 929kHz pulse ID
     unsigned control() const; // internal bits for alternate interpretation
     //   of XTC header fields
-    unsigned vector() const; // 15-bit seed for event-level distribution
-    //   ( events since configure )
 public:
     TimeStamp& operator=(const TimeStamp&);
     bool operator==(const TimeStamp&) const;
+    bool operator!=(const TimeStamp&) const;
     bool operator>=(const TimeStamp&) const;
     bool operator<=(const TimeStamp&) const;
     bool operator<(const TimeStamp&) const;
     bool operator>(const TimeStamp&) const;
 
 private:
-    uint32_t _low;
-    uint32_t _high;
+    uint64_t _value;
 };
 }
 
