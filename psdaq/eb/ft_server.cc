476ac50607987dc372cf4290b32860acbe442791
Daniel Damiani <ddamiani@slac.stanford.edu>
2017-10-25 09:16:01 -0700

    added proper help usage to ft_server/ft_client

--- psdaq/eb/ft_server.cc
+++ psdaq/eb/ft_server.cc
@@ -1,10 +1,24 @@
-#include "Endpoint.hh"
+#include "psdaq/eb/Endpoint.hh"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 using namespace Pds::Fabrics;
+
+static const char* PORT_DEF = "12345";
+static const size_t BUFFER_DEF = 800;
+
+static void showUsage(const char* p)
+{
+  printf("Simple libfabrics server example (its pair is the ft_client example).\n\n"
+         "Usage: %s [-h|--help]\n"
+         "    -a|--addr     the local address to which the server binds (default: libfabrics 'best' choice)\n"
+         "    -b|--buffer   the size of the local memory buffer to allocate (default: %lu bytes)\n"
+         "    -p|--port     the port or libfaric 'service' the server will use (default: %s)\n"
+         "    -h|--help     print this message and exit\n", p, BUFFER_DEF, PORT_DEF);
+}
 
 int listen(PassiveEndpoint* pendp, MemoryRegion* mr, char* key_buff, size_t key_size, char* data_buff)
 {
@@ -62,17 +76,72 @@ int listen(PassiveEndpoint* pendp, MemoryRegion* mr, char* key_buff, size_t key_
 int main(int argc, char *argv[])
 {
   int ret = FI_SUCCESS;
-  size_t buff_size = 32*1024*1024;
-  char* buff = new char[buff_size];
-  char* data_buff = buff + sizeof(RemoteAddress);
-  
+  bool show_usage = false;
+  const char* addr = NULL;
+  const char* port = PORT_DEF;
+  size_t buff_size = sizeof(RemoteAddress);
+  size_t data_size = BUFFER_DEF;
+  char* buff = NULL;
+  char* data_buff = NULL;
+
+  const char* str_opts = ":ha:b:p:";
+  const struct option lo_opts[] = 
+  {
+    {"help",    0, 0, 'h'},
+    {"addr",    1, 0, 'a'},
+    {"buffer",  1, 0, 'b'},
+    {"port",    1, 0, 'p'},
+    {0,         0, 0,  0 }
+  };
+
+  int option_idx = 0;
+  while (int opt = getopt_long(argc, argv, str_opts, lo_opts, &option_idx )) {
+    if ( opt == -1 ) break;
+
+    switch(opt) {
+      case 'h':
+        showUsage(argv[0]);
+        return 0;
+      case 'a':
+        addr = optarg;
+        break;
+      case 'b':
+        data_size = strtoul(optarg, NULL, 0);
+        break;
+      case 'p':
+        port = optarg;
+        break;
+      default:
+        show_usage = true;
+        break;
+    }
+  }
+
+  if (optind < argc) {
+    printf("%s: invalid argument -- %s\n", argv[0], argv[optind]);
+    show_usage = true;
+  }
+
+  if (!data_size) {
+    printf("%s: invalid buffer size requested -- %lu\n", argv[0], data_size);
+    return 1;
+  }
+
+  if (show_usage) {
+    showUsage(argv[0]);
+    return 1;
+  }
+
+  buff_size += data_size;
+  buff = new char[buff_size];
+  data_buff = buff + sizeof(RemoteAddress);
   
   ((uint64_t*) data_buff)[1] = 0xadd;
   ((uint64_t*) data_buff)[2] = 0xdeadbeef;
-  for (unsigned i=3; i< ((buff_size-sizeof(RemoteAddress)) / sizeof(uint64_t)); i++)
+  for (unsigned i=3; i< (data_size / sizeof(uint64_t)); i++)
     ((uint64_t*) data_buff)[i] = i;
 
-  PassiveEndpoint* pendp = new PassiveEndpoint(NULL, "1234");
+  PassiveEndpoint* pendp = new PassiveEndpoint(addr, port);
   if (pendp->state() != EP_UP) {
     fprintf(stderr, "Failed to initialize fabrics endpoint: %s\n", pendp->error());
     return pendp->error_num();
@@ -80,13 +149,14 @@ int main(int argc, char *argv[])
 
   // get a pointer to the fabric
   Fabric* fab = pendp->fabric();
+  printf("using %s provider\n", fab->provider());
   // register the memory buffer
   MemoryRegion* mr = fab->register_memory(buff, buff_size);
   if (!mr) {
     fprintf(stderr, "Failed to register memory region: %s\n", fab->error());
     return fab->error_num();
   }
-  RemoteAddress keys(mr->rkey(), (uint64_t) data_buff, buff_size-sizeof(RemoteAddress));
+  RemoteAddress keys(mr->rkey(), (uint64_t) data_buff, data_size);
   memcpy(buff, &keys, sizeof(keys));
 
   ret = listen(pendp, mr, buff, sizeof(keys), data_buff);
