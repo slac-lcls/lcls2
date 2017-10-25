04ad18dcb97682a5b20b5110c9d59d9adaab7c2b
Randall J. Radmer <radmer@gmail.com>
2017-10-25 09:48:43 -0700

    Clean lots of verbose info from DataSourceContainer.py

--- testDgram/DataSourceContainer.py
+++ testDgram/DataSourceContainer.py
@@ -6,11 +6,10 @@ import time
 import getopt
 #import pprint
 
-#import inspect;
-import gc
-
 sys.path.append('../build/xtcdata')
 import dgram
+
+import gc
 #
 
 class DataSource:
@@ -30,7 +29,6 @@ class DataSource:
                       debug=self.debug)
         for key in sorted(d.__dict__.keys()):
             setattr(self, key, getattr(d, key))
-            print("key:", key)
         return self
 
     def get_verbose(self):
@@ -47,16 +45,14 @@ class DataSource:
 
 
 def parse_command_line():
-    opts, args_proper = getopt.getopt(sys.argv[1:], 'hvd:gf:')
+    opts, args_proper = getopt.getopt(sys.argv[1:], 'hvd:f:')
     verbose=0
     debug=0
-    gc_info=False
     xtcdata_filename="data.xtc"
     for option, parameter in opts:
         if option=='-h': usage_error()
         if option=='-v': verbose+=1
         if option=='-d': debug = int(parameter)
-        if option=='-g': gc_info = True
         if option=='-f': xtcdata_filename = parameter
     if xtcdata_filename is None:
         xtcdata_filename="data.xtc"
@@ -66,59 +62,31 @@ def parse_command_line():
         sys.stdout.write("debug: %d\n" % debug)
     elif debug>0:
         sys.stdout.write("debug: %d\n" % debug)
-    return (args_proper, xtcdata_filename, verbose, debug, gc_info)
+    return (args_proper, xtcdata_filename, verbose, debug)
 
 def getMemSize():
     pid=os.getpid()
     ppid=os.getppid()
     cmd="/usr/bin/ps -q %d --no-headers -eo size" % pid
     p=os.popen(cmd)
-    size=int(p.read()) * 1024
+    size=int(p.read())
     return size
 
-def show_garbage(header=""):
-    gc.collect()
-    if header:
-        print("%s\n" % header)
-    for x in gc.garbage:
-        s = str(x)
-        if len(s) > 120: s = s[:120]
-        print(type(x),"\n  ", s)
-
 def main():
-    args_proper, xtcdata_filename, verbose, debug, gc_info = parse_command_line()
-#    if gc_info:
-#        gc.enable()
-#        gc.set_debug(gc.DEBUG_LEAK)
-
-    gc.disable()
+    args_proper, xtcdata_filename, verbose, debug = parse_command_line()
     size0=getMemSize()
-    for ii in range(10):
-        ds=DataSource(xtcdata_filename, verbose=verbose, debug=debug)
-        evt=ds.__next__
-        print(dir(evt))
-#        a=evt.array0_pgp
-        size=getMemSize()
-        #print("%d--ds, getMemSize():" % ii, size, size-size0)
-        #print("%d--refcnt(ds), sys.getrefcount(ds):" % ii, sys.getrefcount(ds))
-        #print("%d--refcnt(evt), sys.getrefcount(evt):" % ii, sys.getrefcount(evt))
-        #print("%d--refcnt(a), sys.getrefcount(a):" % ii, sys.getrefcount(a))
-        size0=size
+    ds=DataSource(xtcdata_filename, verbose=verbose, debug=debug)
+    for evt in ds:
+        a0=evt.array0_pgp
+        del(evt)
+        del(a0)
+        gc.collect()
         time.sleep(1)
-
-#    ds=DataSource(xtcdata_filename, verbose=verbose, debug=debug)
-#    for evt in ds:
-#        #a=evt.array0_pgp
-#        #print(a)
-#        del(evt)
-#        if gc_info:
-#            show_garbage("GC: del(evt)")
-#
-#    del(ds)
-    
-#    if gc_info:
-#        show_garbage("GC: del(ds)")
-
+    del(ds)
+    gc.collect()
+    time.sleep(2)
+    size1=getMemSize()
+    print("size=%d; Delta size=%d" % (size1, size1-size0))
     return
 
 def usage_error():
