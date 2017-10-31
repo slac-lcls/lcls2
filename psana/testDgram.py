0a5718229f0842255598324c931c589e57940340
Randall J. Radmer <radmer@gmail.com>
2017-10-31 08:00:34 -0700

    fix refcount in testDgram.py

--- psana/testDgram.py
+++ psana/testDgram.py
@@ -3,7 +3,7 @@
 
 import sys
 from sys import getrefcount as getref
-sys.path.append('../build/xtcdata')
+sys.path.append('../build/psana')
 from dgram import Dgram
 import numpy as np
 from DataSource import DataSource
@@ -11,33 +11,33 @@ from DataSource import DataSource
 
 def myroutine2():
   ds = DataSource('data.xtc')
+  assert getref(ds)==2
   d = ds.__next__()
   assert getref(d)==3
-  arr1 = d.array0_pgp #do not increase refcount of d
-  assert getref(d)==3
+  arr1 = d.array0_pgp
+  dgramObj=arr1.base
   assert getref(arr1)==3
-  s1 = arr1[2:4] #increase refcount of arr1
+  assert getref(dgramObj)==5
+  s1 = arr1[2:4]
   assert s1.base is arr1
   assert getref(arr1)==4
   assert getref(s1)==2
 
-  arr2 = d.array0_pgp #do not increase refcount of d
-  assert getref(d)==3
-  s2 = arr2[3:5] # increase refcount of arr2
-  assert getref(d)==3
+  arr2 = d.array0_pgp
+  assert getref(dgramObj)==5
+  s2 = arr2[3:5]
+  assert getref(dgramObj)==5
   assert getref(arr2)==6
 
-  return s1,d # destroys ds, should decrement d refcnt by 1
+  return s1,d
 
 def myroutine1():
   s1,d =  myroutine2()
-  assert getref(d)==2
   assert getref(s1)==2
 
   return d
 
 d = myroutine1()
-assert getref(d)==2
 assert d.float_pgp==1.0
 assert d.int_pgp==2
 testarr = np.array([[0,0,0],[0,1,2],[0,2,4]],dtype=np.float32)
