9e6d3826c0c27afb6bae4f1e0f6cf1e7ef2193ee
Christopher O'Grady <cpo@slac.stanford.edu>
2017-12-01 15:29:47 -0800

    integrate eb with drp

--- drp/main.cc
+++ drp/main.cc
@@ -19,13 +19,63 @@
 #include "xtcdata/xtc/NamesIter.hh"
 #include "psdaq/hdf5/Hdf5Writer.hh"
 
+#include "psdaq/eb/BatchManager.hh"
+#include "psdaq/eb/EbFtClient.hh"
+#include "psdaq/eb/EbFtServer.hh"
+
 #include "main.hh"
 #include "spscqueue.hh"
 #include "PgpCardMod.h"
 
 using PebbleQueue = SPSCQueue<Pebble*>;
 using namespace XtcData;
+using namespace Pds::Eb;
 
+// these parameters must agree with the server side
+unsigned maxBatches = 1000; // size of the pool of batches
+unsigned maxEntries = 10; // maximum number of events in a batch
+unsigned BatchSizeInPulseIds = 8; // age of the batch. should never exceed maxEntries above, must be a power of 2
+
+unsigned EbId = 0; // from 0-63, maximum number of event builders
+unsigned ContribId = 0; // who we are
+
+class TheSrc : public Src
+{
+public:
+    TheSrc(Level::Type level, unsigned id) :
+        Src(level)
+    {
+        _log |= id;
+    }
+};
+
+class MyDgram : public Dgram {
+public:
+    MyDgram(unsigned pulseId, uint64_t val) {
+        seq = Sequence(Sequence::Event, TransitionId::L1Accept, ClockTime(), TimeStamp(pulseId));
+        env = Env(0);
+        xtc = Xtc(TypeId(TypeId::Data, 0), TheSrc(Level::Segment, ContribId));
+        _data = val;
+        xtc.alloc(sizeof(_data));
+    }
+private:
+    uint64_t _data;
+};
+
+size_t maxSize = sizeof(MyDgram);
+
+class MyBatchManager: public BatchManager {
+public:
+    MyBatchManager(EbFtClient& ebFtClient) :
+        BatchManager(TheSrc(Level::Event, ContribId), BatchSizeInPulseIds, maxBatches, maxEntries, maxSize),
+        _ebFtClient(ebFtClient)
+    {}
+    void post(Batch* batch) {
+        _ebFtClient.post(batch->buffer(), batch->extent(), EbId, batch->index() * maxBatchSize());
+    }
+private:
+    EbFtClient& _ebFtClient;
+};
 
 struct EventHeader {
     uint64_t pulseId;
@@ -114,13 +164,50 @@ private:
 };
 
 
-void pgp_reader(SPSCQueue<uint32_t>& index_queue, PebbleQueue& pgp_queue, uint32_t** dma_buffers, SPSCQueue<int>& collector_queue, std::vector<PebbleQueue>& worker_input_queues, unsigned first_lane)
+void eb_rcvr(MyBatchManager& myBatchMan)
 {
-    int nlanes = 4;
+    std::string srvPort = "32832"; // add 64 to the client base
+    unsigned numEb = 1;
+    size_t maxBatchSize = sizeof(Dgram) + maxEntries * maxSize;
+    EbFtServer myEbFtServer(srvPort, numEb, maxBatches * maxBatchSize, EbFtServer::PEERS_SHARE_BUFFERS);
+    printf("*** rcvr %d %zd\n",maxBatches,maxBatchSize);
+    myEbFtServer.connect(ContribId);
+    unsigned nreceive = 0;
+    unsigned none = 0;
+    unsigned nzero = 0;
+    while(1) {
+        Dgram* batch = (Dgram*)myEbFtServer.pend();
+        unsigned idx = ((char*)batch - myEbFtServer.base()) / maxBatchSize;
+        // printf("received batch %p %d\n",batch,idx);
+        const Batch*  input  = myBatchMan.batch(idx, batch->seq.stamp().pulseId());
+
+        const Dgram*  result = (Dgram*)batch->xtc.payload();
+        const Dgram*  last   = (Dgram*)batch->xtc.next();
+        while(result != last) {
+            nreceive++;
+            // printf("--- result %lx\n",*(uint64_t*)(result->xtc.payload()));
+            uint64_t val = *(uint64_t*)(result->xtc.payload());
+            result = (Dgram*)result->xtc.next();
+            if (val==0) {
+                nzero++;
+            } else if (val==1) {
+                none++;
+            } else {
+                printf("error %ld\n",val);
+            }
+            if (nreceive%10000==0) printf("%d %d %d\n",nreceive,none,nzero);
+        }
+        delete input;
+    }
+}
+
+void pgp_reader(SPSCQueue<uint32_t>& index_queue, PebbleQueue& pgp_queue, uint32_t** dma_buffers, SPSCQueue<int>& collector_queue, std::vector<PebbleQueue>& worker_input_queues, std::vector<unsigned> lanes)
+{
+    int nlanes = lanes.size();
 
     // open pgp card fd
     char dev_name[128];
-    snprintf(dev_name, 128, "/dev/pgpcardG3_0_%u", first_lane+1);
+    snprintf(dev_name, 128, "/dev/pgpcardG3_0_%u", lanes[0]+1);
     int fd = open(dev_name, O_RDWR);
     if (fd < 0) {
         std::cout << "Failed to open pgpcard" << dev_name << std::endl;
@@ -369,7 +456,8 @@ public:
     int save(Dgram& dgram)
     {
         if (fwrite(&dgram, sizeof(Dgram) + dgram.xtc.sizeofPayload(), 1, file) != 1) {
-            printf("Error writing to output xtc file.\n");
+            printf("Error writing to output xtc file. %p %lu\n",&dgram, sizeof(Dgram) + dgram.xtc.sizeofPayload());
+            perror("save");
             _exit(1);
         }
         fflush(file);
@@ -382,12 +470,18 @@ public:
             iovlen+=iov[i].iov_len;
         }
         if (fwrite(&dgram, sizeof(Dgram) + dgram.xtc.sizeofPayload()-iovlen, 1, file) != 1) {
-            printf("Error writing to output xtc file.\n");
+            printf("Error writing to output xtc file. %p %lu\n",&dgram,sizeof(Dgram) + dgram.xtc.sizeofPayload()-iovlen);
+            perror("saveIov hdr");
             _exit(1);
         }
         for (int i=0; i<iovcnt; i++) {
+            if (iov[i].iov_len==0) {
+                printf("zero len iov\n");
+                continue;
+            }
             if (fwrite(iov[i].iov_base, iov[i].iov_len, 1, file) != 1) {
-                printf("Error writing IOV to output xtc file.\n");
+                printf("Error writing IOV to output xtc file %p %lu.\n",iov[i].iov_base, iov[i].iov_len);
+                perror("saveIov");
                 _exit(1);
             }
         }
@@ -402,6 +496,24 @@ int main()
 {
     int queue_size = 16384;
     std::vector<unsigned> lanes = {0,1,2,3}; // must be contiguous
+
+    // StringList peers;
+    // peers.push_back("172.21.52.136"); //acc05
+    // StringList port;
+    // port.push_back("32768");
+    // size_t rmtSize = maxBatches * (sizeof(Dgram) + maxEntries * maxSize);
+    // printf("*** rmtsize %zd\n",rmtSize);
+    // EbFtClient myEbFtClient(peers,port,rmtSize);
+
+    // MyBatchManager myBatchMan(myEbFtClient);
+
+    // unsigned timeout = 120;
+    // myEbFtClient.connect(ContribId, timeout);
+
+    // myEbFtClient.registerMemory(myBatchMan.batchRegion(), myBatchMan.batchRegionSize());
+    // printf("*** myEb %p %zd\n",myBatchMan.batchRegion(), myBatchMan.batchRegionSize());
+    // // start eb receiver thread
+    // std::thread eb_rcvr_thread(eb_rcvr, std::ref(myBatchMan));
 
     // pin main thread
     pin_thread(pthread_self(), 1);
@@ -434,7 +546,7 @@ int main()
 
     // start pgp reader thread
     std::thread pgp_thread(pgp_reader, std::ref(index_queue), std::ref(pebble_queue), dma_buffers, std::ref(collector_queue),
-                           std::ref(worker_input_queues), lanes[0]);
+                           std::ref(worker_input_queues), lanes);
     pin_thread(pgp_thread.native_handle(), 2);
 
     // start worker threads
@@ -459,6 +571,16 @@ int main()
         worker_output_queues[worker].pop(pebble_data);
 
         Dgram& dgram = *reinterpret_cast<Dgram*>(pebble_data->fex_data());
+
+        // uint64_t val;
+        // if (i%3==0) {
+        //     val = 0xdeadbeef;
+        // } else {
+        //     val = 0xabadcafe;
+        // }
+        // MyDgram dg(i,val);
+        // myBatchMan.process(&dg);
+
         if (i==0) {
             xtcfile.save(dgram);
         } else {
