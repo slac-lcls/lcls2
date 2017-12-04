d4bbc01c047c3e7c9e7d48486c936f7636a239c5
egmb <eliseo@slac.stanford.edu>
2017-12-04 14:29:00 -0800

    Updated XTC mode

--- evtbuild/xtc_lite/read_xtc_lite.py
+++ evtbuild/xtc_lite/read_xtc_lite.py
@@ -4,23 +4,30 @@ import numpy as np
 import time
 
 #logistical MPI setup
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
 #assert size>1, 'At least 2 MPI ranks required'
 
 
 import os
 
-mb_per_img = 16
-batches = 16
-arr_size = mb_per_img*500000
-file_name = '/ztest/eliseo/data/xtc_lite/xtc_lite_%i.xtc' % rank
 
-def read_client():
+def read_client(comm):
 
-#    file_name = '../neh_dir/xtc_lite/xtc_lite.xtc'
- 
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    mb_per_img = 1
+    batches = 16
+    arr_size = mb_per_img*500000
+    path = '/drpffb/eliseo/data/xtc_lite/'
+    file_name = path + 'xtc_lite_%i.xtc' % rank
+
+    # Check if the file exists
+    while True:
+        if os.path.exists(file_name):
+            break
+        else:
+            time.sleep(0.05)
+    
 
     with open(file_name, 'rb') as f:
         f.seek(0)
@@ -31,34 +38,48 @@ def read_client():
             file_info = os.stat(file_name)
             size_file_mb = os.stat(file_name).st_size/10**6
 
-            # if ct + 1000 > size_file_mb:
-            #     print('Near end of file. Waiting for more data')
-            #     while True:
-            #         time.sleep(0.1)
-            #         size_file_mb = os.stat(file_name).st_size/10**6
+            if ct + 1000 > size_file_mb and size_file_mb < 19000:
+#                print('Near end of file. Waiting for more data')
+                while True:
+                    time.sleep(0.1)
+                    size_file_mb = os.stat(file_name).st_size/10**6
                 
-            #         if ct + 2000 < size_file_mb:
-            #             break
+                    if ct + 2000 < size_file_mb:
+                        break
 
             img = f.read(mb_per_img*batches*10**6)
             ct+=1
             if ct%100 == 0:
-                print('Read image %i' % ct)
+                pass
+#                print('Read image %i' % ct)
             if img == '':
                 break
 
-comm.Barrier()
-if rank == 0:
-    size_file_mb = os.stat(file_name).st_size/10**6
-    global_start = time.time()
-    
-read_client()
+def do_read(comm):
+   # comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-comm.Barrier()
-if rank == 0:
-    global_end = time.time()
-    wrt_gb = size*size_file_mb/1000
-    av_spd = wrt_gb/(global_end-global_start)
-    print('Number of clients %i' % (size))
-    print('File size %i' % wrt_gb) 
-    print('Read %.2f GB at an average of %.2f GB/s' % (wrt_gb, av_spd))
+    mb_per_img = 1
+    batches = 16
+    arr_size = mb_per_img*500000
+    path = '/drpffb/eliseo/data/xtc_lite/'
+    file_name = path + 'xtc_lite_%i.xtc' % rank
+
+
+    comm.Barrier()
+    if rank == 0:
+       # 
+        global_start = time.time()
+
+    read_client(comm)
+
+    comm.Barrier()
+    if rank == 0:
+        size_file_mb = os.stat(file_name).st_size/10**6
+        global_end = time.time()
+        wrt_gb = size*size_file_mb/1000
+        av_spd = wrt_gb/(global_end-global_start)
+        print('Number of clients %i' % (size))
+        print('File size %i' % wrt_gb) 
+        print('Read %.2f GB at an average of %.2f GB/s' % (wrt_gb, av_spd))
--- evtbuild/xtc_lite/write_xtc_lite.py
+++ evtbuild/xtc_lite/write_xtc_lite.py
@@ -4,37 +4,38 @@ import numpy as np
 import time
 
 #logistical MPI setup
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
 #assert size>1, 'At least 2 MPI ranks required'
 
 
 import os
 
-
-
-write_limit = 100 #GB
-mb_per_img = 16
+write_limit = 20 #GB
+mb_per_img = 1
 batch_size = 16
 
 arr_size = mb_per_img*500000
+
+
 def create_image():
     image = np.random.randint(0,2**16,size=(250,250,8*mb_per_img), dtype='uint16')
    # arr = np.tile(image,2*mb_per_img)
     arr = image.ravel()
     return arr
 
-out_img = np.array([create_image() for x in range(batch_size)])
 
-img_mb = out_img.nbytes/10**6
-
-
-def write_client():
+def write_client(comm):
+    rank = comm.Get_rank()
 
    # file_name = '/drp_dir/eliseo/data/xtc_lite/xtc_lite_%i.xtc' % rank
 
-    file_name = '/ztest/eliseo/data/xtc_lite/xtc_lite_%i.xtc' % rank
+    
+    out_img = np.array([create_image() for x in range(batch_size)])
+
+    img_mb = out_img.nbytes/10**6
+
+    path = '/drpffb/eliseo/data/xtc_lite/'
+    file_name = path + 'xtc_lite_%i.xtc' % rank
+
     try:
         os.remove(file_name)
     except OSError:
@@ -48,25 +49,31 @@ def write_client():
             ct+=1
             written_mb += img_mb
             if ct%10 == 0:
-                print('Wrote image %i' % ct)
+                pass
+              #  print('Wrote image %i' % ct)
             if written_mb > write_limit*1000:
                 break
     
+def do_write(comm):
+#    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-comm.Barrier()
-if rank == 0:
-    global_start = time.time()
 
-write_client()
+    comm.Barrier()
+    if rank == 0:
+        global_start = time.time()
 
-comm.Barrier()
-if rank == 0:
-    global_end = time.time()
-    wrt_gb = size*write_limit
-    av_spd = wrt_gb/(global_end-global_start)
-    print('Number of clients %i' % (size))
-    print('File size %i' % wrt_gb) 
-    print('Wrote %.2f GB at an average of %.2f GB/s' % (wrt_gb, av_spd))
+    write_client(comm)
+
+    comm.Barrier()
+    if rank == 0:
+        global_end = time.time()
+        wrt_gb = size*write_limit
+        av_spd = wrt_gb/(global_end-global_start)
+        print('Number of clients %i' % (size))
+        print('File size %i GB' % wrt_gb) 
+        print('Wrote %.2f GB at an average of %.2f GB/s' % (wrt_gb, av_spd))
 
 
 
