7d327e868938a233d2c13655eec7164d59307705
Randall J. Radmer <radmer@gmail.com>
2017-12-14 11:12:50 -0800

    Add comments explaining need for a separate Event (container) class

--- psana/event.py
+++ psana/event.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 
-import sys, os
+import sys
 
 sys.path.append('../build/psana')
 import dgram
@@ -10,7 +10,28 @@ class container:
     pass
 
 class Event:
-    """Stores variables and arrays for a single event, loaded from an XTC source.\n"""
+    """Stores variables and arrays for a single event, loaded from an XTC source.
+
+       The sole purpose of the Event class is to hold variables and numpy
+       arrays that were created by a dGram object.  Ideally the variables
+       and arrays would stay in the dGram object, but this creates cyclic
+       references that Python is unable to garbage collect.  Moving the
+       variables and arrays to an Event object breaks this cycle,
+       allowing Python to reclaim array memory once it is no longer
+       referenced by users.
+
+       The basic problem is array memory is created and managed by the
+       dGram object, meaning all arrays have references (i.e. pointers)
+       to the dGram object (where their data lives).  The cyclic
+       references exist as long as dGram objects also hold references
+       back to these arrays.  Simply moving the array references to a
+       different place breaks this cycle.
+
+       A better solution would be to add support for cyclic garbage
+       collection to the dGram class, but at the time of this writing
+       numpy arrays do not support this.  We may revisit this issue
+       when numpy arrays support cyclic garbage collection.
+    """
     def __init__(self, d):
         #d=dgram.Dgram(config=config)
         #for key in sorted(d.__dict__.keys()):
