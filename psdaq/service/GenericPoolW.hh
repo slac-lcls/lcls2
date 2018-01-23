6cc27c4f804980c825f7498a56ca1e454157fd70
Ric Claus <claus@slac.stanford.edu>
2018-01-23 14:13:12 -0800

    Use Queue::removeW()

--- psdaq/service/GenericPoolW.hh
+++ psdaq/service/GenericPoolW.hh
@@ -3,30 +3,25 @@
 
 #include "GenericPool.hh"
 
-#include "Semaphore.hh"
-
 namespace Pds {
 
   class GenericPoolW : public GenericPool {
   public:
     GenericPoolW(size_t sizeofObject, int numberofObjects);
     virtual ~GenericPoolW();
-    int           depth()           const;
+    //int           depth()           const;
   protected:
-    virtual void* deque(); 
-    virtual void  enque(PoolEntry*);
-  private:
-    Semaphore    _sem;
+    virtual void* deque();
   };
 
 }
 
 
-inline int Pds::GenericPoolW::depth() const
-{
-  int val;
-  sem_getvalue(const_cast<sem_t*>(reinterpret_cast<const sem_t*>(&_sem)),&val);
-  return val; 
-}
+//inline int Pds::GenericPoolW::depth() const
+//{
+//  int val;
+//  sem_getvalue(const_cast<sem_t*>(reinterpret_cast<const sem_t*>(&_sem)),&val);
+//  return val;
+//}
 
 #endif
