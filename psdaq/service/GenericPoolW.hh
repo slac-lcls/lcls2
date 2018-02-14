bfbe93d3680fc0762fadaa7771d73564768e33e9
Ric Claus <claus@slac.stanford.edu>
2018-02-13 18:07:44 -0800

    Protect Queue links with a SpinLock; Wait on an empty Queue with a mutex

--- psdaq/service/GenericPoolW.hh
+++ psdaq/service/GenericPoolW.hh
@@ -3,25 +3,41 @@
 
 #include "GenericPool.hh"
 
+//#include <mutex>
+//#include <condition_variable>
+
 namespace Pds {
 
   class GenericPoolW : public GenericPool {
   public:
     GenericPoolW(size_t sizeofObject, int numberofObjects);
     virtual ~GenericPoolW();
-    //int           depth()           const;
   protected:
     virtual void* deque();
+    virtual void  enque(PoolEntry*);
+  private:
+    //mutable std::mutex      _mutex;
+    //std::condition_variable _condVar;
   };
 
 }
 
 
-//inline int Pds::GenericPoolW::depth() const
-//{
-//  int val;
-//  sem_getvalue(const_cast<sem_t*>(reinterpret_cast<const sem_t*>(&_sem)),&val);
-//  return val;
-//}
+inline void* Pds::GenericPoolW::deque()
+{
+  //std::unique_lock<std::mutex> lk(_mutex);
+  //_condVar.wait(lk, [this]{ return atHead() != empty(); });
+  //return Pds::GenericPool::deque();
+  Pds::PoolEntry* entry = removeW();
+  return (void*)&entry[1];
+}
+
+inline void Pds::GenericPoolW::enque(PoolEntry* entry)
+{
+  //std::lock_guard<std::mutex> lk(_mutex);
+  //Pds::GenericPool::enque(entry);
+  //_condVar.notify_one();
+  insertN(entry);
+}
 
 #endif
