4a842b4f9e84faab9b87c4565760516f112124fb
egmb <eliseo@slac.stanford.edu>
2018-03-06 13:14:41 -0800

    Added variable length writing test

--- evtbuild/hdf5_tests/HFbuild.sh
+++ evtbuild/hdf5_tests/HFbuild.sh
@@ -2,6 +2,6 @@
 
 rm -f /nvme1n1/hfdata.h5  
 #h5c++ -o HFWrite HighFreqHDFWrite.cc
-./HFWrite /nvme1n1/hfdata.h5 | tee -a "test_results/2bytes_HF.txt"
+./HFWrite /nvme1n1/hfdata.h5 | tee -a "test_results/HF_2bytes.txt"
 
 
