ced39c08259f15e19a7a0521874d47164059c996
msdubrovin <msdubrovin@gmail.com>
2018-03-12 09:59:18 -0700

    update modules for cdb lci

--- psana/psana/pscalib/proc/SubprocUtils.py
+++ psana/psana/pscalib/proc/SubprocUtils.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python
+#####!/usr/bin/env python
 #------------------------------
 
-import os
-import sys
+#import os
 from time import time
 import subprocess # for subprocess.Popen
-from PSCalib.GlobalUtils import get_login
-
-#import PSCalib.GlobalUtils as gu
+# import psana.pyalgos.generic.Utils as gu
 """
 :py:class:`SubprocUtils` contains utils to use subproccesses in specific apps
 =============================================================================
@@ -15,7 +12,7 @@ from PSCalib.GlobalUtils import get_login
 Usage::
 
     # Import
-    import PSCalib.SubprocUtils as su
+    import psana.pscalib.proc.SubprocUtils as su
 
     # Parameters
     cmd='bsub -q psnehq -o log-%%J.txt ls -l'
@@ -47,6 +44,7 @@ This software was developed for the SIT project.
 If you use all or part of it, please give an appropriate acknowledgment.
 
 Created on 2017-10-11 by Mikhail Dubrovin
+Adopted for LCLS2 on 2018-03-05
 """
 #------------------------------
 
@@ -90,11 +88,11 @@ def batch_job_submit(cmd='bsub -q psnehq -o log-%%J.txt ls -l', env=None, shell=
 
 def _str_status(msg) :
     lines  = msg.split('\n')
-    #for line in lines : print 'batch_job_status: ' + line
+    #for line in lines : print('batch_job_status: ' + line)
     if len(lines)<2 : return None
     line   = lines[1].strip('\n')
     status = line.split()[2]
-    #print 'status: ', status
+    #print('status: ', status)
     return status # it might None, 'RUN', 'PEND', 'EXIT', 'DONE', etc 
 
 #------------------------------
@@ -169,7 +167,7 @@ def batch_job_ids(status=None, user=None, qname=None, addopts='') : # qname='psn
         if i==0 :
            if fields[0] != 'JOBID' : return job_ids
            continue
-        #print line
+        #print(line)
         jobid, jobst = fields[0], fields[2]
         if status in (None, jobst) : job_ids.append(jobid)
     return job_ids
@@ -179,56 +177,58 @@ def batch_job_ids(status=None, user=None, qname=None, addopts='') : # qname='psn
 #------------------------------
 #------------------------------
 
-def test_subproc(cmd='ls -ltra', env=None, shell=False, do_wait=True) :
+if __name__ == "__main__" :
+
+  def test_subproc(cmd='ls -ltra', env=None, shell=False, do_wait=True) :
     out, err = subproc(cmd, env, shell, do_wait)
-    print 'Command: "%s"' % cmd
-    print 'out:\n"%s"' % out
-    print 'err:\n"%s"' % err
+    print('Command: "%s"' % cmd)
+    print('out:\n"%s"' % out)
+    print('err:\n"%s"' % err)
  
 #------------------------------
 
-def test_batch_job_submit(cmd='bsub -q psnehq -o log-%J.txt ls -l', env=None, shell=False) :
+  def test_batch_job_submit(cmd='bsub -q psnehq -o log-%J.txt ls -l', env=None, shell=False) :
     out, err, jobid = batch_job_submit(cmd, env, shell)
-    print 'Command: "%s"' % cmd
-    print 'out:\n"%s"' % out
-    print 'err:\n"%s"' % err
-    print 'jobid: "%s"' % jobid
-    print 'check log file log-%s.txt' % jobid
+    print('Command: "%s"' % cmd)
+    print('out:\n"%s"' % out)
+    print('err:\n"%s"' % err)
+    print('jobid: "%s"' % jobid)
+    print('check log file log-%s.txt' % jobid)
 
 #------------------------------
 
-def test_number_of_batch_jobs(user=None, qname='psnehq', addopts='') :
+  def test_number_of_batch_jobs(user=None, qname='psnehq', addopts='') :
     njobs = number_of_batch_jobs(user, qname, addopts)
-    print 'user   : %s' % str(user)
-    print 'qname  : %s' % qname
-    print 'addopts: %s' % addopts
-    print 'njobs  : %d' % njobs
+    print('user   : %s' % str(user))
+    print('qname  : %s' % qname)
+    print('addopts: %s' % addopts)
+    print('njobs  : %d' % njobs)
 
 #------------------------------
 
-def test_batch_job_kill(jobid, qname='psnehq', addopts='') :
+  def test_batch_job_kill(jobid, qname='psnehq', addopts='') :
     out, err = batch_job_kill(jobid, qname, addopts)
-    print 'qname  : %s'  % qname
-    print 'jobid  : "%s"' % jobid
-    print 'addopts: %s'% addopts
-    print 'out:\n"%s"' % out
-    print 'err:\n"%s"' % err
+    print('qname  : %s'  % qname)
+    print('jobid  : "%s"' % jobid)
+    print('addopts: %s'% addopts)
+    print('out:\n"%s"' % out)
+    print('err:\n"%s"' % err)
 
 #------------------------------
 
-def test_batch_job_ids(status=None, user=None, qname=None, addopts='') :
+  def test_batch_job_ids(status=None, user=None, qname=None, addopts='') :
     job_ids = batch_job_ids(status, user, qname, addopts) 
-    print 'status : %s' % status
-    print 'user   : %s' % user
-    print 'qname  : %s' % qname
-    print 'addopts: %s' % addopts
-    print 'job ids: %s' % str(job_ids)
+    print('status : %s' % status)
+    print('user   : %s' % user)
+    print('qname  : %s' % qname)
+    print('addopts: %s' % addopts)
+    print('job ids: %s' % str(job_ids))
 
 #------------------------------
 #------------------------------
 #------------------------------
 
-def usage() :
+  def usage() :
     return  'python PSCalib/src/SubprocUtils.py <test_name> [<jobid>]\n'\
            +'       <test_name> = 1  - test subproc\n'\
            +'                   = 2  - test batch_job_submit\n'\
@@ -238,7 +238,10 @@ def usage() :
 #------------------------------
 
 if __name__ == "__main__" :
-    print 80*'_'
+
+    import sys
+
+    print(80*'_')
     tname = sys.argv[1] if len(sys.argv)>1 else '1'
     t0_sec = time()
 
@@ -249,9 +252,9 @@ if __name__ == "__main__" :
     elif tname=='5': test_batch_job_ids(status=None, user=None, qname='psnehq')
     elif tname=='6': test_batch_job_ids(status='SSUSP', user=None, qname=None)
     else : sys.exit ('Not recognized test name: "%s"' % tname)
-    print 'Test %s time (sec) = %.3f' % (tname, time()-t0_sec)
+    print('Test %s time (sec) = %.3f' % (tname, time()-t0_sec))
 
-    if len(sys.argv)<2 : print usage()
+    if len(sys.argv)<2 : print(usage())
 
     sys.exit ('End of %s' % sys.argv[0])
 
