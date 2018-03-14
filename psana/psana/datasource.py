32fa85a1a69f57baaa2b135df6c0525efe70581e
monarin <monarin@gmail.com>
2018-03-13 19:11:41 -0700

    Add batch read

--- psana/psana/datasource.py
+++ psana/psana/datasource.py
@@ -3,10 +3,32 @@ from psana import dgram
 from psana.dgrammanager import DgramManager
 import numpy as np
 
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+rank = 0
+size = 1
+try:
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+except ImportError:
+    pass # do single core read if no mpi
+
+FN_L = 100
+
+def debug(evt):
+    if os.environ.get('PSANA_DEBUG'):
+        print("Debug Mode")
+        test_vals = [d.xpphsd.fex.intFex==42 for d in evt]
+        assert all(test_vals)
+
+def process_batch(dm, offset_batch, analyze):
+    if (offset_batch == -1).all(): return False
+    for offsets in offset_batch:
+        if all(offsets == -1): break
+        evt = dm.next(offsets=offsets)
+        analyze(evt)
+        debug(evt)
+    return True
 
 def reader(ds, analyze, filter):
     smd_dm = DgramManager(ds.smd_files)
@@ -18,64 +40,63 @@ def reader(ds, analyze, filter):
         evt = dm.next(offsets=offsets)
         
         analyze(evt)
+        debug(evt)
         
-        if os.environ.get('PSANA_DEBUG'):
-            print("Debug Mode")
-            test_vals = [d.xpphsd.fex.intFex==42 for d in evt]
-            assert all(test_vals)
-
 def server(ds, filter):
     dm = DgramManager(ds.smd_files)
     byterank = bytearray(32)
-    offsets = np.zeros(len(dm.configs), dtype='i')
+    offset_batch = np.ones([ds.lbatch, ds.nfiles], dtype='i') * -1
+    nevent = 0
     for evt in dm:
         if filter: 
             if not filter(evt): continue
+        offset_batch[nevent % ds.lbatch, :] = [d.info.offsetAlg.intOffset for d in evt]
+        if nevent % ds.lbatch == ds.lbatch - 1:
+            comm.Recv(byterank, source=MPI.ANY_SOURCE)
+            rankreq = int.from_bytes(byterank, byteorder=sys.byteorder)
+            comm.Send(offset_batch, dest=rankreq)
+            offset_batch = offset_batch * 0 -1
+        nevent += 1
+
+    if not (offset_batch==-1).all():
         comm.Recv(byterank, source=MPI.ANY_SOURCE)
         rankreq = int.from_bytes(byterank, byteorder=sys.byteorder)
-        offsets = np.asarray([d.info.offsetAlg.intOffset for d in evt], dtype='i')
-        comm.Send(offsets, dest=rankreq)
+        comm.Send(offset_batch, dest=rankreq)
+
     for rankreq in range(size-1):
         comm.Recv(byterank, source=MPI.ANY_SOURCE)
         rankreq = int.from_bytes(byterank, byteorder=sys.byteorder)
-        comm.Send(offsets*0-1, dest=rankreq)
+        comm.Send(offset_batch * 0 -1, dest=rankreq)
 
 def client(ds, configs, analyze):
     dm = DgramManager(ds.xtc_files, configs=configs)
     while True:
         comm.Send(rank.to_bytes(32, byteorder=sys.byteorder), dest=0)
-        offsets = np.empty(ds.nfiles, dtype='i')
-        comm.Recv(offsets, source=0)
-        if all(offsets == -1): break
-        
-        evt = dm.next(offsets=offsets)
-        analyze(evt)
-        
-        if os.environ.get('PSANA_DEBUG'):
-            print("Debug Mode")
-            test_vals = [d.xpphsd.fex.intFex==42 for d in evt]
-            assert all(test_vals)
-
+        offset_batch = np.empty([ds.lbatch, ds.nfiles], dtype='i')
+        comm.Recv(offset_batch, source=0)
+        if not process_batch(dm, offset_batch, analyze): break
 
 class DataSource(object):
     """ Uses DgramManager to read XTC files  """ 
     def __init__(self, expstr):
         if size == 1:
-            self.xtc_files = np.array(['data.xtc', 'data_1.xtc'], dtype='U25')
-            self.smd_files = np.array(['smd.xtc', 'smd_1.xtc'], dtype='U25')
+            self.xtc_files = np.array(['data.xtc', 'data_1.xtc'], dtype='U%s'%FN_L)
+            self.smd_files = np.array(['smd.xtc', 'smd_1.xtc'], dtype='U%s'%FN_L)
         else:
             self.nfiles = 2
             if rank == 0:
-                self.xtc_files = np.array(['data.xtc','data_1.xtc'], dtype='U25')
-                self.smd_files = np.array(['smd.xtc', 'smd_1.xtc'], dtype='U25')
+                self.xtc_files = np.array(['data.xtc','data_1.xtc'], dtype='U%s'%FN_L)
+                self.smd_files = np.array(['smd.xtc', 'smd_1.xtc'], dtype='U%s'%FN_L)
             else:
-                self.xtc_files = np.empty(self.nfiles, dtype='U25')
-                self.smd_files = np.empty(self.nfiles, dtype='U25')
+                self.xtc_files = np.empty(self.nfiles, dtype='U%s'%FN_L)
+                self.smd_files = np.empty(self.nfiles, dtype='U%s'%FN_L)
             
             comm.Bcast([self.xtc_files, MPI.CHAR], root=0)
             comm.Bcast([self.smd_files, MPI.CHAR], root=0)
  
-    def start(self, analyze, filter=None):
+    def start(self, analyze, filter=None, lbatch=1):
+        assert lbatch > 0
+        self.lbatch = lbatch
         if size == 1:
             reader(self, analyze, filter)
             return
