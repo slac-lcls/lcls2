b38d4b16f5d332761cb4789e769840461f8f7e15
Chris Ford <caf@slac.stanford.edu>
2018-03-14 14:30:11 -0700

    Add 64bits of header to the front of the Tpr Frame class.

--- psdaq/psdaq/tpr/Frame.hh
+++ psdaq/psdaq/tpr/Frame.hh
@@ -5,6 +5,9 @@ namespace Pds {
   namespace Tpr {
     class Frame {
     public:
+      uint16_t channels;  // bit mask of channels receiving this event
+      uint16_t msg_type;  // 0=Event, 1=BsaControl, 2=BsaChannel
+      uint32_t msg_size;  // size of the message in 32-bit words
       uint64_t pulseId;
       uint64_t timeStamp;
       uint16_t rates;
