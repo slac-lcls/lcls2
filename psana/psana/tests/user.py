c275433dd340c1eade8ae9e2c79aa2285798305d
monarin <monarin@gmail.com>
2018-03-14 14:39:56 -0700

    Clean up

--- psana/psana/tests/user.py
+++ psana/psana/tests/user.py
@@ -1,15 +1,16 @@
+# Todo
+# - In future may need beginjob/beginrun/updateconfig/endrun/endjob callbacks in addition 
+#   to analyze()
+# - Use detector interface in callbacks
+
 from psana import DataSource
 
 def filter(evt):
     return True
-
-# in future may need beginjob/beginrun/updateconfig/endrun/endjob callbacks in addition 
-# to analyze() 
         
-def analyze(evt): # Use detector interface here and in filter
+def analyze(evt): 
     pass
 
 ds = DataSource('exp=xpptut13:run=1')
-ds.start(analyze, filter=filter, lbatch=2)
+ds.start(analyze, filter=filter)
 
-######## end of user.py
