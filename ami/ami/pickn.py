159728ba286ae5f28a9eb1e6111918e8cfb9f5f4
Christopher O'Grady <cpo@slac.stanford.edu>
2018-03-23 17:32:21 -0700

    update pickn

--- ami/ami/pickn.py
+++ ami/ami/pickn.py
@@ -6,12 +6,20 @@ worker_id = rank - 1
 size = comm.Get_size()
 nworkers = size - 1
 
+# issues:
+# - when n workers send on a heartbeat ideally shouldn't all be on a node
+# - how can we learn "events_per_s" for each detector?  maybe readout groups?
+# - there is no guarantee of precise number of events because of load-balancing
+#   deadtime. in principle the collector could know about the statistics of
+#   all workers and do something more precise.
+
 def worker():
 
-    reqevent_per_s = 5
-    hb_per_s       = 10
-    event_per_s    = 100
-    num_hb         = 10
+    reqevent_per_s = 5    # user defined
+    event_per_s    = 100  # per-detector.  can we reliably learn this?
+    hb_per_s       = 10   # fixed by the daq
+
+    num_hb         = 30
 
     event_per_hb = event_per_s/hb_per_s
 
@@ -28,7 +36,7 @@ def worker():
         if nsend_all_workers_per_hb==0:
             hb_period = hb_per_s/reqevent_per_s
             if hb%hb_period==0:
-                if worker_id==0: print('worker',worker_id,'hb',hb,1)
+                if (hb/hb_period)%nworkers==worker_id: print('worker',worker_id,'hb',hb,1)
         elif worker_nevent_to_send_per_hb<1:
             first_sending_worker_id = (hb*nsend_all_workers_per_hb)%nworkers
             # ideally the senders would be "sparse" so not only one node is sending
