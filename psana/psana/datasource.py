8012651550a48c155ba54c89d399c58e1010930e
monarin <monarin@gmail.com>
2018-04-04 14:47:01 -0700

    Change psana interface to in-line iterator.

--- psana/psana/datasource.py
+++ psana/psana/datasource.py
@@ -17,38 +17,28 @@ FN_L = 100
 
 def debug(evt):
     if os.environ.get('PSANA_DEBUG'):
-        print("Debug Mode")
         test_vals = [d.xpphsd.fex.intFex==42 for d in evt]
+        print("-debug mode- rank: %d %s"%(rank, test_vals))
         assert all(test_vals)
 
-def process_batch(dm, offset_batch, analyze):
-    if (offset_batch == -1).all(): return False
-    for offsets in offset_batch:
-        if all(offsets == -1): break
-        evt = dm.next(offsets=offsets, read_chunk=False)
-        analyze(evt)
-        debug(evt)
-    return True
-
-def reader(ds, analyze, filter):
+def reader(ds):
     smd_dm = DgramManager(ds.smd_files)
     dm = DgramManager(ds.xtc_files, configs=smd_dm.configs)
     for offset_evt in smd_dm:
-        if filter: 
-            if not filter(offset_evt): continue
+        if ds.filter: 
+            if not ds.filter(offset_evt): continue
         offsets = np.asarray([d.info.offsetAlg.intOffset for d in offset_evt], dtype='i')
         evt = dm.next(offsets=offsets, read_chunk=False)
-        
-        analyze(evt)
         debug(evt)
+        yield evt
         
-def server(ds, filter):
+def server(ds):
     rankreq = np.empty(1, dtype='i')
     offset_batch = np.ones([ds.lbatch, ds.nfiles], dtype='i') * -1
     nevent = 0
     for evt in ds.dm:
-        if filter: 
-            if not filter(evt): continue
+        if ds.filter: 
+            if not ds.filter(evt): continue
         offset_batch[nevent % ds.lbatch, :] = [d.info.offsetAlg.intOffset for d in evt]
         if nevent % ds.lbatch == ds.lbatch - 1:
             comm.Recv(rankreq, source=MPI.ANY_SOURCE)
@@ -64,16 +54,41 @@ def server(ds, filter):
         comm.Recv(rankreq, source=MPI.ANY_SOURCE)
         comm.Send(offset_batch * 0 -1, dest=rankreq[0])
 
-def client(ds, analyze):
-    while True:
+def client(ds):
+    is_done = False
+    while not is_done:
         comm.Send(np.array([rank], dtype='i'), dest=0)
         offset_batch = np.empty([ds.lbatch, ds.nfiles], dtype='i')
         comm.Recv(offset_batch, source=0)
-        if not process_batch(ds.dm, offset_batch, analyze): break
+        for offsets in offset_batch:
+            if all(offsets == -1): 
+                is_done = True
+                break
+            evt = ds.dm.next(offsets=offsets, read_chunk=False)
+            debug(evt)
+            yield evt
+
+class Run(object):
+    """ Run generator """
+    def __init__(self, ds):
+        self.ds = ds
+
+    def events(self): # second generator: expose Event object
+        if size == 1:
+            for evt in reader(self.ds): yield evt       # safe for python2
+        else:
+            if rank == 0:
+                server(self.ds)
+            else:
+                for evt in client(self.ds): yield evt 
 
 class DataSource(object):
     """ Uses DgramManager to read XTC files  """ 
-    def __init__(self, expstr):
+    def __init__(self, expstr, filter=filter, lbatch=1):
+        assert lbatch > 0
+        self.lbatch = lbatch
+        self.filter = filter
+
         if size == 1:
             self.xtc_files = np.array(['data.xtc', 'data_1.xtc'], dtype='U%s'%FN_L)
             self.smd_files = np.array(['smd.xtc', 'smd_1.xtc'], dtype='U%s'%FN_L)
@@ -88,29 +103,24 @@ class DataSource(object):
             
             comm.Bcast([self.xtc_files, MPI.CHAR], root=0)
             comm.Bcast([self.smd_files, MPI.CHAR], root=0)
+        
+            if rank == 0:
+                self.dm = DgramManager(self.smd_files) 
+                configs = self.dm.configs
+                nbytes = np.array([memoryview(config).shape[0] for config in configs], dtype='i')
+            else:
+                self.dm = None
+                configs = [dgram.Dgram() for i in range(self.nfiles)]
+                nbytes = np.empty(self.nfiles, dtype='i')
+    
+            comm.Bcast(nbytes, root=0) # no. of bytes is required for mpich
+            for i in range(self.nfiles): 
+                comm.Bcast([configs[i], nbytes[i], MPI.BYTE], root=0)
+            
+            if rank > 0:
+                self.dm = DgramManager(self.xtc_files, configs=configs)
  
-    def start(self, analyze, filter=None, lbatch=1):
-        assert lbatch > 0
-        self.lbatch = lbatch
-        if size == 1:
-            reader(self, analyze, filter)
-            return
-
-        if rank == 0:
-            self.dm = DgramManager(self.smd_files) 
-            configs = self.dm.configs
-            nbytes = np.array([memoryview(config).shape[0] for config in configs], dtype='i')
-        else:
-            self.dm = None
-            configs = [dgram.Dgram() for i in range(self.nfiles)]
-            nbytes = np.empty(self.nfiles, dtype='i')
-    
-        comm.Bcast(nbytes, root=0) # no. of bytes is required for mpich
-        for i in range(self.nfiles): 
-            comm.Bcast([configs[i], nbytes[i], MPI.BYTE], root=0)
-    
-        if rank == 0:
-            server(self, filter)
-        else:
-            self.dm = DgramManager(self.xtc_files, configs=configs)
-            client(self, analyze)
+    def runs(self): # first generator: expose Run object
+        nruns = 1
+        for run_no in range(nruns):
+            yield Run(self)
