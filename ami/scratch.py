fb16712db774388c7e6f341581a641e88624b006
TJ Lane <tjlane@stanford.edu>
2018-04-20 17:30:32 -0700

    first implementation of python exec backend

--- ami/scratch.py
+++ ami/scratch.py
@@ -1,51 +1,91 @@
 
 import json
+import numpy as np
+
+
 
 def simple_tree(json):
+    """
+    Generate a dependency tree from a json input
+
+    Parameters
+    ----------
+    json : json object
+
+    Returns
+    -------
+    graph : dict
+        dict where the keys are operations, the value are upstream
+        (dependent) operations that should be done first
+    """
     graph = {}
     for op in json:
         graph[op] = json[op]['inputs']
     return graph
 
 
-def build_dep_tree(json):
+def resolve_dependencies(dependencies):
+    """
+    Dependency resolver
 
-    graph = {}
+    Parameters
+    ----------
+    dependencies : dict
+        the values are the dependencies of their respective keys
 
-    for op in json:
-        for inps in json[op]['inputs']:
-            if inps not in graph:
-                graph[inps] = set()
-            graph[inps].add(op)
+    Returns
+    -------
+    order : list
+        a list of the order in which to resolve dependencies
+    """
 
-    return graph
+    # this just makes sure there are no duplicate dependencies
+    d = dict( (k, set(dependencies[k])) for k in dependencies )
 
+    # output list
+    r = []
 
-def dep(arg):
-    '''
-        Dependency resolver
-
-    "arg" is a dependency dictionary in which
-    the values are the dependencies of their respective keys.
-    '''
-    d=dict((k, set(arg[k])) for k in arg)
-    r=[]
     while d:
-        # values not in keys (items without dep)
-        t=set(i for v in d.values() for i in v)-set(d.keys())
-        # and keys without value (items without dep)
+
+        # find all nodes without dependencies (roots)
+        t = set(i for v in d.values() for i in v) - set(d.keys())
+
+        # and items with no dependencies (disjoint nodes)
         t.update(k for k, v in d.items() if not v)
-        # can be done right away
-        r.append(t)
-        # and cleaned up
-        d=dict(((k, v-t) for k, v in d.items() if v))
+
+        # both of these can be resolved
+        r.extend(t)
+
+        # remove resolved depedencies from the tree
+        d = dict(((k, v-t) for k, v in d.items() if v))
+
     return r
 
 
+class FeatureStore(object):
+
+    def __init__(self):
+        self.namespace = {'cspad' : np.random.randn(1000,1000)}
+        return
+
+    def put(self, name, value):
+        print('PUT:' + name)
+        self.namespace[name] = value
+
+
 j = json.load(open('examples/python.json'))
-
-
-#t = build_dep_tree(j)
 t = simple_tree(j)
-print dep(t)
+ops = resolve_dependencies(t)
+
+st = FeatureStore()
+
+
+for op in ops:
+    if op in st.namespace:
+        continue
+    store_put = '\n' + '; '.join(['store.put("%s", %s)'%(output, output) for output in j[op]['outputs']]) + '\n'
+    loc = {'store': st}
+    loc.update(st.namespace)
+    exec(j[op]['code'] + store_put, {"np" : np}, loc)
+
 
