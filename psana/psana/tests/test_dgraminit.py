fc135ff35ac7ad0e9078f9203b969925b0b42119
monarin <monarin@gmail.com>
2018-05-13 19:07:27 -0700

    Fix path

--- psana/psana/tests/test_dgraminit.py
+++ psana/psana/tests/test_dgraminit.py
@@ -18,17 +18,24 @@ class TestDgramInit:
         assert raised
 
     def testInvalidSequentialRead(self):
-        fd = os.open("smd.xtc", os.O_RDONLY)
-        d = dgram.Dgram(file_descriptor=fd)
-        raised = False
-        try: 
-            another_d = dgram.Dgram(file_descriptor=fd)
-        except StopIteration:
-            raised = True
-        assert raised
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        full_path = os.path.join(dir_path, "smd.xtc")
+        if os.path.isfile(full_path):
+            print(full_path)
+            fd = os.open(full_path, os.O_RDONLY)
+            d = dgram.Dgram(file_descriptor=fd)
+            raised = False
+            try: 
+                another_d = dgram.Dgram(file_descriptor=fd)
+            except StopIteration:
+                raised = True
+            assert raised
 
 def run():
     test = TestDgramInit()
     test.testCreateEmptyDgram()
     test.testInvalidFileDescriptor()
     test.testInvalidSequentialRead()
+
+if __name__ == "__main__":
+    run()
