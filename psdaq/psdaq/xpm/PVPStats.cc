9915627865e7761b2340cfead2126d366bc26884
Matt Weaver <weaver@slac.stanford.edu>
2018-06-19 10:53:37 -0700

    Remove extra parameters for xpm partition config calls.  Fix message delay for parttition>0

--- psdaq/psdaq/xpm/PVPStats.cc
+++ psdaq/psdaq/xpm/PVPStats.cc
@@ -57,7 +57,7 @@ namespace Pds {
       const L0Stats& os = _last;
       L0Stats ns(_dev.l0Stats());
       PVPUT(9, double(ns.l0Enabled)*14.e-6/13.);
-      PVPUT(10, double(_dev.getL0Delay(_partition)));
+      PVPUT(10, double(_dev.getL0Delay()));
       unsigned l0Enabled = ns.l0Enabled - os.l0Enabled;
       double dt = double(l0Enabled)*14.e-6/13.;
       unsigned numl0     = ns.numl0    - os.numl0;
