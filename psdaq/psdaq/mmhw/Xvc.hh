da4fa50a9fda3d624c46f1bc64f857feb01d239e
Matt Weaver <weaver@slac.stanford.edu>
2018-07-09 15:14:02 -0700

    Add XVC

---
+++ psdaq/psdaq/mmhw/Xvc.hh
@@ -0,0 +1,25 @@
+#ifndef Pds_Mmhw_Xvc_hh
+#define Pds_Mmhw_Xvc_hh
+
+#include <stdint.h>
+
+namespace Pds {
+  namespace Mmhw {
+
+    class Jtag {
+    public:
+      uint32_t  length_offset;
+      uint32_t  tms_offset;
+      uint32_t  tdi_offset;
+      uint32_t  tdo_offset;
+      uint32_t  ctrl_offset;
+    };
+
+    class Xvc {
+    public:
+      static void* launch(Jtag*,bool lverbose=false);
+    };
+  };
+};
+
+#endif
