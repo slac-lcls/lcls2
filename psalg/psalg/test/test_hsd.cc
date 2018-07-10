d4b04e71457aba62728c4c39c938b606fc50eead
chuckie82 <chuckie82@gmail.com>
2018-07-10 01:13:36 -0700

    modify test_hsd

--- psalg/psalg/test/test_hsd.cc
+++ psalg/psalg/test/test_hsd.cc
@@ -82,17 +82,6 @@ public:
 };
 
 int main (int argc, char* argv[]) {
-/*
-    //open file
-    std::ifstream infile("/reg/neh/home/yoon82/temp1/lcls2/chan0_e0.bin");
-    //get length of file
-    infile.seekg(0, infile.end);
-    size_t length = infile.tellg();
-    infile.seekg(0, infile.beg);
-    char buffer[length];
-    //read file
-    infile.read(buffer, length);
-*/
 
     int c;
     char* xtcname = 0;
@@ -140,61 +129,25 @@ int main (int argc, char* argv[]) {
                dg->env, dg->xtc.sizeofPayload());
         printf("*** dg xtc extent %d\n",dg->xtc.extent);
 
-        if (dg->seq.isEvent()) {
+        if (dg->seq.isEvent()) { // FIXME: this is always false for now
             printf("Found event\n");
         }
 
         if (counter == 1) {
-
-
-            Heap heap;
-            //unsigned nChan = 4;
-
-            //Pds::HSD::Client *pClient = new Pds::HSD::Client(&heap, "1.2.3", nChan);
-            //Pds::HSD::HsdEventHeaderV1 *pHsd = pClient->getHsd();
-            //Pds::HSD::Hsd_v1_2_3 *vHsd = (Pds::HSD::Hsd_v1_2_3*) pHsd;
-
-            const Pds::HSD::EventHeader& eh = *reinterpret_cast<const Pds::HSD::EventHeader*>(dg);
-            printf("Address of eh: %u %u\n", &eh, &eh+1);
-            printf("Dumping eventheader\n");
-            eh.dump();
-
             HsdIter hsdIter(&dg->xtc, namesIter.namesVec());
             hsdIter.iterate();
+
             const Pds::HSD::StreamHeader* sh_rawx = 0;
-            sh_rawx = reinterpret_cast<const Pds::HSD::StreamHeader*>(hsdIter.chanPtr["chan3"]);
-            sh_rawx->dump();
-
-/*
-            const Pds::HSD::EventHeader* ehx = reinterpret_cast<const Pds::HSD::EventHeader*>(dg);
-            ehx->dump();
-
-            printf("sizeof: %u %u\n", sizeof(Pds::HSD::EventHeader), sizeof(Pds::HSD::StreamHeader));
-
-            //const char* nextx = reinterpret_cast<const char*>(ehx+1);
-            const Pds::HSD::StreamHeader* sh_rawx = 0;
-            sh_rawx = reinterpret_cast<const Pds::HSD::StreamHeader*>(ehx+1);
+            sh_rawx = reinterpret_cast<const Pds::HSD::StreamHeader*>(hsdIter.chanPtr["chan0"]);
             const uint16_t* rawx = reinterpret_cast<const uint16_t*>(sh_rawx+1);
             sh_rawx->dump();
-*/
-            //numPixels.push_back((unsigned) (sh_rawx->samples()-sh_rawx->eoffs()-sh_rawx->boffs()));
-            //rawPtr.push_back((uint16_t *) (rawx+sh_rawx->boffs()));
-            //numFexPeaksx.push_back(0);
 
             // ------------ FEX --------------
-            //nextx = reinterpret_cast<const char*>(&rawx[sh_rawx->samples()]);
-            //const Pds::HSD::StreamHeader& sh_fexx = *reinterpret_cast<const Pds::HSD::StreamHeader*>(nextx);
-            //sh_fexx.dump();
-            //const unsigned end = sh_fexx.samples() - sh_fexx.eoffs() - sh_fexx.boffs();
-            //const uint16_t* p_thr = &reinterpret_cast<const uint16_t*>(&sh_fexx+1)[sh_fexx.boffs()];
+            const char* nextx = reinterpret_cast<const char*>(&rawx[sh_rawx->samples()]);
+            const Pds::HSD::StreamHeader& sh_fexx = *reinterpret_cast<const Pds::HSD::StreamHeader*>(nextx);
+            printf("FEX DUMP\n");
+            sh_fexx.dump();
 
-
-/*
-            for (unsigned chan = 0; chan < nChan; chan++) {
-                vHsd->parseChan((const uint8_t*)dg+sizeof(Pds::HSD::EventHeader)+chan*sizeof(Pds::HSD::StreamHeader), chan);
-                printf("\n\n");
-            }
-*/
             break;
         }
         counter++;
