d4851d5d067ace9d9610f35eb2919c54133ddd7f
Clemens Weninger <weninc@slac.stanford.edu>
2018-07-17 17:06:36 -0700

    build xtcdata and psalg in build_python2_psana.sh

--- build_python2_psana.sh
+++ build_python2_psana.sh
@@ -6,6 +6,18 @@ source setup_env_python2.sh
 export INSTDIR=`pwd`/install
 export PYTHONPATH=$INSTDIR/lib/python2.7/site-packages
 
+function cmake_build() {
+    cd $1
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_BUILD_TYPE=-DCMAKE_BUILD_TYPE=Release ..
+    make -j 4 install                                                          
+    cd ../..                                                 
+}
+
+cmake_build xtcdata
+cmake_build psalg
+
 mkdir -p $INSTDIR/lib/python2.7/site-packages/
 cd psana
 python setup.py build_ext --xtcdata=$INSTDIR -f --inplace
