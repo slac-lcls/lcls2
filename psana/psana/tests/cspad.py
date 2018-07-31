e055522ae56bef85a8203f7cccfedc257a36c06d
chuckie82 <chuckie82@gmail.com>
2018-07-31 10:45:17 -0700

    replace broken link

--- psana/psana/tests/cspad.py
+++ psana/psana/tests/cspad.py
@@ -1,5 +1,5 @@
 #------------------------------
-# nosetests -sv psana/psana/tests
+# This script is not intended to be tested during continuous integration
 #
 #------------------------------
 
@@ -14,7 +14,7 @@ if doPlot: import matplotlib.pyplot as plt
 import os
 os.environ['PS_CALIB_DIR'] = "/reg/common/package/mona/cxid9114"
 
-ds = psana.DataSource('/reg/d/psdm/cxi/cxitut13/scratch/yoon82/crystal_101_evts.xtc')
+ds = psana.DataSource('/reg/common/package/lcls2_data/crystal_99_evts.xtc')
 det = ds.Detector("DscCsPad")
 run = 101
 for i, evt in enumerate(ds.events()):
