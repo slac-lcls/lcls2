90ccbf397958764ef6553574fac1d6c9736d4228
msdubrovin <msdubrovin@gmail.com>
2018-08-21 18:12:49 -0700

    update MDBWebUtils

--- psalg/psalg/calib/src/MDBWebUtils.cc
+++ psalg/psalg/calib/src/MDBWebUtils.cc
@@ -11,7 +11,7 @@
 // psdaq/pydaq/README.JSON
 // psdaq/drp/drp_eval.cc
 //
-
+#include <iomanip> // setw
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>
 //using namespace rapidjson;
@@ -20,13 +20,13 @@
 
 namespace psalg {
 
-  static std::string STR_RESP; //will hold the url's contents
+  static std::string STR_RESP; // holds responce
   static rapidjson::Document JSON_DOC;
-  std::vector<std::string> LIST_OF_NAMES;
+  //std::vector<std::string> LIST_OF_NAMES;
 
 //-------------------
 
-void print_json_value(const rapidjson::Value &value) {
+void print_json_value(const rapidjson::Value& value) {
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
     value.Accept(writer);
@@ -35,18 +35,24 @@ void print_json_value(const rapidjson::Value &value) {
 
 //-------------------
 
-void print_json_doc(const rapidjson::Value &doc, const std::string& offset) {
+void print_json_doc(const rapidjson::Value& doc, const std::string& offset) {
   // std::cout << "XXX In print_json_doc: isArray: " << doc.IsArray() << '\n';
   const rapidjson::Value& a = doc;
-  std::string gap("  ");
+  std::string gap("    ");
   gap += offset;
-  if      (a.IsString()) std::cout << gap << " (string) " << a.GetString() << '\n';
-  else if (a.IsInt())    std::cout << gap << " (int) " << a.GetInt() << '\n';
-  else if (a.IsObject()) std::cout << gap << " (object) TBD\n";
-  //else if (a.IsNum())    std::cout << gap << " (number) " << a.GetNum() << '\n';
+  if      (a.IsString()) std::cout << gap << " (string): " << a.GetString() << '\n';
+  else if (a.IsBool())   std::cout << gap << " (int)   : " << a.GetBool() << '\n';
+  else if (a.IsNumber()) std::cout << gap << " (number): " << a.GetInt() << '\n';
+  else if (a.IsInt())    std::cout << gap << " (int)   : " << a.GetInt() << '\n';
+  else if (a.IsObject()) {
+    for (rapidjson::Value::ConstMemberIterator itr = a.MemberBegin(); itr != a.MemberEnd(); ++itr) {
+      std::cout << gap << " (object): " << itr->name.GetString() << '\n';
+      print_json_doc(a[itr->name.GetString()], gap);
+    }
+  }  
   else if (a.IsArray()) {
     for (rapidjson::SizeType i = 0; i < a.Size(); i++) {
-	std::cout << gap << "[" << i << "]: ";
+      std::cout << gap << "[" << std::setw(3) << i << "]: ";
 	print_json_doc(a[i], gap);
     }
   }
@@ -55,15 +61,15 @@ void print_json_doc(const rapidjson::Value &doc, const std::string& offset) {
 
 //-------------------
 
-void print_vector_of_strings(const std::vector<std::string>& v) {
+void print_vector_of_strings(const std::vector<std::string>& v, const char* gap) {
   std::cout << "In print_vector_of_strings\n";
   for(std::vector<std::string>::const_iterator it  = v.begin(); it != v.end(); it++) 
-    std::cout << "  " << *it << '\n';
+    std::cout << gap << *it << '\n';
 }
 
 //-------------------
 
-std::string json_doc_to_string(const rapidjson::Document &doc) {
+std::string json_doc_to_string(const rapidjson::Document& doc) {
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
     doc.Accept(writer);
@@ -74,23 +80,23 @@ std::string json_doc_to_string(const rapidjson::Document &doc) {
 
 rapidjson::Document chars_to_json_doc(const char* s) {
     rapidjson::Document document;
-    document.Parse(s);
+    rapidjson::ParseResult ok = document.Parse(s);
+    if (!ok) MSG(ERROR, "JSON parse error: " << ok.Code() << " " << ok.Offset());
     return std::move(document);
 }
 
 //-------------------
 
-const std::vector<std::string>& json_doc_to_vector_of_strings(const rapidjson::Document &doc) {
-  LIST_OF_NAMES.clear();
-  if(! doc.IsArray() || doc.Size()<1) return LIST_OF_NAMES;
-  LIST_OF_NAMES.resize(doc.Size());
-  LIST_OF_NAMES.clear();
+void json_doc_to_vector_of_strings(const rapidjson::Document& doc, std::vector<std::string>& vout) {
+  vout.clear();
+  if(! doc.IsArray() || doc.Size()<1) return;
+  vout.resize(doc.Size());
+  vout.clear();
   for (rapidjson::Value::ConstValueIterator itr = doc.Begin(); itr != doc.End(); ++itr) {
-    LIST_OF_NAMES.push_back(itr->GetString());
+    vout.push_back(itr->GetString());
     printf("%s ", itr->GetString());
   }
   printf("\n");
-  return LIST_OF_NAMES;
 }
 
 //-------------------
@@ -98,90 +104,123 @@ const std::vector<std::string>& json_doc_to_vector_of_strings(const rapidjson::D
 size_t string_callback(char* buf, size_t size, size_t nmemb, void* up) {
     //callback must have this declaration
     //buf is a pointer to the data that curl has for us
-    //size*nmemb is the size of the buffer
     MSG(DEBUG, "In string_callback size:" << size << " nmemb:" << nmemb);
-    size_t nbytes = size*nmemb;
-    STR_RESP.clear();
-    STR_RESP.resize(nbytes);
-    STR_RESP = buf;
-    return STR_RESP.size();
-    //for (size_t c = 0; c<nbytes; c++) STR_RESP.push_back(buf[c]);
-    //return nbytes; //tell curl how many bytes we handled
-  }
+    size_t nbytes = size*nmemb; // size of the buffer
+    STR_RESP += buf;
+    return nbytes; // STR_RESP.size();
+}
 
 //-------------------
 
-size_t json_callback(char* buf, size_t size, size_t nmemb, void* up) {
-    MSG(DEBUG, "In json_callback size:" << size << " nmemb:" << nmemb);
-    JSON_DOC.Parse(buf);
-    return size*nmemb; //tell curl how many bytes we handled
-  }
-
-//-------------------
-
+/// Returns reference to the static STR_RESP filled out in responce
 const std::string& string_response() { 
-  STR_RESP = json_doc_to_string(JSON_DOC);
+  //s = json_doc_to_string(JSON_DOC);
   return STR_RESP;
 }
 
 //-------------------
 
-const rapidjson::Document & json_doc_response() { 
+/// Returns reference to the static JSON_DOC
+const rapidjson::Document& json_doc_response() { 
+  rapidjson::ParseResult ok = JSON_DOC.Parse(STR_RESP.c_str());
+  if (!ok) MSG(ERROR, "JSON parse error: " << ok.Code() << " " << ok.Offset());
   return JSON_DOC;
 }
 
 //-------------------
 
-void request(const char* url, const char* query) {
-  MSG(DEBUG, "In request url:" << url << " query:" << query);
+/// Parses STR_RESP to the externally defined Document& doc.
+void json_doc_response(rapidjson::Document& doc) { 
+  rapidjson::ParseResult ok = doc.Parse(STR_RESP.c_str());
+  // rapidjson::GetParseError_En(ok.Code())
+  if (!ok) MSG(ERROR, "JSON parse error: " << ok.Code() << " " << ok.Offset());
+}
+
+//-------------------
+
+//Returns string like "https://pswww-dev.slac.stanford.edu//calib_ws/cdb_cspad_0001/cspad_0001?query_string=%7B%22ctype%22%3A+%22pedestals%22%7D"
+
+void string_url_with_query(std::string& surl, const char* dbname, const char* colname, const char* query, const char* urlws) {
+  surl = urlws;
+  surl += '/';
+  surl += dbname;
+  surl += '/';
+  surl += colname;
+  MSG(DEBUG, "string_url_with_query url:" << surl << " query:");
+
+  if(query) {
+    std::string squery("?query_string=");
+    squery += curl_easy_escape(NULL, query, 0);
+    MSG(DEBUG, "query: "  << query << " escaped: " << squery << " size: " << squery.size());
+    surl += squery;
+    MSG(DEBUG, "url + query: "  << surl);
+  }
+}
+
+//-------------------
+// curl -s "https://pswww-dev.slac.stanford.edu//calib_ws/cdb_cspad_0001/cspad_0001?query_string=%7B%22ctype%22%3A+%22pedestals%22%7D"
+
+// perform request with specified url and seves response in static std::string STR_RESP through string_callback.
+
+void request(const char* url) {
+  MSG(DEBUG, "In request url:" << url);
 
   CURL *curl = curl_easy_init();
 
   if(curl) {
-    //curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-    curl_easy_setopt(curl, CURLOPT_URL, url); // .edu:9306
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &json_callback);
-    //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &string_callback);
-    //curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); //tell curl to output its progress
+    //if (LOGGER.logging(LL::DEBUG)) curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &string_callback);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
 
+    STR_RESP.clear();
     CURLcode res = curl_easy_perform(curl);
- 
-    if(CURLE_OK == res) {
-      // ask for the content-type
+    MSG(DEBUG, "In request STR_RESP:" << STR_RESP << '\n');
+
+    if(CURLE_OK == res) { // ask for the content-type
       char *ct;
       res = curl_easy_getinfo(curl, CURLINFO_CONTENT_TYPE, &ct);
-
       if((CURLE_OK == res) && ct) {MSG(DEBUG, "Received Content-Type: " << ct);}
       else                         MSG(WARNING, "RESPONSE IS NOT RECEIVED");
     }
-    // always cleanup
-    curl_easy_cleanup(curl);
-    //curl_global_cleanup();
 
-    //MSG(DEBUG, "Exit request");
+    curl_easy_cleanup(curl); // always cleanup
   }
 }
 
 //-------------------
 
-const std::vector<std::string>& database_names(const char* url) {
-  request(url);
-  //const std::string& s = string_response();
-  //std::cout << "XXX resp: " << s << '\n';
-  //const rapidjson::Document jdoc = chars_to_json_doc(s.c_str());
-  //print_json_value(jdoc); 
-  const rapidjson::Document& doc = json_doc_response();
-  return json_doc_to_vector_of_strings(doc);
+void database_names(std::vector<std::string>& dbnames, const char* urlws) {
+  request(urlws);
+  rapidjson::Document doc;
+  json_doc_response(doc);
+  //print_json_value(doc); 
+  json_doc_to_vector_of_strings(doc, dbnames);
 }
 
 //-------------------
 
-const std::vector<std::string>& collection_names(const char* dbname, const char* url) {
-  std::string url_tot(url); url_tot += '/'; url_tot += dbname;
+void collection_names(std::vector<std::string>& colnames, const char* dbname, const char* urlws) {
+  std::string url_tot(urlws); url_tot += '/'; url_tot += dbname;
   request(url_tot.c_str());
-  const rapidjson::Document& doc = json_doc_response();
-  return json_doc_to_vector_of_strings(doc);
+  rapidjson::Document doc;
+  json_doc_response(doc);
+  json_doc_to_vector_of_strings(doc, colnames);
 }
+
+//-------------------
+
+void find_docs(const char* dbname, const char* colname, const char* query, const char* urlws) {
+  std::string surl;
+  string_url_with_query(surl, dbname, colname, query, urlws);
+  request(surl.c_str());
+}
+
+//-------------------
+// curl -s "https://pswww-dev.slac.stanford.edu/calib_ws/cdb_cxic0415/cspad_0001/5b6893e81ead141643fe4344"
+//void get_doc_for_docid(dbname, colname, docid, urlws=cc.URLWS) :
+
+// curl -s "https://pswww-dev.slac.stanford.edu/calib_ws/cdb_cxic0415/gridfs/5b6893d91ead141643fe3f6a" 
+//void get_data_for_id(dbname, dataid, urlws=cc.URLWS) :
 
 //-------------------
 
--- psalg/psalg/tests/test_MDBWebUtils.cc
+++ psalg/psalg/tests/test_MDBWebUtils.cc
@@ -50,18 +50,64 @@ int test_getinfo(void) {
  
 void test_request() {
   printf("In test_request\n");
-  request(); // (const char* url=URL, const char* query="");
+  request(); // (const char* url=URLWS, const char* query=NONE);
   const std::string& s = string_response();
   std::cout << "XXX resp: " << s << '\n';
+}
+
+//-------------------
+// returns "https://pswww-dev.slac.stanford.edu//calib_ws/cdb_cspad_0001/cspad_0001?query_string=%7B%22ctype%22%3A+%22pedestals%22%7D"
+std::string test_string_url_with_query() {
+  printf("In test_string_url_with_query\n");  
+  std::string url;
+  const char* dbname  = "cdb_cspad_0001";
+  const char* colname = "cspad_0001"; 
+                        //'{"ctype": "pedestals", "run" : { "$gte":  "85"}}')
+  const char* query   =  "{\"ctype\":\"pedestals\", \"run\":{\"$gte\": 85}}";
+  //const char* query   =  "{\"ctype\":\"pedestals\"}";
+  string_url_with_query(url, dbname, colname, query);
+  std::cout << "test_string_url_with_query: " << url << '\n';
+  return url;
+}
+
+//-------------------
+ 
+// curl -s "https://pswww-dev.slac.stanford.edu//calib_ws/cdb_cspad_0001/cspad_0001?query_string=..."
+void test_request_with_query() {
+  printf("In test_request_with_query\n");  
+  std::string url = test_string_url_with_query(); 
+  //request(url.c_str(), "{\"ctype\":\"pedestals\"}");
+  request(url.c_str());//, "{\"ctype\":\"pedestals\"}");
+
+  //const rapidjson::Document& doc = json_doc_response();
+  rapidjson::Document doc;
+  json_doc_response(doc);
+  print_json_doc(doc);
+}
+
+//-------------------
+ 
+void test_find_docs() {
+  printf("In test_find_docs\n");  
+  const char* dbname  = "cdb_cspad_0001";
+  const char* colname = "cspad_0001"; 
+                        //'{"ctype": "pedestals", "run" : { "$gte":  "85"}}')
+  const char* query   =  "{\"ctype\":\"pedestals\", \"run\":{\"$gte\": 87}}";
+  find_docs(dbname, colname, query);
+
+  const std::string& s = string_response();
+  std::cout << "XXX resp: " << s << '\n';
+  //rapidjson::Document doc;
+  //json_doc_response(doc);
+  //print_json_doc(doc);
 }
 
 //-------------------
  
 void test_database_names() {
   printf("In database_names\n");
-  const std::vector<std::string>& v = database_names();
-  //const std::string& s = string_response();
-  //std::cout << "XXX resp: " << s << '\n';
+  std::vector<std::string> v;
+  database_names(v);
   print_vector_of_strings(v);
 }
 
@@ -69,11 +115,14 @@ void test_database_names() {
  
 void test_collection_names() {
   printf("In test_collection_names\n");
-  const std::vector<std::string>& v = collection_names("cdb_cspad_0001");
-  //const std::string& s = string_response();
-  //std::cout << "XXX resp: " << s << '\n';
+  std::vector<std::string> v;
+  collection_names(v, "cdb_cspad_0001");
   print_vector_of_strings(v);
 }
+
+//-------------------
+
+
 
 //-------------------
 
@@ -87,6 +136,10 @@ int main(void)
   test_request(); print_hline(80,'_');
   test_database_names(); print_hline(80,'_');
   test_collection_names(); print_hline(80,'_');
+  std::string url =
+  test_string_url_with_query(); print_hline(80,'_');
+  //test_request_with_query(); print_hline(80,'_');
+  test_find_docs(); print_hline(80,'_');
 }
 
 //-------------------
