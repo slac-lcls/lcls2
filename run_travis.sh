5817c883a5c782a373e3db4921dbdc650749ac28
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-09-06 09:47:16 -0700

    Refactor run script.

--- run_travis.sh
+++ run_travis.sh
@@ -12,8 +12,7 @@ export PYTHONPATH="$PWD/install/lib/python$PYVER/site-packages"
 
 if [[ $PYVER == 2.7 ]]; then
     ./build_python2_psana.sh
-    pytest psana/psana/tests
 elif [[ $PYVER == 3.* ]]; then
     ./build_all.sh -p install
-    pytest psana/psana/tests
 fi
+pytest psana/psana/tests
