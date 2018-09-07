cfcd4f7c2fb17e07af690d8bf61de02f2cf29f11
Christopher O'Grady <cpo@slac.stanford.edu>
2018-09-07 16:21:19 -0700

    clean up py2 build

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,5 @@
-source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh
+# user can select py2 environment with "-py2" option
+source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh $@
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
 # temporary, until we install psdaq binaries
