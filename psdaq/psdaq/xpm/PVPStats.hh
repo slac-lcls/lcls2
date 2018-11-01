9025b1bada2a34376faf02344824449efc08b787
Matt Weaver <weaver@slac.stanford.edu>
2018-11-01 11:57:19 -0700

    Resolve xpm conflict

--- psdaq/psdaq/xpm/PVPStats.hh
+++ psdaq/psdaq/xpm/PVPStats.hh
@@ -14,7 +14,7 @@ namespace Pds {
   namespace Xpm {
     class PVPStats {
     public:
-      PVPStats(Module&, unsigned partition);
+      PVPStats(Module&, unsigned shelf, unsigned partition);
       ~PVPStats();
     public:
       void allocate(const std::string& title,
@@ -22,6 +22,7 @@ namespace Pds {
       void update();
     private:
       Module&                           _dev;
+      unsigned                          _shelf;
       unsigned                          _partition;
       std::vector<Pds_Epics::PVWriter*> _pv;
       L0Stats                           _begin;
