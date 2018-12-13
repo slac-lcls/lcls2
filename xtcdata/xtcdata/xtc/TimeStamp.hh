cc9da695f4824c11b57ffa1a6c4ae8f72b075f20
Ric Claus <claus@slac.stanford.edu>
2018-12-12 16:34:06 -0800

    Make small methods inline

--- xtcdata/xtcdata/xtc/TimeStamp.hh
+++ xtcdata/xtcdata/xtc/TimeStamp.hh
@@ -17,29 +17,98 @@ public:
     TimeStamp(uint64_t stamp);
 
 public:
-    uint64_t value() const
-    {
-        return ((uint64_t)_high << 32) | _low;
-    }
-    unsigned seconds() const
-    {
-        return _high;
-    }
-    unsigned nanoseconds() const
-    {
-        return _low;
-    }
-    double asDouble() const;
-    bool isZero() const;
+    uint64_t value() const;
+    unsigned seconds() const;
+    unsigned nanoseconds() const;
+    double   asDouble() const;
+    bool     isZero() const;
 
 public:
     TimeStamp& operator=(const TimeStamp&);
-    bool operator>(const TimeStamp&) const;
-    bool operator==(const TimeStamp&) const;
+    bool       operator>(const TimeStamp&) const;
+    bool       operator==(const TimeStamp&) const;
 
 private:
     uint32_t _low;
     uint32_t _high;
 };
+
+
+inline
+XtcData::TimeStamp::TimeStamp()
+{
+}
+
+inline
+XtcData::TimeStamp::TimeStamp(const TimeStamp& t) : _low(t._low), _high(t._high)
+{
+}
+
+inline
+XtcData::TimeStamp::TimeStamp(const timespec& ts) : _low(ts.tv_nsec), _high(ts.tv_sec)
+{
+}
+
+inline
+XtcData::TimeStamp::TimeStamp(unsigned sec, unsigned nsec) : _low(nsec), _high(sec)
+{
+}
+
+inline
+XtcData::TimeStamp::TimeStamp(uint64_t stamp)
+{
+    _low  =  stamp        & 0xffffffff;
+    _high = (stamp >> 32) & 0xffffffff;
+}
+
+inline
+uint64_t XtcData::TimeStamp::value() const
+{
+    return ((uint64_t)_high << 32) | _low;
+}
+
+inline
+unsigned XtcData::TimeStamp::seconds() const
+{
+    return _high;
+}
+
+inline
+unsigned XtcData::TimeStamp::nanoseconds() const
+{
+    return _low;
+}
+
+inline
+bool XtcData::TimeStamp::isZero() const
+{
+    return _low == 0 && _high == 0;
+}
+
+inline
+double XtcData::TimeStamp::asDouble() const
+{
+    return _high + _low / 1.e9;
+}
+
+inline
+XtcData::TimeStamp& XtcData::TimeStamp::TimeStamp::operator=(const TimeStamp& input)
+{
+    _low  = input._low;
+    _high = input._high;
+    return *this;
+}
+
+inline
+bool XtcData::TimeStamp::TimeStamp::operator>(const TimeStamp& t) const
+{
+    return (_high > t._high) || (_high == t._high && _low > t._low);
+}
+
+inline
+bool XtcData::TimeStamp::TimeStamp::operator==(const TimeStamp& t) const
+{
+    return (_high == t._high) && (_low == t._low);
+}
 }
 #endif
--- xtcdata/xtcdata/xtc/TypeId.hh
+++ xtcdata/xtcdata/xtc/TypeId.hh
@@ -27,14 +27,47 @@ public:
     uint32_t value() const;
 
     static const char* name(Type type);
-    static uint32_t _sizeof()
-    {
-        return sizeof(TypeId);
-    }
+    static uint32_t _sizeof();
 
 private:
     uint32_t _value;
 };
+
+
+inline
+XtcData::TypeId::TypeId(Type type, uint32_t version)
+  : _value((version << 16) | type)
+{
+}
+
+inline
+XtcData::TypeId::TypeId(const TypeId& v) : _value(v._value)
+{
+}
+
+inline
+uint32_t XtcData::TypeId::value() const
+{
+    return _value;
+}
+
+inline
+uint32_t XtcData::TypeId::version() const
+{
+    return (_value & 0xffff0000) >> 16;
+}
+
+inline
+XtcData::TypeId::Type XtcData::TypeId::id() const
+{
+    return (XtcData::TypeId::Type)(_value & 0xffff);
+}
+
+inline
+uint32_t XtcData::TypeId::_sizeof()
+{
+    return sizeof(TypeId);
+}
 }
 
 #endif
