077b6aacccfff273c0d8dc89a062b53449507f48
Sergio Maldonado <smaldona@psbuild-rhel7-01.slac.stanford.edu>
2019-02-14 18:43:28 -0800

    Improve shmem pytest

--- psana/psana/tests/test_shmem.py
+++ psana/psana/tests/test_shmem.py
@@ -1,25 +1,27 @@
 import os, shutil
 import subprocess
 import sys, os
-sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 from psana import DataSource
 
-
 class Test:
-    @classmethod
-    def setup_class(cls):
-        subprocess.call(['xtcwriter'])
-        subprocess.call(['shmemServer'])
-
     def launch_server(self):
         cmd_args = ['shmemServer','-n','8','-f','data_shmem.xtc2','-p','shmem_test','-s','0x80000','-c','4']
         return subprocess.Popen(cmd_args, stdout=subprocess.PIPE)
 
     def launch_client(self):
+        dg_count = 0
         ds = DataSource('shmem','shmem_test')
         run = next(ds.runs())
         for evt in run.events():
-            print("event transitionId",evt._dgrams[0].seq.service())
+            if not evt:
+                break
+            if not evt._dgrams:
+                break
+            if not len(evt._dgrams):
+                break   
+            if evt._dgrams[0].seq.service() == 12:
+                dg_count += 1
+        assert dg_count == 4
                 
     def setup_input_files(self):
         subprocess.call(['xtcwriter','-n','4','-f','data_shmem.xtc2', '-t'])
