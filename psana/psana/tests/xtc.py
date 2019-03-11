b50badc9a3d9d575be63bc7becdd0d817432e854
Christopher O'Grady <cpo@slac.stanford.edu>
2019-03-11 14:40:42 -0700

    eliminate xpphsd from cydgram tests since it contains unsupported charstr/enum types

--- psana/psana/tests/xtc.py
+++ psana/psana/tests/xtc.py
@@ -3,7 +3,7 @@ from sys import getrefcount as getref
 import numpy as np
 from psana import DataSource
 
-def refcnt_test(fname,nsegments):
+def refcnt_test(fname,nsegments,cydgram):
   ds = DataSource(fname)  
   for nevent,evt in enumerate(ds.events()):
       if nevent==0:
@@ -15,7 +15,13 @@ def refcnt_test(fname,nsegments):
   # these reference count numbers. - cpo
   dgrambytes_event1 = dgram_event1._dgrambytes
   # 4 arrays per segment, 1 for dgram, 1 for getref, 1 for dgrambytes_event1
-  assert getref(dgrambytes_event1)==4*nsegments+3
+  # cydgram test eliminates 3 of these arrays however (a hack since
+  # the xpphsd detector has unsupported cydgram types of charstr/enum)
+  if cydgram:
+    assert getref(dgrambytes_event1)==1*nsegments+3
+  else:
+    assert getref(dgrambytes_event1)==4*nsegments+3
+
   # event0 dgram is deleted, so only 1 for dgrambytes_event0 and 1 for getref
   assert getref(dgrambytes_event0)==2
 
@@ -25,20 +31,19 @@ from vals import testvals
 
 def valtester(dg, nsegments, cydgram):
   for iseg in range(nsegments):
-    hsdfex = dg.xpphsd[iseg].fex
-    hsdraw = dg.xpphsd[iseg].raw
-    cspadraw = dg.xppcspad[iseg].raw
-
-    assert np.array_equal(hsdraw.array0Pgp,testvals['array0Pgp'])
-    assert np.array_equal(hsdraw.array1Pgp,testvals['array1Pgp'])
-    assert hsdraw.floatPgp == testvals['floatPgp']
-    assert hsdraw.intPgp == testvals['intPgp']
-
-    assert hsdfex.floatFex == testvals['floatFex']
-    assert np.array_equal(hsdfex.arrayFex,testvals['arrayFex'])
-    assert hsdfex.intFex == testvals['intFex']
 
     if not cydgram:
+      hsdfex = dg.xpphsd[iseg].fex
+      hsdraw = dg.xpphsd[iseg].raw
+      assert np.array_equal(hsdraw.array0Pgp,testvals['array0Pgp'])
+      assert np.array_equal(hsdraw.array1Pgp,testvals['array1Pgp'])
+      assert hsdraw.floatPgp == testvals['floatPgp']
+      assert hsdraw.intPgp == testvals['intPgp']
+
+      assert hsdfex.floatFex == testvals['floatFex']
+      assert np.array_equal(hsdfex.arrayFex,testvals['arrayFex'])
+      assert hsdfex.intFex == testvals['intFex']
+
       assert hsdfex.charStrFex == testvals['charStrFex']
       assert hsdfex.enumFex1.value == testvals['enumFex1']['value']
       assert hsdfex.enumFex1.names == testvals['enumFex1']['names']
@@ -47,13 +52,14 @@ def valtester(dg, nsegments, cydgram):
       assert hsdfex.enumFex3.value == testvals['enumFex3']['value']
       assert hsdfex.enumFex3.names == testvals['enumFex3']['names']
 
-      assert np.array_equal(cspadraw.arrayRaw,testvals['arrayRaw'])
+    cspadraw = dg.xppcspad[iseg].raw
+    assert np.array_equal(cspadraw.arrayRaw,testvals['arrayRaw'])
 
 def xtc(fname, nsegments, cydgram=False):
 
   import sys, os
   sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
 
-  dgram, config = refcnt_test(fname,nsegments)
+  dgram, config = refcnt_test(fname,nsegments,cydgram)
   valtester(config, nsegments, cydgram)
   valtester(dgram, nsegments, cydgram)
