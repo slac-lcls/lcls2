628623d279be28c182734b3f0e0093a1d55a4f7c
Christopher O'Grady <cpo@slac.stanford.edu>
2019-04-10 15:17:30 -0700

    start using configdb for hsd

--- psdaq/drp/Digitizer.cc
+++ psdaq/drp/Digitizer.cc
@@ -3,6 +3,7 @@
 #include "xtcdata/xtc/VarDef.hh"
 #include "xtcdata/xtc/DescData.hh"
 #include "xtcdata/xtc/NamesLookup.hh"
+#include "xtcdata/xtc/Json2Xtc.hh"
 #include "rapidjson/document.h"
 #include "xtcdata/xtc/XtcIterator.hh"
 #include "psalg/digitizer/Stream.hh"
@@ -32,70 +33,6 @@ public:
     }
 };
 
-// TODO: this is not ideal. Should be done dynamically using information returned in the json
-class HsdConfigDef:public VarDef
-{
-public:
-  enum index
-    {
-      enable,
-      raw_prescale,
-      lane_mask,
-    };
-
-  HsdConfigDef()
-   {
-       NameVec.push_back({"enable",Name::UINT64,1});
-       NameVec.push_back({"raw_prescale",Name::UINT64,1});
-       NameVec.push_back({"lane_mask",Name::UINT64,1});
-   }
-};
-static HsdConfigDef myHsdConfigDef;
-
-class HsdIter : public XtcIterator
-{
-public:
-    enum { Stop, Continue };
-    HsdIter(Xtc* xtc, NamesLookup& namesLookup) : XtcIterator(xtc), _namesLookup(namesLookup)
-    {
-    }
-
-    int process(Xtc* xtc)
-    {
-        switch (xtc->contains.id()) {
-
-        case (TypeId::Parent): {
-            iterate(xtc);
-            break;
-        }
-        case (TypeId::ShapesData): {
-            ShapesData& shapesdata = *(ShapesData*)xtc;
-            // lookup the index of the names we are supposed to use
-            NamesId& namesId = shapesdata.namesId();
-            DescData descdata(shapesdata, _namesLookup[namesId]);
-            Names& names = descdata.nameindex().names();
-
-            for (unsigned i = 0; i < names.num(); i++) {
-                Name& name = names.get(i);
-                // Here we check algorithm and version can be analyzed
-                if (strcmp(name.alg().name(), "fpga") == 0) {
-                    if (name.alg().version() == 0x010203) {
-                        auto array = descdata.get_array<uint8_t>(i);
-                        chans[std::string(name.name())] = array.data();
-                    }
-                }
-            }
-            break;
-        }
-        default:
-            break;
-        }
-        return Continue;
-    }
-    NamesLookup& _namesLookup;
-    std::map<std::string, uint8_t*> chans;
-};
-
 Digitizer::Digitizer(Parameters* para) :
     Detector(para),
     m_evtcount(0),
@@ -103,62 +40,53 @@ Digitizer::Digitizer(Parameters* para) :
 {
 }
 
-unsigned addJson(Xtc& xtc, NamesLookup& namesLookup, NamesId& configNamesId) {
+static void check(PyObject* obj) {
+    if (!obj) {
+        PyErr_Print();
+        throw "**** python error\n";
+    }
+}
 
-    FILE* file;
+unsigned addJson(Xtc& xtc, NamesId& configNamesId) {
+
     Py_Initialize();
-    PyObject* main_module = PyImport_AddModule("__main__");
-    PyObject* main_dict = PyModule_GetDict(main_module);
-    file = fopen("hsdconfig.py","r");
-    PyObject* pyrunfileptr = PyRun_File(file, "hsdconfig.py", Py_file_input, main_dict, main_dict);
-    assert(pyrunfileptr!=NULL);
-    PyObject* mybytes = PyDict_GetItemString(main_dict,"config");
-    PyObject * temp_bytes = PyUnicode_AsEncodedString(mybytes, "UTF-8", "strict");
-    char* json = (char*)PyBytes_AsString(temp_bytes);
-    Py_Finalize();
+    // returns new reference
+    PyObject* pModule = PyImport_ImportModule("psalg.configdb.hsd_config");
+    check(pModule);
+    // returns borrowed reference
+    PyObject* pDict = PyModule_GetDict(pModule);
+    check(pDict);
+    // returns borrowed reference
+    PyObject* pFunc = PyDict_GetItemString(pDict, (char*)"hsd_config");
+    check(pFunc);
+    // returns new reference
+    PyObject* mybytes = PyObject_CallFunction(pFunc,"sssss",
+                                              "mcbrowne:psana@psdb-dev:9306",
+                                              "configDB", "TMO", "BEAM",
+                                              "xpphsd1");
+    check(mybytes);
+    // returns new reference
+    PyObject * json_bytes = PyUnicode_AsASCIIString(mybytes);
+    check(json_bytes);
+    char* json = (char*)PyBytes_AsString(json_bytes);
+    printf("json: %s\n",json);
 
-    Document d;
-    d.Parse(json);
+    // convert to json to xtc
+    char buffer[4*1024*1024];
+    unsigned len = translateJson2Xtc(json, buffer, configNamesId);
+    if (len <= 0) {
+        fprintf(stderr, "Parse errors, exiting.\n");
+        throw "**** json translation error\n";
+    }
 
-    Value& software = d["alg"]["software"];
-    Value& version = d["alg"]["version"];
-    for (SizeType i = 0; i < version.Size(); i++) // Uses SizeType instead of size_t
-        printf("version[%d] = %d\n", i, version[i].GetInt());
+    // append the config xtc to the dgram
 
-    // High level algorithm information
-    Value& ninfo_software = d["ninfo"]["software"];
-    Value& ninfo_detector = d["ninfo"]["detector"];
-    Value& ninfo_serialNum = d["ninfo"]["serialNum"];
-    // Value& ninfo_seg = d["ninfo"]["seg"];
+    Py_DECREF(pModule);
+    Py_DECREF(mybytes);
+    Py_DECREF(json_bytes);
 
-    Alg hsdConfigAlg(software.GetString(),version[0].GetInt(),version[1].GetInt(),version[2].GetInt());
-    Names& configNames = *new(xtc) Names(ninfo_software.GetString(), hsdConfigAlg,
-                                         ninfo_detector.GetString(), ninfo_serialNum.GetString(), configNamesId);
-    configNames.add(xtc, myHsdConfigDef);
-    namesLookup[configNamesId] = NameIndex(configNames);
-    printf("Done configNames\n");
-
-    CreateData config(xtc, namesLookup, configNamesId);
-
-    // TODO: dynamically discover
-
-    Value& enable = d["xtc"]["ENABLE"];
-    unsigned shape[MaxRank] = {enable.Size()};
-    printf("enable: %d\n", shape[0]);
-    Array<uint64_t> arrayT = config.allocate<uint64_t>(HsdConfigDef::enable,shape); //FIXME: figure out avoiding hardwired zero
-    unsigned lane_mask = 0;
-    for(unsigned i=0; i<shape[0]; i++){
-        arrayT(i) = (uint64_t) enable[i].GetInt();
-        lane_mask += enable[i].GetInt() << (shape[0]-(i+1)); // convert enable to unsigned using bitshift
-    };
-
-    Value& raw_prescale = d["xtc"]["RAW_PS"];
-    shape[MaxRank] = {raw_prescale.Size()};
-    printf("raw_ps shape: %d\n", shape[0]);
-    Array<uint64_t> arrayT1 = config.allocate<uint64_t>(HsdConfigDef::raw_prescale,shape); //FIXME: figure out avoiding hardwired zero
-    for(unsigned i=0; i<shape[0]; i++){
-        arrayT1(i) = (uint64_t) raw_prescale[i].GetInt();
-    };
+    // cpo short-term hack: should get this from the json
+    unsigned lane_mask = 1;
 
     return lane_mask;
 }
@@ -166,29 +94,31 @@ unsigned addJson(Xtc& xtc, NamesLookup& namesLookup, NamesId& configNamesId) {
 void Digitizer::configure(Dgram& dgram, PGPData* pgp_data)
 {
     // copy Event header into beginning of Datagram
+    // cpo: feels like this should not be in the detector-specific code.
     int index = __builtin_ffs(pgp_data->buffer_mask) - 1;
     Pds::TimingHeader* timing_header = reinterpret_cast<Pds::TimingHeader*>(pgp_data->buffers[index].data);
     dgram.seq = timing_header->seq;
     dgram.env = timing_header->env;
 
     unsigned lane_mask;
-    //NamesId configNamesId(m_nodeId,ConfigNamesIndex);
-    //lane_mask = addJson(dgram.xtc, m_namesVec, configNamesId);
+    // set up the names for the configuration data
+    NamesId configNamesId(m_nodeId,ConfigNamesIndex);
+    lane_mask = addJson(dgram.xtc, configNamesId);
 
-    Alg hsdAlg("hsd", 1, 2, 3); // TODO: shouldn't this be configured by hsdconfig.py?
+    // set up the names for L1Accept data
+    Alg hsdAlg("hsd", 1, 2, 3); // TODO: should this be configured by hsdconfig.py?
     unsigned segment = 0;
     Names& eventNames = *new(dgram.xtc) Names("xpphsd", hsdAlg, "hsd", "detnum1235", m_evtNamesId, segment);
     HsdDef myHsdDef(lane_mask);
     eventNames.add(dgram.xtc, myHsdDef);
     m_namesLookup[m_evtNamesId] = NameIndex(eventNames);
-
-    HsdIter hsdIter(&dgram.xtc, m_namesLookup);
-    hsdIter.iterate();
-    // unsigned nChan = hsdIter.chans.size();
 }
 
 void Digitizer::event(Dgram& dgram, PGPData* pgp_data)
 {
+    // copy Event header into beginning of Datagram
+    // cpo: feels like this should not be in the detector-specific code.
+    printf("**** event\n");
     m_evtcount+=1;
     int index = __builtin_ffs(pgp_data->buffer_mask) - 1;
     Pds::TimingHeader* timing_header = reinterpret_cast<Pds::TimingHeader*>(pgp_data->buffers[index].data);
