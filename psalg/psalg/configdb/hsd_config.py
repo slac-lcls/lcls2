a48c4aa26b0bf848950716912792873bd4968820
Christopher O'Grady <cpo@slac.stanford.edu>
2019-04-19 11:37:01 -0700

    make hsd config more official

---
+++ psalg/psalg/configdb/hsd_config.py
@@ -0,0 +1,58 @@
+from psalg.configdb.get_config import get_config
+from p4p.client.thread import Context
+from bson.json_util import dumps
+
+def epics_names_values(pvtable,cfg,names,values):
+    for k, v1 in pvtable.items():
+        v2 = cfg[k]
+        if isinstance(v1, dict):
+            epics_names_values(v1,v2,names,values)
+        else:
+            names.append(v1)
+            values.append(v2)
+
+def hsd_config(epics_prefix,dburl,dbname,hutch,cfgtype,detname):
+
+    cfg = get_config(dburl,dbname,hutch,cfgtype,detname)
+
+    # this structure of epics variable names must mirror
+    # the configdb.  alternatively, we could consider
+    # putting these in the configdb, perhaps as readonly fields.
+    pvtable = {'enable':'ENABLE',
+               'raw' : {'start'    : 'RAW_START',
+                        'gate'     : 'RAW_GATE',
+                        'prescale' : 'RAW_PS'},
+               'fex' : {'start'    : 'FEX_START',
+                        'gate'     : 'FEX_GATE',
+                        'prescale' : 'FEX_PS',
+                        'ymin'     : 'FEX_YMIN',
+                        'ymax'     : 'FEX_YMAX',
+                        'xpre'     : 'FEX_XPRE',
+                        'xpost'    : 'FEX_XPOST'},
+               'expert' : {'datamode'   : 'TESTPATTERN',
+                           'syncelo'    : 'SYNCELO',
+                           'syncehi'    : 'SYNCEHI',
+                           'syncolo'    : 'SYNCOLO',
+                           'syncohi'    : 'SYNCOHI',
+                           'fullthresh' : 'FULLEVT',
+                           'fullsize'   : 'FULLSIZE',
+                           'trigshift'  : 'TRIGSHIFT',
+                           'pgpskip'    : 'PGPSKPINTVL'}
+    }
+
+    names = []
+    values = []
+    # look in the cfg dictionary for values that match the epics
+    # variables in the pvtable
+    epics_names_values(pvtable,cfg,names,values)
+    names = [epics_prefix+':'+name for name in names]
+
+    # program the values
+    ctxt = Context('pva')
+    myctxt = ctxt.put(names,values)
+    #for name,value in zip(names,values):
+    #    print('***',name,value)
+    #    ctxt.put(name,value)
+    ctxt.close()
+
+    return dumps(cfg)
--- psdaq/drp/hsd_config_store.py
+++ psdaq/drp/hsd_config_store.py
@@ -12,30 +12,43 @@ mycdb = cdb.configdb('mcbrowne:psana@psdb-dev:9306', instrument, create, dbname)
 print('Configs:')
 mycdb.print_configs()
 print(70*'-')
-# this needs to be called once to create the
-# collection name (same as detType in top.setInfo)
+# this needs to be called once per detType at the
+# "beginning of time" to create the collection name (same as detType
+# in top.setInfo).  It doesn't hurt to call it again if the collection
+# already exists, however.
 mycdb.add_device_config('hsd')
 
 top = cdict()
 
 top.setInfo('hsd', 'xpphsd', 'serial1234', 'No comment')
 top.setAlg('hsdConfig', [0,0,1])
-# Scalars
-top.define_enum('EnableEnum', {'Disable': 0, 'Enable': 1})
-#top.set('top.enable', 1, 'EnableEnum')
-top.set('enable', 1, 'EnableEnum')
 
-top.set('raw.start', 4, 'UINT16')
-top.set('raw.gate', 20, 'UINT16')
-top.set('raw.prescale', 1, 'UINT16')
+# this should be an array of enums, but not yet supported
+top.set('enable', 1*[1], 'UINT32')
 
-top.set('fex.start', 4, 'UINT16')
-top.set('fex.gate', 20, 'UINT16')
-top.set('fex.prescale', 1, 'UINT16')
-top.set('fex.ymin', 0, 'UINT16')
-top.set('fex.ymax', 2000, 'UINT16')
-top.set('fex.xpre', 2, 'UINT16')
-top.set('fex.xpost', 1, 'UINT16')
+top.set('raw.start', 1*[4], 'UINT32')
+top.set('raw.gate', 1*[20], 'UINT32')
+top.set('raw.prescale', 1*[1], 'UINT32')
+
+top.set('fex.start', 1*[4], 'UINT32')
+top.set('fex.gate', 1*[20], 'UINT32')
+top.set('fex.prescale', 1*[1], 'UINT32')
+top.set('fex.ymin', 1*[0], 'UINT32')
+top.set('fex.ymax', 1*[2000], 'UINT32')
+top.set('fex.xpre', 1*[2], 'UINT32')
+top.set('fex.xpost', 1*[1], 'UINT32')
+
+top.define_enum('dataModeEnum', {'Data': 0xffffffff, 'Ramp': 0, 'Spike11': 1, 'Spike12': 3, 'Spike16': 5})
+top.set('expert.datamode', 0xffffffff, 'dataModeEnum')
+
+top.set('expert.syncelo', 11300, 'UINT32')
+top.set('expert.syncehi', 11600, 'UINT32')
+top.set('expert.syncolo', 1600, 'UINT32')
+top.set('expert.syncohi', 1900, 'UINT32')
+top.set('expert.fullthresh', 4, 'UINT32')
+top.set('expert.fullsize', 3072, 'UINT32')
+top.set('expert.trigshift', 0, 'UINT32')
+top.set('expert.pgpskip', 65520, 'UINT32')
 
 mycdb.modify_device('BEAM', top)
 mycdb.print_configs()
