1a7a7e73d51679baf3da0d0dd74403f39e227ec4
Chris Ford <caf@slac.stanford.edu>
2019-04-26 09:00:02 -0700

    Default readout group is platform number.

--- psdaq/psdaq/control/selectPlatform.py
+++ psdaq/psdaq/control/selectPlatform.py
@@ -9,18 +9,22 @@ import argparse
 def main():
 
     # Process arguments
-    parser = argparse.ArgumentParser(epilog='The -R argument is required when selecting drp.')
+    parser = argparse.ArgumentParser()
     parser.add_argument('-p', metavar='PLATFORM', type=int, choices=range(0, 8), default=0, help='platform (default 0)')
     parser.add_argument('-C', metavar='COLLECT_HOST', default='localhost', help='collection host (default localhost)')
     parser.add_argument('-t', type=int, metavar='TIMEOUT', default=2000,
                         help='timeout msec (default 2000)')
-    parser.add_argument('-R', metavar='READOUT_GROUP', type=int, choices=range(0, 8), help='readout group (0-7) (drp only)')
+    parser.add_argument('-R', metavar='READOUT_GROUP', type=int, choices=range(0, 8), help='readout group (0-7, default platform)')
     parser.add_argument('-s', metavar='SELECT', action='append', help='select one alias (may be repeated)')
     parser.add_argument('--select-all', action='store_true', help='select all', dest='select_all')
     parser.add_argument('-u', metavar='UNSELECT', action='append', help='unselect one alias (may be repeated)')
     args = parser.parse_args()
 
     platform = args.p
+    if args.R is None:
+        readout = platform
+    else:
+        readout = args.R
 
     # instantiate DaqControl object
     control = DaqControl(host=args.C, platform=args.p, timeout=args.t)
@@ -41,13 +45,10 @@ def main():
                     if args.select_all or (args.s is not None and alias in args.s):
                         if level == 'drp':
                             # select drp
-                            if args.R is None:
-                                # drp requires readout group
-                                parser.error('select drp: requires -R')
-                            if v['active'] != 1 or v['readout'] != args.R:
+                            if v['active'] != 1 or v['readout'] != readout:
                                 changed = True
                                 v['active'] = 1
-                                v['readout'] = args.R
+                                v['readout'] = readout
                         else:
                             # select teb or meb
                             if v['active'] != 1:
@@ -59,8 +60,6 @@ def main():
                         if v['active'] != 0:
                             changed = True
                             v['active'] = 0
-                            if level == 'drp':
-                                v['readout'] = platform
         except Exception:
             pprint.pprint(body)
             raise
