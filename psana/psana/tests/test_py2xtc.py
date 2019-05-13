7835d5e1268c2479b48d6f33950cfdae01eb817e
Christopher O'Grady <cpo@slac.stanford.edu>
2019-05-13 14:40:49 -0700

    add test demonstrating writing xtc2 from python

--- psana/psana/tests/test_py2xtc.py
+++ psana/psana/tests/test_py2xtc.py
@@ -18,7 +18,9 @@ def test_py2xtc(tmp_path):
     image_array = np.array([[1,2,3,4],[9,8,7,6]])
     orientations_array = np.array([4,3,2,1])
 
-    f = open(os.path.join(tmp_path,'junk.xtc2'),'wb')
+    fname = os.path.join(tmp_path,'junk.xtc2')
+
+    f = open(fname,'wb')
     for i in range(3):
         my_data = {
             'image': image_array+i,
@@ -31,7 +33,7 @@ def test_py2xtc(tmp_path):
     f.close()
 
     from psana import DataSource
-    ds = DataSource('junk.xtc2')
+    ds = DataSource(fname)
     myrun = next(ds.runs())
     for nevt,evt in enumerate(myrun.events()):
         assert np.array_equal(evt._dgrams[0].spi_cspad[0].raw.image,image_array+nevt+1)
