0c6d5eaf221da93365d7f77f65132422449cd737
Christopher O'Grady <cpo@slac.stanford.edu>
2019-05-13 14:29:30 -0700

    add test demonstrating writing xtc2 from python

---
+++ psana/psana/tests/test_py2xtc.py
@@ -0,0 +1,38 @@
+import dgramCreate as dc
+import numpy as np
+import os
+
+def test_py2xtc(tmp_path):
+
+    config = {}
+    detname = 'spi_cspad'
+    dettype = 'cspad'
+    serial_number = '1234'
+    namesid = 0
+
+    nameinfo = dc.nameinfo(detname,dettype,serial_number,namesid)
+    alg = dc.alg('raw',[1,2,3])
+
+    cydgram = dc.CyDgram()
+
+    image_array = np.array([[1,2,3,4],[9,8,7,6]])
+    orientations_array = np.array([4,3,2,1])
+
+    f = open(os.path.join(tmp_path,'junk.xtc2'),'wb')
+    for i in range(3):
+        my_data = {
+            'image': image_array+i,
+            'orientations': orientations_array+i
+        }
+
+        cydgram.addDet(nameinfo, alg, my_data)
+        xtc_bytes = cydgram.get(0,0,0)
+        f.write(xtc_bytes)
+    f.close()
+
+    from psana import DataSource
+    ds = DataSource('junk.xtc2')
+    myrun = next(ds.runs())
+    for nevt,evt in enumerate(myrun.events()):
+        assert np.array_equal(evt._dgrams[0].spi_cspad[0].raw.image,image_array+nevt+1)
+        assert np.array_equal(evt._dgrams[0].spi_cspad[0].raw.orientations,orientations_array+nevt+1)
