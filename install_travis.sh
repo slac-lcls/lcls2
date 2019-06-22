e843b6c837cb347458ebbc624f333e07afac7f48
Daniel Damiani <dsdamiani@gmail.com>
2019-06-21 21:53:53 -0700

    macos sed is not gnu sed.......

--- install_travis.sh
+++ install_travis.sh
@@ -3,14 +3,14 @@
 set -e
 
 if [[ $TRAVIS_OS_NAME == osx ]]; then
+  git clone https://github.com/slac-lcls/relmanage.git $HOME/relmanage
+  echo ${TRAVIS_PYTHON_VERSION}
+  sed -i.old "s|PYTHONVER|${TRAVIS_PYTHON_VERSION}|g" "$HOME/relmanage/env_create_macos.yaml"
   wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
   tar xJf MacOSX10.9.sdk.tar.xz -C $HOME/
   rm MacOSX10.9.sdk.tar.xz
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/miniconda
-  git clone https://github.com/slac-lcls/relmanage.git $HOME/relmanage
-  ls -l $HOME/relmanage
-  sed -i "s|PYTHONVER|${TRAVIS_PYTHON_VERSION}|g" "$HOME/relmanage/env_create_macos.yaml"
   source "$HOME/miniconda/etc/profile.d/conda.sh"
   conda config --set always_yes yes --set changeps1 no
   conda install conda-build anaconda-client
