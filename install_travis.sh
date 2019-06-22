eda2cecb7d55b57f1e8b44ca50ada6900ae65e67
Daniel Damiani <dsdamiani@gmail.com>
2019-06-21 21:48:17 -0700

    some debug for travis

--- install_travis.sh
+++ install_travis.sh
@@ -9,7 +9,8 @@ if [[ $TRAVIS_OS_NAME == osx ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/miniconda
   git clone https://github.com/slac-lcls/relmanage.git $HOME/relmanage
-  sed -i "s|PYTHONVER|${TRAVIS_PYTHON_VERSION}|g" $HOME/relmanage/env_create_macos.yaml
+  ls -l $HOME/relmanage
+  sed -i "s|PYTHONVER|${TRAVIS_PYTHON_VERSION}|g" "$HOME/relmanage/env_create_macos.yaml"
   source "$HOME/miniconda/etc/profile.d/conda.sh"
   conda config --set always_yes yes --set changeps1 no
   conda install conda-build anaconda-client
