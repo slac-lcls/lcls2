b4edc47eb0b3ede87f1e3f32c43d25281b88be3f
Chris Ford <caf@slac.stanford.edu>
2019-08-08 16:53:31 -0700

    Remove ConfigUpdate transition.

--- psalg/psalg/shmem/TransitionCache.hh
+++ psalg/psalg/shmem/TransitionCache.hh
@@ -46,12 +46,7 @@ namespace psalg {
 #define TR_HASH(a,b)                (((a)*1000)+(b))
 
 #define CONFIGURE_UNCONFIGURE       TR_HASH(XtcData::TransitionId::Configure, XtcData::TransitionId::Unconfigure)
-#define CONFIGURE_ENABLE            TR_HASH(XtcData::TransitionId::Configure, XtcData::TransitionId::Enable)
-#define CONFIGURE_CONFIGUPDATE      TR_HASH(XtcData::TransitionId::Configure, XtcData::TransitionId::ConfigUpdate)
-
-#define CONFIGUPDATE_UNCONFIGURE    TR_HASH(XtcData::TransitionId::ConfigUpdate, XtcData::TransitionId::Unconfigure)
-#define CONFIGUPDATE_CONFIGUPDATE   TR_HASH(XtcData::TransitionId::ConfigUpdate, XtcData::TransitionId::ConfigUpdate)
-#define CONFIGUPDATE_ENABLE         TR_HASH(XtcData::TransitionId::ConfigUpdate, XtcData::TransitionId::Enable)
+#define CONFIGURE_BEGINRUN          TR_HASH(XtcData::TransitionId::Configure, XtcData::TransitionId::BeginRun)
 
 #define ENABLE_DISABLE              TR_HASH(XtcData::TransitionId::Enable, XtcData::TransitionId::Disable)
 #define ENABLE_SLOWUPDATE           TR_HASH(XtcData::TransitionId::Enable, XtcData::TransitionId::SlowUpdate)
