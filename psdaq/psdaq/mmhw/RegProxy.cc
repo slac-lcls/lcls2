975c939663d77b9c4743dd93c7f025ceab7137bc
Matt Weaver <weaver@slac.stanford.edu>
2019-08-20 11:47:56 -0700

    Autodetect RegProxy for hsd

--- psdaq/psdaq/mmhw/RegProxy.cc
+++ psdaq/psdaq/mmhw/RegProxy.cc
@@ -14,16 +14,34 @@ void RegProxy::initialize(void* base, void* csr)
 {
   _base = reinterpret_cast<uint64_t>(base);
   _csr  = reinterpret_cast<uint32_t*>(csr);
+  //  Test if proxy exists
+  { 
+    printf("Testing RegProxy @ %p\n",_csr);
+    const unsigned t = 0xdeadbeef;
+    _csr[2] = t;
+    printf("\tWrote %x\n",t);
+    volatile unsigned v = _csr[2];
+    printf("\tRead %x\n",v);
+    if (v != t)
+      _csr = 0;
+  }
 }
 
 RegProxy& RegProxy::operator=(const unsigned r)
 {
+  if (!_csr) {
+    _reserved = r;
+    return *this;
+  }
+
   _sem.take();
 
   //  launch transaction
   _csr[3] = r;
   _csr[2] = reinterpret_cast<uint64_t>(this)-_base;
   _csr[0] = 0;
+
+  printf("RegProxy write 0x%llx\n", reinterpret_cast<uint64_t>(this)-_base);
 
   //  wait until transaction is complete
   unsigned tmo=0;
@@ -44,11 +62,17 @@ RegProxy& RegProxy::operator=(const unsigned r)
 
 RegProxy::operator unsigned() const 
 {
+  if (!_csr) {
+    return _reserved;
+  }
+
   _sem.take();
 
   //  launch transaction
   _csr[2] = reinterpret_cast<uint64_t>(this)-_base;
   _csr[0] = 1;
+
+  printf("RegProxy read 0x%llx\n", reinterpret_cast<uint64_t>(this)-_base);
 
   //  wait until transaction is complete
   unsigned tmo=0;
