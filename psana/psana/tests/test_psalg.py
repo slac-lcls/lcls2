7eb1590a45392e381396499c50a3a1ace073d5af
Christopher O'Grady <cpo@slac.stanford.edu>
2019-08-22 13:04:14 -0700

    fix issue with CFD include path in psana/setup.py and enable Seshu's test

--- psana/psana/tests/test_psalg.py
+++ psana/psana/tests/test_psalg.py
@@ -7,6 +7,28 @@ doPlot = 0
 
 #------------------------------
 
+def test_cfd():
+    import constFracDiscrim as cfd
+    import numpy as np
+    import math
+
+    num_samples = 10000
+    sample_interval = math.pi
+    horpos = 0
+    gain = 10
+    offset = 0
+
+    times = np.linspace(0, math.pi, num=num_samples)
+    waveform = gain*np.sin(times)
+
+    delay = 1
+    walk = 40
+    threshold = 8
+    fraction = 0.5
+
+    peak_time = cfd.cfd(sample_interval, horpos, gain, offset, waveform, delay, walk, threshold, fraction)
+    assert(abs(peak_time - 1.58732246) < 1e-8)
+
 def test_peakFinder():
     import peakFinder
     import numpy as np
@@ -97,6 +119,7 @@ def test_peakFinder():
 
 def psalg() :
     test_peakFinder()
+    test_cfd()
 
 #------------------------------
 
