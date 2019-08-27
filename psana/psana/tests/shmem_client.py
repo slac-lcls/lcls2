088dd8e8f5386226cadf8da3021a83a5182878f0
Christopher O'Grady <cpo@slac.stanford.edu>
2019-08-27 16:55:34 -0700

    test Detector xface in shmem test

--- psana/psana/tests/shmem_client.py
+++ psana/psana/tests/shmem_client.py
@@ -2,11 +2,15 @@ import os, shutil
 import subprocess
 import sys, os
 from psana import DataSource
+import numpy as np
+import vals
 
 def launch_client(pid):
     dg_count = 0
     ds = DataSource(shmem='shmem_test_'+pid)
     run = next(ds.runs())
+    cspad = run.Detector('xppcspad')
+    hsd = run.Detector('xpphsd')
     for evt in run.events():
         if not evt:
             break
@@ -15,6 +19,11 @@ def launch_client(pid):
         if not len(evt._dgrams):
             break
         # check for L1 accept transition ID 12
+        assert(hsd.raw.calib(evt).shape==(5,))
+        assert(hsd.fex.calib(evt).shape==(6,))
+        padarray = vals.padarray
+        assert(np.array_equal(cspad.raw.calib(evt),np.stack((padarray,padarray))))
+        assert(np.array_equal(cspad.raw.image(evt),np.vstack((padarray,padarray))))
         if evt._dgrams[0].seq.service() == 12:
             # immediately release datagram to server
             del evt._dgrams[0]
