29258af8de07467885607ac71574a9dd388a9ae1
Sioan <zohar.sioan@gmail.com>
2019-09-11 07:56:53 -0700

    DEV: got c calls to python working. now to start integrating json interpreter.

--- psdaq/drp/pgpread.cc
+++ psdaq/drp/pgpread.cc
@@ -9,6 +9,8 @@
 #include "xtcdata/xtc/Dgram.hh"
 #include <unistd.h>
 #include <getopt.h>
+#include <time.h>
+#include <Python.h>
 
 #define MAX_RET_CNT_C 1000
 static int fd;
@@ -25,9 +27,85 @@ void int_handler(int dummy)
     // dmaUnMapDma();
 }
 
-int main(int argc, char* argv[])
+
+int main(int argc, char *argv[])
 {
+    PyObject *pName, *pModule, *pFunc;
+    PyObject *pArgs, *pValue;
+    int i;
+
+    if (argc < 3) {
+        fprintf(stderr,"Usage: call pythonfile funcname [args]\n");
+        return 1;
+    }
+
+    Py_Initialize();
+
+    PyObject* sysPath = PySys_GetObject((char*)"path");
+    PyList_Append(sysPath, PyUnicode_FromString("."));
+
+    pName = PyUnicode_DecodeFSDefault(argv[1]);
+    /* Error checking of pName left out */
+
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+
+    if (pModule != NULL) {
+        pFunc = PyObject_GetAttrString(pModule, argv[2]);
+        /* pFunc is a new reference */
+
+        if (pFunc && PyCallable_Check(pFunc)) {
+            pArgs = PyTuple_New(argc - 3);
+            for (i = 0; i < argc - 3; ++i) {
+                pValue = PyLong_FromLong(atoi(argv[i + 3]));
+                if (!pValue) {
+                    Py_DECREF(pArgs);
+                    Py_DECREF(pModule);
+                    fprintf(stderr, "Cannot convert argument\n");
+                    return 1;
+                }
+                /* pValue reference stolen here: */
+                PyTuple_SetItem(pArgs, i, pValue);
+            }
+            pValue = PyObject_CallObject(pFunc, pArgs);
+            Py_DECREF(pArgs);
+            if (pValue != NULL) {
+                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
+                Py_DECREF(pValue);
+            }
+            else {
+                Py_DECREF(pFunc);
+                Py_DECREF(pModule);
+                PyErr_Print();
+                fprintf(stderr,"Call failed\n");
+                return 1;
+            }
+        }
+        else {
+            if (PyErr_Occurred())
+                PyErr_Print();
+            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
+        }
+        Py_XDECREF(pFunc);
+        Py_DECREF(pModule);
+    }
+    else {
+        PyErr_Print();
+        fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
+        return 1;
+    }
+    if (Py_FinalizeEx() < 0) {
+        return 120;
+    }
+    return 0;
+}
+
+int main_b(int argc, char* argv[])
+{
+
     int c, channel;
+
+    timespec ts; 
 
     channel = 0;
     std::string device;
@@ -72,6 +150,13 @@ int main(int argc, char* argv[])
     int32_t dmaRet[MAX_RET_CNT_C];
     uint32_t dmaIndex[MAX_RET_CNT_C];
     uint32_t dmaDest[MAX_RET_CNT_C];
+
+    uint8_t *raw_data;
+
+    uint8_t expected_next_count = 0;
+
+    uint32_t raw_counter = 0;
+
     while (1) {
         if (terminate.load(std::memory_order_acquire) == true) {
             close(fd);
@@ -79,17 +164,41 @@ int main(int argc, char* argv[])
             break;
         }
 
+        clock_gettime(CLOCK_REALTIME, &ts);
+
         int32_t ret = dmaReadBulkIndex(fd, MAX_RET_CNT_C, dmaRet, dmaIndex, NULL, NULL, dmaDest);
         for (int b=0; b < ret; b++) {
             uint32_t index = dmaIndex[b];
             uint32_t size = dmaRet[b];
-            uint32_t dest = dmaDest[b] >> 8;
-            Pds::TimingHeader* event_header = reinterpret_cast<Pds::TimingHeader*>(dmaBuffers[index]);
-            XtcData::TransitionId::Value transition_id = event_header->seq.service();
+            //uint32_t dest = dmaDest[b] >> 8;
+            raw_data = reinterpret_cast<uint8_t *>(dmaBuffers[index]);
 
-            printf("Size %u B | Dest %u | Transition id %d | pulse id %lu | event counter %u | index %u\n",
-                   size, dest, transition_id, event_header->seq.pulseId().value(), event_header->evtCounter, index);
-            printf("env %08x\n", event_header->env);
+            if(size !=2112){
+                printf("corrupted frame. size = %d",size);
+            }
+            
+
+            if(raw_counter%100000 == 0){
+                printf("%x %x %x %x %d %d %d %d",raw_data[1],expected_next_count,raw_data[32],raw_data[32],ts.tv_sec,ts.tv_nsec,raw_counter,size);
+                printf("\n");
+            }
+
+
+            raw_counter = raw_counter + 1;
+
+            if(expected_next_count != raw_data[1]){
+                printf("Dropped shot");
+                printf("\n");
+            }
+
+            expected_next_count = (raw_data[1]+1)%256;
+
+            //Pds::TimingHeader* event_header = reinterpret_cast<Pds::TimingHeader*>(dmaBuffers[index]);
+            //XtcData::TransitionId::Value transition_id = event_header->seq.service();
+
+            //printf("Size %u B | Dest %u | Transition id %d | pulse id %lu | event counter %u | index %u\n",
+            //       size, dest, transition_id, event_header->seq.pulseId().value(), event_header->evtCounter, index);
+            //printf("env %08x\n", event_header->env);
         }
 	    if ( ret > 0 ) dmaRetIndexes(fd, ret, dmaIndex);
 	    //sleep(0.1)
