ae3acf1677dd7d832b767bd886a913e487839157
Chris Ford <caf@slac.stanford.edu>
2019-09-12 13:51:57 -0700

    Clean up SysLog.hh.

--- psdaq/psdaq/service/SysLog.cc
+++ psdaq/psdaq/service/SysLog.cc
@@ -6,10 +6,54 @@ extern char *program_invocation_short_name;
 
 using namespace Pds;
 
-SysLog::SysLog(const char *instrument, int level) 
+void SysLog::init(const char *instrument, int level)
 {
     static char ident[20];
-    snprintf(ident, sizeof(ident)-1, "%s-%s", instrument, program_invocation_short_name);
+    if (instrument) {
+        snprintf(ident, sizeof(ident)-1, "%s-%s", instrument, program_invocation_short_name);
+    } else {
+        snprintf(ident, sizeof(ident)-1, "%s", program_invocation_short_name);
+    }
     openlog(ident, LOG_PERROR | LOG_PID, LOG_USER);
     setlogmask(LOG_UPTO(level));
+}
+
+void SysLog::debug(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vsyslog(LOG_DEBUG, fmt, args);
+    va_end(args);
+}
+
+void SysLog::info(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vsyslog(LOG_INFO, fmt, args);
+    va_end(args);
+}
+
+void SysLog::warning(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vsyslog(LOG_WARNING, fmt, args);
+    va_end(args);
+}
+
+void SysLog::error(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vsyslog(LOG_ERR, fmt, args);
+    va_end(args);
+}
+
+void SysLog::critical(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vsyslog(LOG_CRIT, fmt, args);
+    va_end(args);
 }
--- psdaq/psdaq/service/SysLog.hh
+++ psdaq/psdaq/service/SysLog.hh
@@ -8,59 +8,15 @@ namespace Pds {
 class SysLog
 {
 public:
-    SysLog(const char *instrument, int level);
+    // initialize
+    static void init(const char *instrument, int level);
 
-    static void init(const char *instrument, int level) 
-    {
-        static char ident[20];
-        if (instrument) {
-            snprintf(ident, sizeof(ident)-1, "%s-%s", instrument, program_invocation_short_name);
-        } else {
-            snprintf(ident, sizeof(ident)-1, "%s", program_invocation_short_name);
-        }
-        openlog(ident, LOG_PERROR | LOG_PID, LOG_USER);
-        setlogmask(LOG_UPTO(level));
-    }
-
-    static void debug(const char *fmt, ...)
-    {
-        va_list args;
-        va_start(args, fmt);
-        vsyslog(LOG_DEBUG, fmt, args);
-        va_end(args);
-    }
-
-    static void info(const char *fmt, ...)
-    {
-        va_list args;
-        va_start(args, fmt);
-        vsyslog(LOG_INFO, fmt, args);
-        va_end(args);
-    }
-
-    static void warning(const char *fmt, ...)
-    {
-        va_list args;
-        va_start(args, fmt);
-        vsyslog(LOG_WARNING, fmt, args);
-        va_end(args);
-    }
-
-    static void error(const char *fmt, ...)
-    {
-        va_list args;
-        va_start(args, fmt);
-        vsyslog(LOG_ERR, fmt, args);
-        va_end(args);
-    }
-
-    static void critical(const char *fmt, ...)
-    {
-        va_list args;
-        va_start(args, fmt);
-        vsyslog(LOG_CRIT, fmt, args);
-        va_end(args);
-    }
+    // log
+    static void debug(const char *fmt, ...);
+    static void info(const char *fmt, ...);
+    static void warning(const char *fmt, ...);
+    static void error(const char *fmt, ...);
+    static void critical(const char *fmt, ...);
 };
 }
 
