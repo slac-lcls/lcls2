09cabead5c98d506fa539623116d147aeb75bcb2
Chris Ford <caf@slac.stanford.edu>
2019-09-13 13:55:16 -0700

    Include severity name when logging to stderr.

--- psdaq/psdaq/service/SysLog.cc
+++ psdaq/psdaq/service/SysLog.cc
@@ -14,46 +14,91 @@ void SysLog::init(const char *instrument, int level)
     } else {
         snprintf(ident, sizeof(ident)-1, "%s", program_invocation_short_name);
     }
-    openlog(ident, LOG_PERROR | LOG_PID, LOG_USER);
+    openlog(ident, LOG_PID, LOG_USER);
     setlogmask(LOG_UPTO(level));
 }
 
 void SysLog::debug(const char *fmt, ...)
 {
     va_list args;
+
+    // syslog
     va_start(args, fmt);
     vsyslog(LOG_DEBUG, fmt, args);
+    va_end(args);
+
+    // stderr
+    char newfmt[256];
+    snprintf(newfmt, sizeof(newfmt), "DEBUG %s\n", fmt);
+    va_start(args, fmt);
+    vfprintf(stderr, newfmt, args);
     va_end(args);
 }
 
 void SysLog::info(const char *fmt, ...)
 {
     va_list args;
+
+    // syslog
     va_start(args, fmt);
     vsyslog(LOG_INFO, fmt, args);
+    va_end(args);
+
+    // stderr
+    char newfmt[256];
+    snprintf(newfmt, sizeof(newfmt), "INFO %s\n", fmt);
+    va_start(args, fmt);
+    vfprintf(stderr, newfmt, args);
     va_end(args);
 }
 
 void SysLog::warning(const char *fmt, ...)
 {
     va_list args;
+
+    // syslog
     va_start(args, fmt);
     vsyslog(LOG_WARNING, fmt, args);
+    va_end(args);
+
+    // stderr
+    char newfmt[256];
+    snprintf(newfmt, sizeof(newfmt), "WARNING %s\n", fmt);
+    va_start(args, fmt);
+    vfprintf(stderr, newfmt, args);
     va_end(args);
 }
 
 void SysLog::error(const char *fmt, ...)
 {
     va_list args;
+
+    // syslog
     va_start(args, fmt);
     vsyslog(LOG_ERR, fmt, args);
+    va_end(args);
+
+    // stderr
+    char newfmt[256];
+    snprintf(newfmt, sizeof(newfmt), "ERR %s\n", fmt);
+    va_start(args, fmt);
+    vfprintf(stderr, newfmt, args);
     va_end(args);
 }
 
 void SysLog::critical(const char *fmt, ...)
 {
     va_list args;
+
+    // syslog
     va_start(args, fmt);
     vsyslog(LOG_CRIT, fmt, args);
+    va_end(args);
+
+    // stderr
+    char newfmt[256];
+    snprintf(newfmt, sizeof(newfmt), "CRIT %s\n", fmt);
+    va_start(args, fmt);
+    vfprintf(stderr, newfmt, args);
     va_end(args);
 }
