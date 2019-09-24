d65c6dd6a7e1635ef432b0c4aabd5e02d619751a
Sioan <zohar.sioan@gmail.com>
2019-09-24 13:36:58 -0700

    TST: cli argument for test file location implemented

---
+++ psdaq/drp/AxiStreamBatcherParser.cc
@@ -0,0 +1,36 @@
+//This code a c++ interpretation of the python code located here.
+//This code reads in the 
+//https://github.com/slaclab/lcls2-pcie-apps/blob/system_dsp_integration_testing/software/TimeTool/python/TimeToolDev/eventBuilderParser.py
+
+
+
+class eventBuilderParser{
+{
+    public:
+        char* frames_to_position(frame_bytearray,position){
+
+        return 
+
+        }
+
+        //an axi stream batcher bytearray can be nested in another stream batcher bytearray.  This method checks
+        def _checkForSubFrames(){
+        return true;
+        }
+
+        int _resolveSubFrames(){
+
+        return true
+        }
+
+
+}
+
+
+class eventBuilderParser:timeToolParser{
+    int parseData(uint8_t *:raw_data){
+        
+    }
+
+
+}
