96af27ea63a916fd7d4d2ce536196a9665347b50
Christopher O'Grady <cpo@slac.stanford.edu>
2019-09-30 11:29:12 -0700

    since we now workaround shmem failure by setting the client count to 2, enable the smallh5 tests

--- psana/psana/tests/test_smalldata.py
+++ psana/psana/tests/test_smalldata.py
@@ -6,9 +6,6 @@ import run_smalldata
 
 def test_smalldata(tmp_path):
 
-    # remove this line when import of SmallData is fixed (due
-    # to shmem test crash)
-    if run_smalldata.DOTEST is False: return
     setup_input_files(tmp_path) # tmp_path is from pytest
     os.environ['TEST_XTC_DIR'] = str(tmp_path)
     run_smalldata.main()
