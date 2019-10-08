6a7d4c7528a584f85ec5ced7ed631010310d6e29
Sioan <zohar.sioan@gmail.com>
2019-10-08 13:19:24 -0700

    MAINT: gcc warnings are now all addressed for pgpread_timetool

--- psdaq/drp/AxiBatcherParser.hh
+++ psdaq/drp/AxiBatcherParser.hh
@@ -41,7 +41,6 @@ class eventBuilderParser {
         int                                   spsft               = 16;       //spsft stand for the size position in the sub frame tail
         int                                   HEADER_WIDTH        = 16;                            
         int                                   version;    
-        char*                                 vector_type_name    = "St6vector" ;
 
         bool                                  DEBUG;             
 
