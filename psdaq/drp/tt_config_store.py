355473a2b9bd16d30aa842633a4568d47fec4aa6
Sioan <zohar.sioan@gmail.com>
2019-10-15 13:58:19 -0700

    DOC: mongo database architecture docuementation

--- psdaq/drp/tt_config_store.py
+++ psdaq/drp/tt_config_store.py
@@ -5,10 +5,22 @@ import sys
 
 def write_scratch_pad(prescaling):
 
+    #database contains collections which are sets of documents (aka json objects).
+    #each type of device has a collection.  The elements of that collection are configurations of that type of device.
+    #e.g. there will be OPAL, EVR, and YUNGFRAU will be collections.  How they are configured will be a document contained within that collection
+    #Each hutch is also a collection.  Documents contained within these collection have an index, alias, and list of devices with configuration IDs
+    #How is the configuration of a state is described by the hutch and the alias found.  E.g. TMO and BEAM.  TMO is a collection.  
+    #BEAM is an alias of some of the documents in that collection. The document with the matching alias and largest index is the current 
+    #configuration for that hutch and alias.  
+    #When a device is configured, the device has a unique name OPAL7.  Need to search through document for one that has an NAME called OPAL7.  This will have
+    #have two fields "collection" and ID field (note how collection here is a field. ID points to a unique document).  This collection field and 
+    #ID point to the actuall Mongo DB collection and document
+
     create = True
-    dbname = 'configDB'
-    instrument = 'TMO'
-    mycdb = cdb.configdb('mcbrowne:psana@psdb-dev:9306', instrument, create, dbname)
+    dbname = 'configDB'     #this is the name of the database running on the server.  Only client care about this name.
+    instrument = 'TMO'      #
+
+    mycdb = cdb.configdb('mcbrowne:psana@psdb-dev:9306', instrument, create, dbname)    #mycdb.client.drop_database('configDB_szTest') will drop the configDB_szTest database
     my_dict = mycdb.get_configuration("BEAM","tmotimetool")
 
     top = cdict()
