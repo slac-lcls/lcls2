39bb0008235f2f679ac4e1eecd4c1558833df12d
msdubrovin <msdubrovin@gmail.com>
2019-10-15 17:47:37 -0700

    add hexanode/examples/ex-11-amox27716-r0100-acqiris.py

---
+++ psana/psana/hexanode/examples/ex-11-amox27716-r0100-acqiris.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+#--------------------
+
+import sys
+import psana
+import numpy as np
+from psana import DataSource
+
+import psana.pyalgos.generic.Graphics as gr
+from psana.pyalgos.generic.NDArrUtils import print_ndarr
+
+#import pyimgalgos.GlobalGraphics as gg
+#from pypsalg import find_edges
+#from time import time
+
+#--------------------
+
+BASE = 0.
+THR = -0.04 # -0.2
+CFR = 0.9
+DEADTIME = 5.0
+LEADINGEDGE = True # False # True
+
+BBAV=1000
+BEAV=2000
+
+#TIME_RANGE=(0.000003,0.000005)
+TIME_RANGE=(0.0000000,0.0000111)
+
+BBEG=6000
+BEND=22000 # 44000-2
+#BBEG=0
+#BEND=43000 # 44000-2
+
+EVSKIP = 0
+EVENTS = 10 + EVSKIP
+
+#dsname = 'exp=amox27716:run=100'
+#src1 = 'AmoEndstation.0:Acqiris.1' # 'ACQ1'
+#src2 = 'AmoEndstation.0:Acqiris.2' # 'ACQ2'
+
+#--------------------
+
+def draw_times(ax, wf, wt) :
+    #wf -= wf[0:1000].mean()
+    t0_sec = time()
+    edges = find_edges(wf, BASE, THR, CFR, DEADTIME, LEADINGEDGE)
+    print('  consumed time = %10.6f(sec)' % (time()-t0_sec))
+    # pairs of (amplitude,sampleNumber)
+    print(' nhits:', edges.shape[0])
+    #print(' edges:', edges)
+
+    for (amp,ind) in edges :
+        x0 = wt[int(ind)]
+        xarr = (x0,x0)
+        yarr = (amp,-amp)
+        gg.drawLine(ax, xarr, yarr, s=10, linewidth=1, color='k')
+
+#--------------------
+
+fig = gr.figure(figsize=(15,15), title='Image')
+fig.clear()
+
+#gr.move_fig(fig, 200, 100)
+##fig.canvas.manager.window.geometry('+200+100')
+
+naxes = 5
+dy = 1./naxes
+
+lw = 1
+w = 0.87
+h = dy - 0.04
+x0, y0 = 0.07, 0.03
+
+ch = (2,3,4,5,6)
+
+gfmt = ('b-', 'r-', 'g-', 'k-', 'm-', 'y-', 'c-', )
+ylab = ('X1', 'X2', 'Y1', 'Y2', 'MCP', 'XX', 'YY', )
+ax = [gr.add_axes(fig, axwin=(x0, y0 + i*dy, w, h)) for i in range(naxes)]
+
+#--------------------
+
+ds = DataSource(files='/reg/g/psdm/detector/data2_test/xtc/data-amox27716-r0100-acqiris-e000100.xtc2')
+myrun = next(ds.runs())
+
+for n,evt in enumerate(myrun.events()):
+
+    if n<EVSKIP : continue
+    if n>EVENTS : break
+
+    print(50*'_', '\n Event # %d' % n)
+    gr.set_win_title(fig, titwin='Event: %d' % n)
+
+    wf = evt._dgrams[0].tmo_hexanode[0].raw.waveforms
+    wt = evt._dgrams[0].tmo_hexanode[0].raw.times
+    print_ndarr(wt, '  times : ')
+    print_ndarr(wf, '  wforms: ')
+
+    for i in range(naxes) :
+        ax[i].clear()
+        ax[i].set_xlim(TIME_RANGE)
+        ax[i].set_ylabel(ylab[i], fontsize=14)
+
+        ich = ch[i]
+        print('  == ch:', ich,)
+
+        wftot = wf[ich,:]
+        wttot = wt[ich,:]
+
+        wfsel = np.copy(wftot[BBEG:BEND])
+        wtsel = np.copy(wttot[BBEG:BEND])
+
+        wfsel -= wftot[BBAV:BEAV].mean()
+        ax[i].plot(wtsel, wfsel, gfmt[i], linewidth=lw)
+
+        gr.drawLine(ax[i], ax[i].get_xlim(), (THR,THR), s=10, linewidth=1, color='k')
+        ##draw_times(ax[i], wfsel, wtsel)
+
+    gr.draw_fig(fig)
+    gr.show(mode='non-hold')
+
+gr.show()
+
+#--------------------
+
+sys.exit(0)
+
+#--------------------
