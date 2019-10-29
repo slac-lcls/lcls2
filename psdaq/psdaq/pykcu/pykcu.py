07de2d7d9a4a4b5e10c8cc8600b8dc839aea27cb
Matt Weaver <weaver@slac.stanford.edu>
2019-10-29 16:08:14 -0700

    Fix for HSD KCU

--- psdaq/psdaq/pykcu/pykcu.py
+++ psdaq/psdaq/pykcu/pykcu.py
@@ -64,10 +64,11 @@ pvdef = {'RxPwr'  : ('af',[0]*4),
 
 class PVStats(object):
 
-    def __init__(self, name, kcu):
+    def __init__(self, name, kcu, hsd=False):
         self.kcu   = kcu
         self.pv    = addPVT(name+':MON',pvdef)
         self.value = toDict(pvdef)
+        self.hsd   = hsd
     
     def init(self):
         pass
@@ -80,10 +81,13 @@ class PVStats(object):
         v = self.kcu.I2cBus.QSFP0.getTxBiasI()
         for i in range(len(v)):
             self.value['TxBiasI'][i] = v[i]
-        v = self.kcu.TDetSemi.getRTT()
-        for i in range(len(v)):
-            self.value['FullTT' ][i] = v[i][0]
-            self.value['nFullTT'][i] = v[i][1]
+        if self.hsd:
+            pass
+        else:
+            v = self.kcu.TDetSemi.getRTT()
+            for i in range(len(v)):
+                self.value['FullTT' ][i] = v[i][0]
+                self.value['nFullTT'][i] = v[i][1]
 
         value = self.pv.current()
         value['value'] = self.value
@@ -98,7 +102,8 @@ def main():
     global provider
 
     parser = argparse.ArgumentParser(prog=sys.argv[0], description='host PVs for KCU')
-    parser.add_argument('-i','--interval',type=int,help='PV update interval',default=10)
+    parser.add_argument('-i','--interval',type=int ,help='PV update interval',default=10)
+    parser.add_argument('-H','--hsd'     ,action='store_true',help='HSD node',default=False)
     args = parser.parse_args()
 
     # Set base
@@ -117,16 +122,16 @@ def main():
 
     kcu = base.KCU
 
-    print(kcu.TDetTiming.getClkRates())
-
-    kcu.I2cBus.selectDevice('QSFP0')
-    print(kcu.I2cBus.QSFP0.getRxPwr())
-
-    print(kcu.TDetSemi.getRTT())
+    if args.hsd:
+        kcu.I2cBus.selectDevice('QSFP0')
+        print(kcu.I2cBus.QSFP0.getRxPwr())
+    else:
+        print(kcu.TDetTiming.getClkRates())
+        print(kcu.TDetSemi.getRTT())
 
     provider = StaticProvider(__name__)
 
-    pvstats = PVStats('DAQ:LAB2:'+socket.gethostname().replace('-','_').upper(),kcu)
+    pvstats = PVStats('DAQ:LAB2:'+socket.gethostname().replace('-','_').upper(),kcu,args.hsd)
 
     # process PVA transactions
     updatePeriod = args.interval
