cc3bbe1cbf924cd19bbb5deb06e2a13ac4ea38cd
Sioan <zohar.sioan@gmail.com>
2019-11-12 14:02:51 -0800

    BUG: reading from correct document

--- psalg/psalg/configdb/tt_config.py
+++ psalg/psalg/configdb/tt_config.py
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     connect_info['body']['control'] = {}
     connect_info['body']['control']['0'] = {}
     connect_info['body']['control']['0']['control_info'] = {}
-    connect_info['body']['control']['0']['control_info']['instrument'] = 'TMO'
+    connect_info['body']['control']['0']['control_info']['instrument'] = 'TST'
     connect_info['body']['control']['0']['control_info']['cfg_dbase'] = 'mcbrowne:psana@psdb-dev:9306/sioanDB'
 
     mystring = json.dumps(connect_info)                             #paste this string into the pgpread_timetool.cc as parameter for the tt_config function call
