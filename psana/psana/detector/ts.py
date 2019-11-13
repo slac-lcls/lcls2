7078184da0338d93bc4703424804bd25a6b1ec29
Matt Weaver <weaver@slac.stanford.edu>
2019-11-13 12:42:48 -0800

    Fix timing system data unpacking - python bitstruct package has a big-endian assumption.

--- psana/psana/detector/ts.py
+++ psana/psana/detector/ts.py
@@ -1,7 +1,40 @@
-import bitstruct
+#import bitstruct
 import numpy as np
 from collections import namedtuple
 from psana.detector.detector_impl import DetectorImpl
+
+def _to_word(data,shift=0,mask=0):
+    rdata = data[::-1]
+    w = 0
+    for b in data[::-1]:
+        w = w<<8
+        w |= b
+    w >>= shift
+    if mask:
+        w &= (1<<mask)-1
+    return w
+
+def _unpack(data):
+    return ( _to_word(data[0:8]),       # pulseId
+             _to_word(data[8:16]),      # timestamp
+             _to_word(data[16:18],0,10),# fixed rate
+             _to_word(data[16:18],10,0),# ac rate
+             _to_word(data[18:20],0,3), # timeslot
+             _to_word(data[18:20],3,0), # phase
+             _to_word(data[20:22],0,1)==1, # beam_present
+             0,                         # reserved1
+             _to_word(data[20:22],4,4), # beam_destn
+             0,                         # reserved2
+             _to_word(data[22:24]),     # beam_charge
+             _to_word(data[24:26]),     # beam_energy_0
+             _to_word(data[26:28]),
+             _to_word(data[28:30]),
+             _to_word(data[30:32]),
+             _to_word(data[32:34]),     # wavelen_0
+             _to_word(data[34:36]),
+             0,                         # reserved3
+             _to_word(data[38:40]),     # mps_limit
+             _to_word(data[40:48]))     # mps_power_class
 
 class ts_ts_1_2_3(DetectorImpl):
     def __init__(self, *args):
@@ -31,7 +64,7 @@ class ts_ts_1_2_3(DetectorImpl):
         }
 
         self.TsData = namedtuple('tsdata',fields.keys())
-        format_string = ''
+        format_string = '>'
         total_len = 0
         for v in fields.values():
             format_string += v
@@ -39,7 +72,7 @@ class ts_ts_1_2_3(DetectorImpl):
         format_string += '<' # indicated least-significant-byte first
         self.total_bytes=total_len//8
 
-        self.bitstructure = bitstruct.compile(format_string)
+        #self.bitstructure = bitstruct.compile(format_string)
 
     def info(self,evt):
         # check for missing data
@@ -47,7 +80,8 @@ class ts_ts_1_2_3(DetectorImpl):
         if segments is None: return None
         # seems reasonable to assume that all TS data comes from one segment
         data = segments[0].data
-        unpacked = self.bitstructure.unpack(data.tobytes())
+        #unpacked = self.bitstructure.unpack(data.tobytes())
+        unpacked = _unpack(data)
         return self.TsData(*unpacked)
 
     def sequencer_info(self,evt):
@@ -59,6 +93,4 @@ class ts_ts_1_2_3(DetectorImpl):
         # look in the remaining bytes for the event codes
         tmp = data[self.total_bytes:]
         seq = tmp.view('uint16')
-        # remove the two extra bytes at the end, perhaps caused by
-        # pgp alignment issues?
-        return seq[:-1]
+        return seq
