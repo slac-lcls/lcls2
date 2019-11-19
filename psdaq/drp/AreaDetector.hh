c431dde51f6d34d78ee754be79371d54addf8ef4
Chris Ford <caf@slac.stanford.edu>
2019-11-19 10:41:52 -0800

    AreaDetector: record runinfo in xtc2 file.

--- psdaq/drp/AreaDetector.hh
+++ psdaq/drp/AreaDetector.hh
@@ -12,9 +12,10 @@ class AreaDetector : public XpmDetector
 public:
     AreaDetector(Parameters* para, MemPool* pool);
     unsigned configure(const std::string& config_alias, XtcData::Xtc& xtc) override;
+    unsigned beginrun(XtcData::Xtc& xtc, const nlohmann::json& runInfo) override;
     void event(XtcData::Dgram& dgram, PGPEvent* event) override;
 private:
-    enum {RawNamesIndex, FexNamesIndex};
+    enum {RawNamesIndex, FexNamesIndex, RunInfoNamesIndex};
     XtcData::NamesLookup m_namesLookup;
     unsigned m_evtcount;
 };
