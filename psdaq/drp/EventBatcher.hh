ead9fa4d1dc13d8bad52e8e9db5cdc83c9aaacfd
Christopher O'Grady <cpo@slac.stanford.edu>
2020-02-28 18:37:23 -0800

    add evtbatcher iterator

--- psdaq/drp/EventBatcher.hh
+++ psdaq/drp/EventBatcher.hh
@@ -11,12 +11,42 @@ namespace Drp {
     };
     class EvtBatcherSubFrameTail {
     public:
+        void* data() {return (void*)((char*)(this)-size);}
         uint32_t size;
         uint8_t  tdest;
         uint8_t  tuser_first;
         uint8_t  tuser_last;
         uint8_t  width;
         uint8_t  _unused[8]; // set for width==3
+    };
+    class EvtBatcherIterator {
+    public:
+        EvtBatcherIterator(EvtBatcherHeader* ebh, size_t bytes) :
+            // compute the first subframe ptr
+            _next(((char*)(ebh))+bytes-sizeof(EvtBatcherSubFrameTail)),
+            _end((char*)(ebh+1)) {}
+
+        // iterate backwards over the subframes
+        EvtBatcherSubFrameTail* next() {
+            EvtBatcherSubFrameTail* save = (EvtBatcherSubFrameTail*)_next;
+            if (!save) return save; // no more subframes
+            if (_next-save->size < _end) {
+                // we've jumped backwards too far
+                printf("*** corrupt EvtBatcherOutput: %p %p\n",_next,_end);
+                throw "*** corrupt EvtBatcherOutput";
+            }
+            // compute the next subframe ptr
+            if (_next-save->size == _end) {
+                // indicates this is the last one
+                _next = 0;
+            } else {
+                _next -= (save->size+sizeof(EvtBatcherSubFrameTail));
+            }
+            return save;
+        }
+    private:
+        char* _next;
+        char* _end;
     };
 #pragma pack(pop)
 };
