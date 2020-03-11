5ce6ff4181d88ddee316e8cbb7f35ed653feb1f1
msdubrovin <msdubrovin@gmail.com>
2020-03-11 15:03:32 -0700

    set experiment and run compalsory positional parameters

--- psana/psana/xtcav/app/xtcavDark.py
+++ psana/psana/xtcav/app/xtcavDark.py
@@ -2,13 +2,13 @@
 
 import sys
 
-usage = './xtcavDark.py amox23616 104'
+usage = 'E.g.: %s amox23616 104' % sys.argv[0]
 print(usage)
 
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--experiment', type=str, default='amox23616', help="psana experiment string (e.g. 'amox23616')")
-parser.add_argument('--run', type=int, default=104, help="run number") # 104
+parser.add_argument('experiment', help="psana experiment string (e.g. 'amox23616')")
+parser.add_argument('run', type=int, help="run number") # 104
 parser.add_argument('--max_shots', type=int, default=400, help='number of events')
 parser.add_argument('-f', '--fname', type=str, default='/reg/g/psdm/detector/data2_test/xtc/data-amox23616-r0104-e000400-xtcav.xtc2', help='xtc2 file')
 args = parser.parse_args()
@@ -17,8 +17,8 @@ print('parser.parse_args()', args)
 from psana.xtcav.DarkBackgroundReference import DarkBackgroundReference
 
 dark_background = DarkBackgroundReference(
-    experiment=args.experiment, 
-    run_number=args.run, 
+    experiment=args.experiment,
+    run_number=args.run,
     max_shots=args.max_shots,
     fname=args.fname)
 
