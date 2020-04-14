02aa3a6ced0fc3bfef3dc70ab6df9f02343a0e9b
Ric Claus <claus@slac.stanford.edu>
2020-04-14 16:55:32 -0700

    Fix SlowUpdate handling

--- psana/psana/dgrammanager.py
+++ psana/psana/dgrammanager.py
@@ -7,6 +7,7 @@ from shmem import PyShmemClient
 from psana import dgram
 from psana.event import Event
 from psana.detector import detectors
+from psana.psexp.event_manager import TransitionId
 import numpy as np
 
 def dumpDict(dict,indent):
@@ -23,8 +24,19 @@ def dumpDgram(d):
 
 FN_L = 200
 
+# Warning: If XtcData::Dgram ever changes, this function will likely need to change
+def _service(view):
+    iSvc = 2                    # Index of service field, in units of uint32_t
+    return (np.array(view, copy=False).view(dtype=np.uint32)[iSvc] >> 24) & 0x0f
+
+# Warning: If XtcData::Dgram ever changes, this function will likely need to change
+def _dgSize(view):
+    iExt = 5                    # Index of extent field, in units of uint32_t
+    txSize = 3 * 4              # sizeof(XtcData::TransitionBase)
+    return txSize + np.array(view, copy=False).view(dtype=np.uint32)[iExt]
+
 class DgramManager():
-    
+
     def __init__(self, xtc_files, configs=[], tag=None, run=None):
         """ Opens xtc_files and stores configs."""
         self.xtc_files = []
@@ -32,7 +44,7 @@ class DgramManager():
         self.shmem_kwargs = {'index':-1,'size':0,'cli_cptr':None}
         self.configs = []
         self.fds = np.ones(len(xtc_files), dtype=np.int32) * -1
-        self._timestamps = [] # built when iterating 
+        self._timestamps = [] # built when iterating
         self._run = run
 
         if isinstance(xtc_files, (str)):
@@ -47,23 +59,28 @@ class DgramManager():
                     #wait for first configure datagram - blocking
                     view = self.shmem_cli.get(self.shmem_kwargs)
                     assert view
+                    # Release shmem buffer after copying Transition data
+                    if _service(view) != TransitionId.L1Accept:
+                        barray = bytes(view[:_dgSize(view)])
+                        self.shmem_cli.freeByIndex(self.shmem_kwargs['index'], self.shmem_kwargs['size'])
+                        view = memoryview(barray)
                     d = dgram.Dgram(view=view, \
                                     shmem_index=self.shmem_kwargs['index'], \
                                     shmem_size=self.shmem_kwargs['size'], \
                                     shmem_cli_cptr=self.shmem_kwargs['cli_cptr'], \
                                     shmem_cli_pyobj=self.shmem_cli)
                     self.configs += [d]
-                else:    
+                else:
                     self.xtc_files = np.asarray(xtc_files, dtype='U%s'%FN_L)
-            
+
         given_configs = True if len(configs) > 0 else False
-        
-        if given_configs: 
+
+        if given_configs:
             self.configs = configs
-        
+
         for i, xtcdata_filename in enumerate(self.xtc_files):
             self.fds[i] = os.open(xtcdata_filename, os.O_RDONLY)
-            if not given_configs: 
+            if not given_configs:
                 d = dgram.Dgram(file_descriptor=self.fds[i])
                 self.configs += [d]
 
@@ -84,6 +101,11 @@ class DgramManager():
         if self.shmem_cli:
             view = self.shmem_cli.get(self.shmem_kwargs)
             if view:
+                # Release shmem buffer after copying Transition data
+                if _service(view) != TransitionId.L1Accept:
+                    barray = bytes(view[:_dgSize(view)])
+                    self.shmem_cli.freeByIndex(self.shmem_kwargs['index'], self.shmem_kwargs['size'])
+                    view = memoryview(barray)
                 # use the most recent configure datagram
                 config = self.configs[len(self.configs)-1]
                 d = dgram.Dgram(config=config,view=view, \
@@ -96,7 +118,7 @@ class DgramManager():
                 raise StopIteration
         else:
             dgrams = [dgram.Dgram(config=config) for config in self.configs]
- 
+
         evt = Event(dgrams, run=self.run())
         self._timestamps += [evt.timestamp]
         return evt
@@ -111,22 +133,22 @@ class DgramManager():
             if offset==0 and size==0:
                 d = None
             else:
-                d = dgram.Dgram(file_descriptor=fd, config=config, offset=offset, size=size)   
+                d = dgram.Dgram(file_descriptor=fd, config=config, offset=offset, size=size)
             dgrams += [d]
-        
+
         evt = Event(dgrams, run=self.run())
         return evt
 
     def get_det_class_table(self):
         """
         this function gets the version number for a (det, drp_class) combo
-        maps (dettype,software,version) to associated python class and 
+        maps (dettype,software,version) to associated python class and
         detector info for a det_name maps to dettype, detid tuple.
         """
         det_classes = {'epics': {}, 'scan': {}, 'normal': {}}
 
         xtc_info = []
-        det_info_table = {} 
+        det_info_table = {}
 
         # loop over the dgrams in the configuration
         # if a detector/drp_class combo exists in two cfg dgrams
@@ -138,25 +160,25 @@ class DgramManager():
                 # they should all be identical
                 first_key = next(iter(det_dict.keys()))
                 det = det_dict[first_key]
-                
+
                 if det_name not in det_classes:
                     det_class_table = det_classes['normal']
                 else:
                     det_class_table = det_classes[det_name]
 
-                
+
                 dettype, detid = (None, None)
                 for drp_class_name, drp_class in det.__dict__.items():
 
                     # collect detname maps to dettype and detid
-                    if drp_class_name == 'dettype': 
+                    if drp_class_name == 'dettype':
                         dettype = drp_class
                         continue
-                    
-                    if drp_class_name == 'detid': 
+
+                    if drp_class_name == 'detid':
                         detid = drp_class
                         continue
-                    
+
                     # FIXME: we want to skip '_'-prefixed drp_classes
                     #        but this needs to be fixed upstream
                     if drp_class_name.startswith('_'): continue
@@ -172,9 +194,9 @@ class DgramManager():
                         det_class_table[(det_name, drp_class_name)] = DetectorClass
                     else:
                         pass
-                
+
                 det_info_table[det_name] = (dettype, detid)
-        
+
         return det_classes, xtc_info, det_info_table
 
     def get_timestamps(self):
--- psana/psana/shmem/shmem.pyx
+++ psana/psana/shmem/shmem.pyx
@@ -6,11 +6,11 @@ cdef extern from "psalg/shmem/ShmemClient.hh" namespace "psalg::shmem":
         void free(int ev_index, int buf_size)
 
 cdef class PyShmemClient:
-    """ Python wrapper for C++ class. 
+    """ Python wrapper for C++ class.
     """
     cdef ShmemClient* client  # holds a C++ pointer to instance
     cdef char[:] pclient           # used to pass C++ pointer to python
-    
+
     def __cinit__(self):
         self.client = new ShmemClient()
         self.pclient = <char[:sizeof(ShmemClient)]><char*>self.client
@@ -22,13 +22,13 @@ cdef class PyShmemClient:
         cdef int status = -1
         status = self.client.connect(tag.encode(),tr_index)
         return status
-    
+
     def get(self,args):
         cdef char* buf
         cdef char[:] cview
         cdef int ev_index = -1
         cdef int buf_size = 0
-                
+
         buf = <char*>self.client.get(ev_index,buf_size)
         if buf == NULL:
           return
@@ -39,8 +39,11 @@ cdef class PyShmemClient:
         args['cli_cptr'] = self.pclient
 
         cview = <char[:buf_size]>buf
-        
+
         return cview
-                 
+
     def free(self,dgram):
         self.client.free(dgram._shmem_index,dgram._shmem_size)
+
+    def freeByIndex(self, index, size):
+        self.client.free(index, size)
