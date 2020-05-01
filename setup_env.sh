0bcbef12b535d64638b21c632882362b02154cf2
Valerio Mariani <valerio.mariani@desy.de>
2020-05-01 07:57:11 -0700

    Adapted setup file for my test environment

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,7 @@
 # user can select py2 environment with "-py2" option
 source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh $@
+conda deactivate
+conda activate /reg/neh/home/valmar/.conda/envs/test_condacompilers 
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
