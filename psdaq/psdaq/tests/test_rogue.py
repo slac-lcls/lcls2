6e7be9e4b04668c2baff5e9c84830e33040bc55f
Christopher O'Grady <cpo@slac.stanford.edu>
2020-05-12 18:00:00 -0700

    add simple rogue python test

---
+++ psdaq/psdaq/tests/test_rogue.py
@@ -0,0 +1,8 @@
+
+def test_rogue():
+    # do this simple test since rogue has a rather complex boost dependency
+    # which we have gotten wrong in the past - valmar and cpo
+    import rogue
+
+if __name__ == "__main__":
+    test_rogue()
