368ca90511eb73e18915124b981f1d0a57ed72ed
Christopher O'Grady <cpo@slac.stanford.edu>
2020-05-12 18:23:22 -0700

    reenable legion/rogue tests now that we've moved to ps-3.1.7

--- psdaq/psdaq/tests/test_rogue.py
+++ psdaq/psdaq/tests/test_rogue.py
@@ -1,6 +1,5 @@
 import pytest
 
-@pytest.mark.skip(reason="waiting for move to ps-3.1.7")
 def test_rogue():
     # do this simple test since rogue has a rather complex boost dependency
     # which we have gotten wrong in the past - valmar and cpo
