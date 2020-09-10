2674f87408507db1f420af7286e28c84587cc079
Christopher O'Grady <cpo@slac.stanford.edu>
2020-09-10 11:58:48 -0700

    add type annotations to step value/docstring

--- psana/psana/psexp/run.py
+++ psana/psana/psexp/run.py
@@ -43,10 +43,10 @@ class StepEvent(object):
         self.env_store = env_store
     def dgrams(self, evt):
         return self.env_store.get_step_dgrams_of_event(evt)
-    def docstring(self, evt):
+    def docstring(self, evt) -> str:
         env_values = self.env_store.values([evt], 'docstring')
         return env_values[0]
-    def value(self, evt):
+    def value(self, evt) -> float:
         env_values = self.env_store.values([evt], 'step_value')
         return env_values[0]
 
