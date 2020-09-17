fix unit conversion

psana/psana/momentum/EleMomentumRemi.py
@@ -15,19 +15,32 @@ class EleMomentumRemi():
         self.VPmm2mmPns = 1.75882014896e-1        
         self.amu2au = 1836.15
         self.au2tesla = 2.35e5
+        self.au2mm = 5.28e-8
         
     def CalcPtr(self,B_tesla,omega_mns,x_mm,y_mm,t_ns):
         R = np.sqrt((x_mm-self.x0)**2 + (y_mm-self.y0)**2)
         angle = omega_mns*(t_ns-self.t0)
         angle %= 2*np.pi
-        Ptr = (B_tesla/self.au2tesla)*R/(2*np.abs(np.sin(angle/2)))
+        Ptr = (B_tesla/self.au2tesla)*R/self.au2mm/(2*np.abs(np.sin(angle/2)))
         
         theta = (np.arctan2(y_mm-self.y0,x_mm-self.x0)+2*np.pi)%(2*np.pi)
         phi = theta - angle/2
         Px = Ptr*np.cos(phi) - self.mmPns2au*self.vjetx
         Py = Ptr*np.sin(phi) - self.mmPns2au*self.vjety
         
-        return Ptr, phi, Px, Py               
+        return Ptr, phi, Px, Py    
+    
+    def CalcR(self,B_tesla,omega_mns,Ptr,t_ns):
+        angle = omega_mns*(t_ns-self.t0)
+        angle %= 2*np.pi
+        R = self.au2mm*(2*np.abs(np.sin(angle/2)))*Ptr/(B_tesla/self.au2tesla)
+        return R      
+    
+    def CalcPx(self,omega_mns,x_mm,y_mm,t_ns):
+        return self.mmPns2au*omega_mns/2*((x_mm-self.x0)*np.cos(omega_mns*(t_ns-self.t0)/2) + (y_mm-self.y0))
+    
+    def CalcPy(self,omega_mns,x_mm,y_mm,t_ns):
+        return self.mmPns2au*omega_mns/2*((y_mm-self.y0)*np.cos(omega_mns*(t_ns-self.t0)/2) - (x_mm-self.x0))    
          
     def CalcPzOneAcc(self,m_amu,t_ns):
         return self.amu2au*m_amu*self.mmPns2au*self.l/(t_ns-self.t0) - 8.04e-2*self.U*(t_ns-self.t0)/(2*self.l)