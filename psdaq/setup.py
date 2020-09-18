a95e941d1a4c2418b6c4201ef695428a8ef6c49b
Chris Ford <caf@slac.stanford.edu>
2020-09-18 09:00:21 -0700

    Add ts_config_scan to build.

--- psdaq/setup.py
+++ psdaq/setup.py
@@ -47,6 +47,7 @@ setup(
                 'control_gui = psdaq.control_gui.app.control_gui:control_gui',
                 'bluesky_simple = psdaq.control.bluesky_simple:main',
                 'opal_config_scan = psdaq.control.opal_config_scan:main',
+                'ts_config_scan = psdaq.control.ts_config_scan:main',
                 'epics_exporter = psdaq.cas.epics_exporter:main',
                 'seqplot = psdaq.seq.seqplot:main',
                 'seqprogram = psdaq.seq.seqprogram:main',
