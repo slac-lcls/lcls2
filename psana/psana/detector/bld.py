92d7cb44787ac38a9e6cb65adb3f92115a2afbe7
Matt Weaver <weaver@slac.stanford.edu>
2020-09-18 11:38:19 -0700

    Add detector interface for ebeamh,pcav,pcavh,gmd,xgmd

--- psana/psana/detector/bld.py
+++ psana/psana/detector/bld.py
@@ -32,15 +32,32 @@ class hpscp_hpscp_0_0_1(DetectorImpl):
 class ebeam_ebeamAlg_0_7_1(DetectorImpl):
     def __init__(self, *args):
         super(ebeam_ebeamAlg_0_7_1, self).__init__(*args)
-
         self._add_fields()
-
-    def _info(self,evt):
-        # check for missing data
-        segments = self._segments(evt)
-        if segments is None: return None
-        return segments[0]
 
 class ebeam_raw_2_0_0(ebeam_ebeamAlg_0_7_1):
     def __init__(self, *args):
         super().__init__(*args)
+
+class ebeamh_raw_2_0_0(ebeam_ebeamAlg_0_7_1):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+class pcav_raw_2_0_0(DetectorImpl):
+    def __init__(self, *args):
+        super(pcav_raw_2_0_0, self).__init__(*args)
+        self._add_fields()
+
+class pcavh_raw_2_0_0(DetectorImpl):
+    def __init__(self, *args):
+        super(pcavh_raw_2_0_0, self).__init__(*args)
+        self._add_fields()
+
+class gmd_raw_2_0_0(DetectorImpl):
+    def __init__(self, *args):
+        super(gmd_raw_2_0_0, self).__init__(*args)
+        self._add_fields()
+
+class xgmd_raw_2_0_0(DetectorImpl):
+    def __init__(self, *args):
+        super(xgmd_raw_2_0_0, self).__init__(*args)
+        self._add_fields()
