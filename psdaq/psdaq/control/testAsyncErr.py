7292cee2d3b3babe47cbc2aee20b9eb06763612f
Chris Ford <caf@slac.stanford.edu>
2020-10-02 14:26:12 -0700

    Demonstrate warnings in addition to errors.

--- psdaq/psdaq/control/testAsyncErr.py
+++ psdaq/psdaq/control/testAsyncErr.py
@@ -7,19 +7,24 @@ Asynchronous Error Test
 import time
 import socket
 import zmq
-from psdaq.control.control import back_pull_port, error_msg
+from psdaq.control.control import back_pull_port, error_msg, warning_msg
 import argparse
 
 class Client:
-    def __init__(self, platform, collectHost, alias, errMsg):
+    def __init__(self, args):
 
         # configure zmq sockets
         self.context = zmq.Context(1)
         self.push = self.context.socket(zmq.PUSH)
-        self.push.connect('tcp://%s:%d' % (collectHost, back_pull_port(platform)))
+        self.push.connect('tcp://%s:%d' % (args.C, back_pull_port(args.p)))
 
-        # send error message
-        self.push.send_json(error_msg("%s: %s" % (alias, errMsg)))
+        if args.error is not None:
+            # send error message
+            self.push.send_json(error_msg("%s: %s" % (args.alias, args.error)))
+
+        if args.warning is not None:
+            # send warning message
+            self.push.send_json(warning_msg("%s: %s" % (args.alias, args.warning)))
 
 def main():
 
@@ -28,12 +33,16 @@ def main():
         parser = argparse.ArgumentParser()
         parser.add_argument('-p', type=int, choices=range(0, 8), default=0, help='platform (default 0)')
         parser.add_argument('-C', metavar='COLLECT_HOST', default='localhost', help='collection host (default localhost)')
-        parser.add_argument('-m', metavar='ERR_MSG', default='SHAZAM!', help='error message (default SHAZAM!)')
-        parser.add_argument('-u', metavar='ALIAS', required=True, help='unique ID')
+        parser.add_argument('--error', metavar='ERR_MSG')
+        parser.add_argument('--warning', metavar='WARN_MSG')
+        parser.add_argument('--alias', metavar='ALIAS', required=True)
         args = parser.parse_args()
 
+        if args.error is None and args.warning is None:
+            parser.error('Must specify --error or --warning or both')
+
         # start client
-        client = Client(args.p, args.C, args.u, args.m)
+        client = Client(args)
 
     except KeyboardInterrupt:
         print('KeyboardInterrupt')
