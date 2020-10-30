bba8383a83a417f4f49f8f1f2881ff1d0613f7b6
Christopher O'Grady <cpo@slac.stanford.edu>
2020-10-30 11:56:49 -0700

    move to /cds conda

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
-source /reg/g/psdm/sw/conda2/manage/bin/psconda.sh
-conda activate ps-3.1.16
+source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
+conda activate ps-4.1.0
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
