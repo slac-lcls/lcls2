d96be12a39a5ad2a543b3885efebec4c96675d81
msdubrovin <msdubrovin@gmail.com>
2021-01-13 19:15:53 -0800

    change default log_prefix, cosmetics

--- psana/psana/graphqt/CMConfigParameters.py
+++ psana/psana/graphqt/CMConfigParameters.py
@@ -21,21 +21,18 @@ See:
 Created on 2016-11-22 by Mikhail Dubrovin
 Adopted for LCLS2 on 2018-02-26 by Mikhail Dubrovin
 """
-#------------------------------
+#---
 
 import logging
 logger = logging.getLogger(__name__)
 
-#------------------------------
-
 from psana.pyalgos.generic.PSConfigParameters import PSConfigParameters
-#from expmon.PSNameManager import nm # It is here for initialization
 from psana.pyalgos.generic.Utils import list_of_hosts as lshosts
 import psana.pscalib.calib.CalibConstants as cc
 
-#------------------------------
+#---
 
-class CMConfigParameters(PSConfigParameters) :
+class CMConfigParameters(PSConfigParameters):
     """A storage of configuration parameters for LCLS-2 Calibration Manager (CM)
     """
     char_expand    = u' \u25BC' # down-head triangle
@@ -48,8 +45,8 @@ class CMConfigParameters(PSConfigParameters) :
     DBS     = 'DBs'
     cc      = cc
  
-    def __init__(self, fname=None) :
-        """fname : str - the file name with configuration parameters, if not specified then use default.
+    def __init__(self, fname=None):
+        """fname: str - the file name with configuration parameters, if not specified then use default.
         """
         PSConfigParameters.__init__(self)
 
@@ -86,18 +83,15 @@ class CMConfigParameters(PSConfigParameters) :
 
         self.last_selection = None # self.DB_COLS, DOCS
 
-#------------------------------
         
-    def declareParameters(self) :
-        # Possible typs for declaration : 'str', 'int', 'long', 'float', 'bool'
+    def declareParameters(self):
+        # Possible typs for declaration: 'str', 'int', 'long', 'float', 'bool'
         self.log_level = self.declareParameter(name='LOG_LEVEL', val_def='INFO', type='str') # val_def='NOTSET'
 
         #self.log_file - DEPRICATED
         self.log_file  = self.declareParameter(name='LOG_FILE_NAME', val_def='cm-log.txt', type='str')
-        self.log_prefix = self.declareParameter(name='LOG_FILE_PREFIX', val_def='/reg/g/psdm/logs/calibman/lcls2', type='str')
-        #self.log_prefix = self.declareParameter(name='LOG_FILE_PREFIX', val_def='./cm-log', type='str')
+        self.log_prefix = self.declareParameter(name='LOG_FILE_PREFIX', val_def='/cds/group/psdm/logs/calibman/lcls2', type='str')
         self.save_log_at_exit = self.declareParameter(name='SAVE_LOG_AT_EXIT', val_def=True,  type='bool')
-        #self.dir_log_global  = self.declareParameter(name='DIR_LOG_FILE_GLOBAL', val_def='/reg/g/psdm/logs/calibman', type='str')
 
         self.main_win_pos_x  = self.declareParameter(name='MAIN_WIN_POS_X',  val_def=5,    type='int')
         self.main_win_pos_y  = self.declareParameter(name='MAIN_WIN_POS_Y',  val_def=5,    type='int')
@@ -105,14 +99,11 @@ class CMConfigParameters(PSConfigParameters) :
         self.main_win_height = self.declareParameter(name='MAIN_WIN_HEIGHT', val_def=700,  type='int')
 
         self.main_vsplitter  = self.declareParameter(name='MAIN_VSPLITTER', val_def=600, type='int')
-        #self.main_tab_name   = self.declareParameter(name='MAIN_TAB_NAME', val_def='Configuration', type='str')
         self.main_tab_name   = self.declareParameter(name='MAIN_TAB_NAME', val_def='CDB', type='str')
 
         self.current_config_tab = self.declareParameter(name='CURRENT_CONFIG_TAB', val_def='Parameters', type='str')
-        #self.current_config_tab = self.declareParameter(name='CURRENT_CONFIG_TAB', val_def='Configuration File', type='str')
         self.cdb_host = self.declareParameter(name='CDB_HOST', val_def=cc.HOST, type='str')
         self.cdb_port = self.declareParameter(name='CDB_PORT', val_def=cc.PORT, type='int')
-        #self.cdb_user = self.declareParameter(name='CDB_USER', val_def='calibuser', type='str')
         self.cdb_hsplitter0 = self.declareParameter(name='CDB_HSPLITTER0', val_def=250, type='int')
         self.cdb_hsplitter1 = self.declareParameter(name='CDB_HSPLITTER1', val_def=1000, type='int')
         self.cdb_hsplitter2 = self.declareParameter(name='CDB_HSPLITTER2', val_def=0, type='int')
@@ -121,27 +112,20 @@ class CMConfigParameters(PSConfigParameters) :
         self.cdb_docw = self.declareParameter(name='CDB_DOC_WIDGET', val_def='List', type='str')
         self.cdb_selection_mode = self.declareParameter(name='CDB_SELECTION_MODE', val_def='extended', type='str')
 
-#------------------------------
         
-#    def __del__(self) :
+#    def __del__(self):
 #        logger.debug('In d-tor: %s')
 #        logger.debug('In CMConfigParameters d-tor')
-#        if self.save_cp_at_exit.value() :
+#        if self.save_cp_at_exit.value():
 #            self.saveParametersInFile()
 #        #ConfigParameters.__del__(self)
 
-#------------------------------
+#---
 
 cp = CMConfigParameters()
 
-#import psana.pscalib.calib.CalibConstants as cc
-#cp.user = None #cc.USERNAME
-#cp.upwd = ''   #cc.USERPW
-#print('XXXX CMConfigParameters set cp.user: %s p: %s' % (cp.user, cp.upwd))
-
-#------------------------------
-
-def test_CMConfigParameters() :
+if __name__ == "__main__":
+  def test_CMConfigParameters():
 
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
@@ -150,11 +134,10 @@ def test_CMConfigParameters() :
     cp.log_level.setValue('DEBUG')
     cp.saveParametersInFile()
 
-#------------------------------
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     import sys
     test_CMConfigParameters()
     sys.exit(0)
 
-#------------------------------
+# EOF
