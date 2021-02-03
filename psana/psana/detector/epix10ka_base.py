b5806fa993f0fba1070fd6831feac91c4d729055
msdubrovin <msdubrovin@gmail.com>
2021-02-02 19:16:42 -0800

    update for calib, _mask etc

--- psana/psana/detector/epix10ka_base.py
+++ psana/psana/detector/epix10ka_base.py
@@ -1,36 +1,46 @@
-"""Data access BASIC METHODS for
-   composite detectors made of epix10ka panels.
 """
+Data access BASIC METHODS for composite detectors made of epix10ka panels
+=========================================================================
 
-#from psana.pscalib.geometry.SegGeometry import *
-from psana.detector.areadetector import AreaDetector, np, DTYPE_MASK, DTYPE_STATUS
-from psana.detector.UtilsEpix10ka import calib_epix10ka_any
+Usage::
+
+  from psana.detector.epix10ka_base import epix10ka_base
+
+  o = epix10ka_base(*args, **kwargs) # enherits from AreaDetector
+  a = o.calib(evt)
+  m = o._mask_from_status(grinds=(0,1,2,3,4), **kwa)
+  m = o._mask_edges(self, edge_rows=1, edge_cols=1, center_rows=0, center_cols=0, dtype=DTYPE_MASK, **kwa)
+
+2020-11-06 created by Mikhail Dubrovin
+"""
 
 from amitypes import Array3d
 
 import logging
 logger = logging.getLogger(__name__)
 
+from psana.detector.areadetector import AreaDetector, np, DTYPE_MASK, DTYPE_STATUS
+from psana.detector.UtilsEpix10ka import calib_epix10ka_any
 from psana.detector.UtilsMask import merge_status
 from psana.pscalib.geometry.SegGeometryEpix10kaV1 import epix10ka_one as seg
-
 from psana.pyalgos.generic.NDArrUtils import info_ndarr
+
 #----
 
 class epix10ka_base(AreaDetector):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwa):
         logger.debug('epix10ka_base.__init__') # self.__class__.__name__
-        AreaDetector.__init__(self, *args, **kwargs)
+        AreaDetector.__init__(self, *args, **kwa)
 
 
-    def calib(self,evt) -> Array3d:
+    def calib(self, evt, **kwa) -> Array3d:
         """
         Create calibrated data array.
         """
-        logger.info('epix10ka_base.calib')
+        logger.debug('epix10ka_base.calib')
         #return self.raw(evt)
-        return calib_epix10ka_any(self, evt)
+        return calib_epix10ka_any(self, evt, **kwa)
 
 
     def _mask_from_status(self, **kwa):
@@ -38,10 +48,11 @@ class epix10ka_base(AreaDetector):
         Parameters **kwa
         ----------------
         'grinds', (0,1,2,3,4)) # gain range indexes for 'FH','FM','FL','AHL-H','AML-M'
-        Returns 
+        Returns
         -------
-        mask made of status: np.array, ndim=3, shape: as full detector data
+        mask made of status: np.array, ndim=3, shaped as full detector data
         """
+        logger.debug('epix10ka_base._mask_from_status')
         _grinds = kwa.get('grinds',(0,1,2,3,4))
         status = self._status() # pixel_status from calibration constants
         statmrg = merge_status(status, grinds=_grinds, dtype=DTYPE_STATUS) # dtype=np.uint64
@@ -51,6 +62,20 @@ class epix10ka_base(AreaDetector):
 
 
     def _mask_edges(self, edge_rows=1, edge_cols=1, center_rows=0, center_cols=0, dtype=DTYPE_MASK, **kwa):
+        """
+        Parameters
+        ----------
+        edge_rows: int number of edge rows to mask on both side of the panel
+        edge_cols: int number of edge columns to mask on both side of the panel
+        center_rows: int number of edge rows to mask for all ASICs
+        center_cols: int number of edge columns to mask for all ASICs
+        dtype: numpy dtype of the output array
+        **kwa: is not used
+        Returns
+        -------
+        mask: np.ndarray, ndim=3, shaped as full detector data, mask of the panel and asic edges
+        """
+        logger.debug('epix10ka_base._mask_edges')
         mask1 = seg.pixel_mask_array(edge_rows, edge_cols, center_rows, center_cols, dtype)
         nsegs = self._number_of_segments_total()
         if nsegs is None:
