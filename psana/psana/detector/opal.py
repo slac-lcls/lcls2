ba3d23d33d22593270b0669f9732d5f2618898bb
msdubrovin <msdubrovin@gmail.com>
2021-02-09 15:08:01 -0800

    for all opal_* classes change dependence from DetectorImpl to opal_base

--- psana/psana/detector/opal.py
+++ psana/psana/detector/opal.py
@@ -1,33 +1,39 @@
 import numpy as np
-from psana.detector.detector_impl import DetectorImpl
+#from psana.detector.detector_impl import DetectorImpl
+#from amitypes import Array2d
+from psana.detector.opal_base import opal_base, logging
+logger = logging.getLogger(__name__)
 
-class opal_raw_2_0_0(DetectorImpl):
-    def __init__(self, *args):
-        super(opal_raw_2_0_0, self).__init__(*args)
+#class opal_raw_2_0_0(DetectorImpl):
+#    def __init__(self, *args, **kwa_base):
+#        super(opal_raw_2_0_0, self).__init__(*args, **kwa)
 
+class opal_raw_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
-class opal_ttfex_2_0_0(DetectorImpl):
-    def __init__(self, *args):
-        super(opal_ttfex_2_0_0, self).__init__(*args)
-
+class opal_ttfex_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        #super(opal_ttfex_2_0_0, self).__init__(*args, **kwa)
+        opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
-class opal_simfex_2_0_0(DetectorImpl):
-    def __init__(self, *args):
-        super(opal_simfex_2_0_0, self).__init__(*args)
-
+class opal_simfex_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        #super(opal_simfex_2_0_0, self).__init__(*args, **kwa)
+        opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
-class opal_simfex_2_0_0(DetectorImpl):
-    def __init__(self, *args):
-        super(opal_simfex_2_0_0, self).__init__(*args)
-
+class opal_simfex_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        #super(opal_simfex_2_0_0, self).__init__(*args, **kwa)
+        opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
-class opal_ref_2_0_0(DetectorImpl):
-    def __init__(self, *args):
-        super(opal_ref_2_0_0, self).__init__(*args)
-
+class opal_ref_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        #super(opal_ref_2_0_0, self).__init__(*args, **kwa)
+        opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
