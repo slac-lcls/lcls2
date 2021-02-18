2a8e211184160b090ace6d7a2880ff5b17c6314e
msdubrovin <msdubrovin@gmail.com>
2021-02-17 16:11:00 -0800

    move in utils_psana.py psana relevant methods

---
+++ psana/psana/detector/utils_psana.py
@@ -0,0 +1,108 @@
+"""
+:py:class:`utils_psana` psana-specific utilities 
+================================================
+
+Usage::
+
+    from psana.detector.utils_psana import seconds, timestamp_run, datasource_kwargs, info_run
+
+    t_sec = seconds(ts, epoch_offset_sec=631152000) #Converts LCLS2 timestamp to unix epoch time
+    ts = timestamp_run(run, fmt='%Y-%m-%dT%H:%M:%S')
+    kwa = datasource_arguments(args) # re-define kwargs for DataSource,  args as a namespace
+    kwa = datasource_kwargs(**kwa) # re-define kwargs for DataSource
+
+    s = info_run_dsparms_det_classes(run, cmt='run.dsparms.det_classes:\n ', sep='\n ')
+    s = info_run(run, cmt='run info:', sep='\n    ', verb=0o377)
+ 
+This software was developed for the LCLS project.
+If you use all or part of it, please give an appropriate acknowledgment.
+
+Created on 2021-02-16 by Mikhail Dubrovin
+"""
+
+from psana.pyalgos.generic.Utils import str_tstamp
+from psana.detector.Utils import info_dict #, info_namespace, info_command_line
+
+
+def seconds(ts, epoch_offset_sec=631152000) -> float:
+    """
+    Converts LCLS2 timestamp to unix epoch time.
+    The epoch used is 1-Jan-1990 rather than 1970. -Matt
+
+    Receives  ts = orun.timestamp  # 4193682596073796843 relative to 1990-01-01
+    Returns unix epoch time in sec # 1607569818.532117 sec
+
+    import datetime
+    epoch_offset_sec=(datetime.datetime(1990, 1, 1)-datetime.datetime(1970, 1, 1)) / datetime.timedelta(seconds=1)
+    """
+    return float(ts>>32) + float(ts&0xffffffff)*1.e-9 + epoch_offset_sec
+
+
+def timestamp_run(run, fmt='%Y-%m-%dT%H:%M:%S'):
+    """converts LCLS2 run.timestamp to human readable timestamp, e.g. 2020-10-27T12:54:47
+    """
+    return str_tstamp(fmt=fmt, time_sec=seconds(run.timestamp))
+
+
+def datasource_arguments(args):
+    """
+    Parameters
+    ----------
+    args.fname: str - xtc2 file name
+    args.expname: str - experiment name
+    args.runs: int run number or str with comma-separated run numbers
+    args.detname: str - detector name
+    args.evtmax: int - maximal number of events to process
+
+    Returns
+    -------
+    dict of DataSource keyword arguments
+    """
+    assert args.fname is not None\
+           or None not in (args.expname, args.runs), 'experiment name and run or xtc2 file name need to be specified for DataSource'
+    kwa = {'files':args.fname,} if args.fname is not None else\
+          {'exp':args.expname,'run':[int(v) for v in args.runs.split(',')]}
+    if args.evtmax: kwa['max_events'] = args.evtmax
+    return kwa
+
+
+def datasource_kwargs(**kwargs):
+    """ The same as datasource_arguments, but use python standard kwargs as input parameters
+    Returns
+    -------
+    - kwa: dict of DataSource keyword arguments
+    """
+    fname   = kwargs.get('fname', None)
+    expname = kwargs.get('expname', )
+    runs    = kwargs.get('runs', None)
+    evtmax  = kwargs.get('evtmax', 100)
+
+    assert fname is not None\
+           or None not in (expname, runs), 'experiment name and run or xtc2 file name need to be specified for DataSource'
+    kwa = {'files':fname,} if fname is not None else\
+          {'exp':expname,'run':[int(v) for v in runs.split(',')]}
+    if evtmax: kwa['max_events'] = evtmax
+    return kwa
+
+def info_run_dsparms_det_classes_v1(run, cmt='run.dsparms.det_classes:', sep='\n '):
+    return cmt + sep.join(['%8s : %s' % (str(k),str(v)) for k,v in run.dsparms.det_classes.items()])
+
+
+def info_run_dsparms_det_classes(run, cmt='run.dsparms.det_classes:\n ', sep='\n '):
+    return cmt + info_dict(run.dsparms.det_classes, fmt='%10s : %s', sep=sep)
+
+
+def info_run(run, cmt='run info:', sep='\n    ', verb=0o377):
+    t_sec = seconds(run.timestamp)
+    ts_run = timestamp_run(run, fmt='%Y-%m-%dT%H:%M:%S')
+    #ts_run = str_tstamp(fmt='%Y-%m-%dT%H:%M:%S', time_sec=t_sec)
+    return cmt\
+      +   'run.runnum    : %d' % (run.runnum)\
+      + '%srun.expt      : %s' % (sep, run.expt)\
+      + '%srun.detnames  : %s' % (sep, ' '.join(run.detnames))\
+      + '%srun.timestamp : %s -> %s' % (sep, run.timestamp, ts_run)\
+      +('%srun.id        : %s' % (sep, run.id) if verb & 1 else '')\
+      +('%s%s' % (sep, info_run_dsparms_det_classes(run, cmt='run.dsparms.det_classes:', sep=sep+'   ')) if verb & 2 else '')
+
+
+#EOF
