db717224c1e8ee0ede15afa5530bb2e68eb18571
Christopher O'Grady <cpo@slac.stanford.edu>
2021-03-08 12:02:47 -0800

    clean up epix test

--- psana/psana/tests/test_epix_calib.py
+++ psana/psana/tests/test_epix_calib.py
@@ -1,7 +1,7 @@
 from psana import DataSource
 import os
 import numpy as np
- #Same as calibsample when first tested.
+
 def test_epix_calib():
     correctanswer=np.array(([-177.67036, -187.09805, -171.72614, -171.77377, -142.82375], [-67.91426, -40.756596, -55.872475, -62.017673, -63.55546]))
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -12,3 +12,6 @@ def test_epix_calib():
         image=epix.raw.image(evt)
         calibsample=image[10][0:5]
         assert np.allclose(correctanswer[nevt][0:5], calibsample, rtol=.001)
+
+if __name__ == "__main__":
+    test_epix_calib()
