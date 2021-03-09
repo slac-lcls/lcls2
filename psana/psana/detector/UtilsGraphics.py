c148f2e7fc8c471bb4fd0a6b027fd2d7aa7c729e
msdubrovin <msdubrovin@gmail.com>
2021-03-08 20:34:44 -0800

    add parameters fraclo=0.05, frachi=0.95

--- psana/psana/detector/UtilsGraphics.py
+++ psana/psana/detector/UtilsGraphics.py
@@ -23,10 +23,15 @@ import psana.pyalgos.generic.Graphics as gr
 
 #----
 
-def arr_median_limits(arr, nneg=1, npos=3):
+def arr_median_limits(arr, nneg=None, npos=None, fraclo=0.05, frachi=0.95):
     """ returns tuple of intensity limits (amin, amax) evaluated from arr.
     """
     if arr is None: return nneg, npos
+    if None in (nneg, npos):
+        qlo = np.quantile(arr, fraclo, interpolation='linear')
+        qhi = np.quantile(arr, frachi, interpolation='linear')
+        logger.debug('quantile(%.3f):%.1f quantile(%.3f):%.1f' % (fraclo, qlo, frachi, qhi))
+        return qlo, qhi
     med = np.median(arr)
     spr = np.median(np.abs(arr-med))
     amin, amax = med-nneg*spr, med+npos*spr
@@ -40,15 +45,17 @@ class fleximage:
         """
         # set local parameters:
         arr          = kwa.setdefault('arr', img)
-        self.nneg    = kwa.setdefault('nneg', 1)
-        self.npos    = kwa.setdefault('npos', 3)
+        self.nneg    = kwa.setdefault('nneg', None) #1
+        self.npos    = kwa.setdefault('npos', None) #3
+        self.fraclo  = kwa.setdefault('fraclo', 0.05)
+        self.frachi  = kwa.setdefault('frachi', 0.95)
         self.alimits = kwa.setdefault('alimits', None)
         amin, amax = self._intensity_limits(img, kwa)
         h_in = kwa.pop('h_in', 8)
 
         kwfig = {}
         _fig=gr.plt.figure(num   = kwa.get('num',None),\
-                       figsize   = kwa.get('figsize',(h_in*float(img.shape[1])/img.shape[0], h_in)),\
+                       figsize   = kwa.get('figsize',(1.+h_in*float(img.shape[1])/img.shape[0], h_in)),\
                        dpi       = kwa.get('dpi',80),\
                        facecolor = kwa.get('facecolor','w'),\
                        edgecolor = kwa.get('edgecolor','w'),\
@@ -60,8 +67,8 @@ class fleximage:
         kwfica={}
         self.fig, self.axim, self.axcb = gr.fig_img_cbar_axes(\
             fig=_fig,\
-            win_axim = kwa.get('win_axim', (0.05,0.03,0.87,0.93)),\
-            win_axcb = kwa.get('win_axcb', (0.923,0.03,0.02,0.93)), **kwfica)
+            win_axim = kwa.get('win_axim', (0.05,0.03,0.86,0.93)),\
+            win_axcb = kwa.get('win_axcb', (0.914,0.03,0.015,0.93)), **kwfica)
 
         # set default pars for gr.imshow_cbar
         kwic={'amin':amin,
@@ -86,14 +93,16 @@ class fleximage:
         """
         alimits = kwa.pop('alimits', self.alimits)
         return arr_median_limits(
-                   arr  = kwa.pop('arr', img),\
-                   nneg = kwa.pop('nneg', self.nneg), 
-                   npos = kwa.pop('npos', self.npos))\
+                   arr    = kwa.pop('arr', img),\
+                   nneg   = kwa.pop('nneg', self.nneg),
+                   npos   = kwa.pop('npos', self.npos),
+                   fraclo = kwa.pop('fraclo', self.fraclo),
+                   frachi = kwa.pop('frachi', self.frachi))\
                if alimits is None else alimits
 
 
     def update(self, img, **kwa):
-        """use kwa: arr=arr, nneg=1, npos=3 OR alimits=(amin,amax)
+        """use kwa: arr=arr, nneg=1, npos=3 OR arr, fraclo=0.05, frachi=0.95 OR alimits=(amin,amax)
         """
         amin, amax = self._intensity_limits(img, kwa)
         self.imsh.set_data(img)
