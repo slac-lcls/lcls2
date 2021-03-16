ea72a1c01f7ce68b5ee9d7389c087ae3204e5f54
msdubrovin <msdubrovin@gmail.com>
2021-03-15 19:30:36 -0700

    remove elf._add_fields() from opal_raw_2_0_0 because it overrides det.raw.image from opal_base

--- psana/psana/detector/opal.py
+++ psana/psana/detector/opal.py
@@ -11,29 +11,30 @@ logger = logging.getLogger(__name__)
 class opal_raw_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
         opal_base.__init__(self, *args, **kwa)
-        self._add_fields()
+        #self._add_fields() < overrides det.raw.image(...)
+
 
 class opal_ttfex_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
-        #super(opal_ttfex_2_0_0, self).__init__(*args, **kwa)
         opal_base.__init__(self, *args, **kwa)
         self._add_fields()
 
-class opal_simfex_2_0_0(opal_base):
-    def __init__(self, *args, **kwa):
-        #super(opal_simfex_2_0_0, self).__init__(*args, **kwa)
-        opal_base.__init__(self, *args, **kwa)
-        self._add_fields()
 
 class opal_simfex_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
-        #super(opal_simfex_2_0_0, self).__init__(*args, **kwa)
         opal_base.__init__(self, *args, **kwa)
         self._add_fields()
+
+
+class opal_simfex_2_0_0(opal_base):
+    def __init__(self, *args, **kwa):
+        opal_base.__init__(self, *args, **kwa)
+        self._add_fields()
+
 
 class opal_ref_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
-        #super(opal_ref_2_0_0, self).__init__(*args, **kwa)
         opal_base.__init__(self, *args, **kwa)
         self._add_fields()
+
 
