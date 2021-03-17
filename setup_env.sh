0641d8ee412f897edc0f13d2da1785d8b9852074
Valerio Mariani <valmar@slac.stanford.edu>
2021-03-17 11:48:10 -0700

    set environment to ps-4.3.0

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,5 @@
 source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
-conda activate  /cds/home/v/valmar/.conda/envs/psana2-feedstock-dev 
+conda activate  ps-4.3.0
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
