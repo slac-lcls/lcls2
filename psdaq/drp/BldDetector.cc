17150672f6d0024bfd3ab0ee9ecd617b1e106cec
Ric Claus <claus@slac.stanford.edu>
2021-03-24 19:36:00 -0700

    BldDetector*: Fix -k handling

--- psdaq/drp/BldDetector.cc
+++ psdaq/drp/BldDetector.cc
@@ -1016,7 +1016,9 @@ int main(int argc, char* argv[])
                 para.instrument = optarg;
                 break;
             case 'k':
-                kwargs_str = std::string(optarg);
+                kwargs_str = kwargs_str.empty()
+                           ? optarg
+                           : kwargs_str + ", " + optarg;
                 break;
             case 'M':
                 para.prometheusDir = optarg;
