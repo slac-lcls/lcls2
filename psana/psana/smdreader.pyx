aef3864943ee3d5a6d99ecdbf72d32369e590011
monarin <monarin@gmail.com>
2021-05-13 15:57:25 -0700

    update print out message

--- psana/psana/smdreader.pyx
+++ psana/psana/smdreader.pyx
@@ -62,7 +62,7 @@ cdef class SmdReader:
                 if all(flag_founds): break
 
                 time.sleep(self.sleep_secs)
-                print(f'smdreader waiting for an event...retry#{cn_retries+1} (max_retries={self.max_retries})')
+                print(f'smdreader waiting for an event...retry#{cn_retries+1} (max_retries={self.max_retries}, use PS_R_MAX_RETRIES for different value)')
                 self.prl_reader.just_read()
                 cn_retries += 1
                 if cn_retries >= self.max_retries:
