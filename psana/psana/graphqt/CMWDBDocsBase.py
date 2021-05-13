9e10f46cc219ad83b17c5631a6dba86fe1d3d277
msdubrovin <msdubrovin@gmail.com>
2021-05-13 16:30:24 -0700

    add switch between pymongo and web interface

--- psana/psana/graphqt/CMWDBDocsBase.py
+++ psana/psana/graphqt/CMWDBDocsBase.py
@@ -1,4 +1,4 @@
-#------------------------------
+
 """Class :py:class:`CMWDBDocsBase` abstract interface
 =====================================================
 
@@ -14,7 +14,6 @@ See:
 
 Created on 2017-04-20 by Mikhail Dubrovin
 """
-#------------------------------
 
 import logging
 logger = logging.getLogger(__name__)
@@ -22,28 +21,27 @@ logger = logging.getLogger(__name__)
 from PyQt5.QtWidgets import QTextEdit
 from PyQt5.QtCore import Qt
 
-from psana.graphqt.CMConfigParameters import cp # used in CMWDBDocsList
-import psana.graphqt.CMDBUtils as dbu
+from psana.graphqt.CMConfigParameters import cp
 
-#------------------------------
+if cp.kwargs.get('webint', True):
+    import psana.graphqt.CMDBUtilsWeb as dbu
+else:
+    import psana.graphqt.CMDBUtils as dbu
+    
 
 class CMWDBDocsBase() :
     def __init__(self) :
-        logger.debug('In c-tor abstract interface')
-
-        self.current_docs = []
+        logger.debug('in c-tor for webint %s' % cp.kwargs.get('webint', True))
         self.dbname  = None
         self.colname = None
-
+        self.current_docs = []
         cp.cmwdbdocswidg = self
 
-#------------------------------
 
     def __del__(self) :
-        #logger.debug('In d-tor')
+        logger.debug('in d-tor')
         cp.cmwdbdocswidg = None
 
-#------------------------------
 
     def show_documents(self, dbname, colname, docs) :
         #msg = 'THIS METHOD NEEDS TO BE IMPLEMENTED IN THE DERIVED CLASS for db: %s col: %s'%\
@@ -51,7 +49,5 @@ class CMWDBDocsBase() :
         #logger.warning(msg)
         self.dbname, self.colname, self.current_docs = dbname, colname, docs
 
-        #cp.cmwdbmain.set_hsplitter_size2(0)
-
-#------------------------------
+# EOF
 
