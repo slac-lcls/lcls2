996414cced20ebd6f324e73c1e673c438dff5e93
Chris Ford <caf@slac.stanford.edu>
2021-05-12 20:00:39 -0700

    New TimedRun class runs the daq for a specified duration.

---
+++ psdaq/psdaq/control/TimedRun.py
@@ -0,0 +1,114 @@
+import logging
+import zmq
+from threading import Thread, Event, Condition
+from psdaq.control.ControlDef import ControlDef, front_pub_port, front_rep_port, create_msg
+from psdaq.control.ControlDef import scan_pull_port
+import time
+
+class TimedRun:
+    def __init__(self, control, *, daqState, args):
+        self.zmq_port = scan_pull_port(args.p)
+        self.control = control
+        self.name = 'mydaq'
+        self.context = zmq.Context()
+        self.push_socket = self.context.socket(zmq.PUSH)
+        self.push_socket.bind('tcp://*:%d' % self.zmq_port)
+        self.pull_socket = self.context.socket(zmq.PULL)
+        self.pull_socket.connect('tcp://localhost:%d' % self.zmq_port)
+        self.comm_thread = Thread(target=self.daq_communicator_thread, args=())
+        self.mon_thread = Thread(target=self.daq_monitor_thread, args=(), daemon=True)
+        self.ready = Event()
+        self.daqState = daqState
+        self.args = args
+        self.daqState_cv = Condition()
+        self.comm_thread.start()
+        self.mon_thread.start()
+        self.verbose = args.v
+
+    # this thread tells the daq to go to a state and waits for the completion
+    def daq_communicator_thread(self):
+        logging.debug('*** daq_communicator_thread')
+        while True:
+            sss = self.pull_socket.recv().decode("utf-8")
+            if ',' in sss:
+                state, phase1 = sss.split(',', maxsplit=1)
+            else:
+                state, phase1 = sss, None
+
+            logging.debug('*** received %s' % state)
+            if state in ControlDef.states:
+                # send 'daqstate(state)' and wait for complete
+                errMsg = self.control.setState(state)
+                if errMsg is not None:
+                    logging.error('%s' % errMsg)
+                    continue
+
+                with self.daqState_cv:
+                    while self.daqState != state:
+                        logging.debug('daqState \'%s\', waiting for \'%s\'...' % (self.daqState, state))
+                        self.daqState_cv.wait(1.0)
+                        # check for shutdown with nonblocking read
+                        try:
+                            ttt = self.pull_socket.recv(flags=zmq.NOBLOCK).decode("utf-8")
+                        except Exception as ex:
+                            pass
+                        else:
+                            if ttt=='shutdown':
+                                return
+
+                    logging.debug('daqState \'%s\'' % self.daqState)
+
+                if self.daqState == state:
+                    self.ready.set()
+
+            elif state=='shutdown':
+                break
+
+            else:
+                logging.error(f'daq_communicator_thread unrecognized input: \'{state}\'')
+
+    def daq_monitor_thread(self):
+        logging.debug('*** daq_monitor_thread')
+        while True:
+            part1, part2, part3, part4, part5, part6, part7, part8 = self.control.monitorStatus()
+            if part1 is None:
+                break
+            elif part1 == 'error':
+                logging.error(f"{part2}")
+            elif part1 == 'warning':
+                logging.warning(f"{part2}")
+            elif part1 not in ControlDef.transitions:
+                continue
+
+            # part1=transition, part2=state, part3=config
+            with self.daqState_cv:
+                self.daqState = part2
+                self.daqState_cv.notify()
+
+    def sleep(self, secs):
+        logging.debug(f'begin {secs} second wait')
+        time.sleep(secs)
+        logging.debug(f'end {secs} second wait')
+
+    def set_connected_state(self):
+        self.push_socket.send_string('connected')
+        # wait for complete
+        self.ready.wait()
+        self.ready.clear()
+
+    def set_running_state(self):
+        self.push_socket.send_string('running')
+        # wait for complete
+        self.ready.wait()
+        self.ready.clear()
+
+    def stage(self):
+        # done once at start of scan
+        # put the daq into the right state ('connected')
+        self.set_connected_state()
+
+    def unstage(self):
+        # done once at end of scan
+        # put the daq into the right state ('connected')
+        self.set_connected_state()
+
---
+++ psdaq/psdaq/control/lab3_timed_run.py
@@ -0,0 +1,73 @@
+import sys
+import logging
+import threading
+from psdaq.control.ControlDef import ControlDef
+from psdaq.control.DaqControl import DaqControl
+from psdaq.control.TimedRun import TimedRun
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', type=int, choices=range(0, 8), default=1,
+                        help='platform (default 1)')
+    parser.add_argument('-C', metavar='COLLECT_HOST', default='drp-tst-dev009',
+                        help='collection host (default drp-tst-dev009)')
+    parser.add_argument('-t', type=int, metavar='TIMEOUT', default=10000,
+                        help='timeout msec (default 10000)')
+    parser.add_argument('-v', action='store_true', help='be verbose')
+    parser.add_argument('--duration', type=int, default=10,
+                        help='run duration seconds (default 10)')
+    args = parser.parse_args()
+
+    # instantiate DaqControl object
+    control = DaqControl(host=args.C, platform=args.p, timeout=args.t)
+
+    try:
+        instrument = control.getInstrument()
+    except KeyboardInterrupt:
+        instrument = None
+
+    if instrument is None:
+        sys.exit('Error: failed to read instrument name (check -C <COLLECT_HOST>)')
+
+    # configure logging handlers
+    if args.v:
+        level=logging.DEBUG
+    else:
+        level=logging.WARNING
+    logging.basicConfig(level=level)
+    logging.info('logging initialized')
+
+    # get initial DAQ state
+    daqState = control.getState()
+    logging.info('initial state: %s' % daqState)
+    if daqState == 'error':
+        sys.exit(1)
+
+    # instantiate TimedRun
+    run = TimedRun(control, daqState=daqState, args=args)
+
+    run.stage()
+
+    try:
+
+        # -- begin script --------------------------------------------------------
+
+        # run daq for the specified time duration
+        run.set_running_state()
+        run.sleep(args.duration)
+
+        # -- end script ----------------------------------------------------------
+
+    except KeyboardInterrupt:
+        run.push_socket.send_string('shutdown') #shutdown the daq communicator thread
+        run.comm_thread.join()
+        sys.exit('interrupted')
+
+    run.unstage()
+
+    run.push_socket.send_string('shutdown') #shutdown the daq communicator thread
+    run.comm_thread.join()
+
+if __name__ == '__main__':
+    main()
