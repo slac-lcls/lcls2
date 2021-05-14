3d5358ecd1a6630e70aa0abd39a14e5e6ef2d7cc
msdubrovin <msdubrovin@gmail.com>
2021-05-14 10:36:33 -0700

    add more references to methods

--- psana/psana/graphqt/CMDBUtilsWeb.py
+++ psana/psana/graphqt/CMDBUtilsWeb.py
@@ -10,3 +10,8 @@ collection_names = wu.collection_names # (dbname) -> list of str
 collection_info  = wu.collection_info  # (dbname, colname) -> str
 document_info    = wu.mu.document_info # (doc) -> str
 document_keys    = wu.mu.document_keys # (doc) -> str
+list_of_documents= wu.list_of_documents
+doc_add_id_ts    = wu.mu.doc_add_id_ts
+ObjectId         = wu.mu.ObjectId
+get_data_for_doc = wu.get_data_for_doc
+
