95856eb39f3f70da19912b771ebd056c6ed93e50
msdubrovin <msdubrovin@gmail.com>
2021-06-17 09:39:49 -0700

    cleanup and update

--- psana/psana/graphqt/IVControl.py
+++ psana/psana/graphqt/IVControl.py
@@ -15,15 +15,14 @@ Created on 2021-06-14 by Mikhail Dubrovin
 import logging
 #logger = logging.getLogger(__name__)
 
+import os
 import sys
-from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout#, QComboBox, QPushButton, QLineEdit
-#from PyQt5.QtCore import QSize # Qt, QEvent, QPoint, 
-#from psana.graphqt.H5VConfigParameters import cp
-#from psana.graphqt.QWIcons import icon
+from psana.graphqt.CMWControlBase import cp, CMWControlBase
+from PyQt5.QtWidgets import QVBoxLayout #QWidget, QLabel, QComboBox, QPushButton, QLineEdit
 from psana.graphqt.QWFileName import QWFileName
 
 
-class IVControl(QWidget):
+class IVControl(CMWControlBase):
     """QWidget for Image Viewer control fields"""
 
     def __init__(self, **kwargs):
@@ -33,13 +32,8 @@ class IVControl(QWidget):
         fname_nda = d + 'det-calib-mfxc00118-r242-e5000-max.txt'
         fname_geo = d + '2021-02-02-epix10ks2m.1-geometry-recentred-for-psana.txt'
 
-        QWidget.__init__(self, parent)
-        #self._name = 'IVControl'
-        #self.lab_ctrl = QLabel('Control:')
+        CMWControlBase.__init__(self, **kwargs)
 
-        #self.but_exp_col  = QPushButton('Collapse')
-
-        #fname = cp.h5vmain.wtree.fname if cp.h5vmain is not None else './test.h5'
         self.w_fname_nda = QWFileName(None, butname='Select', label='N-d array:',\
            path=fname_nda, fltr='*.txt *.npy\n*', show_frame=True)
 
@@ -50,9 +44,11 @@ class IVControl(QWidget):
         #self.box1.addWidget(self.lab_ctrl)
         #self.box1.addWidget(self.but_exp_col)
         self.box1.addWidget(self.w_fname_geo)
-        self.box1.addStretch(1) 
+        self.box1.addStretch(1)
         self.box1.addWidget(self.w_fname_nda)
         #self.box1.addSpacing(20)
+        self.box1.addStretch(1)
+        self.box1.addWidget(self.but_tabs)
 
         #self.box1.addLayout(self.grid)
         self.setLayout(self.box1)
@@ -67,57 +63,29 @@ class IVControl(QWidget):
         #self.set_style()
         #self.set_buttons_visiable()
 
+        #from psana.graphqt.UtilsFS import list_of_instruments
+        #print('lst:', list_of_instruments(cp.instr_dir.value()))
+
 
     def set_tool_tips(self):
         self.setToolTip('Control fields/buttons')
 
 
     def set_style(self):
-        from psana.graphqt.Styles import style
+        pass
+        #from psana.graphqt.Styles import style
         #self.         setStyleSheet(style.styleBkgd)
         #self.lab_db_filter.setStyleSheet(style.styleLabel)
         #self.lab_ctrl.setStyleSheet(style.styleLabel)
 
-        icon.set_icons()
+        #icon.set_icons()
         #self.but_exp_col.setIcon(icon.icon_folder_open)
 
 
-    def on_but_exp_col(self):
-        if cp.h5vmain is None: return
-
-        wtree = cp.h5vmain.wtree
-        but = self.but_exp_col
-        if but.text() == 'Expand':
-            wtree.process_expand()
-            self.but_exp_col.setIcon(icon.icon_folder_closed)
-            but.setText('Collapse')
-        else:
-            wtree.process_collapse()
-            self.but_exp_col.setIcon(icon.icon_folder_open)
-            but.setText('Expand')
-
-
-#    def on_but_clicked(self):
-#        for but in self.list_of_buts:
-#            if but.hasFocus(): break
-#        logger.info('Click on "%s"' % but.text())
-#        if   but == self.but_exp_col : self.expand_collapse_dbtree()
-#        elif but == self.but_tabs    : self.view_hide_tabs()
-#        elif but == self.but_buts    : self.select_visible_buttons()
-#        elif but == self.but_del     : self.delete_selected_items()
-#        elif but == self.but_docs    : self.select_doc_widget()
-#        elif but == self.but_selm    : self.set_selection_mode()
-#        elif but == self.but_add     : self.add_selected_item()
-#        elif but == self.but_save    : self.save_selected_item()
-#        #elif but == self.but_level   : self.set_logger_level()
-
-
 if __name__ == "__main__":
-    import os
     os.environ['LIBGL_ALWAYS_INDIRECT'] = '1' #export LIBGL_ALWAYS_INDIRECT=1
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
-    #logger.setPrintBits(0o177777)
     w = IVControl()
     #w.setGeometry(200, 400, 500, 200)
     w.setWindowTitle('IV Control Panel')
