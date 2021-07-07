142f8071f9d904dfe9b119310bc272e81421cf98
Christopher O'Grady <cpo@slac.stanford.edu>
2021-07-07 13:09:00 -0700

    update cnf files

---
+++ psdaq/psdaq/cnf/copy_hutch_cnf.sh
@@ -0,0 +1,6 @@
+cp /cds/group/pcds/dist/pds/tmo/scripts/tmo.cnf .
+cp /cds/group/pcds/dist/pds/rix/scripts/rix.cnf .
+cp /cds/group/pcds/dist/pds/ued/scripts/ued.cnf .
+cp /cds/group/pcds/dist/pds/tmo/scripts/setup_env.sh setup_env_tmo.sh
+cp /cds/group/pcds/dist/pds/rix/scripts/setup_env.sh setup_env_rix.sh
+cp /cds/group/pcds/dist/pds/ued/scripts/setup_env.sh setup_env_ued.sh
---
+++ psdaq/psdaq/cnf/setup_env_ued.sh
@@ -0,0 +1,19 @@
+source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
+
+conda activate ps-4.3.2
+RELDIR="/cds/home/opr/uedopr/git/lcls2_210605"
+
+# in production until June 5, 2021
+#RELDIR="/cds/home/opr/uedopr/git/lcls2_210514"
+
+#RELDIR="/cds/home/opr/uedopr/git/lcls2_210323"
+export PATH=$RELDIR/install/bin:${PATH}
+pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
+export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages
+# for procmgr
+export TESTRELDIR=$RELDIR/install
+
+# Add some UED specific EPICS environment.
+# This is needed for running control scanning scripts from drp-ued-cmp002.
+export EPICS_CA_SERVER_PORT=5058
+export EPICS_CA_ADDR_LIST="172.27.99.255 172.21.36.255:5064"
