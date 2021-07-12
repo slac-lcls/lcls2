b928e505afeabd7bc742f8545334af86ce64a832
msdubrovin <msdubrovin@gmail.com>
2021-07-12 10:58:49 -0700

    update in development

--- psana/psana/graphqt/IVControl.py
+++ psana/psana/graphqt/IVControl.py
@@ -23,7 +23,8 @@ from psana.graphqt.QWFileNameV2 import QWFileNameV2
 
 import psana.pyalgos.generic.PSUtils as psu
 from psana.pyalgos.generic.NDArrUtils import reshape_to_2d, info_ndarr
-
+from psana.graphqt.CMConfigParameters import cp
+from psana.graphqt.QWUtils import change_check_box_dict_in_popup_menu
 
 def image_from_ndarray(nda):
     if nda is None:
@@ -48,64 +49,209 @@ class IVControl(CMWControlBase):
         fname_geo = d + 'Select'
 
         CMWControlBase.__init__(self, **kwargs)
+        cp.ivcontrol = self
 
-        self.w_fname_nda = QWFileNameV2(None, label='Array:',\
+        self.wfnm_nda = QWFileNameV2(None, label='Array:',\
            path=fname_nda, fltr='*.txt *.npy *.data *.dat\n*', show_frame=True)
 
-        self.w_fname_geo = QWFileNameV2(None, label='Geometry:',\
+        self.wfnm_geo = QWFileNameV2(None, label='Geometry:',\
            path=fname_geo, fltr='*.txt *.data\n*', show_frame=True)
 
         self.but_reset = QPushButton('Reset')
-
+        self.but_buts  = QPushButton('Buts %s' % cp.char_expand)
 
         self.box = QGridLayout()
-        self.box.addWidget(self.w_fname_nda, 0, 0, 1, 9)
-        self.box.addWidget(self.w_fname_geo, 1, 0, 1, 9)
-        self.box.addWidget(self.but_tabs,    0, 10)
-        self.box.addWidget(self.but_reset,   1, 10)
-
-        #self.box1.addLayout(self.grid)
+        self.box.addWidget(self.wfnm_nda, 0, 0, 1, 7)
+        self.box.addWidget(self.but_reset,   0, 7)
+        self.box.addWidget(self.but_buts,    0, 8)
+        self.box.addWidget(self.but_tabs,    0, 9)
+        self.box.addWidget(self.wfnm_geo, 1, 0, 1, 7)
         self.setLayout(self.box)
  
         self.but_reset.clicked.connect(self.on_but_reset)
-        self.w_fname_nda.connect_path_is_changed_to_recipient(self.on_changed_fname_nda)
-        self.w_fname_geo.connect_path_is_changed_to_recipient(self.on_changed_fname_geo)
+        self.wfnm_nda.connect_path_is_changed_to_recipient(self.on_changed_fname_nda)
+        self.wfnm_geo.connect_path_is_changed_to_recipient(self.on_changed_fname_geo)
+        self.but_buts.clicked.connect(self.on_buts)
+
+        self.connect_image_scene_rect_changed()
+        self.connect_histogram_scene_rect_changed()
+        self.connect_image_pixmap_changed()
+
+        w = cp.ivspectrum
+        if w is not None: w.wcbar.connect_new_color_table_to(self.on_color_table_changed)
 
         self.set_tool_tips()
         self.set_style()
-        #self.set_buttons_visiable()
+        self.set_buttons_visiable()
 
-        #from psana.graphqt.UtilsFS import list_of_instruments
-        #print('lst:', list_of_instruments(cp.instr_dir.value()))
+
+    def on_color_table_changed(self):
+        w = cp.ivspectrum
+        if w is None:
+            logger.debug('on_color_table_changed - do nothing here')
+            return
+        ctab =  w.wcbar.color_table()
+        logger.debug(info_ndarr(ctab, 'TBD on_color_table_changed: new color table'))
+        w = cp.ivimageaxes
+        if w is not None:
+            wi = w.wimg
+            wi.set_coltab(coltab=ctab)
+            rs=wi.scene().sceneRect() # preserve current scene rect
+            wi.set_pixmap_from_arr(wi.arr, set_def=False)
+            wi.set_rect_scene(rs, set_def=False)
 
 
     def set_tool_tips(self):
         CMWControlBase.set_tool_tips(self)
         self.but_reset.setToolTip('Reset original image size')
+        self.but_buts.setToolTip('Show/hide buttons')
+        self.but_tabs.setToolTip('Show/hide tabs')
 
 
     def set_style(self):
         self.but_tabs.setFixedWidth(50)
         self.but_reset.setFixedWidth(50)
+        self.but_buts.setFixedWidth(50)
+         #self.but_buts.setStyleSheet(style.styleButton)
+        #self.but_tabs.setVisible(True)
+
+
+    def set_signal_fast(self, is_fast=True):
+        for w in (cp.ivimageaxes, cp.ivspectrum):
+          if w is not None: w.set_signal_fast(is_fast)
+
+
+    def connect_image_scene_rect_changed(self):
+        w = cp.ivimageaxes
+        if w is not None: w.connect_image_scene_rect_changed(self.on_image_scene_rect_changed)
+
+
+    def disconnect_image_scene_rect_changed(self):
+        w = cp.ivimageaxes
+        if w is not None: w.disconnect_image_scene_rect_changed(self.on_image_scene_rect_changed)
+
+
+    def on_image_scene_rect_changed(self, r):
+        logger.debug('on_image_scene_rect_changed: %s'%str(r))
+        wimg = cp.ivimageaxes.wimg
+        #coltab = wimg.coltab
+        a = wimg.array_in_rect(r)
+        logger.debug(info_ndarr(a, 'on_image_scene_rect_changed: selected array in rect'))
+        self.set_spectrum_from_arr(a)
+
+
+    def connect_image_pixmap_changed(self):
+        w = cp.ivimageaxes
+        if w is not None: w.wimg.connect_image_pixmap_changed(self.on_image_pixmap_changed)
+
+
+    def disconnect_image_pixmap_changed(self):
+        w = cp.ivimageaxes
+        if w is not None: w.wimg.disconnect_image_pixmap_changed(self.on_image_pixmap_changed)
+
+
+    def on_image_pixmap_changed(self):
+        logger.debug('TBD on_image_pixmap_changed')
+        wimg = cp.ivimageaxes.wimg
+        arr = wimg.arr
+        coltab = wimg.coltab
+        self.set_spectrum_from_arr(arr)
+
+
+    def set_spectrum_from_arr(self, arr):
+        w = cp.ivspectrum
+        if w is not None: w.set_spectrum_from_arr(arr)
+
+
+    def connect_histogram_scene_rect_changed(self):
+        w = cp.ivspectrum
+        if w is not None: w.connect_histogram_scene_rect_changed(self.on_histogram_scene_rect_changed)
+
+
+    def disconnect_histogram_scene_rect_changed(self):
+        w = cp.ivspectrum
+        if w is not None: w.disconnect_histogram_scene_rect_changed(self.on_histogram_scene_rect_changed)
+
+
+    def on_histogram_scene_rect_changed(self, r):
+        logger.debug('TBD on_histogram_scene_rect_changed: %s'%str(r))
 
 
     def on_but_reset(self):
         logger.debug('on_but_reset')
-        if cp.wimage is not None:
-           cp.wimage.reset_original_size()
+        if cp.ivimageaxes is not None:
+           cp.ivimageaxes.reset_original_size()
+
+        if cp.ivspectrum is not None:
+           cp.ivspectrum.reset_original_size()
 
 
     def on_changed_fname_nda(self, fname):
         logger.debug('on_changed_fname_nda: %s' % fname)
-        if cp.wimage is not None:
+        w = cp.ivimageaxes
+        if w is not None:
            nda = psu.load_ndarray_from_file(fname)
            logger.debug(info_ndarr(nda,'nda'))
            img = image_from_ndarray(nda)
-           cp.wimage.set_pixmap_from_arr(img, set_def=True)
+           w.wimg.set_pixmap_from_arr(img, set_def=True)
 
 
     def on_changed_fname_geo(self, s):
         logger.debug('on_changed_fname_geo: %s' % s)
+
+
+    def on_buts(self):
+        logger.debug('on_buts')
+        d = self.buttons_dict()
+        logger.info('initial visible buttons: %s' % str(d))
+        resp = change_check_box_dict_in_popup_menu(d, 'Select buttons', msg='Check visible buttons then click Apply or Cancel', parent=self.but_buts)
+        #logger.debug('select_visible_buttons resp: %s' % resp)
+        if resp == 0:
+            logger.info('Visible buttons selection is cancelled')
+            return
+        else:
+            logger.info('selected visible buttons: %s' % str(d))
+            self.set_buttons_visiable(d)
+            self.set_buttons_config_bitword(d)
+
+
+    def set_buttons_visiable(self, dic_buts=None):
+        d = self.buttons_dict() if dic_buts is None else dic_buts
+        #logger.info('dic_buts: %s' % str(d))
+        self.wfnm_geo.setVisible(d['Geometry'])
+        self.but_reset.setVisible(d['Reset'])
+        self.but_tabs.setVisible(d['Tabs'])
+        if cp.ivimageaxes is not None: cp.ivimageaxes.but_reset.setVisible(d['Reset image'])
+        if cp.ivimageaxes is not None: cp.ivimageaxes.set_info_visible(d['Cursor position'])
+        if cp.ivspectrum  is not None: cp.ivspectrum.but_reset.setVisible(d['Reset spectrum'])
+
+
+    def buttons_dict(self):
+        r = cp.iv_buttons.value()
+        return {'Geometry'       : r & 1,\
+                'Tabs'           : r & 2,\
+                'Reset'          : r & 4,\
+                'Reset image'    : r & 8,\
+                'Reset spectrum' : r & 16,\
+                'Cursor position': r & 32,\
+               }
+
+
+    def set_buttons_config_bitword(self, d):
+        w = 0
+        if d['Geometry']       : w |= 1
+        if d['Tabs']           : w |= 2
+        if d['Reset']          : w |= 4
+        if d['Reset image']    : w |= 8
+        if d['Reset spectrum'] : w |= 16
+        if d['Cursor position']: w |= 32
+        cp.iv_buttons.setValue(w)
+
+
+    def closeEvent(self, e):
+        #logger.debug('closeEvent')
+        CMWControlBase.closeEvent(self, e)
+        cp.ivcontrol = None
 
 
 if __name__ == "__main__":
