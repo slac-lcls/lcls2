789fef59bf4966757a7f3bfbc36bfaf2c7b53c74
Christopher O'Grady <cpo@slac.stanford.edu>
2021-07-31 12:47:18 -0700

    add dia.cnf

--- psdaq/psdaq/cnf/copy_hutch_cnf.sh
+++ psdaq/psdaq/cnf/copy_hutch_cnf.sh
@@ -2,6 +2,7 @@ cp /cds/group/pcds/dist/pds/tmo/scripts/tmo.cnf .
 cp /cds/group/pcds/dist/pds/rix/scripts/rix.cnf .
 cp /cds/group/pcds/dist/pds/ued/scripts/ued.cnf .
 cp /cds/group/pcds/dist/pds/tmo/scripts/hsd.cnf .
+cp /cds/group/pcds/dist/pds/tmo/scripts/dia.cnf .
 cp /cds/group/pcds/dist/pds/tmo/scripts/neh-base.cnf .
 cp /cds/group/pcds/dist/pds/rix/scripts/rix-hsd.cnf .
 cp /cds/group/pcds/dist/pds/tmo/scripts/setup_env.sh setup_env_tmo.sh
