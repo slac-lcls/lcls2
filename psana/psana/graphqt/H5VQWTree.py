6c42d315d867019dcf6db32eb9e6f300a613256b
msdubrovin <msdubrovin@gmail.com>
2021-08-05 10:16:50 -0700

    use file name in the root item, and path in the tree header

--- psana/psana/graphqt/H5VQWTree.py
+++ psana/psana/graphqt/H5VQWTree.py
@@ -15,6 +15,7 @@ Created on 2019-11-12 by Mikhail Dubrovin
 import logging
 logger = logging.getLogger(__name__)
 
+import os
 import sys
 import h5py
 from psana.graphqt.QWTree import Qt, QWTree, icon, QStandardItem
@@ -27,8 +28,7 @@ def name_in_path(path,sep='/'):
 class H5VQWTree(QWTree):
     """Widget for HDF5 tree
     """
-    #TEST_FNAME = '/reg/g/psdm/detector/data_test/hdf5/amox27716-r0100-e060000-single-node.h5'
-    TEST_FNAME = '/reg/g/psdm/detector/calib/jungfrau/jungfrau-171113-154920171025-3d00fb.h5'
+    TEST_FNAME = '/cds/group/psdm/detector/calib/jungfrau/jungfrau-171113-154920171025-3d00fb.h5'
 
     def __init__(self, **kwargs):
 
@@ -36,25 +36,7 @@ class H5VQWTree(QWTree):
         self.fname = kwargs.get('fname', self.TEST_FNAME)
 
         QWTree.__init__(self, wparent)
-
         self.h5py = h5py
-
-        #self._name = self.__class__.__name__
-
-        #icon.set_icons()
-
-        #self.model = QStandardItemModel()
-        #self.set_selection_mode()
-
-        #self.fill_tree_model() # in QWTree
-
-        #self.expanded.connect(self.on_item_expanded)
-        #self.collapsed.connect(self.on_item_collapsed)
-        ##self.model.itemChanged.connect(self.on_item_changed)
-        #self.connect_item_selected_to(self.on_item_selected)
-        #self.clicked[QModelIndex].connect(self.on_click)
-        ##self.doubleClicked[QModelIndex].connect(self.on_double_click)
-
         self.process_expand()
  
 
@@ -73,17 +55,15 @@ class H5VQWTree(QWTree):
         if g is None: # on first call
             self.clear_model()
             logger.debug('Open file: %s' % self.fname)
-            self.model.setHorizontalHeaderLabels(('.../%s'%name_in_path(self.fname),))
+            self.model.setHorizontalHeaderLabels(('%s/'%os.path.dirname(self.fname),))
             g=h5py.File(self.fname, 'r')
             self.fill_tree_model(g)
             return # !!!
 
         if isinstance(g, h5py.File):
             logger.debug('(File) %s %s' % (g.filename, g.name))
-            #logger.debug(g.__dir__())
             root_item = self.model.invisibleRootItem()
-            item = QStandardItem('(file) %s'%g.name)
-            #item.setCheckable(True) 
+            item = QStandardItem(os.path.basename(self.fname))
             item.setIcon(icon.icon_folder_open)
             item.setData(g)
             root_item.appendRow(item)
@@ -91,7 +71,6 @@ class H5VQWTree(QWTree):
         elif isinstance(g,h5py.Group):
             logger.debug('(Group) %s' % g.name)
             item = QStandardItem(name_in_path(g.name))
-            #item.setCheckable(True) 
             item.setIcon(icon.icon_folder_closed)
             item.setData(g)
             parent_item.appendRow(item)
@@ -125,8 +104,6 @@ class H5VQWTree(QWTree):
         QWTree.set_style(self)
         #self.header().hide()
         self.header().show()
-        #self.setMinimumSize(400, 900)
-        #self.setSizeHint(400, 900)
 
 
     def full_path(self, item, path=''):
@@ -154,25 +131,14 @@ class H5VQWTree(QWTree):
                 logger.debug(msg)
 
 
-    #def set_style(self):
-    #    #from psana.graphqt.Styles import style
-    #    self.setWindowIcon(icon.icon_monitor)
-    #    self.setContentsMargins(0,0,0,0)
-    #    self.setStyleSheet("QWTree::item:hover{background-color:#00FFAA;}")
+#    #def resizeEvent(self, e):
+#        #self.frame.setGeometry(self.rect())
+#        #logger.debug('resizeEvent')
 
-
-    #def resizeEvent(self, e):
-        #pass
-        #self.frame.setGeometry(self.rect())
-        #logger.debug('resizeEvent') 
-
-
-    #def moveEvent(self, e):
-        #logger.debug('moveEvent') 
-        #self.position = self.mapToGlobal(self.pos())
-        #self.position = self.pos()
-        #logger.debug('moveEvent - pos:' + str(self.position) + __name__)       
-        #pass
+#    #def moveEvent(self, e):
+#        #self.position = self.mapToGlobal(self.pos())
+#        #self.position = self.pos()
+#        #logger.debug('moveEvent - pos:' + str(self.position))
 
 
     def closeEvent(self, e):
@@ -186,29 +152,24 @@ class H5VQWTree(QWTree):
         #except: pass
 
 
-    #def on_exit(self):
-    #    logger.debug('on_exit')
-    #    self.close()
+    if __name__ == "__main__":
 
-
-#    if __name__ == "__main__":
-
-    def key_usage(self):
+      def key_usage(self):
         return 'Keys:'\
                '\n  ESC - exit'\
                '\n  E - expand'\
                '\n  C - collapse'\
                '\n'
 
-    def keyPressEvent(self, e):
-        #logger.debug('keyPressEvent, key = %s'%e.key())       
+      def keyPressEvent(self, e):
+        logger.debug('keyPressEvent, key = %s'%e.key())
         if   e.key() == Qt.Key_Escape:
             self.close()
 
-        elif e.key() == Qt.Key_E: 
+        elif e.key() == Qt.Key_E:
             self.process_expand()
 
-        elif e.key() == Qt.Key_C: 
+        elif e.key() == Qt.Key_C:
             self.process_collapse()
 
         else:
@@ -217,14 +178,12 @@ class H5VQWTree(QWTree):
 
 
 if __name__ == "__main__":
-    import sys
     from PyQt5.QtWidgets import QApplication
     fmt = '%(asctime)s %(name)s %(levelname)s: %(message)s'
     logging.basicConfig(format=fmt, datefmt='%H:%M:%S', level=logging.DEBUG)
-    #logger.setPrintBits(0o177777)
     app = QApplication(sys.argv)
     w = H5VQWTree()
-    #w.setGeometry(10, 25, 200, 600)
+    w.setGeometry(10, 25, 400, 800)
     w.setWindowTitle('H5VQWTree')
     w.move(50,20)
     w.show()
