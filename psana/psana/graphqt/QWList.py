e37de9e8d08c164025c33de9e1296f428df80a17
msdubrovin <msdubrovin@gmail.com>
2021-08-10 16:41:29 -0700

    extend import for derived modules, cosmitic

--- psana/psana/graphqt/QWList.py
+++ psana/psana/graphqt/QWList.py
@@ -1,4 +1,4 @@
-#------------------------------
+
 """Class :py:class:`QWList` is a QListView->QWidget for list model
 ===================================================================
 
@@ -11,37 +11,36 @@ Usage ::
 
 Created on 2017-04-20 by Mikhail Dubrovin
 """
-#------------------------------
+
 import logging
 logger = logging.getLogger(__name__)
 
-from PyQt5.QtWidgets import QListView, QVBoxLayout, QAbstractItemView
-from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from PyQt5.QtCore import Qt, QModelIndex
+from PyQt5.QtWidgets import QApplication, QListView, QVBoxLayout, QAbstractItemView
+from PyQt5.QtGui import QStandardItemModel, QStandardItem, QBrush
+from PyQt5.QtCore import Qt, QModelIndex, QSize
 
-#from psana.graphqt.CMConfigParameters import cp
-#from psana.pyalgos.generic.Logger import logger
+
+
+
 from psana.graphqt.QWIcons import icon
 
-#------------------------------
 
-class QWList(QListView) :
-    """Widget for List
+class QWList(QListView):
+    """Widget for the list of QListView
     """
-    def __init__(self, parent=None) :
-
-        QListView.__init__(self, parent)
-        #self._name = self.__class__.__name__
+    def __init__(self, **kwa):
+        kwa.setdefault('parent', None)
+        QListView.__init__(self, **kwa)
 
         icon.set_icons()
 
         self.model = QStandardItemModel()
         self.set_selection_mode()
-        self.fill_list_model(None, None) # defines self.model
+        self.fill_list_model() # defines self.model
         self.setModel(self.model)
 
         self.set_style()
-        self.show_tool_tips()
+        self.set_tool_tips()
 
         self.model.itemChanged.connect(self.on_item_changed)
         self.connect_item_selected_to(self.on_item_selected)
@@ -49,7 +48,7 @@ class QWList(QListView) :
         self.doubleClicked[QModelIndex].connect(self.on_double_click)
  
 
-    def set_selection_mode(self, smode='extended') :
+    def set_selection_mode(self, smode='extended'):
         logger.debug('Set selection mode: %s'%smode)
         mode = {'single'      : QAbstractItemView.SingleSelection,
                 'contiguous'  : QAbstractItemView.ContiguousSelection,
@@ -59,11 +58,11 @@ class QWList(QListView) :
         self.setSelectionMode(mode)
 
 
-    def connect_item_selected_to(self, recipient) :
+    def connect_item_selected_to(self, recipient):
         self.selectionModel().currentChanged[QModelIndex, QModelIndex].connect(recipient)
 
 
-    def disconnect_item_selected_from(self, recipient) :
+    def disconnect_item_selected_from(self, recipient):
         self.selectionModel().currentChanged[QModelIndex, QModelIndex].disconnect(recipient)
 
 
@@ -81,12 +80,13 @@ class QWList(QListView) :
         self.model.removeRows(0, rows)
 
 
-    def fill_list_model(self, docs, recs):
+    def fill_list_model(self, **kwa):
         self.clear_model()
         for i in range(20):
             item = QStandardItem('%02d item text'%(i))
             item.setIcon(icon.icon_table)
-            item.setCheckable(True) 
+            item.setCheckable(True)
+            item.setSizeHint(QSize(-1,22))
             self.model.appendRow(item)
 
 
@@ -97,14 +97,9 @@ class QWList(QListView) :
 
     def on_item_selected(self, selected, deselected):
         itemsel = self.model.itemFromIndex(selected)
-        if itemsel is not None :
+        if itemsel is not None:
             msg = 'on_item_selected row:%02d selected: %s' % (selected.row(), itemsel.text()) 
             logger.info(msg)
-
-        #itemdes = self.model.itemFromIndex(deselected)
-        #if itemdes is not None :
-        #    msg = 'on_item_selected row: %d deselected %s' % (deselected.row(), itemdes.text())
-        #    logger.info(msg)
 
 
     def on_item_changed(self, item):
@@ -126,11 +121,8 @@ class QWList(QListView) :
         msg = 'on_double_click item in row:%02d text: %s' % (index.row(), item.text())
         logger.debug(msg)
 
-    #--------------------------
-    #--------------------------
-    #--------------------------
 
-    def show_tool_tips(self):
+    def set_tool_tips(self):
         self.setToolTip('List model') 
 
 
@@ -138,46 +130,23 @@ class QWList(QListView) :
         #from psana.graphqt.Styles import style
         self.setWindowIcon(icon.icon_monitor)
         #self.layout().setContentsMargins(0,0,0,0)
-
         self.setStyleSheet("QListView::item:hover{background-color:#00FFAA;}")
 
-        #self.palette = QPalette()
-        #self.resetColorIsSet = False
-        #self.butELog    .setIcon(icon.icon_mail_forward)
-        #self.butFile    .setIcon(icon.icon_save)  
-        #self.setMinimumHeight(250)
-        #self.setMinimumWidth(550)
-        #self.adjustSize()
-        #self.        setStyleSheet(style.styleBkgd)
-        #self.butSave.setStyleSheet(style.styleButton)
-        #self.butFBrowser.setVisible(False)
-        #self.butExit.setText('')
-        #self.butExit.setFlat(True)
- 
 
-    #def resizeEvent(self, e):
-        #pass
-        #self.frame.setGeometry(self.rect())
-        #logger.debug('resizeEvent') 
+#    def resizeEvent(self, e):
+#        self.frame.setGeometry(self.rect())
+#        logger.debug('resizeEvent') 
 
 
-    #def moveEvent(self, e):
-        #logger.debug('moveEvent') 
-        #self.position = self.mapToGlobal(self.pos())
-        #self.position = self.pos()
-        #logger.debug('moveEvent - pos:' + str(self.position), __name__)       
-        #pass
+#    def moveEvent(self, e):
+#        logger.debug('moveEvent') 
+#        self.position = self.mapToGlobal(self.pos())
+#        self.position = self.pos()
 
 
     def closeEvent(self, e):
         logger.debug('closeEvent')
         QListView.closeEvent(self, e)
-
-        #try    : self.gui_win.close()
-        #except : pass
-
-        #try    : del self.gui_win
-        #except : pass
 
 
     def on_exit(self):
@@ -185,42 +154,38 @@ class QWList(QListView) :
         self.close()
 
 
-    def process_selected_items(self) :
+    def process_selected_items(self):
         selitems = self.selected_items()
         msg = '%d Selected items:' % len(selitems)
-        for i in selitems :
+        for i in selitems:
             msg += '\n  %s' % i.text()
         logger.info(msg)
 
-    if __name__ == "__main__" :
 
-      def key_usage(self) :
+    if __name__ == "__main__":
+
+      def key_usage(self):
         return 'Keys:'\
                '\n  ESC - exit'\
                '\n  S - show selected items'\
                '\n'
 
 
-      def keyPressEvent(self, e) :
-        #logger.info('keyPressEvent, key=', e.key())       
-        if   e.key() == Qt.Key_Escape :
+      def keyPressEvent(self, e):
+        logger.info('keyPressEvent, key=%s' % e.key())       
+        if   e.key() == Qt.Key_Escape:
             self.close()
 
-        elif e.key() == Qt.Key_S : 
+        elif e.key() == Qt.Key_S: 
             self.process_selected_items()
 
-        else :
+        else:
             logger.info(self.key_usage())
 
-#------------------------------
-#------------------------------
-#------------------------------
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     import sys
-    from PyQt5.QtWidgets import QApplication
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-    #logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
+    logging.basicConfig(format='[%(levelname).1s] L:%(lineno)03d %(name)s %(message)s', level=logging.DEBUG)
 
     app = QApplication(sys.argv)
     w = QWList()
@@ -232,4 +197,4 @@ if __name__ == "__main__" :
     del w
     del app
 
-#------------------------------
+# EOF
