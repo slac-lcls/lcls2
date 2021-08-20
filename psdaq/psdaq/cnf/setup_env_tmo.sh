9e78373cdd6588ec6c68ff8cb2a450e8e4ad2984
Christopher O'Grady <cpo@slac.stanford.edu>
2021-08-19 17:17:15 -0700

    latest cnf files

--- psdaq/psdaq/cnf/setup_env_tmo.sh
+++ psdaq/psdaq/cnf/setup_env_tmo.sh
@@ -2,7 +2,11 @@ source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
 conda deactivate
 
 conda activate ps-4.3.2
-RELDIR="/cds/home/opr/tmoopr/git/lcls2_060821"
+RELDIR="/cds/home/opr/tmoopr/git/lcls2_080421"
+
+# official running until Aug 4, 2021
+#conda activate ps-4.3.2
+#RELDIR="/cds/home/opr/tmoopr/git/lcls2_060821"
 
 # official running until June 5, 2021
 #conda activate ps-4.3.2
