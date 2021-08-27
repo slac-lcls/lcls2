3d0365a90eba3d685d96a15ca14611aebd1b08a5
msdubrovin <msdubrovin@gmail.com>
2021-08-26 17:00:32 -0700

    remove PATH from env1

--- psana/psana/graphqt/test-subproc-lcls1-env.py
+++ psana/psana/graphqt/test-subproc-lcls1-env.py
@@ -3,7 +3,7 @@
 cmd_seq = ['/bin/bash', '-l', '-c', '. /reg/g/psdm/etc/psconda.sh; echo "PATH: $PATH"; echo "CONDA_DEFAULT_ENV: $CONDA_DEFAULT_ENV"; detnames exp=xppx44719:run=11'] 
 #cmd = '/bin/bash -l -c "source /reg/g/psdm/etc/psconda.sh; echo $PATH"' # works with shell=True
 
-env1 = {'PATH':'/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/puppetlabs/bin'}
+env1 = {}#'PATH':'/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/puppetlabs/bin'}
 import subprocess
 p = subprocess.Popen(cmd_seq, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, executable='/bin/bash', env=env1) # , shell=True, env=None,
 text = p.communicate()[0].decode("utf-8")
