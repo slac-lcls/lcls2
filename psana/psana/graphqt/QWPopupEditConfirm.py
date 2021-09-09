5dd4ccf143b4284103a6dce8708b67e3bc6f3ca2
msdubrovin <msdubrovin@gmail.com>
2021-09-09 09:35:59 -0700

    add edit_and_confirm_or_cancel_dialog_box

---
+++ psana/psana/graphqt/QWPopupEditConfirm.py
@@ -0,0 +1,119 @@
+
+"""
+:py:class:`QWPopupEditConfirm` - Popup GUI
+============================================
+
+Usage::
+
+    # Test: python lcls2/psana/psana/graphqt/QWPopupEditConfirm.py
+
+    # Import
+    from psana.graphqt.QWPopupEditConfirm import QWPopupEditConfirm
+
+    # Methods - see test
+
+See:
+    - :py:class:`QWPopupEditConfirm`
+    - `lcls2 on github <https://github.com/slac-lcls/lcls2>`_.
+
+This software was developed for the LCLS2 project.
+If you use all or part of it, please give an appropriate acknowledgment.
+
+Created on 2021-09-08 by Mikhail Dubrovin
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+from PyQt5.QtWidgets import QDialog, QHBoxLayout, QVBoxLayout, QPushButton, QTextEdit, QFrame, QSizePolicy, QApplication
+from PyQt5.QtCore import Qt
+
+
+class QWPopupEditConfirm(QDialog):
+    """
+    """
+    def __init__(self, **kwa):
+        QDialog.__init__(self, kwa.get('parent', None))
+        win_title = kwa.get('win_title', 'Edit and confirm or cancel')
+        if win_title: self.setWindowTitle(win_title)
+        self.edi_msg = QTextEdit(kwa.get('msg', 'text to edit and confirm'))
+        self.but_apply = QPushButton(kwa.get('but_title_apply', 'Apply'))
+        self.but_cancel = QPushButton(kwa.get('but_title_cancel', 'Cancel'))
+        self.but_cancel.clicked.connect(self.on_cancel)
+        self.but_apply.clicked.connect(self.on_apply)
+
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(self.but_cancel)
+        self.hbox.addWidget(self.but_apply)
+        self.hbox.addStretch(1)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.edi_msg)
+        self.vbox.addLayout(self.hbox)
+        self.setLayout(self.vbox)
+
+        self.but_cancel.setFocusPolicy(Qt.NoFocus)
+
+        self.set_style()
+#        self.set_icons()
+#        self.set_tool_tips()
+
+
+    def set_style(self):
+        self.layout().setContentsMargins(2,2,2,2)
+        styleGray = "background-color: rgb(230, 240, 230); color: rgb(0, 0, 0);" # Gray
+#        styleDefault = ""
+#        self.setStyleSheet(styleDefault)
+        self.but_cancel.setStyleSheet(styleGray)
+        self.but_apply.setStyleSheet(styleGray)
+        self.edi_msg.setMinimumSize(500,60)
+        self.setFixedHeight(100)
+#        self.setModal(True)
+#        self.setFixedWidth(200)
+
+#    def set_icons(self):
+#        from psana.graphqt.QWIcons import icon
+#        icon.set_icons()
+#        self.but_cancel.setIcon(icon.icon_button_cancel)
+#        self.but_apply .setIcon(icon.icon_button_ok)
+
+#    def set_tool_tips(self):
+#        self.but_apply.setToolTip('Apply')
+#        self.but_cancel.setToolTip('Cancel')
+
+ 
+    def on_cancel(self):
+        logger.debug('on_cancel')
+        self.reject()
+
+
+    def on_apply(self):
+        logger.debug('on_apply')  
+        self.accept()
+
+
+    def message(self):
+        return str(self.edi_msg.toPlainText())
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    logging.getLogger('h5py').setLevel(logging.WARNING)
+    logging.getLogger('psana.pscalib.geometry').setLevel(logging.WARNING)
+    os.environ['LIBGL_ALWAYS_INDIRECT'] = '1'
+    logging.basicConfig(format='[%(levelname).1s] L:%(lineno)03d %(name)s %(message)s', level=logging.DEBUG)
+
+    app = QApplication(sys.argv)
+    w = QWPopupEditConfirm(msg='Text to edit and confirm', win_title='Edit & confirm or cancel')
+    w.setGeometry(20, 40, 500, 200)
+    resp=w.exec_()
+    logger.debug('resp=%s' % resp)
+    logger.debug('QtWidgets.QDialog.Rejected: %d' % QDialog.Rejected)
+    logger.debug('QtWidgets.QDialog.Accepted: %d' % QDialog.Accepted)
+    logger.debug('output message: %s' % w.message())
+
+    del w
+    del app
+
+# EOF
--- psana/psana/graphqt/QWUtils.py
+++ psana/psana/graphqt/QWUtils.py
@@ -1,4 +1,4 @@
-#------------------------------
+
 """
 :py:class:`QWUtils` - Popup GUIs
 ================================
@@ -21,17 +21,15 @@ If you use all or part of it, please give an appropriate acknowledgment.
 Created on 2017-02-08 by Mikhail Dubrovin
 Adopted for LCLS2 on 2018-02-15
 """
-#------------------------------
 
 import os
 import logging
 logger = logging.getLogger(__name__)
 
-from PyQt5.QtWidgets import QMenu, QDialog, QFileDialog, QMessageBox, QColorDialog, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMenu, QDialog, QFileDialog, QMessageBox, QColorDialog, QWidget, QVBoxLayout, QApplication
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QCursor
 
-#------------------------------
 
 def select_item_from_popup_menu(lst, title=None, default=None, parent=None):
     """Shows the list as a pop-up menu and returns the selected item as a string or None"""
@@ -41,15 +39,14 @@ def select_item_from_popup_menu(lst, title=None, default=None, parent=None):
     atitle = w.addAction(_title)
     atitle.setDisabled(True)
     asep = w.addSeparator()
-    for name in lst : 
+    for name in lst:
        action = w.addAction(name)
-       if name == default :
+       if name == default:
            #w.setDefaultAction(action)
            w.setActiveAction(action)
     item = w.exec_(QCursor.pos())
     return None if item is None else str(item.text()) # str(QString)
 
-#------------------------------
 
 def select_color(colini=Qt.blue, parent=None):
     """Select color using QColorDialog"""
@@ -61,11 +58,10 @@ def select_color(colini=Qt.blue, parent=None):
     #color = QColorDialog.getColor()
     return None if color is None else color # QColor or None
 
-#------------------------------
 
 def change_check_box_list_in_popup_menu(list, win_title='Set check boxes', parent=None):
     """Shows the list of check-boxes as a dialog pop-up menu and returns the (un)changed list"""
-    if list is None : return 0
+    if list is None: return 0
 
     from psana.graphqt.QWPopupCheckList import QWPopupCheckList
 
@@ -74,21 +70,21 @@ def change_check_box_list_in_popup_menu(list, win_title='Set check boxes', paren
     popupMenu.move(QCursor.pos())
     response = popupMenu.exec_()
 
-    if   response == QDialog.Accepted :
+    if   response == QDialog.Accepted:
         #logger.debug('New checkbox list is accepted')         
         return 1
-    elif response == QDialog.Rejected :
+    elif response == QDialog.Rejected:
         #logger.debug('Will use old checkbox list')
         return 0
-    else                                    :
+    else:
         #logger.error('Unknown response...')
         return 2
 
-#------------------------------
+
 
 def change_check_box_dict_in_popup_menu(dict, win_title='Set check boxes', parent=None, msg=''):
     """Shows the dict of check-boxes as a dialog pop-up menu and returns the (un)changed dict"""
-    if dict is None : return 0
+    if dict is None: return 0
 
     from psana.graphqt.QWPopupCheckDict import QWPopupCheckDict
 
@@ -97,17 +93,16 @@ def change_check_box_dict_in_popup_menu(dict, win_title='Set check boxes', paren
     popupMenu.move(QCursor.pos())
     response = popupMenu.exec_()
 
-    if   response == QDialog.Accepted :
+    if   response == QDialog.Accepted:
         #logger.debug('New checkbox dict is accepted',)         
         return 1
-    elif response == QDialog.Rejected :
+    elif response == QDialog.Rejected:
         #logger.debug('Will use old checkbox dict')
         return 0
-    else                                    :
+    else:
         #logger.error('Unknown response...')
         return 2
 
-#------------------------------
 
 def select_radio_button_in_popup_menu(dict_of_pars, win_title='Select option', do_confirm=False, parent=None):
     """Popup GUI to select radio button from the list:  dict_of_pars = {'checked':'radio1', 'list':['radio0', 'radio1', 'radio2']}
@@ -119,15 +114,13 @@ def select_radio_button_in_popup_menu(dict_of_pars, win_title='Select option', d
     popupMenu.move(QCursor.pos()-QPoint(100,100))
     return popupMenu.exec_() # QDialog.Accepted or QDialog.Rejected
 
-#------------------------------
 
-def print_rect(r, cmt='') :
+def print_rect(r, cmt=''):
     x, y, w, h = r.x(), r.y(), r.width(), r.height()
     L, R, T, B = r.left(), r.right(), r.top(), r.bottom()
     logger.debug('%s x=%8.2f  y=%8.2f  w=%8.2f  h=%8.2f' % (cmt, x, y, w, h))
     logger.debug('%s L=%8.2f  B=%8.2f  R=%8.2f  T=%8.2f' % (len(cmt)*' ', L, B, R, T))
 
-#------------------------------
 
 def get_save_fname_through_dialog_box(parent, path0, title, filter='*.txt'):       
 
@@ -136,77 +129,58 @@ def get_save_fname_through_dialog_box(parent, path0, title, filter='*.txt'):
                                              directory = path0,
                                              filter    = filter
                                              )
-    if path == '' :
+    if path == '':
         #logger.debug('Saving is cancelled. get_save_fname_through_dialog_box')
         return None
-    #logger.debug('Output file: ' + path + ' get_save_fname_through_dialog_box')
     return path
 
-#------------------------------
 
 def get_open_fname_through_dialog_box(parent, path0, title, filter='*.txt'):       
 
     path, fext = QFileDialog.getOpenFileName(parent, title, path0, filter=filter)
 
-    #logger.debug('XXX: get_open_fname_through_dialog_box path =', path)
-    #logger.debug('XXX: get_open_fname_through_dialog_box fext =', fext)
-
     dname, fname = os.path.split(path)
-    if dname == '' or fname == '' :
+    if dname == '' or fname == '':
         #logger.debug('Input directiry name or file name is empty... keep file path unchanged...'
         return None
     #logger.info('Input file: ' + path + 'get_open_fname_through_dialog_box') 
     return path
 
-#------------------------------
 
 def get_existing_directory_through_dialog_box(parent, path0, title, options = QFileDialog.ShowDirsOnly):       
 
     path = QFileDialog.getExistingDirectory(parent, title, path0, options)
 
-    #logger.debug('XXX: get_open_fname_through_dialog_box path =', path)
-    #logger.debug('XXX: get_open_fname_through_dialog_box fext =', fext)
-
     dname = path #, fname = os.path.split(path)
-    if dname == '' :
+    if dname == '':
         # logger.debug('Input directiry name or file name is empty... keep file path unchanged...'
         return None
     logger.info('Selected directory: %s' % path) 
     return path
 
-#------------------------------
 
-def confirm_dialog_box(parent=None, text='Please confirm that you aware!', title='Please acknowledge') :
+def confirm_dialog_box(parent=None, text='Please confirm that you aware!', title='Please acknowledge'):
         """Pop-up MODAL box for confirmation"""
 
         mesbox = QMessageBox(parent, windowTitle=title,
                                            text=text,
                                            standardButtons=QMessageBox.Ok)
-               #standardButtons=QtGui.QMessageBox.Save | QtGui.QMessageBox.Discard | QtGui.QMessageBox.Cancel)
-        #mesbox.setDefaultButton(QtGui.QMessageBox.Ok)
-        #mesbox.setMinimumSize(400, 200)
-        #style = "background-color: rgb(255, 200, 220); color: rgb(0, 0, 100);" # Pinkish
-        #style = "background-color: rgb(255, 255, 220); color: rgb(0, 0, 0);" # Yellowish
-        #mesbox.setStyleSheet (style)
 
         clicked = mesbox.exec_() # DISPLAYS THE QMessageBox HERE
         mesbox.setDefaultButton(QMessageBox.Ok)
 
-        #if   clicked == QtGui.QMessageBox.Save :
-        #    logger.info('Saving is requested', __name__)
-        #elif clicked == QtGui.QMessageBox.Discard :
-        #    logger.info('Discard is requested', __name__)
-        #else :
-        #    logger.info('Cancel is requested', __name__)
-        #return clicked
-
-        #logger.info('You acknowkeged that saw the message:\n' + text, 'confirm_dialog_box')
-
         return True if clicked == QMessageBox.Ok else False
 
-#------------------------------
 
-def confirm_or_cancel_dialog_box(parent=None, text='Please confirm or cancel', title='Confirm or cancel') :
+def edit_and_confirm_or_cancel_dialog_box(parent=None, text='Text confirm or cancel', title='Edit and confirm or cancel'):
+    from psana.graphqt.QWPopupEditConfirm import QWPopupEditConfirm
+    w = QWPopupEditConfirm(parent=parent, msg=text, win_title=title, but_title_apply='Confirm', but_title_cancel='Cancel')
+    #w.setGeometry(20, 40, 500, 200)
+    resp=w.exec_()
+    return w.message() if resp == QDialog.Accepted else None
+
+
+def confirm_or_cancel_dialog_box(parent=None, text='Please confirm or cancel', title='Confirm or cancel'):
         """Pop-up MODAL box for confirmation"""
 
         mesbox = QMessageBox(parent, windowTitle=title,
@@ -214,57 +188,44 @@ def confirm_or_cancel_dialog_box(parent=None, text='Please confirm or cancel', t
                                            standardButtons=QMessageBox.Ok | QMessageBox.Cancel)
                #standardButtons=QtGui.QMessageBox.Save | QtGui.QMessageBox.Discard | QtGui.QMessageBox.Cancel)
         mesbox.setDefaultButton(QMessageBox.Ok)
-        #mesbox.setMinimumSize(400, 200)
-        #style = "background-color: rgb(255, 200, 220); color: rgb(0, 0, 100);" # Pinkish
-        #style = "background-color: rgb(255, 255, 220); color: rgb(0, 0, 0);" # Yellowish
-        #mesbox.setStyleSheet (style)
 
         clicked = mesbox.exec_() # DISPLAYS THE QMessageBox HERE
 
-        if   clicked == QMessageBox.Ok     : return True
-        elif clicked == QMessageBox.Cancel : return False
-        else                               : return False
+        if   clicked == QMessageBox.Ok    : return True
+        elif clicked == QMessageBox.Cancel: return False
+        else: return False
 
-#------------------------------
 
-def help_dialog_box(parent=None, text='Help message goes here', title='Help') :
+def help_dialog_box(parent=None, text='Help message goes here', title='Help'):
         """Pop-up NON-MODAL box for help etc."""
         mesbox = QMessageBox(parent, windowTitle=title,
                                       text=text,
                                       standardButtons=QMessageBox.Ok)
                                       #standardButtons=QMessageBox.Close)
-        #messbox.setStyleSheet(cp.styleBkgd)
+
         mesbox.setDefaultButton(QMessageBox.Ok)
-        #mesbox.setWindowModality(Qt.NonModal)
         mesbox.setModal(False)
         mesbox.update()
         clicked = mesbox.exec_() # For MODAL dialog
-        #clicked = mesbox.show()  # For NON-MODAL dialog
-        #logger.info('Help window is open' + text, 'help_dialog_box')
         return mesbox
 
-#------------------------------
 
-def widget_from_layout(l) :
+def widget_from_layout(l):
     w = QWigget()
     w.setLayout(l)
     return w
 
-#------------------------------
 
-def layout_from_widget(w, layout=QVBoxLayout) :
+def layout_from_widget(w, layout=QVBoxLayout):
     l = layout()
     l.addWidget(w)
     return l
 
-#------------------------------
-#------------------------------
- 
-if __name__ == "__main__" :
 
-  def test(tname) :
+if __name__ == "__main__":
 
-    from PyQt5.QtWidgets import QApplication
+  def test(tname):
+
     app = QApplication(sys.argv)
 
     if tname == '0':
@@ -275,13 +236,13 @@ if __name__ == "__main__" :
     elif tname == '1':
         list_of_cbox = [['VAR1', True], ['VAR2', False], ['VAR3', False], ['VAR4', False], ['VAR5', False]]
         resp = change_check_box_list_in_popup_menu(list_of_cbox, win_title='Select vars(s)')
-        for (var,stat) in list_of_cbox : logger.debug('%s: %s' % (var, stat))
+        for (var,stat) in list_of_cbox: logger.debug('%s: %s' % (var, stat))
         logger.debug('resp: %s' % resp)
         
     elif tname == '2': 
         dict_of_pars = {'checked':'radio1', 'list':['radio0', 'radio1', 'radio2']}
         resp = select_radio_button_in_popup_menu(dict_of_pars, win_title='Select vars(s)', do_confirm=True)
-        for (k,v) in dict_of_pars.items() : logger.debug('%s: %s' % (k, v))
+        for (k,v) in dict_of_pars.items(): logger.debug('%s: %s' % (k, v))
         logger.debug('resp: %s' % resp)
 
     elif tname == '3':
@@ -315,17 +276,20 @@ if __name__ == "__main__" :
     elif tname == '9':
         dict_of_cbox = {'VAR1':True, 'VAR2':False, 'VAR3':False, 'VAR4':False, 'VAR5':False}
         resp = change_check_box_dict_in_popup_menu(dict_of_cbox, win_title='Select vars(s)')
-        for (var,stat) in dict_of_cbox.items() : logger.debug('%s: %s' % (var, stat))
+        for (var,stat) in dict_of_cbox.items(): logger.debug('%s: %s' % (var, stat))
         logger.debug('resp: %s' % resp)
         
-    else :
+    elif tname == '10':
+        resp=edit_and_confirm_or_cancel_dialog_box(parent=None, text='Text confirm or cancel', title='Edit and confirm or cancel')
+        logger.debug('resp=%s' % resp)
+
+    else:
         logger.debug('Sorry, not-implemented test "%s"' % tname)
 
     del app
 
-#------------------------------
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     import sys; global sys
 
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
@@ -335,4 +299,4 @@ if __name__ == "__main__" :
     test(tname)
     sys.exit('End of test %s' % tname)
 
-#------------------------------
+#EOF
