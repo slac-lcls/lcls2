6a584abef49c40e2c1b3b25232e87b7793215360
msdubrovin <msdubrovin@gmail.com>
2021-09-23 10:52:06 -0700

    code cleaning

--- psana/psana/detector/opal.py
+++ psana/psana/detector/opal.py
@@ -1,12 +1,7 @@
+
 import numpy as np
-#from psana.detector.detector_impl import DetectorImpl
-#from amitypes import Array2d
 from psana.detector.opal_base import opal_base, logging
 logger = logging.getLogger(__name__)
-
-#class opal_raw_2_0_0(DetectorImpl):
-#    def __init__(self, *args, **kwa_base):
-#        super(opal_raw_2_0_0, self).__init__(*args, **kwa)
 
 class opal_raw_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
@@ -36,5 +31,7 @@ class opal_ref_2_0_0(opal_base):
     def __init__(self, *args, **kwa):
         opal_base.__init__(self, *args, **kwa)
         self._add_fields()
+
+# EOF
 
 
