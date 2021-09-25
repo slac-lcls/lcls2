4a94749270f647fccd0f63f63f4a075f340115d6
msdubrovin <msdubrovin@gmail.com>
2021-09-24 17:48:47 -0700

    code cleaning, add rowslice in rint_pixel_size_arrs

--- psana/psana/pscalib/geometry/SegGeometryEpix10kaV1.py
+++ psana/psana/pscalib/geometry/SegGeometryEpix10kaV1.py
@@ -116,8 +116,8 @@ class SegGeometryEpix10kaV1(SegGeometry):
 
 
     def __init__(sp, **kwa):
-        logger.debug('%s.__init__()'%sp._name)
         sp.use_wide_pix_center = kwa.get('use_wide_pix_center', True)
+        logger.debug('SegGeometryEpix10kaV1.__init__() use_wide_pix_center: %s' % sp.use_wide_pix_center)
 
         SegGeometry.__init__(sp)
 
@@ -176,16 +176,17 @@ class SegGeometryEpix10kaV1(SegGeometry):
         logger.info(s)
 
 
-    def print_pixel_size_arrs(sp):
+    def print_pixel_size_arrs(sp, rowslice=slice(171,181)):
         sp.make_pixel_size_arrs()
         s = 'SegGeometryEpix10kaV1.print_pixel_size_arrs()'\
-          + '\n  sp.x_pix_size_um[171:181,187:197]:\n'+ str(sp.x_pix_size_um[171:181,187:197])\
+          + '\n  rowslice=%s' % str(rowslice)\
+          + '\n  sp.x_pix_size_um[rowslice,187:197]:\n'+ str(sp.x_pix_size_um[rowslice,187:197])\
           + '\n  sp.x_pix_size_um.shape = '           + str(sp.x_pix_size_um.shape)\
           + '\n  sp.y_pix_size_um:\n'                 + str(sp.y_pix_size_um)\
           + '\n  sp.y_pix_size_um.shape = '           + str(sp.y_pix_size_um.shape)\
           + '\n  sp.z_pix_size_um:\n'                 + str(sp.z_pix_size_um)\
           + '\n  sp.z_pix_size_um.shape = '           + str(sp.z_pix_size_um.shape)\
-          + '\n  sp.pix_area_arr[171:181,187:197]:\n' + str(sp.pix_area_arr[171:181,187:197])\
+          + '\n  sp.pix_area_arr[rowslice,187:197]:\n'+ str(sp.pix_area_arr[rowslice,187:197])\
           + '\n  sp.pix_area_arr.shape  = '           + str(sp.pix_area_arr.shape)
         logger.info(s)
 
@@ -514,6 +515,7 @@ if __name__ == "__main__":
 
 
 if __name__ == "__main__":
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
     tname = sys.argv[1] if len(sys.argv) > 1 else '0'
     if len(sys.argv)==1: logger.info(usage())
