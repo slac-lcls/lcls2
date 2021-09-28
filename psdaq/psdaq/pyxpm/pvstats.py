8614be2b31a81e86ea6bf6af38ef5e76b5dc8826
Matt Weaver <weaver@slac.stanford.edu>
2021-09-28 09:53:48 -0700

    Enable sfp status readout

--- psdaq/psdaq/pyxpm/pvstats.py
+++ psdaq/psdaq/pyxpm/pvstats.py
@@ -438,6 +438,7 @@ class PVStats(object):
             self._usTiming.update()
             self._cuTiming.update()
             self._cuGen   .update()
+            self._sfpStat .update()
         except:
             exc = sys.exc_info()
             if exc[0]==KeyboardInterrupt:
