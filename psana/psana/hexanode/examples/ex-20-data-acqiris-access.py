50a46fe80e634447588742a39a33799ff88a1b37
msdubrovin <msdubrovin@gmail.com>
2022-01-11 10:12:52 -0800

    code cleaning

--- psana/psana/hexanode/examples/ex-20-data-acqiris-access.py
+++ psana/psana/hexanode/examples/ex-20-data-acqiris-access.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
-#----------
-"""- loop over events of psana dataset (xtc2 file) and 
+
+"""- loop over events of psana dataset (xtc2 file) and
    - print raw acqiris waveforms and associated sample times
 """
 
 from psana import DataSource
 from psana.pyalgos.generic.NDArrUtils import print_ndarr
 
-#ds   = DataSource(files='/reg/g/psdm/detector/data2_test/xtc/data-amox27716-r0100-acqiris-e001000.xtc2')
 ds   = DataSource(files='/reg/g/psdm/detector/data2_test/xtc/data-amox27716-r0100-acqiris-e000100.xtc2')
 orun = next(ds.runs())
 det  = orun.Detector('tmo_quadanode') # 'tmo_hexanode'
@@ -18,4 +17,4 @@ for nev,evt in enumerate(orun.events()):
     print_ndarr(det.raw.times(evt),     '  times : ', last=4)
     print_ndarr(det.raw.waveforms(evt), '  wforms: ', last=4)
 
-#----------
+# EOF
