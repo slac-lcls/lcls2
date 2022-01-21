a428ec55f4a7b52c808883b806c36d5bf9e6c40e
msdubrovin <msdubrovin@gmail.com>
2022-01-21 10:26:16 -0800

    add the 1st version of epix100.py

---
+++ psana/psana/detector/epix100.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import psana.detector.areadetector as ad
+
+class epix100hw_raw_2_0_1(ad.AreaDetector):
+
+    def __init__(self, *args, **kwa):
+        ad.AreaDetector.__init__(self, *args, **kwa)
+        self._seg_geo = ad.sgs.Create(segname='EPIX100:V1')
+
+#    def image(self,evt):
+#        segments = self._segments(evt)
+#        return segments[0].raw
