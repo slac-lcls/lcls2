c74c0bdf80d5d0824f61764290e7be2403f5d7e0
msdubrovin <msdubrovin@gmail.com>
2022-01-24 16:52:02 -0800

    rename epix100hw to epix100

--- psana/psana/detector/epix100.py
+++ psana/psana/detector/epix100.py
@@ -11,3 +11,9 @@ class epix100hw_raw_2_0_1(ad.AreaDetector):
 #    def image(self,evt):
 #        segments = self._segments(evt)
 #        return segments[0].raw
+
+class epix100_raw_2_0_1(ad.AreaDetector):
+
+    def __init__(self, *args, **kwa):
+        ad.AreaDetector.__init__(self, *args, **kwa)
+        self._seg_geo = ad.sgs.Create(segname='EPIX100:V1')
