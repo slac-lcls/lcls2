98ede9ce5302411e42ab0f9ebfbc02761ad40c31
msdubrovin <msdubrovin@gmail.com>
2022-03-02 09:56:14 -0800

    add issue_2022_03_01

--- psana/psana/detector/test_issues_2022.py
+++ psana/psana/detector/test_issues_2022.py
@@ -100,7 +100,24 @@ def issue_2022_02_15():
     for nevt,evt in enumerate(myrun.events()):
         print('*** event',nevt,det.raw.raw(evt).shape,det.raw.calib(evt).shape,det.raw.image(evt))
 
+def issue_2022_03_01():
+    """O'Grady, Paul Christopher <cpo@slac.stanford.edu> Mon 2/28/2022 11:44 PM
+    Hi Mikhail, Today Matt upgraded the firmware for the ued epix camera.
+    At the moment det.raw.calib works but det.raw.image returns None for exp=detdaq02,run=569,
+    which suggests there is no geometry.  I?m guessing this may have happened because
+    the firmware version is part of the detector ?unique id? and the firmware changed today.
+    You can see that ?detnames -i? returns different id?s for runs 500 (a while ago) and 569 (today)
+    """
+    from psana import DataSource
+    ds = DataSource(exp='ueddaq02',run=569)
+    myrun = next(ds.runs())
+    det = myrun.Detector('epixquad')
+    for nevent,evt in enumerate(myrun.events()):
+        print(det.raw.calib(evt).shape,det.raw.image(evt))
+        if nevent>10: break
 
+def issue_2022_01_dd():
+    print('template')
 
 USAGE = '\nUsage:'\
       + '\n  python %s <test-name> <loglevel-e.g.-DEBUG-or-INFO>' % SCRNAME\
@@ -110,6 +127,7 @@ USAGE = '\nUsage:'\
       + '\n    2 - issue_2022_01_26 - test epix100 raw, calib, image and calib constants'\
       + '\n    3 - issue_2022_02_08 - test copy xtc2 file to .../public01/xtc/, epix100 common mode timing'\
       + '\n    4 - issue_2022_02_15 - test epix100 cpo - missing geometry'\
+      + '\n    5 - issue_2022_03_01 - test epixquad cpo - copy constants'\
 
 
 TNAME = sys.argv[1] if len(sys.argv)>1 else '0'
@@ -118,6 +136,7 @@ if   TNAME in  ('1',): issue_2022_01_21()
 elif TNAME in  ('2',): issue_2022_01_26()
 elif TNAME in  ('3',): issue_2022_02_08()
 elif TNAME in  ('4',): issue_2022_02_15()
+elif TNAME in  ('5',): issue_2022_03_01()
 elif TNAME in  ('0',): issue_2022_01_dd()
 else:
     print(USAGE)
