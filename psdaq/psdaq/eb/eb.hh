b93a49e2f9cc6c2694cf78390d21c7d459391142
Ric Claus <claus@slac.stanford.edu>
2022-03-10 18:08:13 -0800

    EB: Make event timeout compatible with control.py

--- psdaq/psdaq/eb/eb.hh
+++ psdaq/psdaq/eb/eb.hh
@@ -40,8 +40,10 @@ namespace Pds {
 
     const unsigned TICK_RATE      = 928500; // System clock rate in Hz
 
-    const unsigned TEB_TMO_MS     = 12000;  // Must be < MAX_LATENCY/TICK_RATE
-    const unsigned MEB_TMO_MS     = 12000;  // <= TEB_TMO_MS
+    // Keep *EB_TMO* below control.py's transition and phase 2 timeout
+    // Too low results in spurious timeouts and split events
+    const unsigned TEB_TMO_MS     = 7000;   // Must be < MAX_LATENCY/TICK_RATE
+    const unsigned MEB_TMO_MS     = 7000;   // <= TEB_TMO_MS
     const unsigned TEB_TR_BUFFERS = 128;    // # of TEB transition buffers
                                             // > TEB_TMO * SlowUpdate rate
     const unsigned MEB_TR_BUFFERS = 24;     // # of MEB transition buffers
