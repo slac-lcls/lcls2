b55187984fc12686d1e9219e2088dcd711f955ac
Christopher O'Grady <cpo@slac.stanford.edu>
2022-05-16 16:25:00 -0700

    make setup_env.sh more like the hutch versions

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,7 @@
 unset LD_LIBRARY_PATH
 unset PYTHONPATH
-source /cds/sw/ds/ana/conda2/inst/etc/profile.d/conda.sh
-# kludge: somehow conda activate is executing rogue stuff before
-# LD_LIBRARY_PATH is set to pick up the ugly location of epics
-export LD_LIBRARY_PATH=/cds/sw/ds/ana/conda2/inst/envs/ps-4.5.10/epics/lib/linux-x86_64:/cds/sw/ds/ana/conda2/inst/envs/ps-4.5.10/pcas/lib/linux-x86_64
+source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
+conda deactivate
 conda activate ps-4.5.10
 RELDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
