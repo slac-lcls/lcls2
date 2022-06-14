9d4b246624fcaf8f2d475e4a1c67ce5f0b2aa543
Christopher O'Grady <cpo@slac.stanford.edu>
2022-06-13 17:18:47 -0700

    update hutch cnf files

--- psdaq/psdaq/cnf/setup_env_rix.sh
+++ psdaq/psdaq/cnf/setup_env_rix.sh
@@ -1,68 +1,16 @@
 source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
 
 #conda deactivate
-#conda activate ps-4.5.11
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_051022"
+conda activate ps-4.5.11
+RELDIR="/cds/home/opr/rixopr/git/lcls2_051022"
 
-# in use until May 10, 2022
-conda activate ps-4.5.10
-RELDIR="/cds/home/opr/rixopr/git/lcls2_021522"
+# in use until May 23, 2022
+#conda activate ps-4.5.10
+#RELDIR="/cds/home/opr/rixopr/git/lcls2_021522"
 
 # in use until Feb 15, 2022
 #conda activate ps-4.5.5
 #RELDIR="/cds/home/opr/rixopr/git/lcls2_012022"
-
-# in use until Jan 20, 2022
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_120821"
-
-# in use until Dec. 8, 2021
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_111921"
-
-# in use until Nov 21, 2021
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_102821"
-
-# in use until Nov 2, 2021
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_100621"
-
-# in use until Oct 6, 2021
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_100421"
-
-# in use until Oct 4, 2021
-#conda activate ps-4.5.5
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_092121"
-
-# in use until Sept 22, 2021
-# conda activate ps-4.5.5
-# RELDIR="/cds/home/opr/rixopr/git/lcls2_090921"
-
-# in use until Sept. 15, 20201
-#conda activate ps-4.3.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_080421"
-
-#conda activate ps-4.5.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_070221_newrogue"
-
-# in use until July 3, 2021
-#conda activate ps-4.3.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_062121"
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_062121_pv"
-
-# in use until June 21, 2021
-#conda activate ps-4.3.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_061521"
-
-# in use until June 15, 2021
-#conda activate ps-4.3.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_060821"
-
-# in use until June 8, 2021
-#conda activate ps-4.3.2
-#RELDIR="/cds/home/opr/rixopr/git/lcls2_052821"
 
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
--- psdaq/psdaq/cnf/setup_env_tmo.sh
+++ psdaq/psdaq/cnf/setup_env_tmo.sh
@@ -1,20 +1,16 @@
 source /cds/sw/ds/ana/conda2/manage/bin/psconda.sh
 conda deactivate
 
-conda activate ps-4.5.10
-RELDIR="/cds/home/opr/tmoopr/git/lcls2_041322"
+conda activate ps-4.5.13
+RELDIR="/cds/home/opr/tmoopr/git/lcls2_060722"
+
+# official running until June 7, 2022
+#conda activate ps-4.5.10
+#RELDIR="/cds/home/opr/tmoopr/git/lcls2_041322"
 
 # official running until Apr 13, 2022
 #conda activate ps-4.5.10
 #RELDIR="/cds/home/opr/tmoopr/git/lcls2_022222"
-
-# official running until Feb 22, 2022
-#conda activate ps-4.5.10
-#RELDIR="/cds/home/opr/tmoopr/git/lcls2_021522"
-
-# official running until Feb 15, 2022
-#conda activate ps-4.5.10
-#RELDIR="/cds/home/opr/tmoopr/git/lcls2_012522"
 
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
