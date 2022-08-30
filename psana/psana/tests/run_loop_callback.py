f97b1745c44c4b4b303f77a2f6b3760e986ee97b
monarin <monarin@gmail.com>
2022-08-30 13:58:46 -0700

    update smd_callback example

--- psana/psana/tests/run_loop_callback.py
+++ psana/psana/tests/run_loop_callback.py
@@ -1,30 +1,18 @@
 from psana import DataSource 
 import os
 
-def filter_fn(evt):
-    if evt.timestamp % 2 == 0:
-        return True
-    else:
-        return False
-
+# - Keep timestamp callback
+# - Eliminate destination and filter_fn
+# - Adding EpicStore and ScanStore
 def smd_callback(smd_ds):
     for i, evt in enumerate(smd_ds.events()):
         #smd_ds.set_destination(evt, rank=3)
-        flag_yield = False
-        if evt.service() != 12: 
-            flag_yield = True
-        else:
-            if evt.timestamp % 2 == 0:
-                flag_yield = True
-        
-        if flag_yield:
+        if evt.timestamp % 2 == 0:
             yield evt
 
 def run_test_loop_callback():
     xtc_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.tmp')
     ds = DataSource(exp='xpptut15', run=1, dir=xtc_dir,
-            #filter=filter_fn,
-            batch_size=1,
             smd_callback=smd_callback
             )
     for run in ds.runs():
