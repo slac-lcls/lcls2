3ecda544d062529ea44e6b746ead62a61c49a638
msdubrovin <msdubrovin@gmail.com>
2022-09-22 08:48:55 -0700

    add a bunch of DET_* parameters in a single place

--- psana/psana/detector/dir_root.py
+++ psana/psana/detector/dir_root.py
@@ -2,6 +2,10 @@
    /reg/g/psdm  # lcls
    /cds/group/psdm  # lcls2
    /sdf/group/psdm/etc  # s3df
+
+
+from psana.detector.dir_root import DIR_REPO_EPIX10KA, DIR_LOG_AT_START, HOSTNAME, ...
+from psana.detector.dir_root import DIR_ROOT
 """
 import os
 
@@ -12,8 +16,11 @@ if HOSTNAME is None:
 print('TEST dir_root.HOSTNAME %s' % HOSTNAME)
 
 DIR_ROOT = os.getenv('DIR_PSDM')  # /cds/group/psdm
-DIR_LOG_AT_START = os.path.join(DIR_ROOT, 'logs/atstart/')  # /cds/group/psdm/logs/atstart
-CALIB_REPO_EPIX10KA = os.path.join(DIR_ROOT, 'detector/gains2/epix10ka/panels')
+DIR_LOG_AT_START    = os.path.join(DIR_ROOT, 'detector/logs/atstart/')          # /cds/group/psdm/logs/atstart
+DIR_LOG_CALIBMAN    = os.path.join(DIR_ROOT, 'detector/logs/calibman/lcls2')    # /cds/group/psdm/logs/calibman/lcls2
+DIR_REPO_EPIX10KA   = os.path.join(DIR_ROOT, 'detector/gains2/epix10ka/panels') # /cds/group/psdm/detector/gains2/epix10ka/panels
+DIR_REPO_DARK_PROC  = os.path.join(DIR_ROOT, 'detector/calib2')                 # /cds/group/psdm/detector/calib2
+DIR_DATA_TEST       = os.path.join(DIR_ROOT, 'detector/data2_test')             # /cds/group/psdm/detector/data2_test/
 
 #DIR_REPO = os.path.join(DIR_ROOT, 'detector/calib/constants/')
 #DIR_ROOT_DATA = '/reg/d/psdm'  # dcs
