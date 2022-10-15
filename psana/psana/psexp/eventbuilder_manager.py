25cfc8d5e2022d434752e6a3ba124f876959bd3a
monarin <monarin@gmail.com>
2022-10-14 18:02:26 -0700

    only use smalldata callback loop (slow) when given

--- psana/psana/psexp/eventbuilder_manager.py
+++ psana/psana/psexp/eventbuilder_manager.py
@@ -16,24 +16,31 @@ class EventBuilderManager(object):
                                            dsparms=dsparms,
                                            run=run,
                                            prometheus_counter=c_filter)
-        self.run_smd        = RunSmallData(run, self.eb)
+        self.run_smd        = RunSmallData(run, self.eb)            # only used by smalldata callback
 
     def batches(self):
         while True: 
-            # Collects list of proxy events to be converted to batches.
-            # Note that we are persistently calling smd_callback until there's nothing
-            # left in all views used by EventBuilder. From this while/for loops, we 
-            # either gets transitions from SmdDataSource and/or L1 from the callback.
-            while self.run_smd.proxy_events == [] and self.eb.has_more():
-                for evt in self.dsparms.smd_callback(self.run_smd):
-                    self.run_smd.proxy_events.append(evt._proxy_evt)
-            
-            if not self.run_smd.proxy_events:
-                break
+            # This eiter calls user-defined smalldata callback, which loops
+            # over smd events or skips (faster). To enable detector inteface 
+            # for smd events, evt.complete() (slow) is called. 
+            if self.dsparms.smd_callback == 0:
+                batch_dict, step_dict = self.eb.build()
+                if self.eb.nevents==0 and self.eb.nsteps==0: break
+            else:
+                # Collects list of proxy events to be converted to batches.
+                # Note that we are persistently calling smd_callback until there's nothing
+                # left in all views used by EventBuilder. From this while/for loops, we 
+                # either gets transitions from SmdDataSource and/or L1 from the callback.
+                while self.run_smd.proxy_events == [] and self.eb.has_more():
+                    for evt in self.dsparms.smd_callback(self.run_smd):
+                        self.run_smd.proxy_events.append(evt._proxy_evt)
+                
+                if not self.run_smd.proxy_events:
+                    break
 
-            # Generate a bytearray representations of all the events in a batch.
-            batch_dict, step_dict = self.eb.gen_bytearray_batch(self.run_smd.proxy_events)
-            self.run_smd.proxy_events = []
+                # Generate a bytearray representations of all the events in a batch.
+                batch_dict, step_dict = self.eb.gen_bytearray_batch(self.run_smd.proxy_events)
+                self.run_smd.proxy_events = []
             
             yield batch_dict, step_dict
 
