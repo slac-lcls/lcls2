8712fa684c5972219c0f39b522d6b17c9408e0f8
Ric Claus <claus@slac.stanford.edu>
2022-11-18 16:07:20 -0800

    Define and use PROCMGR_EXPORT

--- setup_env.sh
+++ setup_env.sh
@@ -9,6 +9,7 @@ pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.ver
 export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages
 # for procmgr
 export TESTRELDIR=$RELDIR/install
+export PROCMGR_EXPORT=RDMAV_FORK_SAFE=1,RDMAV_HUGEPAGES_SAFE=1  # See fi_verbs man page regarding fork()
 
 # needed by Ric to get correct libfabric man pages
 export MANPATH=$CONDA_PREFIX/share/man${MANPATH:+:${MANPATH}}
