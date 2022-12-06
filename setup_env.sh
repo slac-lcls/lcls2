2ff977319c20a615edaf5175ec235985fce02d62
Christopher O'Grady <cpo@slac.stanford.edu>
2022-12-05 16:39:25 -0800

    add missing env vars to setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -14,5 +14,20 @@ export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages
 export TESTRELDIR=$RELDIR/install
 export PROCMGR_EXPORT=RDMAV_FORK_SAFE=1,RDMAV_HUGEPAGES_SAFE=1  # See fi_verbs man page regarding fork()
 
+# cpo: seems that in more recent versions blas is creating many threads
+export OPENBLAS_NUM_THREADS=1
+# cpo: getting intermittent file-locking issue on ffb, so try this
+export HDF5_USE_FILE_LOCKING=FALSE
+# for libfabric. decreases performance a little, but allows forking
+export RDMAV_FORK_SAFE=1
+export RDMAV_HUGEPAGES_SAFE=1
+# Mikhail: root of psdm directories
+export DIR_PSDM=/cds/group/psdm
+
+# cpo: workaround a qt bug which may no longer be there (dec 5, 2022)
+if [ ! -d /usr/share/X11/xkb ]; then
+    export QT_XKB_CONFIG_ROOT=${CONDA_PREFIX}/lib
+fi
+
 # needed by Ric to get correct libfabric man pages
 export MANPATH=$CONDA_PREFIX/share/man${MANPATH:+:${MANPATH}}
