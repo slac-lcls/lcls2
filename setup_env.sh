ea7cc6d719cfe8fd82dd5d9dc54ef8fe05468b8f
Valerio Mariani <valmar@slac.stanford.edu>
2023-01-08 19:02:16 -0800

    Adapted setup_env.sh to use a spack environment

--- setup_env.sh
+++ setup_env.sh
@@ -2,14 +2,18 @@ unset LD_LIBRARY_PATH
 unset PYTHONPATH
 
 # these three lines should be customized for the site
-source /cds/sw/ds/ana/conda2-v2/inst/etc/profile.d/conda.sh
-export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda2/inst/envs/
-conda activate ps-4.5.24
+#source /cds/sw/ds/ana/conda2-v2/inst/etc/profile.d/conda.sh
+#export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda2/inst/envs/
+#conda activate ps-4.5.24
+
+source ~/.local/spack/activate_lcls-ii-dev.sh
+export CC=$(which gcc)
+export CXX=(which cxx)
 
 RELDIR="$( cd "$( dirname $(readlink -f "${BASH_SOURCE[0]}") )" && pwd )"
 export PATH=$RELDIR/install/bin:${PATH}
 pyver=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
-export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages
+export PYTHONPATH=$RELDIR/install/lib/python$pyver/site-packages:$PYTHONPATH
 # for procmgr
 export TESTRELDIR=$RELDIR/install
 export PROCMGR_EXPORT=RDMAV_FORK_SAFE=1,RDMAV_HUGEPAGES_SAFE=1  # See fi_verbs man page regarding fork()
