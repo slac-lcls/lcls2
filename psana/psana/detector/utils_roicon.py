2be30aec4c89d79fb54d78b7ed86166287dce012
msdubrovin <msdubrovin@gmail.com>
2023-02-13 09:52:13 -0800

    replace print with logger.info

--- psana/psana/detector/utils_roicon.py
+++ psana/psana/detector/utils_roicon.py
@@ -25,7 +25,7 @@ def mask_test_ring(shape):
 def image_of_sensors(gfname, afname=None, ofname='mask.txt', mbits=0xffff, dotest=False, save_plots=False, **kwargs):
     """ Makes and plot an image or mask of sensors for geometry file
     """
-    print('Geometry file: %s' % gfname)
+    logger.info('Geometry file: %s' % gfname)
 
     geo = GeometryAccess(gfname, 0)
     ix, iy = geo.get_pixel_coord_indexes(**kwargs)
@@ -45,14 +45,14 @@ def image_of_sensors(gfname, afname=None, ofname='mask.txt', mbits=0xffff, dotes
     if afname is not None:
         mean = np.mean(arr)
         std  = np.std(arr)
-        print('Input array mean=%f   std=%f' % (mean, std))
+        logger.info('Input array mean=%f   std=%f' % (mean, std))
         amp_range=[mean-2*std, mean+2*std]
 
     mask = geo.get_pixel_mask(mbits=mbits)
     mask.shape = ix.shape
 
     if mbits: arr *= mask
-    print('ix, iy, W shape:', ix.shape, iy.shape, arr.shape)
+    logger.info('shape ix: %s  iy: %s  W: %s' % (str(ix.shape), str(iy.shape), str(arr.shape)))
 
     img = img_from_pixel_arrays(ix, iy, W=arr)
 
@@ -68,7 +68,7 @@ def image_of_sensors(gfname, afname=None, ofname='mask.txt', mbits=0xffff, dotes
     if ofext == '.npy': np.save(ofname, img)
     else              : np.savetxt(ofname, img, fmt='%d', delimiter=' ')
 
-    print('Image or mask of sensors is saved in the file %s' % ofname)
+    logger.info('Image or mask of sensors is saved in the file %s' % ofname)
 
 
 def roi_mask_editor(ifname='image.txt', mfname='mask', mbits=0xffff):
@@ -78,9 +78,9 @@ def roi_mask_editor(ifname='image.txt', mfname='mask', mbits=0xffff):
 
     #from subprocess import getoutput
     #cmd = 'med -i %s -m %s' % (ifname, mfname)
-    #print('Start process with mask editor by the command: %s' % cmd)
+    #logger.info('Start process with mask editor by the command: %s' % cmd)
     #output = getoutput(cmd)
-    #print('%s' % output)
+    #logger.info('%s' % output)
 
 
 def roi_mask_to_ndarray(gfname, ifname='roi-mask.txt', ofname='mask-nda.txt', mbits=0xffff, save_plots=False, **kwargs):
@@ -92,23 +92,23 @@ def roi_mask_to_ndarray(gfname, ifname='roi-mask.txt', ofname='mask-nda.txt', mb
     ifext = os.path.splitext(ifname)[1]
     ofext = os.path.splitext(ofname)[1]
 
-    print('1. Load ROI mask from file: %s' % ifname)
+    logger.info('1. Load ROI mask from file: %s' % ifname)
     mask_roi = np.load(ifname) if ifext == '.npy' else np.loadtxt(ifname, dtype=np.uint16)
 
-    print('2. Define geometry from file: %s' % gfname)
+    logger.info('2. Define geometry from file: %s' % gfname)
     geo = GeometryAccess(gfname, 0)
     ix, iy = geo.get_pixel_coord_indexes(**kwargs)
     reshape_to_3d(ix)
     reshape_to_3d(iy)
-    print('3. Check shapes of pixel image-index arrays ix, iy:', ix.shape, iy.shape)
+    logger.info('3. Check shapes of pixel image-index arrays ix: %s iy: %s' %  str(ix.shape), str(iy.shape)))
 
-    print('4. Plot image of the mask', info_ndarr(mask_roi, 'mask_roi'))
+    logger.info('4. Plot image of the mask %s' % info_ndarr(mask_roi, 'mask_roi'))
     axim = gr.plotImageLarge(mask_roi,amp_range=[0,1], title='Image of the mask')
     gr.save(fname='img-mask2d.png', do_save=save_plots)
     gr.move(400,10)
     gr.show()
 
-    print('5. Evaluate ndarray with mask')
+    logger.info('5. Evaluate ndarray with mask')
     mask_nda = um.convert_mask2d_to_ndarray_using_pixel_coord_indexes(mask_roi, ix, iy)
     #mask_nda = um.convert_mask2d_to_ndarray_using_geo(mask_roi, geo, **kwargs)  # for test only
     #mask_nda = um.convert_mask2d_to_ndarray_using_geometry_file(mask_roi, gfname, **kwargs)  # for test only
@@ -119,20 +119,20 @@ def roi_mask_to_ndarray(gfname, ifname='roi-mask.txt', ofname='mask-nda.txt', mb
         reshape_to_3d(mask_geo)
         mask_nda *= mask_geo
 
-    print('6. Cross-checks: shape of mask_nda: %s, mask_nda.size=%d, ix.size=%d ' % \
+    logger.info('6. Cross-checks: shape of mask_nda: %s, mask_nda.size=%d, ix.size=%d ' % \
           (mask_nda.shape, mask_nda.size, ix.size))
 
-    print('7. Save mask for ndarray in the file %s' % ofname)
+    logger.info('7. Save mask for ndarray in the file %s' % ofname)
     if ofext == '.npy': np.save(ofname, mask_nda)
     else              :
         mask_nda.shape = [ix.size//ix.shape[-1],ix.shape[-1]]
-        print('7a. Re-shape for saving in txt to 2-d:', mask_nda.shape)
+        logger.info('7a. Re-shape for saving in txt to 2-d: %s' % str(mask_nda.shape))
         np.savetxt(ofname, mask_nda, fmt='%d', delimiter=' ')
 
-    print('8. Test new mask-ndarray to generate image (CLOSE image to continue)')
-    print(info_ndarr(mask_nda, 'mask_nda'))
+    logger.info('8. Test new mask-ndarray to generate image (CLOSE image to continue)')
+    logger.info(info_ndarr(mask_nda, 'mask_nda'))
     mask_nda.shape = [ix.size//ix.shape[-1], ix.shape[-1]]
-    print(info_ndarr(mask_nda, 'reshape for 2-d image of panels'))
+    logger.info(info_ndarr(mask_nda, 'reshape for 2-d image of panels'))
     axim = gr.plotImageLarge(mask_nda, amp_range=[0,1], figsize=(6,12), title='mask as ndarray')
     gr.save(fname='img-mask-ndarr.png', do_save=save_plots)
     gr.move(400,10)
@@ -148,10 +148,9 @@ def roi_mask_to_ndarray(gfname, ifname='roi-mask.txt', ofname='mask-nda.txt', mb
 def do_main(parser):
     nspace = parser.parse_args()
     proc = nspace.args # [0]
-
     if   proc == '1': image_of_sensors   (nspace.gfname, nspace.afname, nspace.ifname, nspace.cbits, nspace.dotest, nspace.save, **eval(nspace.kwargs))
     elif proc == '2': roi_mask_editor    (nspace.ifname, nspace.mfname)
     elif proc == '3': roi_mask_to_ndarray(nspace.gfname, nspace.mfname, nspace.nfname, nspace.cbits, nspace.save, **eval(nspace.kwargs))
-    else: print('Non-recognized process number "%s"; implemented options: 1, 2, or 3' % proc)
+    else: logger.info('Non-recognized process number "%s"; implemented options: 1, 2, or 3' % proc)
 
 # EOF
