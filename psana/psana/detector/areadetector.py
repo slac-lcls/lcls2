10e723c5a00eb5edb5bb56287b35dadd5e76c079
msdubrovin <msdubrovin@gmail.com>
2023-03-20 08:47:55 -0700

    remove parameter stci_bits

--- psana/psana/detector/areadetector.py
+++ psana/psana/detector/areadetector.py
@@ -32,12 +32,12 @@ Usage::
   a = o._mask_calib_or_default(dtype=DTYPE_MASK)
   a = o._mask_from_status(status_bits=(1<<64)-1,\
                           stextra_bits=(1<<64)-1,\
-                          stci_bits=(1<<64)-1, dtype=DTYPE_MASK, **kwa) # gain_range_inds=(0,1,2,3,4)
+                          dtype=DTYPE_MASK, **kwa) # gain_range_inds=(0,1,2,3,4)
   a = o._mask_neighbors(mask, rad=9, ptrn='r')
   a = o._mask_edges(width=0, edge_rows=1, edge_cols=1, dtype=DTYPE_MASK, **kwa)
   a = o._mask_center(wcenter=0, center_rows=1, center_cols=1, dtype=DTYPE_MASK, **kwa)
   a = o._mask_comb(**kwa) # the same as _mask but w/o caching
-  a = o._mask(status=True, status_bits=0xffff, stextra_bits=(1<<64)-1, stci_bits=(1<<64)-1, gain_range_inds=(0,1,2,3,4),\
+  a = o._mask(status=True, status_bits=0xffff, stextra_bits=(1<<64)-1, gain_range_inds=(0,1,2,3,4),\
               neighbors=False, rad=3, ptrn='r',\
               edges=True, width=0, edge_rows=10, edge_cols=5,\
               center=True, wcenter=0, center_rows=5, center_cols=3,\
@@ -226,7 +226,6 @@ class AreaDetector(DetectorImpl):
            - status   : bool : True  - mask from pixel_status constants,
                                        kwa: status_bits=0xffff - status bits for calib-type pixel_status to use in mask,
                                        kwa: stextra_bits=(1<<64)-1 - status bits for calib-type status_extra to use in mask.
-                                       kwa: stci_bits=(1<<64)-1 - status bits for calib-type status_ci to use in mask.
                                        Status bits show why pixel is considered as bad.
                                        Content of the bitword depends on detector and code version.
                                        It is wise to exclude pixels with any bad status by setting status_bits=(1<<64)-1.
