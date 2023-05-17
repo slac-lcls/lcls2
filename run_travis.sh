65717198914c94b9c73503b952522a9a3b150e09
Christopher O'Grady <cpo@slac.stanford.edu>
2023-05-16 17:12:02 -0700

    debug

--- run_travis.sh
+++ run_travis.sh
@@ -13,9 +13,10 @@ if [[ $OS == linux ]]; then
   #  time curl -s "https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8" | wc
   #  echo "done checking calibdb access"
   #done
+  pip install requests
+  python -u -c "while 1: import requests; print('*** requesting'); requests.get('https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8',None,timeout=60); print('*** done fetch')"
   source activate $CONDA_ENV
 
-  #python -u -c "while 1: import requests; print('*** requesting'); requests.get('https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8',None,timeout=60); print('*** done fetch')"
 
   cd "$(dirname "${BASH_SOURCE[0]}")"
   export PATH="$PWD/install/bin:$PATH"
