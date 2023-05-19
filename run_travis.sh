00695603f8d9cbac862d56db60567c49dfc05902
Christopher O'Grady <cpo@slac.stanford.edu>
2023-05-19 08:22:56 -0700

    debug

--- run_travis.sh
+++ run_travis.sh
@@ -1,41 +1,11 @@
 #!/bin/bash
 
-OS=${1:-linux}
-
 set -e
 
-
-export LCLS_TRAVIS=1
-if [[ $OS == linux ]]; then
-  #while true
-  #do
-  #  echo "ci-debug branch: checking calibdb access"
-  #  time curl -s "https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8" | wc
-  #  echo "done checking calibdb access"
-  #done
-  #sudo apt install pip
-  #pip install requests
-  #python -u -c "import requests; print('*** requests version:',requests.__version__)"
-  #python -u -c "import urllib.request; print('*** urllib version:',urllib.request.__version__)"
-  #python -u -c "import requests; print(requests)"
-  #python -u -c "import urllib; print(urllib)"
-  #python -u -c "while 1: import requests; print('*** requesting'); requests.get('https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8',None,timeout=60); print('*** done fetch')"
-  source activate $CONDA_ENV
-
-
-  cd "$(dirname "${BASH_SOURCE[0]}")"
-  export PATH="$PWD/install/bin:$PATH"
-
-  PYVER=$(python -c "import sys; print(str(sys.version_info.major)+'.'+str(sys.version_info.minor))")
-  export PYTHONPATH="$PWD/install/lib/python$PYVER/site-packages"
-  export TESTRELDIR="$PWD/install"
-
-  pip install pytest-timeout
-  ./build_all.sh -d -p install
-  pytest -s --capture=no psana/psana/tests
-elif [[ $OS == osx ]]; then
-  echo "ignore macos"
-else
-  echo "Unknown OS type: ${OS}"
-  exit 1
-fi
+#while true
+#do
+#  echo "ci-debug branch: checking calibdb access"
+#  time curl -s "https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8" | wc
+#  echo "done checking calibdb access"
+#done
+python -u -c "while 1: import requests; print('*** requesting'); requests.get('https://pswww.slac.stanford.edu/calib_ws/cdb_ueddaq02/gridfs/6035d64545db0b188f7c78e8',None,timeout=60); print('*** done fetch')"
