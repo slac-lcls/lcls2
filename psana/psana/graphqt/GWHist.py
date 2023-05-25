7da0017683ddabd94680efff9a05a66135beb25f
msdubrovin <msdubrovin@gmail.com>
2023-05-25 15:54:41 -0700

    add new module

---
+++ psana/psana/graphqt/GWHist.py
@@ -0,0 +1,140 @@
+
+"""
+Class :py:class:`GWHist`
+========================
+
+Usage ::
+    from psana.graphqt.GWHist import *
+
+    rs = QRectF(0, 0, 1, 1)
+    s = QGraphicsScene(rs)
+    v = QGraphicsView(s)
+    v.setGeometry(20, 20, 600, 400)
+
+    v.fitInView(rs, Qt.IgnoreAspectRatio)
+
+    ruler1 = GWHist(v, 'L')
+    hist1.remove()
+
+See:
+    - :class:`FWView`
+    - :class:`FWViewImage`
+    - :class:`QWSpectrum`
+    - `lcls2 on github <https://github.com/slac-lcls/lcls2>`_.
+
+This software was developed for the LCLS2 project.
+If you use all or part of it, please give an appropriate acknowledgment.
+
+Created on 2020-11-02 by Mikhail Dubrovin
+"""
+
+import sip
+from PyQt5.QtGui import QPen, QBrush, QColor, QPainterPath
+from PyQt5.QtCore import Qt, QPointF
+import numpy as np
+
+class GWHist():
+
+    def __init__(self, view, **kwargs):
+
+        self.view   = view
+        self.scene  = view.scene()
+        #self.rect   = self.scene.sceneRect()
+
+        color       = kwargs.get('color', QColor(Qt.black)) # color for default pen (histogram sline)
+        self.pen_def = QPen(color, 0, Qt.SolidLine)
+        self.hbins  = kwargs.get('hbins', None)             # histogram container HBins
+        self.orient = kwargs.get('orient','H')              # histogram orientation H or V
+        self.pen    = kwargs.get('pen',   self.pen_def)     # histogram line pen
+        self.brush  = kwargs.get('brush', QBrush())         # histogram filling brush
+        self.zvalue = kwargs.get('zvalue',10)               # z value for heirarchial visibility
+
+        self.pen.setCosmetic(self.pen == self.pen_def)
+
+        self.path = None
+        self.path_item = None
+        self.lst_of_items = []
+
+        if self.hbins is None: self.add_test()
+        else:                  self.add_hist()
+
+
+    def remove(self):
+        if not sip.isdeleted(self.scene):
+          for item in self.lst_of_items:
+            self.scene.removeItem(item)
+        self.lst_of_items=[]
+        #self.scene.removeItem(self.path_item)
+        #self.scene.destroyItemGroup(self.item_group)
+
+
+    def update(self, hbins):
+        self.remove()
+        self.hbins = hbins
+        self.add_hist()
+
+
+    def __del__(self):
+        self.remove()
+
+
+    def add_hist(self):
+
+        if self.path_item is not None: self.scene.removeItem(self.path_item)
+
+        edges = self.hbins.binedges() # n+1
+        values = self.hbins.bin_data() #dtype=np.float64)
+
+        self.path = QPainterPath() #QPointF(edges[0], 0))
+
+        #self.path.closeSubpath()
+        if self.orient == 'V':
+          self.path.moveTo(QPointF(0, edges[0]))
+          for el, er, v in zip(edges[:-1], edges[1:], values):
+            self.path.lineTo(QPointF(v, el))
+            self.path.lineTo(QPointF(v, er))
+          self.path.lineTo(QPointF(0, edges[-1]))
+
+        else:
+          self.path.moveTo(QPointF(edges[0], 0))
+          for el, er, v in zip(edges[:-1], edges[1:], values):
+            self.path.lineTo(QPointF(el,v))
+            self.path.lineTo(QPointF(er,v))
+          self.path.lineTo(QPointF(edges[-1], 0.))
+
+        # add path with hist lines to scene
+        self.lst_of_items=[]
+        self.path_item = self.scene.addPath(self.path, self.pen, self.brush)
+        self.path_item.setZValue(self.zvalue)
+        self.lst_of_items.append(self.path_item)
+
+        #print('path_item is created')
+
+#def bifurgauss(x, *p):
+#    """Bifurcated Gaussian - assumes ydata = f(xdata, *params) + eps."""
+#    a0, x0, s1, s2 = p
+#    sig = np.select((x<x0,), (s1,), default=s2)
+#    return a0 * np.exp(-0.5*((x-x0)/sig)**2)
+
+def gauss(x, *p):
+    a0, x0, s = p
+    return a0 * np.exp(-0.5*((x-x0)/s)**2)
+
+
+def test_histogram():
+    import psana.pyalgos.generic.NDArrGenerators as ag
+    from psana.pyalgos.generic.HBins import HBins
+    xmin, xmax = 100, 1100
+    nbins = 1000
+    a0, x0, s = 80, 400, 40
+    #x = np.arange(-5,5,.01)
+    x = np.linspace(xmin, xmax, num=nbins, endpoint=True)
+    f = ag.random_standard((nbins,), mu=50, sigma=10, dtype=ag.np.float64)
+    f += gauss(x, a0, x0, s) + gauss(x, a0/3, x0+50, s+50)
+    f[10]=10000
+    f[11]=5000
+    hb = HBins((xmin, xmax), nbins=nbins)
+    hb.set_bin_data(f, dtype=ag.np.float64)
+    return hb
+
+# EOF
---
+++ psana/psana/graphqt/GWSpectrum.py
@@ -0,0 +1,244 @@
+
+"""Class :py:class:`GWSpectrum` is a QWidget with histogram, two axes, and color bar
+====================================================================================
+
+Usage ::
+
+    # Run test: python lcls2/psana/psana/graphqt/GWSpectrum.py
+
+    from psana.graphqt.GWSpectrum import GWSpectrum
+    w = GWSpectrum()
+
+Created on 2021-06-22 by Mikhail Dubrovin
+"""
+import logging
+logger = logging.getLogger(__name__)
+
+from psana.graphqt.GWViewHist import * # GWViewHist # , test_histogram
+from psana.graphqt.GWViewAxis import GWViewAxis
+from psana.graphqt.GWViewColorBar import GWViewColorBar
+import psana.graphqt.ColorTable as ct
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QTextEdit
+from PyQt5.QtCore import Qt, pyqtSignal, QRectF
+from psana.pyalgos.generic.NDArrGenerators import test_image
+from psana.graphqt.CMConfigParameters import cp
+
+
+class GWSpectrum(QWidget):
+    """QWidget for Image Viewer"""
+    histogram_scene_rect_changed = pyqtSignal('QRectF')
+
+    def __init__(self, **kwargs):
+
+        parent= kwargs.get('parent', None)
+        image = kwargs.get('image', test_image())
+        ctab  = kwargs.get('ctab', ct.color_table_default())
+        nbins = kwargs.get('nbins', 1000)
+        amin  = kwargs.get('amin', None)
+        amax  = kwargs.get('amax', None)
+        frmin = kwargs.get('frmin', 0.0001)
+        frmax = kwargs.get('frmax', 0.9999)
+        edgemode = kwargs.get('edgemode', 0)
+        signal_fast = kwargs.get('signal_fast', True)
+
+        QWidget.__init__(self, parent)
+
+        cp.ivspectrum = self
+
+        self.whi = GWViewHist(parent=self, rscene=None, origin='DR', scale_ctl='V',\
+                              fgcolor='yellow', bgcolor='dark', orient='V', signal_fast=signal_fast, hbins=None)
+        self.whi.set_histogram_from_arr(image, nbins, amin, amax, frmin, frmax, edgemode, update_hblimits=True)
+        self.wcbar = GWViewColorBar(self, coltab=ctab, orient='V')
+
+        self.rsh_old = None
+        r = self.whi.scene_rect()
+        logger.debug('self.whi.scene_rect: %s' % qu.info_rect_xywh(r))
+
+        rscx = QRectF(r.x(), 0, r.width(), 1)
+        rscy = QRectF(0, r.y(), 1, r.height())
+
+        self.wax = GWViewAxis(None, rscx, side='U', origin='UR', scale_ctl=True, wwidth=30, wlength=200, signal_fast=signal_fast, label_rot=20)
+        self.way = GWViewAxis(None, rscy, side='L', origin='DL', scale_ctl=True, wwidth=60, wlength=200, signal_fast=signal_fast, label_rot=-70)
+
+        self.but_reset = QPushButton('Reset')
+        self.edi_info = QTextEdit('Info')
+
+        self.box = QGridLayout()
+        self.box.setSpacing(0)
+        self.box.setVerticalSpacing(0)
+        self.box.setHorizontalSpacing(0)
+        self.box.addWidget(self.edi_info,   0,  0,  1, 11)
+        self.box.addWidget(self.way,        1, 10,  9,  1)
+        self.box.addWidget(self.whi,        1,  0,  9,  9)
+        self.box.addWidget(self.wax,       10,  0,  1,  9)
+        self.box.addWidget(self.wcbar,      1,  9,  9,  1)
+        self.box.addWidget(self.but_reset, 10,  9,  1,  2, alignment=Qt.AlignCenter)
+        self.setLayout(self.box)
+
+        self.set_tool_tips()
+        self.set_style()
+
+        self.connect_scene_rect_changed()
+        self.but_reset.clicked.connect(self.on_but_reset)
+
+
+    def set_signal_fast(self, is_fast=True):
+        self.whi.signal_fast = is_fast
+        self.wax.signal_fast = is_fast
+        self.way.signal_fast = is_fast
+
+
+    def connect_scene_rect_changed(self):
+        self.whi.connect_scene_rect_changed(self.on_whi_scene_rect_changed)
+        self.wax.connect_scene_rect_changed(self.on_wax_scene_rect_changed)
+        self.way.connect_scene_rect_changed(self.on_way_scene_rect_changed)
+
+
+    def disconnect_scene_rect_changed(self):
+        self.whi.disconnect_scene_rect_changed(self.on_whi_scene_rect_changed)
+        self.wax.disconnect_scene_rect_changed(self.on_wax_scene_rect_changed)
+        self.way.disconnect_scene_rect_changed(self.on_way_scene_rect_changed)
+
+
+    def on_but_reset(self):
+        logger.debug('on_but_reset whi.scene_rect: %s' % qu.info_rect_xywh(self.whi.scene_rect()))
+        self.wax.reset_scene_rect()
+        self.way.reset_scene_rect()
+        self.whi.reset_scene_rect()
+
+
+    def on_whi_scene_rect_changed(self, r):
+        logger.debug('on_whi_scene_rect_changed: %s' % qu.info_rect_xywh(r))
+        self.wax.set_axis_limits(r.x(), r.x()+r.width())   # fit_in_view(QRectF(r.x(), 0, r.width(), 1))
+        self.way.set_axis_limits(r.y(), r.y()+r.height())  # fit_in_view(QRectF(0, r.y(), 1, r.height()))
+        self.update_info()
+        self.emit_signal_if_histogram_scene_rect_changed()
+
+
+    def on_wax_scene_rect_changed(self, r):
+        #logger.debug('on_wax_scene_rect_changed: %s' % qu.info_rect_xywh(r))
+        rs = self.whi.scene_rect()
+        self.whi.fit_in_view(QRectF(r.x(), rs.y(), r.width(), rs.height()))
+        self.emit_signal_if_histogram_scene_rect_changed()
+
+
+    def on_way_scene_rect_changed(self, r):
+        #logger.debug('on_way_scene_rect_changed: %s' % qu.info_rect_xywh(r))
+        rs = self.whi.scene_rect()  # scene().sceneRect()
+        self.whi.fit_in_view(QRectF(rs.x(), r.y(), rs.width(), r.height()))
+        self.update_info()
+        self.emit_signal_if_histogram_scene_rect_changed()
+
+
+    def emit_signal_if_histogram_scene_rect_changed(self):
+        """Checks if scene rect have changed and submits signal with new rect."""
+        rs = self.whi.scene_rect()
+        if rs != self.rsh_old:
+            self.rsh_old = rs
+            self.histogram_scene_rect_changed.emit(rs)
+
+
+    def connect_histogram_scene_rect_changed(self, recip):
+        self.histogram_scene_rect_changed.connect(recip)
+
+
+    def disconnect_histogram_scene_rect_changed(self, recip):
+        self.histogram_scene_rect_changed.disconnect(recip)
+
+
+    def update_info(self):
+        """update text information in stat box."""
+        hb = self.whi.hbins
+        r = self.whi.scene_rect()  # scene().sceneRect()
+        vmin, vmax = r.y(), r.y()+r.height()
+        s = 'spectrum min: %d max: %d' % (vmin, vmax)
+        if hb is not None:
+          resp = hb.histogram_statistics(vmin, vmax)
+          if resp is not None:
+            mean, rms, err_mean, err_rms, neff, skew, kurt, err_err, sum_w, ibeg, iend = resp
+            s += '\nentries: %d  nbins: %d [%d:%d]' % (sum_w, iend-ibeg, ibeg, iend)\
+              + u'\nmean: %.3f \u00B1 %.3f\nrms: %.3f \u00B1 %.3f' % (mean, err_mean, rms, err_rms)\
+              + u'\n\u03B31 skew: %.3f  \u03B32 kurt: %.3f' % (skew, kurt)
+        self.edi_info.setText(s)
+
+
+    def set_tool_tips(self):
+        self.whi.setToolTip('Spectral intensity\ndistribution')
+        self.wax.setToolTip('Spectral\nintensity')
+        self.way.setToolTip('Spectral\nvalue')
+        self.edi_info.setToolTip('Spectral statistics')
+        self.but_reset.setToolTip('Reset to default\nspectrum')
+        self.wcbar.setToolTip('Color bar for color\nto intensity conversion\nclick on it to select another')
+
+
+    def set_style(self):
+        self.layout().setContentsMargins(0,0,0,0)
+        self.wcbar.setFixedWidth(25)
+        self.edi_info.setMaximumHeight(80)
+
+
+    def set_spectrum_from_arr(self, arr, nbins=1000, amin=None, amax=None, frmin=0.0001, frmax=0.9999, edgemode=0, update_hblimits=True):
+        """shotcut"""
+        logger.info('set_spectrum_from_arr')
+        self.whi.set_histogram_from_arr(arr, nbins, amin, amax, frmin, frmax, edgemode, update_hblimits)
+
+
+    def reset_original_size(self):
+        """Shortcut to whi.reset_original_size."""
+        #logger.debug('reset_original_size')
+        self.whi.reset_original_size()
+
+
+    def closeEvent(self, e):
+        logger.debug('closeEvent')
+        QWidget.closeEvent(self, e)
+        cp.ivspectrum = None
+
+
+    if __name__ == "__main__":
+
+      def key_usage(self):
+        return 'Keys:'\
+               '\n  ESC - exit'\
+               '\n  R - reset original size'\
+               '\n  N - set new spectrum'\
+               '\n'
+
+
+      def keyPressEvent(self, e):
+        print('keyPressEvent key=%s' % e.key())
+        if   e.key() == Qt.Key_Escape:
+            print('Close app')
+            self.close()
+
+        elif e.key() == Qt.Key_R:
+            print('Reset original size')
+            self.on_but_reset()
+
+        elif e.key() == Qt.Key_N:
+            print('Set new histogram')
+            a = test_image(shape=(500,500))
+            self.set_spectrum_from_arr(a)
+
+        else:
+            print(self.key_usage())
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    os.environ['LIBGL_ALWAYS_INDIRECT'] = '1' #export LIBGL_ALWAYS_INDIRECT=1
+    from PyQt5.QtWidgets import QApplication
+
+    logging.basicConfig(format='[%(levelname).1s] L%(lineno)04d %(name)s : %(message)s', level=logging.INFO)
+
+    app = QApplication(sys.argv)
+    w = GWSpectrum()
+    w.setGeometry(100, 50, 300, 800)
+    w.setWindowTitle('Image with two axes')
+    w.show()
+    app.exec_()
+    del w
+    del app
+
+# EOF
