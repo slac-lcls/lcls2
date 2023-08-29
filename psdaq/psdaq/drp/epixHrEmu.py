762d4a67136caf57992ebeb1b037e7129ce9c4e4
Ric Claus <claus@slac.stanford.edu>
2023-08-29 15:25:11 -0700

    epixHrEmu: Switch to SZ3

--- psdaq/psdaq/drp/epixHrEmu.py
+++ psdaq/psdaq/drp/epixHrEmu.py
@@ -5,21 +5,43 @@ import sys
 import numpy as np
 from libpressio import PressioCompressor
 import json
+import time
+from prometheus_client import Counter,Gauge
 
 # Define compressor configuration:
 lpjson = {
-    "compressor_id": "sz", #the compression algo.
+    "compressor_id": "sz3", #the compression algo.
     "compressor_config": {
         #"sz:data_type"           : lp.pressio_uint16_dtype,
         #"sz:data_type"           : np.dtype('uint16'),
-        "sz:error_bound_mode_str": "abs",
-        "sz:abs_err_bound"       : 10, # max error
-        "sz:metric"              : "size"
+        ###"sz:error_bound_mode_str" : "abs",
+        ###"sz:abs_err_bound"        : 10, # max error
+        "sz3:abs_error_bound"     : 10, # max error
+        "sz3:metric"              : "size",
+        #"pressio:nthreads"        : 4
     },
 }
 
-ds = DataSource(drp=drp_info)
+ds = DataSource(drp=drp_info, monitor=True)
 thread_num = drp_info.worker_num
+
+alias      = f'{drp_info.det_name}_{drp_info.det_segment}'
+detname    = drp_info.det_name
+instrument = 'tst'
+partition  = 3
+
+compT = Counter('psana_compress_time', 'time spent (s) in compressor()',
+                ['alias','detname','instrument','partition','unit'])
+compT.labels(alias, detname, instrument, partition, 'seconds')
+ds.prom_man.register(compT)
+calibT = Counter('psana_calibration_time', 'time spent (s) in det.raw.calib()',
+                ['alias','detname','instrument','partition','unit'])
+calibT.labels(alias, detname, instrument, partition, 'seconds')
+ds.prom_man.register(calibT)
+triggers = Counter('psana_trigger_count', 'count of the number of triggers',
+                ['alias','detname','instrument','partition','unit'])
+triggers.labels(alias, detname, instrument, partition, 'seconds')
+ds.prom_man.register(triggers)
 
 #print(f'*** [Thread {thread_num}] ds._configs:', ds._configs.keys())
 
@@ -44,14 +66,22 @@ ds.add_data(cfg.config)
 
 # configure
 compressor = PressioCompressor.from_config(lpjson)
+#print(compressor.get_config())
 
 for myrun in ds.runs():
     epixhr = myrun.Detector('epixhr_emu')
     for nevt,evt in enumerate(myrun.events()):
+        triggers.labels(alias, detname, instrument, partition, 'counts').inc()
+        st = time.time()
         cal = epixhr.raw.calib(evt)
+        en = time.time()
+        calibT.labels(alias, detname, instrument, partition, 'seconds').inc(en-st)
         #print(f'*** cal is a {type(cal)} of len {len(cal)}, dtype {cal.dtype}, shape {cal.shape}')
         #print(f'*** cal {cal}')
+        st = time.time()
         det.fex.fex = compressor.encode(cal)
+        en = time.time()
+        compT.labels(alias, detname, instrument, partition, 'seconds').inc(en-st)
         #print(f'*** det.fex.fex is a {type(det.fex.fex)} of len {len(det.fex.fex)}, dtype {det.fex.fex.dtype}, shape {det.fex.fex.shape}, ndim {det.fex.fex.ndim}, size {det.fex.fex.size}')
         ds.add_data(det.fex)
         if nevt%1000!=0: ds.remove_data('epixhr_emu','raw')
