838186f743c29ff0013e1fa34d3e5d5d2e7dec95
msdubrovin <msdubrovin@gmail.com>
2023-09-06 10:50:15 -0700

    cosmetic - remove extra-offset

--- psana/psana/graphqt/examples/ex_GWSpectrum.py
+++ psana/psana/graphqt/examples/ex_GWSpectrum.py
@@ -4,37 +4,36 @@ from psana.graphqt.GWSpectrum import *
 
 class TestGWSpectrum(GWSpectrum):
 
-      def __init__(self, **kwa):
-          GWSpectrum.__init__(self, **kwa)
-          print(self.key_usage())
+    KEY_USAGE = 'Keys:'\
+             '\n  ESC - exit'\
+             '\n  R - reset original size'\
+             '\n  N - set new spectrum'\
+             '\n'
 
-      def key_usage(self):
-        return 'Keys:'\
-               '\n  ESC - exit'\
-               '\n  R - reset original size'\
-               '\n  N - set new spectrum'\
-               '\n'
+    def __init__(self, **kwa):
+        GWSpectrum.__init__(self, **kwa)
+        print(self.KEY_USAGE)
 
-      def keyPressEvent(self, e):
-        logger.debug('==  keyPressEvent key=%s' % e.key())
-        if   e.key() == Qt.Key_Escape:
-            print('Close app')
-            self.close()
+    def keyPressEvent(self, e):
+      logger.debug('==  keyPressEvent key=%s' % e.key())
+      if   e.key() == Qt.Key_Escape:
+          print('Close app')
+          self.close()
 
-        elif e.key() == Qt.Key_R:
-            r = self.whi.scene_rect()
-            print('Reset original size whi.scene_rect: %s' % qu.info_rect_xywh(r))
-            self.on_but_reset()
+      elif e.key() == Qt.Key_R:
+          r = self.whi.scene_rect()
+          print('Reset original size whi.scene_rect: %s' % qu.info_rect_xywh(r))
+          self.on_but_reset()
 
-        elif e.key() == Qt.Key_N:
-            shran = np.maximum(random_standard(shape=(2,), mu=500, sigma=500, dtype=np.int), (100,100))
-            mu, sigma = random_standard(shape=(2,), mu=500, sigma=200, dtype=np.float64)
-            print('\nset new histogram for img shape: %s mu: %.3f sigma: %.3f' % (str(shran), mu, sigma))
-            a = test_image(shape=shran, mu=mu, sigma=sigma)
-            self.set_spectrum_from_arr(a)
+      elif e.key() == Qt.Key_N:
+          shran = np.maximum(random_standard(shape=(2,), mu=500, sigma=500, dtype=int), (100,100))
+          mu, sigma = random_standard(shape=(2,), mu=500, sigma=200, dtype=np.float64)
+          print('\nset new histogram for img shape: %s mu: %.3f sigma: %.3f' % (str(shran), mu, sigma))
+          a = test_image(shape=shran, mu=mu, sigma=sigma)
+          self.set_spectrum_from_arr(a)
 
-        else:
-            print(self.key_usage())
+      else:
+          print(self.KEY_USAGE)
 
 
 if __name__ == "__main__":
@@ -53,8 +52,7 @@ if __name__ == "__main__":
 
     w.show()
     app.exec_()
-    #w.disconnect_histogram_scene_rect_changed(w.test_histogram_scene_rect_changed)
-    #w.wcbar.disconnect_new_color_table(w.wcbar.test_new_color_table_reception)
+
     del w
     del app
 
