682ab8d46a4519f1e7e0e456ffac7e47f7d2b533
monarin <monarin@gmail.com>
2023-09-07 12:59:55 -0700

    psplotdb server must use zmq bind; add detname argument.

--- psana/psana/app/psplot_latest.py
+++ psana/psana/app/psplot_latest.py
@@ -3,20 +3,22 @@
 # (run_no, node_name, port_no)and display psplot when 
 # new info arrive.
 ######################################################
-from psana.psexp.zmq_utils import SubSocket
+from psana.psexp.zmq_utils import PubSocket
 import socket
 import zmq
 import subprocess
+import sys
 
 
-pull_socket = None
+srv_socket = None
+my_subproc = None
 
 
 def get_db_info():
-    global pull_socket
+    global srv_socket
     # We acquire an available port using a socket then closing it immediately 
     # so that this port can be used later.
-    if not pull_socket:
+    if not srv_socket:
         IPAddr = socket.gethostbyname(socket.gethostname())
         sock = socket.socket()
         sock.bind(('', 0))
@@ -25,34 +27,37 @@ def get_db_info():
         supervisor_ip_addr = f'{IPAddr}:{port_no}'
         socket_name = f"tcp://{supervisor_ip_addr}"
         print(f'Run psana with this socket: {socket_name}', flush=True)
-        pull_socket = SubSocket(socket_name, socket_type=zmq.PULL)
-    # Fetch data (received data are a tubple of compressed and uncompressed dict)
-    obj = pull_socket.recv_zipped_pickle()[1]
+        srv_socket = PubSocket(socket_name, socket_type=zmq.PULL)
+    # Fetch data 
+    print(f'Getting data from client')
+    obj = srv_socket.recv()
     return obj 
 
 
-def start_psplot():
-    p = subprocess.Popen(["tail", "-f", "~/.bashrc"])
-    print(f"Process ID of subprocess {p.pid}")
-    # Send SIGTER 
-    p.terminate()
-    # Wait for process to terminate
-    returncode = p.wait()
-    print(f"Returncode of subprocess: {returncode}")
+def start_psplot(node, port, detname):
+    """ Start psplot as a subprocess with check to kill if
+    there's an existing one."""
+    global my_subproc
+    if my_subproc is not None:
+        # Send SIGTER 
+        my_subproc.terminate()
+        # Wait for process to terminate
+        returncode = my_subproc.wait()
+        print(f"Returncode of subprocess: {returncode}")
+        my_subproc = None
+    my_subproc = subprocess.Popen(["psplot", "-s", node, "-p", str(port), detname])
+    print(f"Process ID of subprocess {my_subproc.pid}")
 
 
 if __name__ == "__main__":
     runnum, node, port = (0, None, None)
+    detname = sys.argv[1]
     while True:
         obj = get_db_info()
         if obj['node'] != node or obj['port'] != port or obj['runnum'] > runnum:
             runnum, node, port = (obj['runnum'], obj['node'], obj['port'])
             print(f'Received new {runnum=} {node=} {port=}', flush=True)
-            start_psplot()
+            start_psplot(node, port, detname)
         else:
             print(f'Received old {obj}')
-
-
-
-
 
