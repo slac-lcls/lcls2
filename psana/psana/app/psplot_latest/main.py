f663d54e88d5c19ca90e12b820f67886f3ea1030
monarin <monarin@gmail.com>
2023-09-13 13:59:04 -0700

    move terminate to kill in main and clean-up at exit

--- psana/psana/app/psplot_latest/main.py
+++ psana/psana/app/psplot_latest/main.py
@@ -9,21 +9,29 @@ import typer
 import asyncio
 import IPython
 import psutil
+import atexit
 
 
 app = typer.Typer()
+proc = SubprocHelper()
+
+
+def _exit_handler():
+    print('Cleaning up subprocesess...')
+    for pid in proc.pids():
+        kill(pid)
+atexit.register(_exit_handler)
 
 
 async def run_monitor(detname):
     runnum, node, port = (0, None, None)
-    proc = SubprocHelper()
     db = DbHelper(port=4242)
     while True:
         obj = db.get_db_info()
         if obj['node'] != node or obj['port'] != port or obj['runnum'] > runnum:
-            runnum, node, port = (obj['runnum'], obj['node'], obj['port'])
-            print(f'Received new {runnum=} {node=} {port=}', flush=True)
-            cmd = f"psplot -s {node} -p {port} {detname}"
+            exp, runnum, node, port = (obj['exp'], obj['runnum'], obj['node'], obj['port'])
+            print(f'Received new {exp=} {runnum=} {node=} {port=}', flush=True)
+            cmd = f"psplot -s {node} -p {port} {detname} exp:{exp},run:{runnum},detname:{detname},node:{node},port:{port}"
             await proc._run(cmd)
         else:
             print(f'Received old {obj}')
@@ -37,19 +45,30 @@ def monitor(detname: str):
 @app.command()
 def start(detname: str):
     cmd = f"xterm -hold -e python main.py monitor {detname}"
-    proc = SubprocHelper()
     asyncio.run(proc._run(cmd, prv='sys'))
     IPython.embed()
 
 
 def show():
-    data = [[pid, " ".join(psutil.Process(pid).cmdline())] for pid in psutil.pids()]
-    headers = ["pid", "cmd"]
-    format_row = "{:<12} {:<25}"
+    headers = ["pid", "username", "detail"]
+    format_row = "{:<8} {:<10} {:<25}"
     print(format_row.format(*headers))
-    for row in data:
-        if row[1].find("psplot") > -1:
+    for proc in psutil.process_iter(['pid', 'cmdline', 'username']):
+        if ' '.join(proc.info['cmdline']).find("psplot") > -1:
+            row = [proc.info['pid'], proc.info['username'], proc.info['cmdline'][-1]]
             print(format_row.format(*row))
+
+
+def kill(pid, timeout=3):
+    def on_terminate(proc):
+        print("process {} terminated with exit code {}".format(proc, proc.returncode))
+    procs = [psutil.Process(pid)] + psutil.Process(pid).children()
+    for p in procs:
+        p.terminate()
+    gone, alive = psutil.wait_procs(procs, timeout=timeout, callback=on_terminate)
+    for p in alive:
+        p.kill()
+
 
 if __name__ == "__main__":
     app()
--- psana/psana/app/psplot_latest/subproc.py
+++ psana/psana/app/psplot_latest/subproc.py
@@ -2,22 +2,20 @@ import asyncio
 import psutil
 
 class SubprocHelper():
+    def __init__(self):
+        self.procs = {}
+
     async def _run(self, cmd, prv='usr'):
         """ Start psplot as a subprocess """
         proc = await asyncio.create_subprocess_shell(
             cmd,
             stdout=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE)
+        self.procs[proc.pid] = proc
 
-    def terminate(self, pid, timeout=3):
-        p = psutil.Process(pid)
-        print(f'Terminating process: {pid}...')
-        p.terminate()
-        p.wait(timeout=timeout)
-        if psutil.pid_exists(pid):
-            print(f'Failed, could not terminate the process.')
-        else:
-            print(f'Succeeded')
+    def pids(self):
+        return list(self.procs.keys())
+
 
 
 
