6ddb3ad77e29e348d3adc5a404a8454df011d75e
Monarin Uervirojnangkoorn <monarin@gmail.com>
2023-10-13 11:32:30 -0700

    add sbatch script for testing psplotdb server

---
+++ psana/psana/tests/submit_run_andor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --partition=milano
+#SBATCH --account=lcls:data
+#SBATCH --job-name=run_andor
+#SBATCH --nodes=1
+#SBATCH --ntasks=5
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+##SBATCH --exclusive
+#SBATCH -t 00:05:00
+
+t_start=`date +%s`
+
+exp=$1
+runnum=$2
+socket=${3}
+#srun python run_andor.py $exp $runnum ${socket}
+mpirun -n 5 python run_andor.py $exp $runnum ${socket}
+
+t_end=`date +%s`
+echo PSJobCompleted TotalElapsed $((t_end-t_start)) 
