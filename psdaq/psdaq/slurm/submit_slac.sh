f905e250561efff2122bba837bb294b6c7240b82
monarin <monarin@gmail.com>
2024-03-25 11:07:41 -0700

    test export flag

--- psdaq/psdaq/slurm/submit_slac.sh
+++ psdaq/psdaq/slurm/submit_slac.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name parallel   ## name that will show up in the queue
-#SBATCH --output slurm-%j.out   ## filename of the output; the %j is equal to jobID; default is slurm-[jobID].out
-#SBATCH --ntasks=3  ## number of tasks (analyses) to run
-##SBATCH --ntasks-per-node=2
-#SBATCH --nodelist=drp-srcf-cmp036,drp-srcf-cmp035
-##SBATCH --cpus-per-task=2  ## the number of threads allocated to each task
-#SBATCH --mem-per-cpu=1G   # memory per CPU core
-#SBATCH --partition=anaq  ## the partitions to run in (comma seperated)
-#SBATCH --time=0-00:10:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --partition=anaq  
+#SBATCH --job-name parallel   
+#SBATCH --output slurm-%j.out   
+#SBATCH --ntasks=1  
+#SBATCH --nodelist=drp-srcf-cmp036
 
 # Execute job steps
-srun --ntasks=1 --nodes=1 --exclusive --nodelist=drp-srcf-cmp036 bash -c "sleep 2; python test_mpi.py" &
-srun --ntasks=1 --nodes=1 --exclusive --nodelist=drp-srcf-cmp036 bash -c "sleep 3; python test_mpi.py" &
-srun --ntasks=1 --nodes=1 --exclusive --nodelist=drp-srcf-cmp035 bash -c "sleep 4; python test_mpi.py" &
+srun -n1 --export=ALL,BLAH=blah python $HOME/psana-nersc/psana2/test_mpi.py
 
 
-wait
 
