a8b1b08c078dcd64018ea3cb197effe8def71967
monarin <monarin@gmail.com>
2024-03-27 14:01:41 -0700

    clean up

--- psdaq/psdaq/slurm/submit_slac.sh
+++ psdaq/psdaq/slurm/submit_slac.sh
@@ -3,11 +3,18 @@
 #SBATCH --partition=anaq  
 #SBATCH --job-name parallel   
 #SBATCH --output slurm-%j.out   
-#SBATCH --ntasks=1  
-#SBATCH --nodelist=drp-srcf-cmp036
+#SBATCH -N1 -n3 --nodelist=drp-srcf-cmp035 
+#SBATCH hetjob
+#SBATCH -N1 -n1 --nodelist=drp-srcf-cmp036
+
 
 # Execute job steps
-srun -n1 --export=ALL,BLAH=blah python $HOME/psana-nersc/psana2/test_mpi.py
+export BLAH="blah"
+srun -n 1 --het-group=0 --exclusive bash -c "sleep 5; echo $BLAH 1" &
+srun -n 1 --het-group=0 --exclusive bash -c "sleep 10; echo $BLAH 2" &
+srun -n 1 --het-group=0 --exclusive bash -c "sleep 15; echo $BLAH 3" &
 
+srun -n 1 --het-group=1 --exclusive bash -c "sleep 15; echo $BLAH 4" &
+wait
 
 
