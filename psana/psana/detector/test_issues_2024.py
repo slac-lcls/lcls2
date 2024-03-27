adb30122826f4c8481df99f3fb5c392adbbcfae6
msdubrovin <msdubrovin@gmail.com>
2024-03-27 10:24:57 -0700

    remove issue_2023* add issue_2024_03_26

--- psana/psana/detector/test_issues_2024.py
+++ psana/psana/detector/test_issues_2024.py
@@ -19,190 +19,6 @@ def ds_run_det(exp='ascdaq18', run=171, detname='epixhr', **kwa):
     return ds, orun, det
 
 
-def issue_2023_04_27():
-    from psana.detector.NDArrUtils import print_ndarr, info_ndarr
-    #from psana import DataSource
-    #ds, detname = DataSource(exp='tstx00417', run=277, dir='/cds/data/drpsrcf/tst/tstx00417/xtc'), 'epixhr_emu'
-    ##ds, detname = DataSource(exp='ascdaq18', run=171, dir='/cds/data/psdm/asc/ascdaq18/xtc/'), 'epixhr'
-    #orun = next(ds.runs())
-    #odet = orun.Detector(detname)
-
-    #ds, orun, odet = ds_run_det(exp='tstx00417',run=277, detname='epixhr_emu', dir='/cds/data/drpsrcf/tst/tstx00417/xtc')
-    ds, orun, odet = ds_run_det(exp='ascdaq18',run=171, detname='epixhr', dir='/cds/data/psdm/asc/ascdaq18/xtc/')
-
-    print('run.runnum: %d detnames: %s expt: %s' % (orun.runnum, str(orun.detnames), orun.expt))
-    print('odet.raw._det_name: %s' % odet.raw._det_name) # epixquad
-    print('odet.raw._dettype : %s' % odet.raw._dettype)  # epix
-
-    for nstep, step in enumerate(orun.steps()):
-        print('\n==== step:%02d' %nstep)
-        if nstep>4: break
-        for k,v in odet.raw._seg_configs().items():
-            cob = v.config
-            print_ndarr(cob.asicPixelConfig, 'seg:%02d trbits: %s asicPixelConfig:'%(k, str(cob.trbit)))
-
-        for nev, evt in enumerate(step.events()):
-            if nev>5: break
-            raw = odet.raw.raw(evt)
-            print(info_ndarr(raw, 'Event:%02d raw:'%nev))
-
-
-def issue_2023_04_28():
-    """epixhremu default geometry implementation
-    """
-    from psana.pscalib.geometry.GeometryAccess import GeometryAccess
-    from psana.detector.NDArrUtils import info_ndarr
-    ds, orun, det = ds_run_det(exp='tstx00417',run=277, detname='epixhr_emu', dir='/cds/data/drpsrcf/tst/tstx00417/xtc')
-    #ds, orun, det = ds_run_det(exp='ascdaq18',run=171, detname='epixhr', dir='/cds/data/psdm/asc/ascdaq18/xtc/')
-
-    x,y,z = det.raw._pixel_coords()
-    print(info_ndarr(x,'det.raw.pixel_coords x:'))
-
-#    for nevt,evt in enumerate(orun.events()):
-#        geotxt = det.raw._det_geotxt_default()
-#        print('_det_geotxt_default:\n%s' % geotxt)
-#        o = GeometryAccess()
-#        o.load_pars_from_str(geotxt)
-#        x,y,z = o.get_pixel_coords()
-#        print(info_ndarr(x,'x:'))
-#        if det.raw.image(evt) is not None: break
-
-
-def issue_2023_05_19():
-    """
-    """
-    from psana import DataSource
-    ds = DataSource(exp='tmoc00118', run=222, dir='/cds/data/psdm/prj/public01/xtc')
-    for run in ds.runs():
-      opal = run.Detector('tmo_opal1')
-      for nev,evt in enumerate(run.events()):
-        if nev>5: break
-        img = opal.raw.image(evt)
-        print('ev:%04d  evt.timestamp: %d image.shape: %s' % (nev, evt.timestamp, str(img.shape)))
-
-
-def issue_2023_07_25():
-    """datinfo -k exp=tstx00417,run=287,dir=/sdf/data/lcls/drpsrcf/ffb/tst/tstx00417/xtc/ -d epixhr_emu
-       on pcds use psffb, which sees data
-       or use s3dff
-    """
-    from psana.detector.NDArrUtils import info_ndarr
-    from psana import DataSource
-    from psana.detector.UtilsGraphics import gr, fleximage#, arr_median_limits
-    flimg = None
-
-    #ds = DataSource(exp='tstx00417', run=286, dir='/sdf/data/lcls/drpsrcf/ffb/tst/tstx00417/xtc') # on s3df 3-panel run
-    ds = DataSource(exp='tstx00417', run=286, dir='/cds/data/drpsrcf/tst/tstx00417/xtc') # on pcds 3-panel run
-    #ds = DataSource(exp='tstx00417', run=287, dir='/cds/data/drpsrcf/tst/tstx00417/xtc') # on pcds 20-panel run detectors=['epixhr_emu'])
-    for run in ds.runs():
-      det = run.Detector('epixhr_emu')
-      for nev,evt in enumerate(run.events()):
-        if nev>10000: break
-        arr = det.fex.calib(evt)
-        if arr is None: continue
-        print(info_ndarr(arr, '==== ev:%05d  evt.timestamp: %d arr:' % (nev, evt.timestamp)))
-
-        img = det.fex.image(evt, value_for_missing_segments=800)
-        #img = det.fex.image(evt)
-        print(info_ndarr(img, 43*' ' + 'image:'))
-
-        if flimg is None:
-           #flimg = fleximage(img, arr=None, h_in=8, w_in=11, amin=9000, amax=11000) #, nneg=1, npos=3)
-           flimg = fleximage(img, arr=None, h_in=8, w_in=11, amin=700, amax=800) #, nneg=1, npos=3)
-        gr.set_win_title(flimg.fig, titwin='Event %d' % nev)
-        flimg.update(img, arr=None)
-        gr.show(mode='DO NOT HOLD')
-    gr.show()
-
-
-def issue_2023_07_26():
-    """
-    """
-    from time import time
-    import psana.pscalib.calib.MDBWebUtils as wu
-    t0_sec = time()
-    det_name = 'epixhremu_00cafe000c-0000000000-0000000000-0000000000-0000000000-0000000000-0000000000'
-    #det_name = 'epixhremu_000002' # works
-    #det_name = 'epixhr_emu' # non-populated  in db
-    calib_const = wu.calib_constants_all_types(det_name, exp='tstx00417', run=0)
-    print('consumed time = %.6f sec' % (time()-t0_sec))
-    print('calib_const.keys:', calib_const.keys())
-
-
-def issue_2023_07_27():
-    """curl commands to access data from GridFS equivalent to previous script:
-        2.031110 sec - get data for ctype: pixel_status
-        2.059888 sec - get data for ctype: geometry
-        2.929590 sec - get data for ctype: pixel_gain
-        3.967860 sec - get data for ctype: pedestals
-        4.862775 sec - get data for ctype: pixel_rms
-    """
-    from requests import get
-    from time import time
-
-    requests = [\
-             'https://pswww.slac.stanford.edu/calib_ws/cdb_tstx00417/gridfs/6450604a3a7ab9e8b9dc63b2',
-             'https://pswww.slac.stanford.edu/calib_ws/cdb_tstx00417/gridfs/64514377ff231530db74d279',
-             'https://pswww.slac.stanford.edu/calib_ws/cdb_tstx00417/gridfs/64506059a42d4cbaddd64eff',
-             'https://pswww.slac.stanford.edu/calib_ws/cdb_tstx00417/gridfs/64506032c67f3e7208e57b33',
-             'https://pswww.slac.stanford.edu/calib_ws/cdb_tstx00417/gridfs/6450604110a2e726664e6a54',
-            ]
-
-    curls = ['curl -s "%s"' % r for r in requests]
-
-    print('\n'.join([s for s in curls]))
-    query=None
-    for url in requests:
-        t0_sec = time()
-        print('====\n  in python: r = get("%s", query=None, timeout=180)' % url)
-        print('  equivalent in os: curl -s "%s"' % url)
-
-        r = get(url, query, timeout=180)
-
-        if r.ok:
-            s = 'content[0:30]: %s...' % r.content[0:30]\
-              + '\nconsumed time = %.6f sec' % (time()-t0_sec)
-            #mu.object_from_data_string(s, doc)
-
-        else:
-            s = 'get url: %s query: %s\n  response status: %s status_code: %s reason: %s'%\
-                (url, str(query), r.ok, r.status_code, r.reason)
-            s += '\nTry command: curl -s "%s"' % url
-        print(s)
-
-
-
-def issue_2023_10_26():
-
-    from psana import DataSource
-    from psana.detector.NDArrUtils import info_ndarr
-
-    ds = DataSource(exp='rixx1003721', run=200, intg_det='epixhr')
-
-    M14 =  0x3fff  # 16383
-    M15 =  0x7fff  # 32767
-
-    print('data bits: %d' % M14)
-
-    for irun,orun in enumerate(ds.runs()):
-      print('\n\n== run:%d' % irun)
-      det = orun.Detector('epixhr')
-      for istep,ostep in enumerate(orun.steps()):
-        print('\n==== step:%d' % istep)
-        for ievt,evt in enumerate(ostep.events()):
-
-          if evt is None:
-              continue
-          raw = det.raw.raw(evt)
-          if raw is None:
-              continue
-          #a = raw[:,:144,:192] # min:0 max:0
-          #a = raw[:,:144,193:] # min:0 max:0
-          #a = raw[:,145:,193:] # min:0 max:0
-          a = raw[:,145:,:192] & M14  # min:4811 max:10054
-          print(info_ndarr(a,'raw:%4d' % ievt), 'min:%6d max:%6d' % (a.min(), a.max()), end='\r')
-
-
 def issue_2024_mm_dd():
     print('template')
 
@@ -293,7 +109,7 @@ def issue_2024_03_19():
     from psana import DataSource
     import sys
     from psana.detector.NDArrUtils import info_ndarr
-    ds = DataSource(exp='tstx00417',run=309,dir='/reg/neh/operator/tstopr/data/drp/tst/tstx00417/xtc/')
+    ds = DataSource(exp='tstx00417',run=317,dir='/reg/neh/operator/tstopr/data/drp/tst/tstx00417/xtc/')
     orun = next(ds.runs())
     det = orun.Detector('tst_epixm')
     calibconst = det.calibconst#['pedestals'][0]
@@ -317,9 +133,24 @@ def issue_2024_03_19():
             print('  cob.CompTH_ePixM', cob.CompTH_ePixM)
             print('  cob.Precharge_DAC_ePixM', cob.Precharge_DAC_ePixM)
 
-        print(info_ndarr(det.raw.raw(evt), 'raw'))
+        #print(info_ndarr(det.raw.raw(evt), 'raw'))
         print(info_ndarr(det.raw.calib(evt), 'calib'))
         print(info_ndarr(det.raw.image(evt), 'image'))
+
+
+def issue_2024_03_26():
+    """generate and save text ndarray
+    """
+    import numpy as np
+    #shape, dtype, fname = (4, 192, 384), np.float32, 'nda-epixm-gains.txt'
+    shape, dtype, fname = (3, 4, 192, 384), np.float32, 'nda-epixm-peds.txt'
+    import psana.pyalgos.generic.NDArrUtils as au
+    nda = np.zeros(shape, dtype=dtype)
+    #nda = np.ones(shape, dtype=dtype)
+    from psana.detector.NDArrUtils import info_ndarr
+    print(info_ndarr(nda, 'ones'))
+    au.save_ndarray_in_textfile(nda, fname, 0o664, ' %d', umask=0o0, group='ps-users')
+    print('saved in file %s' % fname)
 
 
 def argument_parser():
@@ -356,14 +187,11 @@ def selector():
     TNAME = args.tname  # sys.argv[1] if len(sys.argv)>1 else '0'
 
     if   TNAME in  ('0',): issue_2024_mm_dd() # template
-    elif TNAME in  ('1',): issue_2023_05_19() # opal.raw.image for Mona'
-    elif TNAME in  ('2',): issue_2023_07_25() # epixhremu.fex.image for Ric
-    elif TNAME in  ('3',): issue_2023_07_26() # calib_constants_all_types for Ric
-    elif TNAME in  ('4',): issue_2023_07_27() # calib_constants_all_types for Ric - curl commands
-    elif TNAME in  ('5',): issue_2024_02_14() # ascdaq18,run=407, epixhr - zero pedestals for Philip
-    elif TNAME in  ('6',): issue_2024_02_22() # calib for epixm320 tstx00417,run=308 for Ric
-    elif TNAME in  ('7',): issue_2024_03_14() # new detector archon exp=rixc00121,run=142 for Chris
-    elif TNAME in  ('8',): issue_2024_03_19() # Ric and Chris - config scan for epixm
+    elif TNAME in  ('1',): issue_2024_02_14() # ascdaq18,run=407, epixhr - zero pedestals for Philip
+    elif TNAME in  ('2',): issue_2024_02_22() # calib for epixm320 tstx00417,run=308 for Ric
+    elif TNAME in  ('3',): issue_2024_03_14() # new detector archon exp=rixc00121,run=142 for Chris
+    elif TNAME in  ('4',): issue_2024_03_19() # Ric and Chris - config scan for epixm
+    elif TNAME in  ('5',): issue_2024_03_26() # generate and save text ndarray
     else:
         print(USAGE())
         exit('\nTEST "%s" IS NOT IMPLEMENTED'%TNAME)
