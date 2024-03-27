95bc496268bf3591a6e13daee9857f0a995b6aa3
msdubrovin <msdubrovin@gmail.com>
2024-03-27 10:28:30 -0700

    remove direct import of psana.detector.NDArrUtils

--- psana/psana/detector/epix_base.py
+++ psana/psana/detector/epix_base.py
@@ -20,12 +20,12 @@ from amitypes import Array3d
 import logging
 logger = logging.getLogger(__name__)
 
-from psana.detector.areadetector import sgs, AreaDetectorRaw, np, ut, DTYPE_MASK, DTYPE_STATUS
+from psana.detector.areadetector import sgs, AreaDetectorRaw, np, ut, DTYPE_MASK, DTYPE_STATUS, au
 from psana.detector.UtilsEpix10ka import np, calib_epix10ka_any, map_gain_range_index,\
   cbits_config_epix10ka, cbits_config_epixhr2x2, cbits_config_epixhr1x4,\
   cbits_config_and_data_detector, M14, M15, B14, B15
 import psana.detector.UtilsMask as um #import merge_status
-from psana.detector.NDArrUtils import info_ndarr
+info_ndarr, reshape_to_3d = au.info_ndarr, au.reshape_to_3d
 
 
 class epix_base(AreaDetectorRaw):
