b2383b322276ba91eff642af85e6e7bb70cec894
monarin <monarin@gmail.com>
2024-04-24 16:38:36 -0700

    support subprocess management

---
+++ psdaq/psdaq/slurm/subproc.py
@@ -0,0 +1,25 @@
+import asyncio
+import psutil
+
+class SubprocHelper():
+    def __init__(self):
+        self.procs = {}
+
+    async def _run(self, cmd, callback=None):
+        """ Start psplot as a subprocess """
+        proc = await asyncio.create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        self.procs[proc.pid] = proc
+        if callback:
+            callback(proc.pid)
+
+    def pids(self):
+        return list(self.procs.keys())
+
+
+
+
+
+
