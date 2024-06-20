d2a4ac8c544fa85236912ca378328915379b6d94
monarin <monarin@gmail.com>
2024-06-19 17:02:02 -0700

    default no. of cores to 10 for drp and teb processes

--- psdaq/psdaq/slurm/cnftopy.py
+++ psdaq/psdaq/slurm/cnftopy.py
@@ -25,8 +25,14 @@ task_set = ''
                 continue
             elif line.find("procstat") > -1:
                 o_file.writelines(" {id: 'psqueue', cmd: 'psqueue -i 5'},")
-                continue
-            o_file.writelines(line)
+            elif line.find("cmd:drp_cmd") > -1 or line.find("cmd:teb_cmd") > -1:
+                # Add 10 cores as a default no. of physical cores need for drp and teb processes
+                cols = line.split(',')
+                cols.insert(1, "cores: 10")
+                new_line = ','.join(cols)
+                o_file.writelines(new_line)
+            else:
+                o_file.writelines(line)
     o_file.close()
     print(f"Done. The cnf file {cnf_file} has been converted to {py_file}")
 
