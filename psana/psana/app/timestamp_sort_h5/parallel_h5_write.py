eb3ecaf3c9c7f73db0b2870e6c51efae2de27653
Monarin Uervirojnangkoorn <monarin@gmail.com>
2024-07-05 13:13:44 -0700

    support h5 groups and scalar/unaligned datasets

--- psana/psana/app/timestamp_sort_h5/parallel_h5_write.py
+++ psana/psana/app/timestamp_sort_h5/parallel_h5_write.py
@@ -5,6 +5,7 @@ import time
 import os
 import dask.array as da
 from psana.app.timestamp_sort_h5.utils import get_dask_client 
+import tables as tb
 
 
 # Get and merge parent comm to get the common comm
@@ -32,14 +33,12 @@ client = get_dask_client(n_procs)
 
 t0 = time.time()
 
-
-# Access the large h5 and slice with the obtained indices
+# Use pytables to access h5 tree keys and h5py for data
+in_pytbl = tb.open_file(in_h5fname, 'r')
 in_f = h5py.File(in_h5fname, 'r')
+ts_len = in_f['timestamp'].shape[0]
 chunk_size = (chunk_size,)
-da_dict = {}
-for key in in_f.keys():
-    da_dict[key] = da.from_array(in_f[key], chunks=chunk_size+in_f[key].shape[1:])
-
+print (f"WRITER RANK:{common_comm.Get_rank()} start slice-and-write ts_len:{ts_len}")
 
 # Start receving data
 while True:
@@ -63,20 +62,29 @@ while True:
 
     out_fname = os.path.join(out_dir, f'{out_basename}_part{tag}.h5')
     out_f = h5py.File(out_fname, 'w') 
-    for key in in_f.keys():
-        t1 = time.time()
-        # Dask slicing with ordered indices
-        access_arr = da_dict[key][access_indices].compute()
-        # Reorder the slice back to the original order
-        in_arr = access_arr[np.argsort(i_data)]
-        print(f'WRITER RANK:{common_comm.Get_rank()} {key=} dask slicing:{time.time()-t1:.2f}s.')
-        t2 = time.time()
-        out_f.create_dataset(key, data=in_arr)
-        print(f'WRITER RANK:{common_comm.Get_rank()} {key=} writing:{time.time()-t2:.2f}s.')
+    for group in in_pytbl.walk_groups("/"):
+        for array in in_pytbl.list_nodes(group):
+            t1 = time.time()
+            if isinstance(array, (tb.array.Array, )):
+                # We only sort 'aligned' data (data with the same size as timestamp)
+                # TODO: once unaligned data are timestamped, we'll sort them too.
+                if in_f[ds_path].shape[0] == ts_len:
+                    # TODO: find a better way to get dataset_path (e.g. /grp1/subgrp1/ds_name)
+                    ds_path = str(array).split()[0][1:]
+                    # Dask slicing with ordered indices
+                    dask_arr = da.from_array(in_f[ds_path], chunks=chunk_size+in_f[ds_path].shape[1:])
+                    access_arr = dask_arr[access_indices].compute()
+                    # Reorder the slice back to the original order
+                    val = access_arr[np.argsort(i_data)]
+                    print(f'WRITER RANK:{common_comm.Get_rank()} ds_path:{ds_path} dask slicing:{time.time()-t1:.2f}s.')
+                    out_f.create_dataset(ds_path, data=val)
+                    t2 = time.time()
+                    print(f'WRITER RANK:{common_comm.Get_rank()} ds_path:{ds_path} writing:{time.time()-t2:.2f}s.')
     out_f.close()
 
 
 in_f.close()
+in_pytbl.close()
 
 en = time.time()
 print(f"WRITER RANK:{common_comm.Get_rank()} DONE setup_cluster:{t0-st:.2f}s. slice_and_write: {en-t0:.2f}s. total: {en-st:.2f}s.")
--- psana/psana/app/timestamp_sort_h5/utils.py
+++ psana/psana/app/timestamp_sort_h5/utils.py
@@ -28,33 +28,69 @@ def get_dask_client(n_procs,
     return client
 
 def create_virtual_dataset(in_h5fname, out_h5fname, n_files):
+    """ Creates virtual dataset with out_h5fname main and N-part sub h5 files.
+    
+    Each *part_n.h5 file contains batch_size events sent by rank 0.
+    For unaligned data (pytables array with size != ts_len) or scalar data
+    (pytables type UnImplemented), we add them to the main out_h5fname h5 file
+    directly prior to joining the file.
+    """
     import h5py
-
+    import tables as tb
+    
+    # Open the original input h5 to determine the layout shapes and traverse (pytable)
     in_f = h5py.File(in_h5fname, 'r')
+    ts_len = in_f['timestamp'].shape[0]
+    in_t = tb.open_file(in_h5fname, 'r')
 
     # Get the output path and basename for _partN.h5 vds
     out_dir = os.path.dirname(out_h5fname)
     out_basename = os.path.splitext(os.path.basename(out_h5fname))[0]
     part_fnames = [os.path.join(out_dir, f'{out_basename}_part{i}.h5') for i in range(n_files)]
     part_files = [h5py.File(filename, 'r') for filename in part_fnames]
-
+    
+    # Obtain the layouts for aligned data and creates datasets for unaligned and scalar data
     layouts = {}
-    for entry_key in in_f.keys():
-        layouts[entry_key] = h5py.VirtualLayout(shape=in_f[entry_key].shape, dtype=in_f[entry_key].dtype)
+    out_f = h5py.File(out_h5fname, 'w', libver='latest')
+    for group in in_t.walk_groups("/"):
+        for array in in_t.list_nodes(group):
+            flag_unaligned = True
+            entry_key = str(array).split()[0][1:]
+            if isinstance(array, (tb.array.Array, )):
+                if in_f[entry_key].shape[0] == ts_len:
+                    layouts[entry_key] = h5py.VirtualLayout(shape=in_f[entry_key].shape, dtype=in_f[entry_key].dtype)
+                    flag_unaligned = False
+            if flag_unaligned:
+                val = None
+                if isinstance(array, (tb.array.Array, )):
+                    val = in_f[entry_key][:]
+                elif isinstance(array, (tb.unimplemented.UnImplemented, )):
+                    val = in_f[entry_key][...]
+                else:
+                    print(f'Warning: found {entry_key} with unsupported data type {type(array)}. This dataset will not be included in the new sorted h5 output file.')
+                if val is not None:
+                    print(f'Create {entry_key} dataset for unaligned/scalar data type')
+                    out_f.create_dataset(entry_key, data=val)
 
-    with h5py.File(out_h5fname, 'w', libver='latest') as out_f:
-        for entry_key in in_f.keys():
-            st,en = (0,0)
-            print(f'Creating virtual source for {entry_key}')
-            for i, filename in enumerate(part_fnames):
-                vsource = h5py.VirtualSource(filename, entry_key, shape=part_files[i][entry_key].shape)
-                en = st + part_files[i][entry_key].shape[0]
-                print(f'  part{i} shape:{part_files[i][entry_key].shape} {st=} {en=}')
-                layouts[entry_key][st:en] = vsource
-                st = en 
-            out_f.create_virtual_dataset(entry_key, layouts[entry_key], fillvalue=0)
+
+    # Creates virtual dataset by joining all part files for each entry_key for aligned data
+    for group in in_t.walk_groups("/"):
+        for array in in_t.list_nodes(group):
+            if isinstance(array, (tb.array.Array, )):
+                entry_key = str(array).split()[0][1:]
+                print(f'Create {entry_key} virtual dataset')
+                if in_f[entry_key].shape[0] == ts_len:
+                    st,en = (0,0)
+                    for i, filename in enumerate(part_fnames):
+                        vsource = h5py.VirtualSource(filename, entry_key, shape=part_files[i][entry_key].shape)
+                        en = st + part_files[i][entry_key].shape[0]
+                        print(f'  part{i} shape:{part_files[i][entry_key].shape} {st=} {en=}')
+                        layouts[entry_key][st:en] = vsource
+                        st = en 
+                    out_f.create_virtual_dataset(entry_key, layouts[entry_key],)
 
     # Close input h5 file
     for i in range(n_files):
         part_files[i].close()
     in_f.close()
+    out_f.close()
