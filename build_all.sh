2e8b1f970d7f0c65676228f9dfc234133cf4990f
mavaylon1 <mavaylon@sdfiana002.sdf.slac.stanford.edu>
2024-07-09 19:18:19 -0700

    poc

--- build_all.sh
+++ build_all.sh
@@ -12,12 +12,13 @@ fi
 cmake_option="RelWithDebInfo"
 pyInstallStyle="develop"
 psana_setup_args=""
-force_clean=1
+force_clean=0
 no_daq=0
 no_ana=0
 no_shmem=0
 build_ext_list=""
 install=1
+export psana_path=`pwd`/psana
 
 while getopts "c:p:s:b:fdam" opt; do
   case $opt in
@@ -74,7 +75,7 @@ function cmake_build() {
     shift
     mkdir -p build
     cd build
-    cmake -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=$cmake_option $@ ..
+    cmake -Dpsana_path=$psana_path -DCMAKE_INSTALL_PREFIX=$INSTDIR -DCMAKE_PREFIX_PATH=$CONDA_PREFIX -DCMAKE_BUILD_TYPE=$cmake_option $@ ..
     if [ $install == 1 ] 
     then
         make -j 4 install
@@ -122,14 +123,14 @@ install=0
 if [ $no_ana == 0 ]; then
     cmake_build psana
     # to build psana with setuptools
-    cd psana
+    # cd psana
     # force build of the extensions.  do this because in some cases
     # setup.py is unable to detect if an external header file changed
     # (e.g. in xtcdata).  but in many cases it is fine without "-f" - cpo
-    if [ $pyInstallStyle == "develop" ]; then
-        python setup.py build_ext -f --inplace
-    fi
-    pip install --no-deps --prefix=$INSTDIR $pipOptions .
+    # if [ $pyInstallStyle == "develop" ]; then
+    #     python setup.py build_ext -f --inplace
+    # fi
+    # pip install --no-deps --prefix=$INSTDIR $pipOptions .
 fi
 # The removal of site.py in setup 49.0.0 breaks "develop" installations
 # which are outside the normal system directories: /usr, /usr/local,
