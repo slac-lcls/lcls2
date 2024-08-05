a0b727e92908449edb8606f379528062b10f9cc9
Monarin Uervirojnangkoorn <monarin@gmail.com>
2024-08-05 10:20:41 -0700

    add check for manual test

--- psana/psana/tests/run_andor.py
+++ psana/psana/tests/run_andor.py
@@ -17,6 +17,20 @@ exp=sys.argv[1]
 runnum=int(sys.argv[2])
 
 
+# Known answer for manual unit test for rixc00221 r49
+# The key is the andor timestamp and the tuple values
+# are no. of andor events and no. of integrating events
+known_answers = {4565831569223779254: (99, 32001),
+        4565831582048691780: (197, 64002),
+        4565831594873607590: (197, 64002),
+        4565831607698521605: (197, 64002),
+        4565831620523437217: (197, 64002),}
+
+def check_answer(ts, cn_atm_events, cn_intg_events):
+    if exp == 'rixc00221' and runnum == 49:
+        assert (cn_atm_events, cn_intg_events) == known_answers[ts]
+
+
 mount_dir = '/sdf/data/lcls/drpsrcf/ffb'
 #mount_dir = '/cds/data/drpsrcf'
 xtc_dir = os.path.join(mount_dir, exp[:3], exp, 'xtc')
@@ -52,15 +66,17 @@ for myrun in ds.runs():
     sum_atmopal = None
     cn_andor_events = 0
     cn_intg_events = 0
+    cn_atm_events = 0
     ts_st = None
     for nstep,step in enumerate(myrun.steps()):
-        print('step:',nstep)
+        print(f'BD{rank-1} step: {nstep}')
         for nevt,evt in enumerate(step.events()):
             if ts_st is None: ts_st = evt.timestamp
             cn_intg_events += 1
             andor_img = andor.raw.value(evt)
             atmopal_img = atmopal.raw.image(evt)
             if atmopal_img is not None:
+                cn_atm_events += 1
                 if sum_atmopal is None:
                     sum_atmopal = atmopal_img[0,:]
                 else:
@@ -76,7 +92,8 @@ for myrun in ds.runs():
             if andor_img is not None:
                 cn_andor_events += 1
                 #print('andor data on evt:',nevt,'ndrop_inhibit:',ndrop_inhibit)
-                print(f'BD{rank-1}: #andor_events: {cn_andor_events} #intg_event:{cn_intg_events} st: {ts_st} en:{evt.timestamp}')
+                print(f'BD{rank-1}: #andor: {cn_andor_events} #atm: {cn_atm_events} #intg:{cn_intg_events} st: {ts_st} en:{evt.timestamp}')
+                check_answer(evt.timestamp, cn_atm_events, cn_intg_events)
                 # check that the high-read readout group (2) didn't
                 # miss any events due to deadtime
                 if ndrop_inhibit[2]!=0: print('*** data lost due to deadtime')
@@ -90,5 +107,6 @@ for myrun in ds.runs():
                 ndrop_inhibit=0
                 sum_atmopal = None
                 cn_intg_events = 0
+                cn_atm_events = 0
                 ts_st = None
     smd.done()
