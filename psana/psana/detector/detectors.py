a27afadbbc8987309015dd5b5fe033978ae5e83f
msdubrovin <msdubrovin@gmail.com>
2024-11-04 09:16:34 -0800

    use epixuhr in stead of epixUHR

--- psana/psana/detector/detectors.py
+++ psana/psana/detector/detectors.py
@@ -13,7 +13,6 @@ from .ts                    import *
 from .wave8                 import *
 from .epix10ka              import *
 from .epixhr2x2             import *
-#from .epixhremu             import *
 from .epix100               import *
 from .epixm320              import *
-from .epixUHR               import *
+from .epixuhr               import *
