bbffc47d46829357d244e1ede8cca64bba43dc5f
Ric Claus <claus@slac.stanford.edu>
2024-11-05 19:02:40 -0800

    Initial commit of a pgpread for GPU nodes

---
+++ psdaq/drp/GpuAsyncLib.h
@@ -0,0 +1,184 @@
+/**
+ * ----------------------------------------------------------------------------
+ * Company    : SLAC National Accelerator Laboratory
+ * ----------------------------------------------------------------------------
+ * Description: Library and utilities for CUDA and the GPU Async protocol.
+ * ----------------------------------------------------------------------------
+ * This file is part of 'axi-pcie-devel'. It is subject to
+ * the license terms in the LICENSE.txt file found in the top-level directory
+ * of this distribution and at:
+ *    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+ * No part of 'axi-pcie-devel', including this file, may be copied, modified,
+ * propagated, or distributed except according to the terms contained in
+ * the LICENSE.txt file.
+ * ----------------------------------------------------------------------------
+ **/
+#pragma once
+
+#include "gpuUtils.h"
+
+#include <string>
+#include <GpuAsync.h>
+#include <assert.h>
+#include <stdint.h>
+
+// What is this for?
+//#if defined(__CUDACC_VER_MAJOR__) || defined(__clangd__)
+//#define _CUDA
+//#endif
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+// What is this for?
+//#if __CUDACC_VER_MAJOR__ < 12
+//#define CU_DEVICE_ATTRIBUTE_CAN_USE_STREAM_MEM_OPS_V1 CU_DEVICE_ATTRIBUTE_CAN_USE_STREAM_MEM_OPS
+//#endif
+
+//--------------------------------------------------------------------------------//
+// CUDA Prototypes
+void checkError(CUresult status);
+void checkError(cudaError status);
+bool wasError(CUresult status);
+//--------------------------------------------------------------------------------//
+
+/**
+ * Wraps a data_gpu device so it can be automatically freed
+ */
+class DataGPU
+{
+public:
+    DataGPU(const char* path);
+    ~DataGPU()
+    {
+        close(fd_);
+    }
+
+    int fd() const { return fd_; }
+
+protected:
+    int fd_;
+};
+
+/**
+ * Wraps a cuda context and handles some initialization for you.
+ */
+class CudaContext
+{
+public:
+
+    /**
+     * Creates a CUDA context, selects a device and ensures that stream memory ops are available.
+     * \param device If >= 0, selects a device to use
+     * \param quiet Wheter to spew or not
+     * \returns Bool if success
+     */
+    bool init(int device = -1, bool quiet = false);
+
+    /**
+     * \brief Dumps a list of devices to stdout
+     */
+    void list_devices();
+
+    CUdevice device() const { return device_; }
+    CUcontext context() const { return context_; }
+
+    CUcontext context_;
+    CUdevice device_;
+};
+
+/**
+ * Dumps a buffer from the GPU to a stream
+ * \param devicePtr Pointer to the block of memory to dump on the GPU
+ * \param sizeInBytes
+ * \param buf The copy buffer (assumed to be sizeInBytes in size). If NULL, one will be allocated
+ * \param stream The stream to dump to
+ */
+template<typename T, int COLS = 16>
+static inline void dumpGpuMem(CUdeviceptr devicePtr, size_t sizeInBytes, void* buf = nullptr, FILE* stream = stdout)
+{
+    bool buffed = !buf;
+    if (buffed) buf = malloc(sizeInBytes);
+    cuMemcpyDtoH(buf, devicePtr, sizeInBytes);
+    dumpMem<T, COLS>(reinterpret_cast<T*>(buf), sizeInBytes);
+    if (buffed) free(buf);
+}
+
+/**
+ * \brief Describes FPGA memory that's mapped via RDMA to the GPU. The below functions wrap
+ * the creation and destruction process of the memory.
+ */
+struct GpuDmaBuffer_t
+{
+    int fd;
+    uint8_t* ptr;       /** Host accessible pointer **/
+    size_t size;        /** Size of the block **/
+    CUdeviceptr dptr;   /** Pointer on the device **/
+    int gpuOnly;        /** 1 if this is FPGA <-> GPU only, not mapped to host at all **/
+};
+
+/**
+ * \brief Maps FPGA memory to the host and allows GPU access to it
+ * This is the "standard" way of doing DMA with CUDA. cuMemHostRegister essentially just gives CUDA
+ * access to some pages and allows the GPU to read/write to them.
+ * Unlike RDMA, this doesn't require any custom code on the kernel side, this will work with any (properly aligned)
+ * buffer given, whether it's IO memory or not. Again unlike RDMA, cuMemHostRegister has more latency
+ * and lower throughput, since a DMA transfer must occur between the GPU <-> HOST <-> FPGA
+ * \param outmem
+ * \param fd File descriptor of the FPGA device
+ * \param offset Offset of the register block
+ * \param size Size of the register block
+ * \returns 0 for success
+ */
+int gpuMapHostFpgaMem(GpuDmaBuffer_t* outmem, int fd, uint64_t offset, size_t size);
+
+/**
+ * \brief Maps GPU memory to the FPGA using RDMA
+ * This function uses gpuAddNvidiaMemory to give the FPGA access to some pages of memory located
+ * on the GPU. This is the other method of doing DMA, and it requires custom driver code and a
+ * very specific PCIe topology.
+ * Regardless, RDMA allows for low-latency, high-bandwidth DMA transfers between two devices on
+ * the bus, without needing to interact with the CPU.
+ * \param outmem
+ * \param fd File descriptor of the FPGA device
+ * \param offset Offset of the register block
+ * \param size Size of the register block
+ * \param write If 1, this will be writable
+ */
+int gpuMapFpgaMem(GpuDmaBuffer_t* outmem, int fd, uint64_t offset, size_t size, int write);
+
+/**
+ * \brief Unmaps memory, clears out the pointer and size
+ */
+void gpuUnMapFpgaMem(GpuDmaBuffer_t* mem);
+
+//-----------------------------------------------------------------------------//
+
+struct GpuBufferState_t
+{
+    uint8_t* swFpgaRegs;
+
+    GpuDmaBuffer_t bread;
+    GpuDmaBuffer_t bwrite;
+};
+
+/**
+ * Allocates and inits buffer pairs on the GPU for RDMA operation.
+ * \param b Buffer state to init
+ * \param gpu The DataGPU instance to allocate for
+ * \param bufSize The size of the buffers to allocate
+ * \return 0 for success, -1 on error
+ */
+int gpuInitBufferState(GpuBufferState_t* b, const DataGPU& gpu, size_t bufSize);
+void gpuDestroyBufferState(GpuBufferState_t* b);
+
+//-----------------------------------------------------------------------------//
+
+/**
+ * Functions to get and set the DMA destination.
+ * \param fd The file descriptor of the datagpu device
+ * \param mode_ The enumerated value of the destination
+ */
+enum DmaDest_t { CPU=0x0000ffff, GPU=0xffff0000, ERR=-1u };
+DmaDest_t dmaDestGet(int fd);
+void dmaDestSet(int fd, DmaDest_t mode_);
---
+++ psdaq/drp/pgpreadgpu.cc
@@ -0,0 +1,297 @@
+// vim: et
+#include <stdint.h>
+#include <string>
+#include <GpuAsync.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <getopt.h>
+#include <exception>
+#include <getopt.h>
+#include <vector>
+#include <thread>
+
+#include "GpuAsyncLib.h"
+
+#include "GpuAsyncOffsets.h"
+
+// Define to disable logging in hot-paths. If you keep logging enabled, latencies will appear longer than they should.
+#define PROFILE
+
+//#define CHECK_MEM
+#undef PROFILE
+#ifndef PROFILE
+#define logInfo(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define logInfo(...)
+#endif
+
+#define GPU_BUFFER_SIZE 0x10000
+
+//-----------------------------------------------------------------------------//
+
+struct GpuTestState_t
+{
+    int fd;
+
+    CUcontext context;
+
+    GpuDmaBuffer_t swFpgaRegs;
+
+    CUstream streams[MAX_BUFFERS];
+
+    GpuBufferState_t buffers[MAX_BUFFERS];
+    int buffer_count;
+
+    GpuDmaBuffer_t swVersionRegs;
+
+    bool verbose;
+
+    // Latencies and averages
+    int iters;
+    int total_l;
+    int gpu_l;
+    int wr_l;
+    int rd_l;
+};
+
+//-----------------------------------------------------------------------------//
+
+void show_help(const char* av0);
+int run_host_test_guts(GpuTestState_t& state, int instance);
+int run_host_test(GpuTestState_t& state);
+
+//-----------------------------------------------------------------------------//
+
+int main(int argc, char* argv[]) {
+    int res = -1;
+
+    char dev[256] = "/dev/datagpu_0";
+
+    ////////////////////////////////////////////
+    // Option parsing
+    ////////////////////////////////////////////
+    int opt = -1;
+    unsigned lverbose = 0;
+    int iters = -1, list_devs = 0, buffers = MAX_BUFFERS, gpu = 0;
+    while ((opt = getopt(argc, argv, "hc:d:lb:g:v")) != -1) {
+        switch(opt) {
+        case 'd':
+            strcpy(dev, optarg);
+            break;
+        case 'c':
+            iters = atoi(optarg);
+            break;
+        case 'l':
+            list_devs = 1;
+            break;
+        case 'b':
+            buffers = atoi(optarg);
+            break;
+        case 'g':
+            gpu = atoi(optarg);
+            break;
+        case 'v':
+            ++lverbose;
+            break;
+        case 'h':
+        default:
+            show_help(argv[0]);
+        }
+    }
+
+    ////////////////////////////////////////////
+    // Validate arguments
+    ////////////////////////////////////////////
+
+    if (buffers <= 0 || buffers > MAX_BUFFERS) {
+        fprintf(stderr, "-b must be in range 0 < x <= MAX_BUFFERS (%d)\n", MAX_BUFFERS);
+        return 1;
+    }
+
+    ////////////////////////////////////////////
+    // Open and setup GPU
+    ////////////////////////////////////////////
+
+    printf("Opening device %s\n", dev);
+    DataGPU gpu0(dev);
+
+    CudaContext context;
+
+    if (list_devs) {
+        context.list_devices();
+        return 0;
+    }
+
+    if (!context.init(gpu)) {
+        fprintf(stderr, "CUDA init failed\n");
+        return 1;
+    }
+
+    int value;
+    cuDeviceGetAttribute(&value, CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING, context.device());
+    if (lverbose)  printf("Device supports unified addressing: %s\n", value ? "YES" : "NO");
+
+    // Make the DMA target the GPU
+    const char* dstType = "";
+    auto dst = dmaDestGet(gpu0.fd());
+    switch (dst) {
+        case DmaDest_t::CPU:  dstType = "CPU";  break;
+        case DmaDest_t::GPU:  dstType = "GPU";  break;
+        default:              dstType = "ERR";  break;
+    }
+    if (lverbose)  printf("DMA destination was %s\n", dstType);
+    if (dst != GPU)
+        dmaDestSet(gpu0.fd(), DmaDest_t::GPU);
+
+    ////////////////////////////////////////////////
+    // Create write and read buffers
+    ////////////////////////////////////////////////
+
+    // This will store everything important for now
+    GpuTestState_t state {};
+
+    state.fd = gpu0.fd();
+    state.context = context.context();
+    state.buffer_count = buffers;
+    state.verbose = lverbose;
+    state.iters = iters;
+
+    // Allocate a buffer list for our data
+    // This handles allocating buffers on the device and registering them with the driver.
+    // After this call, the FPGA will be properly configured to run the test code.
+    for ( int i = 0; i < buffers; ++i ) {
+        if (gpuInitBufferState(&state.buffers[i], gpu0, GPU_BUFFER_SIZE) < 0) {
+            fprintf(stderr, "Failed to alloc buffer list\n");
+            exit(1);
+        }
+    }
+
+    ////////////////////////////////////
+    // Allocate stream and run tests
+    ////////////////////////////////////
+
+    if (state.verbose)  logInfo("Create streams\n");
+
+    /** Allocate a stream per buffer **/
+    for (int i = 0; i < state.buffer_count; ++i) {
+        if (cudaStreamCreate(&state.streams[i]) != cudaSuccess) {
+            fprintf(stderr, "Error creating streams\n");
+            return 1;
+        }
+    }
+
+    int ret = run_host_test(state);
+
+    uint32_t freq;
+    dmaReadRegister(state.fd, PCIE_AXI_VERSION_OFFSET + PCIE_AXI_VERSION_CLK_FREQ, &freq);
+    printf("===== Test Averages =====\n");
+    printf(" Averaged over %d times\n", state.iters);
+    printf("  total   : %d (%2.f us)\n", state.total_l, (double(state.total_l) / freq) * 1e6);
+    printf("  gpu     : %d (%2.f us)\n", state.gpu_l, (double(state.gpu_l) / freq) * 1e6);
+    printf("  wr      : %d (%2.f us)\n", state.wr_l, (double(state.wr_l) / freq) * 1e6);
+    printf("  rd      : %d (%2.f us)\n", state.rd_l, (double(state.rd_l) / freq) * 1e6);
+
+    ////////////////////////////////////
+    // Final cleanup
+    ////////////////////////////////////
+
+    for (int i = 0; i < state.buffer_count; ++i)
+        gpuDestroyBufferState(&state.buffers[i]);
+
+    res = gpuRemNvidiaMemory(gpu0.fd());
+    if (res < 0) fprintf(stderr, "Error in IOCTL_GPUDMA_MEM_UNLOCK\n");
+
+    return ret;
+}
+
+int run_host_test(GpuTestState_t& state)
+{
+    std::vector<std::thread> threads;
+    for (int i = 0; i < state.buffer_count; ++i) {
+        threads.emplace_back([&state, i]{
+            run_host_test_guts(state, i);
+        });
+    }
+
+    for (int i = 0; i < state.buffer_count; ++i) {
+        threads[i].join();
+    }
+    //return run_host_test_guts(state, 0); // TODO: thread me!
+
+    return 0;
+}
+
+static void show_buf(CUdeviceptr dptr, size_t size, CUstream stream = 0) {
+    uint8_t buf[size];
+    //cuMemcpyDtoH(buf, dptr, size);
+    checkError(cudaMemcpyAsync(buf, (void*)dptr, size, cudaMemcpyDeviceToHost, stream));
+    cuStreamSynchronize(stream);
+    for (unsigned i = 0; i < size / 4; ++i) {
+        printf("%2d: offset=0x%X,  0x%X\n",i,i*4,*((uint32_t*)(buf+(i*4))));
+    }
+}
+
+/*******************************************************************
+ * Implementation of the HOST test
+ * \return -1 on error, 0 on success
+ *******************************************************************/
+int run_host_test_guts(GpuTestState_t& state, int instance)
+{
+    const auto& stream = state.streams[instance];
+    auto  hwWritePtr   = state.buffers[instance].bwrite.dptr;
+    auto  iters        = state.iters;
+
+    while(iters < 0 || iters-- > 0) {
+        // Clear the GPU memory handshake space to zero
+        if (state.verbose)  logInfo("%d Clear memory\n", instance);
+        checkError(cuStreamWriteValue32(stream, hwWritePtr + 4, 0x00, 0));
+
+        // Write to the DMA start register in the FPGA
+        if (state.verbose)  logInfo("%d Trigger write\n", instance);
+        //checkError(cuStreamWriteValue32(stream, state.hwWriteStart + 4 * instance, 0x01, 0));
+        cuStreamSynchronize(stream);
+        //auto rc = gpuSetWriteEn(state.fd, instance);
+        auto rc = dmaWriteRegister(state.fd, 0xD00300 + 4 * instance, 1);
+        if (rc < 0) {
+          logInfo("Failed to reenable buffer %d for write: %zd\n", instance, rc);
+          perror("gpuSetWriteEn");
+          abort();
+        }
+
+#ifdef CHECK_MEM
+        //while(1)
+        {
+            cuStreamSynchronize(stream);
+            show_buf(hwWritePtr, 0x100);
+        }
+#endif
+
+        // Spin on the handshake location until the value is greater than or equal to 1
+        // This waits for the data to arrive before starting the processing
+        if (state.verbose)  logInfo("%d Wait memory value\n", instance);
+        checkError(cuStreamWaitValue32(stream, hwWritePtr + 4, 0x1, CU_STREAM_WAIT_VALUE_GEQ));
+
+        {
+            cuStreamSynchronize(stream);
+            if (state.verbose)  logInfo("%d Dump:\n", instance);
+            show_buf(hwWritePtr, 2 * 8 * sizeof(uint32_t), stream);
+        }
+    }
+
+    return 0;
+}
+
+
+void show_help(const char* av0)
+{
+    printf("USAGE: %s [-t type] [-h] [-l] [-g gpu_number] [-b buff_count]\n", av0);
+    printf("   -d <dev>    : The device to use. Defaults to /dev/datagpu_0\n");
+    printf("   -h          : Show this help text\n");
+    printf("   -l          : List the available GPUs\n");
+    printf("   -g <number> : Select the specified GPU\n");
+    printf("   -c <iters>  : Do this many iterations of the test\n");
+    printf("   -b <count>  : Allocate this many buffers for round robin operation. Must be 0 < X <= MAX_BUFFERS (%d)\n", MAX_BUFFERS);
+    exit(0);
+}
