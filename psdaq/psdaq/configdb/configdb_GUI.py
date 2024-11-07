c9759d57959512e78c7c3a5710b707334901bbac
Riccardo Melchiorri <melchior@slac.stanford.edu>
2024-11-06 16:29:42 -0800

    kerberos ticket validation

--- psdaq/psdaq/configdb/configdb_GUI.py
+++ psdaq/psdaq/configdb/configdb_GUI.py
@@ -10,10 +10,11 @@ import os
 from psdaq.configdb.configdb import configdb
 from psdaq.configdb.configdb_multimod import configdb_multimod
 import threading
+import subprocess
     
 class ConfigdbGUI(QWidget):
     
-    def __init__(self):
+    def __init__(self,):
         super().__init__()
         self.data={}
         self.hutch=''
@@ -21,78 +22,85 @@ class ConfigdbGUI(QWidget):
         self.confdb=''
         self.detType={}
         
-        # Select Database to use Prod or Dev
-        URI_Label=QLabel('database: ')            
-        self.radio_prod = QRadioButton("Prod")
-        self.radio_prod.setChecked(True)
-        self.radio_dev = QRadioButton("Dev")
-        self.radio_dev.setChecked(False)
-        self.radio_dev.toggled.connect(self.radioURIselected)
-        #self.radio_prod.toggled.connect(self.radioURIselected)
-        # needs to be triggered only once
-        
-        # Select which detector to be searched
-        self.list_detector_Box = QComboBox()
-        self.list_detector_Box.currentTextChanged.connect(self.load_data_second_column)
+        if self.check_kerb():
+                            
+            # Select Database to use Prod or Dev
+            URI_Label=QLabel('database: ')            
+               
+            # Select which detector to be searched
+            self.list_detector_Box = QComboBox()
+            self.list_detector_Box.currentTextChanged.connect(self.load_data_second_column)
 
-        #progress bar for data load
-        self.pbar = QProgressBar(self)
+            #progress bar for data load
+            self.pbar = QProgressBar(self)
 
-        # String defining the value to changed
-        self.paramValue=QLineEdit()
-        #button to run the change, button is disabled when no key is selected
-        self.modButton=QPushButton('Modify')
-        self.modButton.clicked.connect(self.modify_database)
-        self.modButton.setEnabled(False)
-        
-        #devices and hutches
-        self.first_column = QTreeWidget()
-        #detectors
-        self.second_column = QListWidget()
-        #detector's parameters (only for one)
-        self.third_column = QTreeWidget()
-        #disabled (log print output)
-        self.forth_column = QTextEdit()
+            # String defining the value to changed
+            self.paramValue=QLineEdit()
+            #button to run the change, button is disabled when no key is selected
+            self.modButton=QPushButton('Modify')
+            self.modButton.clicked.connect(self.modify_database)
+            self.modButton.setEnabled(False)
+            
+            #devices and hutches
+            self.first_column = QTreeWidget()
+            #detectors
+            self.second_column = QListWidget()
+            #detector's parameters (only for one)
+            self.third_column = QTreeWidget()
+            #disabled (log print output)
+            self.forth_column = QTextEdit()
 
-        self.first_column.itemSelectionChanged.connect(self.first_column_select)       
-        self.first_column.setHeaderLabel("Prod") 
-        self.third_column.setHeaderLabel("Parameters") 
-        self.second_column.setSelectionMode(QAbstractItemView.MultiSelection)
-        self.second_column.itemSelectionChanged.connect(self.second_column_select)
-        self.third_column.itemSelectionChanged.connect(self.third_column_select)
-        
-        #to speed up start up, data are loaded on a thread
-        thread_first_column_data = threading.Thread(target=self.load_data_first_column)
-        self.set_default()
-        #self.load_data_first_column()
-        thread_first_column_data.start()
-        
-        layout_columns = QHBoxLayout()
-        layout_commands = QHBoxLayout()
-        layout = QVBoxLayout()
-        
-        layout.addLayout(layout_commands)
-        layout.addLayout(layout_columns)
-        
-        self.setLayout(layout)
-        
-        layout_columns.addWidget(self.first_column)
-        layout_columns.addWidget(self.second_column)
-        layout_columns.addWidget(self.third_column)
-        layout_columns.addWidget(self.forth_column)
-        
-        #layout_columns.addWidget(self.forth_column)
-        
-        layout_commands.addWidget(self.radio_dev)
-        layout_commands.addWidget(self.radio_prod)
-        layout_commands.addWidget(self.list_detector_Box)
-        layout_commands.addWidget(self.pbar)
-        layout_commands.addWidget(self.paramValue)
-        layout_commands.addWidget(self.modButton)
-        
-        self.setGeometry(50, 50, 800,400)
-        self.setWindowTitle("ConfigDB modifier")
-        self.show()
+            self.first_column.itemSelectionChanged.connect(self.first_column_select)       
+            self.first_column.setHeaderLabel("Prod") 
+            self.third_column.setHeaderLabel("Parameters") 
+            self.second_column.setSelectionMode(QAbstractItemView.MultiSelection)
+            self.second_column.itemSelectionChanged.connect(self.second_column_select)
+            self.third_column.itemSelectionChanged.connect(self.third_column_select)
+            
+            #to speed up start up, data are loaded on a thread
+            thread_first_column_data = threading.Thread(target=self.load_data_first_column)
+            self.set_default()
+            #self.load_data_first_column()
+            thread_first_column_data.start()
+            
+            layout_columns = QHBoxLayout()
+            layout_commands = QHBoxLayout()
+            layout = QVBoxLayout()
+            
+            layout.addLayout(layout_commands)
+            layout.addLayout(layout_columns)
+            
+            self.setLayout(layout)
+            
+            layout_columns.addWidget(self.first_column)
+            layout_columns.addWidget(self.second_column)
+            layout_columns.addWidget(self.third_column)
+            layout_columns.addWidget(self.forth_column)
+            
+            #layout_columns.addWidget(self.forth_column)
+            
+            #layout_commands.addWidget(self.radio_dev)
+            #layout_commands.addWidget(self.radio_prod)
+            layout_commands.addWidget(self.list_detector_Box)
+            layout_commands.addWidget(self.pbar)
+            layout_commands.addWidget(self.paramValue)
+            layout_commands.addWidget(self.modButton)
+            
+            self.setGeometry(50, 50, 800,400)
+            self.setWindowTitle("ConfigDB modifier")
+            self.show()
+        else:
+            msgBox = QMessageBox()
+            msgBox.setIcon(QMessageBox.Warning)
+            msgBox.setStandardButtons(QMessageBox.Ok)
+            msgBox.setText(f"Please activate your kerberos ticket: kinit");
+            msgBox.exec();
+            exit()
+    
+    
+    # check if kerberos ticket is present
+    def check_kerb(self):
+        return True if subprocess.call(['klist', '-s']) == 0 else False
 
     # when Modify is clicked, a popup shows which modifications are going to be performed 
     def modify_database(self):
@@ -105,7 +113,7 @@ class ConfigdbGUI(QWidget):
 
     #If from popup OK then continue with modification, Cancel otherwise
     def msgbtn(self,i):
-        print(i.text())
+        #print(i.text())
         if i.text() == "OK":
             if self.paramValue.text() != '':
                 stdout = configdb_multimod(URI_CONFIGDB = self.data["URI"], DEV = self.dev, ROOT_CONFIGDB = self.data["Root"], INSTRUMENT = self.hutch, DETECTOR=self.second_column_selection, CONFIG_KEY=self.full_path_param[::-1] , CONFIG_VALUE=self.paramValue.text(), MODIFY=True)
@@ -113,21 +121,6 @@ class ConfigdbGUI(QWidget):
                 
         else:
             return
-
-    # choosing dev or prod
-    def radioURIselected(self):
-        if self.radio_dev.isChecked():
-            self.data['URI']='https://pswww.slac.stanford.edu/ws-kerb/devconfigdb/ws'
-            self.first_column.setHeaderLabel("Dev")
-        else:
-            self.data['URI']='https://pswww.slac.stanford.edu/ws-kerb/configdb/ws'
-            self.first_column.setHeaderLabel("Prod")
-        
-        self.third_column.clear()
-        self.second_column.clear()
-        self.first_column.clear()
-        self.second_column_selection=[]
-        self.load_data_first_column()
     
     #function to create tree in column 1 and 3
     def buildtree(self, value, parent):
@@ -209,7 +202,7 @@ class ConfigdbGUI(QWidget):
     def set_default(self):
         self.data["URI"]    = 'https://pswww.slac.stanford.edu/ws-kerb/configdb/ws'
         self.data["Root"]   = 'configDB'
-        self.data["Inst"]   = 'TMO'
+        self.data["hutch"]   = 'TMO'
          
     #load data for first column and dropdown
     def load_data_first_column(self):
@@ -218,7 +211,7 @@ class ConfigdbGUI(QWidget):
         self.third_column.clear()
         self.second_column_select=[]
         
-        self.confdb = configdb(self.data['URI'], self.data['Inst'], create=False, root=self.data['Root'])
+        self.confdb = configdb(self.data['URI'], self.data['hutch'], create=False, root=self.data['Root'])
         self.first_column.clear()
         list_of_hutch_names = self.confdb.get_hutches() # ['TMO', 'CXI', etc.]
         #print(list_of_hutch_names)
@@ -228,7 +221,7 @@ class ConfigdbGUI(QWidget):
         config_hutch={}
         
         pbar_steps = range(0, 100, int(100./len(list_of_hutch_names)))
-        print(len(list_of_hutch_names))
+        #print(len(list_of_hutch_names))
         i=0
         self.pbar.setValue(i) 
         for myhutch in list_of_hutch_names:
