85754deb7220d75cc5bd04ed9fb7a3640ec33c32
valmar <valmar@users.noreply.github.com>
2024-11-18 13:12:19 -0800

    Update conda installations to v4

--- setup_env.sh
+++ setup_env.sh
@@ -4,13 +4,13 @@ unset PYTHONPATH
 if [ -d "/sdf/group/lcls/" ]
 then
     # for s3df
-    source /sdf/group/lcls/ds/ana/sw/conda2/inst/etc/profile.d/conda.sh
+    source /sdf/group/lcls/ds/ana/sw/conda2-v4/inst/etc/profile.d/conda.sh
     export CONDA_ENVS_DIRS=/sdf/group/lcls/ds/ana/sw/conda2/inst/envs
     export DIR_PSDM=/sdf/group/lcls/ds/ana/
     export SIT_PSDM_DATA=/sdf/data/lcls/ds/
 else
     # for psana
-    source /cds/sw/ds/ana/conda2-v2/inst/etc/profile.d/conda.sh
+    source /cds/sw/ds/ana/conda2-v4/inst/etc/profile.d/conda.sh
     export CONDA_ENVS_DIRS=/cds/sw/ds/ana/conda2/inst/envs/
     export DIR_PSDM=/cds/group/psdm
     export SIT_PSDM_DATA=/cds/data/psdm
