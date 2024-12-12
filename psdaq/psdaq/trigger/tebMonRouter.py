7b24da7e4881625ef5382f0848156dafac704718
Matt Weaver <weaver@slac.stanford.edu>
2024-12-12 08:04:43 -0800

    Fixed cython EbDgram.xtc.payload truncation. Added TimingTebData trigger data type.  Fixed tebPyTrigger EbDgram copies to shmem to use buffer sized for correct contribution.

---
+++ psdaq/psdaq/trigger/tebMonRouter.py
@@ -0,0 +1,103 @@
+from psdaq.trigger import tebTrigger
+from TimingTebData import TimingTebData
+from TmoTebData import TmoTebData
+import json
+import logging
+
+logging.warning(f"[Python] tebMonRouter script starting")
+
+ds = tebTrigger.TriggerDataSource()
+
+connect_info = json.loads(ds.connect_json)
+
+acr_eventcode = 30
+ami_eventcode = 256
+
+class MebScheduler(object):
+
+    def __init__(self):
+        self._idx = 0
+        self._val = []
+        self._nval = 0
+
+    def insert(self, node):
+        self._val.append(1<<node)
+        self._nval += 1
+
+    #  Attempt some round-robin here, since a partial mask 
+    #  disables the teb round-robin
+    def schedule(self):
+        if self._nval==0:
+            return 0
+        idx = self._idx
+        self._idx += 1
+        if self._idx == self._nval:
+            self._idx = 0
+        return self._val[idx]
+
+# Identify the available MEBs, if any
+acr_mebs = MebScheduler()
+ami_mebs = MebScheduler()
+usr_mebs = MebScheduler()
+
+if 'meb' in connect_info['body'].keys():
+    for nodes in connect_info['body']['meb'].values():
+        node = nodes['meb_id']
+        if   'acr-meb' in nodes['proc_info']['alias']:
+            acr_mebs.insert(node)
+        elif 'ami-meb' in nodes['proc_info']['alias']:
+            ami_mebs.insert(node)
+        else:
+            usr_mebs.insert(node)
+
+logging.warning(f'[Python] MEBs:  acr {acr_mebs._val}  ami {ami_mebs._val}  usr {usr_mebs._val}')
+
+drp_ids = dict()
+drps    = dict()
+for nodes in connect_info['body']['drp'].values():
+    # For doing "ctrb.xtc.src.value() == <detector>_id" conditionals
+    drp_ids[nodes['proc_info']['alias']] = nodes['drp_id']
+
+    # For doing "drps[ctrb.xtc.src.value()] == '<detector>'" conditionals
+    drps[nodes['drp_id']] = nodes['proc_info']['alias']
+
+logging.warning(f'[Python] drp_ids {drp_ids}')
+
+# For doing "ctrb.xtc.src.value() == <detector>_id" conditionals
+timing_id  = drp_ids["timing_1"]      if "timing_1"      in drp_ids else -1
+logging.warning(f'[Python] timing_id {timing_id}')
+
+num_event = 0
+
+for event in ds.events():
+    num_event += 1
+
+    persist = True
+    mebs    = 0
+
+    for ctrb in event:
+
+        if ctrb.xtc.src.value() == timing_id:
+
+            payld = ctrb.xtc.payload()
+            data = TimingTebData(payld)
+            eventcodes = data.eventcodes
+            
+            logging.warning(f'[Python] Eventcodes: {eventcodes}')
+
+            if acr_eventcode in eventcodes:
+                mebs = acr_mebs.schedule()
+            elif ami_eventcode in eventcodes:
+                mebs = ami_mebs.schedule()
+            else:
+                mebs = usr_mebs.schedule()
+                
+        else:
+            payld = ctrb.xtc.payload()
+            data = TmoTebData(payld)
+
+            logging.warning(f'[Python] write {data.write:x}')
+
+    ds.result(persist, mebs)
+
+print(f"[Python] tebMonRouter script exiting; {num_event} events handled")
---
+++ psdaq/psdaq/trigger/timingTebPrimitive.cc
@@ -0,0 +1,64 @@
+#include "TriggerPrimitive.hh"
+#include "TimingTebData.hh"
+#include "utilities.hh"
+#include "drp/drp.hh"
+#include "drp/TimingDef.hh"
+#include "xtcdata/xtc/Dgram.hh"
+
+#include <cstdint>
+#include <stdio.h>
+
+using namespace rapidjson;
+using json = nlohmann::json;
+
+namespace Pds {
+  namespace Trg {
+
+    class TimingTebPrimitive : public TriggerPrimitive
+    {
+    public:
+      int    configure(const Document& top,
+                       const json&     connectMsg,
+                       size_t          collectionId) override;
+      void   event(const Drp::MemPool& pool,
+                   uint32_t            idx,
+                   const XtcData::Xtc& ctrb,
+                   XtcData::Xtc&       xtc,
+                   const void*         bufEnd) override;
+      size_t size() const  { return sizeof(TimingTebData); }
+    };
+  };
+};
+
+
+using namespace Pds::Trg;
+
+int Pds::Trg::TimingTebPrimitive::configure(const Document& top,
+                                            const json&     connectMsg,
+                                            size_t          collectionId)
+{
+    return 0;
+}
+
+void Pds::Trg::TimingTebPrimitive::event(const Drp::MemPool& pool,
+                                         uint32_t            idx,
+                                         const XtcData::Xtc& ctrb,
+                                         XtcData::Xtc&       xtc,
+                                         const void*         bufEnd)
+{
+    //  Step through the xtc: shapesdata->data
+    const XtcData::Xtc& shdat = *reinterpret_cast<const XtcData::Xtc*>(ctrb.payload());
+    const XtcData::Xtc& dat = *reinterpret_cast<const XtcData::Xtc*>(shdat.payload());
+    const Drp::TimingData& tdat = *new(dat.payload()) Drp::TimingData;
+
+    const uint32_t* eventcodes_ = reinterpret_cast<const uint32_t*>(tdat.sequenceValues);
+
+    new(xtc.alloc(sizeof(TimingTebData), bufEnd)) TimingTebData(eventcodes_);
+}
+
+// The class factory
+
+extern "C" Pds::Trg::TriggerPrimitive* create_producer_timing()
+{
+    return new Pds::Trg::TimingTebPrimitive;
+}
