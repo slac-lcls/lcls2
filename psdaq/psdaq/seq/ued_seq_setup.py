358693d6625c4100c87bff242f0f66897aeb8063
Christopher O'Grady <cpo@slac.stanford.edu>
2025-01-31 17:34:09 -0800

    update documentation for unspecified ued trigger period

--- psdaq/psdaq/seq/ued_seq_setup.py
+++ psdaq/psdaq/seq/ued_seq_setup.py
@@ -8,7 +8,7 @@ def main():
     parser = argparse.ArgumentParser(description='ued sequencer programming')
     parser.add_argument('--pv', type=str, default='DAQ:UED:XPM:0', help="sequence engine pv; default DAQ:UED:XPM:0")
     parser.add_argument("--eng", type=int, default=0, help="sequence engine; default=0")
-    parser.add_argument("--period", type=float, default=None, help="period (sec)")
+    parser.add_argument("--period", type=float, default=None, help="period (sec). If unspecified: default to 360Hz on timeslot 1.")
     args = parser.parse_args()
 
     if args.period is None:
