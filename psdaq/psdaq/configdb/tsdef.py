87314d3d1870b8de3a3054612ca6dcaed3e76f6e
Matt Weaver <weaver@slac.stanford.edu>
2025-02-11 12:59:30 -0800

    Add timing definitions for UED marker ordering.

--- psdaq/psdaq/configdb/tsdef.py
+++ psdaq/psdaq/configdb/tsdef.py
@@ -14,6 +14,18 @@ if 'ued' in socket.gethostname():
                       "1kH"  :{"intv":500   ,"marker":5}, 
                       "500H" :{"intv":1000  ,"marker":6}, 
                       "1H"   :{"intv":500000,"marker":7}}
+
+    acRates     = ['60Hz','30Hz','10Hz','5Hz','1Hz','0.5Hz']
+    acTS        = ['TS%u'%(i+1) for i in range(6)]
+    acRateHzToMarker    = {'60Hz':0, '30Hz':1, '10Hz':2, '5Hz':3, '1Hz':4, '0_5Hz':5 }
+    ACIntvs    = [1, 2, 6, 12, 60, 120]
+    
+    ACIntvsDict = {"0.5H":{"intv":910000,"marker":5}, 
+                   "1H"  :{"intv":91000 ,"marker":4}, 
+                   "5H"  :{"intv":9100  ,"marker":3}, 
+                   "10H" :{"intv":910   ,"marker":2}, 
+                   "30H" :{"intv":91    ,"marker":1}, 
+                   "60H" :{"intv":13    ,"marker":0}}
 else:
     fixedRates  = ['1.02Hz','10.2Hz','102Hz','1.02kHz','10.2kHz','71.4kHz','929kHz']
     fixedRateHzToMarker = {'929kHz':6, '71kHz':5, '10kHz':4, '1kHz':3, '100Hz':2, '10Hz':1, '1Hz':0}
@@ -26,17 +38,17 @@ else:
                       "70kH" :{"intv":13    ,"marker":5}, 
                       "910kH":{"intv":1     ,"marker":6}}
 
+    acRates     = ['0.5Hz','1Hz','5Hz','10Hz','30Hz','60Hz']
+    acTS        = ['TS%u'%(i+1) for i in range(6)]
+    acRateHzToMarker    = {'60Hz':5, '30Hz':4, '10Hz':3, '5Hz':2, '1Hz':1, '0_5Hz':0 }
+    ACIntvs    = [120, 60, 12, 6, 2, 1]
+    
+    ACIntvsDict = {"0.5H":{"intv":910000,"marker":0}, 
+                   "1H"  :{"intv":91000 ,"marker":1}, 
+                   "5H"  :{"intv":9100  ,"marker":2}, 
+                   "10H" :{"intv":910   ,"marker":3}, 
+                   "30H" :{"intv":91    ,"marker":4}, 
+                   "60H" :{"intv":13    ,"marker":5}}
 
-acRates     = ['0.5Hz','1Hz','5Hz','10Hz','30Hz','60Hz']
-acTS        = ['TS%u'%(i+1) for i in range(6)]
-acRateHzToMarker    = {'60Hz':5, '30Hz':4, '10Hz':3, '5Hz':2, '1Hz':1, '0_5Hz':0 }
-ACIntvs    = [120, 60, 12, 6, 2, 1]
-
-ACIntvsDict = {"0.5H":{"intv":910000,"marker":0}, 
-               "1H"  :{"intv":91000 ,"marker":1}, 
-               "5H"  :{"intv":9100  ,"marker":2}, 
-               "10H" :{"intv":910   ,"marker":3}, 
-               "30H" :{"intv":91    ,"marker":4}, 
-               "60H" :{"intv":13    ,"marker":5}}
 
 
