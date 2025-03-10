04f69f77d1600dc6394e270498017d9613ddd54e
msdubrovin <msdubrovin@gmail.com>
2025-03-10 11:52:56 -0700

    add script to list location of logfiles and results

---
+++ psana/psana/app/logfiles.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from psana.detector.Utils import str_tstamp
+
+DIR_ATSTART = '/sdf/group/lcls/ds/ana/detector/logs/atstart'
+DIR_CONSTANTS = '/sdf/group/lcls/ds/ana/detector/calib2/constants'
+SCRNAME = sys.argv[0].rsplit('/')[-1]
+
+USAGE = 'Usage:'\
+      + '\n%s [-d <detector-name>] [-y <year>] [-L <logging-mode>] [...]' % SCRNAME\
+      + '\nTEST COMMAND:'\
+      + '\n  %s -d jungfrau' % SCRNAME\
+      + '\n  %s ' % SCRNAME\
+      + '\nREGULAR COMMAND:'\
+      + '\n  %s -d jungfrau' % SCRNAME
+
+def argument_parser():
+    from argparse import ArgumentParser
+
+#    d_dskwargs= None
+    d_detname = None # 'jungfrau'
+    d_year = str_tstamp(fmt='%Y', time_sec=None) # 2025
+    d_diratstart = DIR_ATSTART
+    d_dirconstants = DIR_CONSTANTS
+    d_deploy  = False
+    d_logmode = 'INFO'
+    d_version = 'V2025-03-10'
+    d_comment = 'no comment'
+
+#    h_dskwargs= 'string of comma-separated (no spaces) simple parameters for DataSource(**kwargs),'\
+#                ' ex: exp=<expname>,run=<runs>,dir=<xtc-dir>, ...,'\
+#                ' or <fname.xtc> or files=<fname.xtc>'\
+#                ' or pythonic dict of generic kwargs, e.g.:'\
+#                ' \"{\'exp\':\'tmoc00318\', \'run\':[10,11,12], \'dir\':\'/a/b/c/xtc\'}\", default = %s' % d_dskwargs
+    h_detname = 'detector name, default = %s' % d_detname
+    h_year = 'year, default = %s' % d_year
+    h_diratstart = 'non-default repository of atstart logfiles, default = %s' % d_diratstart
+    h_dirconstants = 'non-default repository of constants logfiles, default = %s' % d_dirconstants
+    h_logmode = 'logging mode, default = %s' % d_logmode
+    h_version = 'constants version, default = %s' % str(d_version)
+    h_comment = 'comment added to constants metadata, default = %s' % str(d_comment)
+
+    parser = ArgumentParser(description='Deploy calibration files from repository to DB.', usage = USAGE)
+#    parser.add_argument('-k', '--dskwargs',default=d_dskwargs, type=str,   help=h_dskwargs)
+    parser.add_argument('-d', '--detname',      default=d_detname,      type=str,   help=h_detname)
+    parser.add_argument('-y', '--year',         default=d_year,         type=str,   help=h_year)
+    parser.add_argument('-s', '--diratstart',   default=d_diratstart,   type=str,   help=h_diratstart)
+    parser.add_argument('-c', '--dirconstants', default=d_dirconstants, type=str,   help=h_dirconstants)
+    parser.add_argument('-L', '--logmode',      default=d_logmode,      type=str,   help=h_logmode)
+    parser.add_argument('-v', '--version',      default=d_version,      type=str,   help=h_version)
+    parser.add_argument('-C', '--comment',      default=d_comment,      type=str,   help=h_comment)
+
+    return parser
+
+
+def do_main():
+
+    parser = argument_parser()
+    args = parser.parse_args()
+    #opts = vars(args)
+
+#    if len(sys.argv)<3: sys.exit('\n%s\n\nEXIT DUE TO MISSING ARGUMENTS\n' % USAGE)
+#    assert args.dskwargs is not None, 'WARNING: option "-k <DataSource-kwargs>" MUST be specified.'
+#    assert args.detname is not None, 'WARNING: option "-d <detector-name>" MUST be specified.'
+#    logger.info('is completed, consumed time %.3f sec' % (time() - t0_sec))
+
+    print('DIR ATSTART: %s' % args.diratstart)
+    print('DIR CONSTANTS: %s' % args.dirconstants)
+    print('current time: %s' % str_tstamp(fmt='%Y%m%d%H%M%S', time_sec=None))
+    print('for year: %s' % args.year)
+
+    path_atstart = os.path.join(args.diratstart, args.year)
+    list_of_files = '\n    '.join(['%s/%s' % (path_atstart,fname) for fname in os.listdir(path_atstart)])
+    print('list of files in %s\n    %s' % (path_atstart, list_of_files))
+
+    list_of_dirs = '\n    '.join(['%s/%s' % (args.dirconstants,fname) for fname in os.listdir(args.dirconstants)])
+    print('list of subdirs for detectors\n    %s' % list_of_dirs)
+
+    path_scrs = os.path.join(args.dirconstants, 'scripts')
+    list_of_scrs = '\n    '.join(['%s/%s' % (path_scrs,fname) for fname in os.listdir(path_scrs)])
+    print('list of subdirs for scripts in %s\n    %s' % (path_scrs, list_of_scrs))
+
+
+if __name__ == "__main__":
+    do_main()
+    sys.exit('End of %s'%SCRNAME)
+
+# EOF
