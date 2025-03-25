73b1dcf84e1c027981cd95fdd15d2bbb13c80a20
Riccardo Melchiorri <melchior@slac.stanford.edu>
2025-03-25 16:50:06 -0700

    removing loop over asics, modify name of automatic gain level, and forcing to use events defined by start stop step

--- psdaq/psdaq/cas/epixuhr_chargeinj_scan.py
+++ psdaq/psdaq/cas/epixuhr_chargeinj_scan.py
@@ -10,11 +10,10 @@ nColumns = 384
 def main():
 
     # default command line arguments
-    defargs = {'--events'  :1024,
-               '--hutch'   :'rix',
-               '--detname' :'epixm_0',
-               '--scantype':'chargeinj',
-               '--record'  :1,
+    defargs = {'--hutch'   : 'asc',
+               '--detname' : 'epixuhr_0',
+               '--scantype': 'chargeinj',
+               '--record'  : 1,
                }
 
     aargs = [('--pulserStep',  {'type':int,'default':1,'help':'charge injection ramp step (default 1)'}),
@@ -31,26 +30,26 @@ def main():
     scan = ConfigScanBase(userargs=aargs, defargs=defargs)
 
     args = scan.args
-
-    events = np.abs((args.pulserStart-args.pulserStop)/args.pulserStep) # The 1024 is hardcoded in firmware
-    print(f' Number of events: {events}')
+    args.events = np.abs((args.pulserStart-args.pulserStop)/args.pulserStep) 
+    
+    print(f' Number of events: {args.events}')
     gains_dict={}
-    gains_dict['FHGIoff'] =32
-    gains_dict['FHGIon']  =36
-    gains_dict['FMGIoff'] =40
-    gains_dict['FMGIon']  =44
-    gains_dict['FLG1Ioff']=1
-    gains_dict['FLG1Ion' ]=5
-    gains_dict['FLG2Ioff']=33
-    gains_dict['FLG2Ion'] =37
-    gains_dict['AHG1Ioff'] =16
-    gains_dict['AHG1Ion'] =20
-    gains_dict['AHG2Ioff'] =48
-    gains_dict['AHG2Ion'] =52
-    gains_dict['AMG1Ioff'] =24
-    gains_dict['AMG1Ion'] =28
-    gains_dict['AMG2Ioff'] =56
-    gains_dict['AMG2Ion'] =60    
+    gains_dict['FHGIoff']   = 32
+    gains_dict['FHGIon']    = 36
+    gains_dict['FMGIoff']   = 40
+    gains_dict['FMGIon']    = 44
+    gains_dict['FLG1Ioff']  = 1
+    gains_dict['FLG1Ion' ]  = 5
+    gains_dict['FLG2Ioff']  = 33
+    gains_dict['FLG2Ion']   = 37
+    gains_dict['AHLG1Ioff'] = 16
+    gains_dict['AHLG1Ion']  = 20
+    gains_dict['AHLG2Ioff'] = 48
+    gains_dict['AHLG2Ion']  = 52
+    gains_dict['AMLG1Ioff'] = 24
+    gains_dict['AMLG1Ion']  = 28
+    gains_dict['AMLG2Ioff'] = 56
+    gains_dict['AMLG2Ion']  = 60    
     
     keys = []
     keys.append(f'{args.detname}:user.App.VINJ_DAC.enable')
@@ -131,13 +130,11 @@ def main():
                 gain_mode=gain_mode.strip()
                 metad['gain_mode'] = gain_mode
                 d[f'{args.detname}:user.Gain.SetGainValue']           = gains_dict[gain_mode]
-                #d[f'{args.detname}:user.gain_mode'] = int(gain_mode_value(gain_mode))
+                
                 print(f'gain_mode {gain_mode}:{gains_dict[gain_mode]}')
-                for asic in args.asics:
-                    metad['asic'] = asic
-                    metad['step'] = step
-                    yield (d, float(step), json.dumps(metad))
-                    step += 1
+                metad['step'] = step
+                yield (d, float(step), json.dumps(metad))
+                step += 1
 
     scan.run(keys,steps)
 
