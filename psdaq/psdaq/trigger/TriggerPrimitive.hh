a795fd253ca834e23e5b6fa326c4f7d48ef016f0
Ric Claus <claus@slac.stanford.edu>
2025-04-07 14:51:00 -0700

    TriggerPrimitives: Provide an event() method for GPUs

--- psdaq/psdaq/trigger/TriggerPrimitive.hh
+++ psdaq/psdaq/trigger/TriggerPrimitive.hh
@@ -6,16 +6,24 @@
 
 #include "rapidjson/document.h"
 
+#ifdef __NVCC__
+#include <cuda_runtime.h>               // For cudaStream_t
+#else
+struct CUstream_st;
+typedef struct CUstream_st* cudaStream_t;
+#endif
+
 #include <cstdint>
 #include <string>
+#include <cassert>
 
 namespace XtcData {
   class Xtc;
-};
+}
 
 namespace Drp {
   class MemPool;
-};
+}
 
 namespace Pds {
   namespace Trg {
@@ -33,9 +41,16 @@ namespace Pds {
                            const XtcData::Xtc& contribution,
                            XtcData::Xtc&       xtc,
                            const void*         bufEnd) = 0;
+      // This method can't be left pure virtual for non-GPU use so it is
+      // defaulted to an empty block that is never called by non-GPU code
+      virtual void   event(cudaStream_t&     stream,
+                           float*            calibBuffers,
+                           uint32_t** const* out,
+                           unsigned&         index,
+                           bool&             done) { assert(false); } //= 0;
       virtual size_t size() const = 0;
     };
-  };
-};
+  }
+}
 
 #endif
