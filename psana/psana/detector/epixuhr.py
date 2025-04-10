0dbdde9eabf5add6d0f1d1565a2fabefe0ca3ab4
msdubrovin <msdubrovin@gmail.com>
2025-04-10 15:02:50 -0700

    cosmetics

--- psana/psana/detector/epixuhr.py
+++ psana/psana/detector/epixuhr.py
@@ -38,14 +38,14 @@ class epixuhrhw_config_2_1_0(DetectorImpl):
 class epixuhr_config_2_0_0(DetectorImpl):
     def __init__(self, *args, **kwargs):
         super().__init__(*args)
-               
+
 class epixuhr_raw_0_0_0(eb.epix_base):
     def __init__(self, *args, **kwargs):
         eb.epix_base.__init__(self, *args, **kwargs)
         self._seg_geo = eb.sgs.Create(segname='EPIXUHRASIC:V1')
         self._path_geo_default = 'pscalib/geometry/data/geometry-def-epixuhr.data'
         self._segment_numbers = [0,1,2,3]
-        
+
     def _array(self, evt) -> Array2d:
         f = None
         segs = self._segments(evt)
