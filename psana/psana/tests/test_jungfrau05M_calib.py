0078d9a99296cfe1f2767a8fa3e72148385d6e4f
msdubrovin <msdubrovin@gmail.com>
2025-04-16 16:37:38 -0700

    add a few prints for debugging

--- psana/psana/tests/test_jungfrau05M_calib.py
+++ psana/psana/tests/test_jungfrau05M_calib.py
@@ -4,19 +4,20 @@ import numpy as np
 
 def test_jungfrau05M_calib():
     correctanswer=np.array(([4.0649414, 9.653076, 15.843018, -12.125977, -24.302979],\
-                            [0.06494141, -6.346924, -27.156982, -3.1259766, -15.3029785],\
-                            [15.064941, 14.653076, 13.843018, 25.874023, 4.6970215]))
+                            [0.06494141, -6.346924, -27.156982, -3.1259766, -15.3029785]))
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    ds = DataSource(files=os.path.join(dir_path,'test_data/detector/test_jungfrau05M_calib.xtc2'))
+    dirscr = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(dirscr,'test_data/detector/test_jungfrau05M_calib.xtc2')
+    print('DEBUG path to data:', path)
+    ds = DataSource(files=path)
     myrun = next(ds.runs())
-    epix = myrun.Detector('jungfrau')
+    odet = myrun.Detector('jungfrau')
     for nevt,evt in enumerate(myrun.events()):
         if nevt>1: break
-        image=epix.raw.image(evt)
+        image=odet.raw.image(evt)
         calibsample=image[10][0:5]
-        #print('DEBUG calibsample  ', calibsample)
-        #print('DEBUG correctanswer', correctanswer[nevt][0:5])
+        print('DEBUG calibsample  ', calibsample)
+        print('DEBUG correctanswer', correctanswer[nevt][0:5])
         assert np.allclose(correctanswer[nevt][0:5], calibsample, rtol=.001)
 
 if __name__ == "__main__":
