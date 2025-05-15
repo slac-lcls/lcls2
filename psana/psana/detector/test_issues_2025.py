a6a794cee61b23a7d7850e9e807f9b184f134ce5
msdubrovin <msdubrovin@gmail.com>
2025-05-15 10:17:11 -0700

    add issue_2025_05_14 for CustomQComboBox

--- psana/psana/detector/test_issues_2025.py
+++ psana/psana/detector/test_issues_2025.py
@@ -877,6 +877,61 @@ def issue_2025_05_07():
         print('median dt, msec: %.3f' % np.median(arrdt))
 
 
+def issue_2025_05_14():
+    """test QComboBox for control_gui"""
+
+    from PyQt5.QtWidgets import QComboBox, QMainWindow, QApplication
+    from PyQt5.QtCore import Qt
+    import sys
+
+    class CustomQComboBox(QComboBox):
+        def __init__(self, parent=None):
+            super().__init__(parent)
+
+        def keyPressEvent(self, event):
+            #print('event.key():', event.key())
+            if event.key() in (Qt.Key_Up, Qt.Key_Down):
+                event.ignore()  # Ignore up and down arrow keys
+            else:
+                super().keyPressEvent(event) # Default behavior for other keys
+
+        def wheelEvent(self, event):
+            event.ignore()
+            #print('event:', dir(event), '\n')
+            #print('event.angleDelta().y():', event.angleDelta().y(), '\n')
+            #super().wheelEvent(event) # Default behavior
+            #if event.angleDelta().y() in (120, -120):
+            #    event.ignore()  # Ignore up and down arrow keys
+            #else:
+            #    super().keyPressEvent(event) # Default behavior for other keys
+
+    class MainWindow(QMainWindow):
+        def __init__(self):
+            super().__init__()
+            combobox = CustomQComboBox()
+            combobox.addItems(['One', 'Two', 'Three', 'Four'])
+            # Connect signals to the methods.
+            combobox.activated.connect(self.activated)
+            combobox.currentTextChanged.connect(self.text_changed)
+            combobox.currentIndexChanged.connect(self.index_changed)
+
+            self.setCentralWidget(combobox)
+
+        def activated(Self, index):
+            print("Activated index:", index)
+
+        def text_changed(self, s):
+            print("Text changed:", s)
+
+        def index_changed(self, index):
+            print("Index changed", index)
+
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    app.exec_()
+
+
 #===
     
 #===
@@ -936,6 +991,7 @@ def selector():
     elif TNAME in ('21',): issue_2025_04_23() # cpo - jungfrau16M image for 4 drp panels
     elif TNAME in ('22',): issue_2025_04_29() # Philip/cpo - jungfrau16M constants shape:(3, 19, 512, 1024).
     elif TNAME in ('23',): issue_2025_05_07() # me - test of the detector axis shape:(796, 6144)
+    elif TNAME in ('24',): issue_2025_05_14() # me - test QComboBox for control_gui
     else:
         print(USAGE())
         exit('\nTEST "%s" IS NOT IMPLEMENTED'%TNAME)
