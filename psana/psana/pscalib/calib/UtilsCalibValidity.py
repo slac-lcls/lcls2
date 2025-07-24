b9e1ba62858a5598bada98734f2261cfea8b72be
msdubrovin <msdubrovin@gmail.com>
2025-07-24 10:53:08 -0700

    add module UtilsCalibValidity.py for calibvalidity CLI

---
+++ psana/psana/pscalib/calib/UtilsCalibValidity.py
@@ -0,0 +1,127 @@
+
+"""calibration validity info
+
+   import psana.pscalib.calib.UtilsCalibValidity as ucv
+"""
+import psana.pscalib.calib.MDBUtils as mu
+import psana.pscalib.calib.MDBWebUtils as wu
+from time import time, gmtime, localtime, strftime
+
+def dict_filter(keys=('experiment', 'detname', 'detector', 'shortname', 'ctype', 'run', 'run_orig',\
+                      'run_beg', 'run_end', 'time_stamp', 'tstamp_orig', 'dettype', 'iofname', 'version')):
+    import psana.detector.utils_psana as ups # seconds, data_source_kwargs
+    return ups.dict_filter(doc, list_keys=keys)
+
+def tstamp_from_sec(tsec, fmt='%Y%m%d_%H%M%S', gmt=False):
+    return strftime(fmt, gmtime(tsec) if gmt else localtime(tsec))
+
+def add_run_from_doc(runs, d, irun_max=9999):
+    for r in (d['run'], d['run_end']):
+        runs.add(r if r != 'end' else irun_max)
+
+def add_time_from_doc(times_sec, time_stamps, d):
+    times_sec.add(d['time_sec'])
+    time_stamps.add(d['time_stamp'])
+
+def select_doc_for_tsec(sorted_docs, tsec, key='time_sec'):
+    for i,d in enumerate(sorted_docs):
+        if d[key] >= tsec:
+            #print('XXX selected sorted document #%d' % i)
+            return d
+
+def info_run_validity_ranges(dbname, colname, ctype='pedestals'):
+    """works with EXPERIMENT DB
+       1) extend documents with tsec_id, tstamp_id evaluated form _id
+       2) make list of begin and end runs
+       2) select doc for each run range interval
+    """
+    docs = wu.find_docs(dbname, colname, query={'ctype':ctype}) # , url=cc.URL)
+    s = '\nlist of docs for dbname:%s colname:%s ctype:%s' % (dbname, colname, ctype)
+    run_boardes = set()
+    for d in docs:
+      d['tsec_id'], d['tstamp_id'] = mu.sec_and_ts_from_id(d['_id'], fmt='%Y%m%d_%H%M%S', gmt=False)
+      s += '\ntsecDB: %d tstampDB: %s run: %4d run_orig: %4d run_beg: %4s run_end: %4s'%\
+            (d['tsec_id'], d['tstamp_id'], d['run'], d['run_orig'], str(d.get('run_beg',None)), str(d['run_end']))
+      add_run_from_doc(run_boardes, d)
+
+    sorted_runs = sorted(run_boardes)
+    s += '\nsorted validity boarder runs: %s' % str(sorted(sorted_runs))
+    s += '\n\nrun ranges for EXPERIMENT DB dbname:%s colname:%s ctype:%s' % (dbname, colname, ctype)
+    for rbeg, rend in zip(sorted_runs[:-1], sorted_runs[1:]):
+        doc = wu.select_doc_in_run_range(docs, rbeg)
+        s += '\nrun range: %4d -%4d tstampDB: %s tsecDB: %d run_orig: %4d'%\
+            (rbeg, rend-1, doc['tstamp_id'], doc['tsec_id'], doc['run_orig'])
+    return s
+
+def info_time_validity_ranges(shortname, ctype='pedestals', fmt='%Y%m%d_%H%M%S', gmt=False):
+    """works with detector DB
+    """
+    dbname = 'cdb_%s' % shortname
+    colname = shortname
+    docs = wu.find_docs(dbname, colname, query={'ctype':ctype}) # , url=cc.URL)
+    s = '\nDB document keys: %s' % str(docs[0].keys())
+    s += '\n\nlist of docs for dbname:%s colname:%s ctype:%s' % (dbname, colname, ctype)
+    times_sec = set()
+    time_stamps = set()
+    sorted_docs = sorted(docs, key=lambda x: x['time_sec'])
+    for d in sorted_docs:
+      s += '\ntime_sec: %d time_stamp: %s tstamp_orig: %s run_orig: %4d experiment: %12s'%\
+            (d['time_sec'], d['time_stamp'], d['tstamp_orig'], d['run_orig'], d['experiment'])
+      add_time_from_doc(times_sec, time_stamps, d)
+
+    sorted_times = sorted(times_sec)
+    s += '\n\nsorted validity boarder times (sec): %s' % str(sorted(sorted_times))
+    s += '\n\ntime ranges for DETECTOR DB dbname:%s colname:%s ctype:%s' % (dbname, colname, ctype)
+    for tbeg, tend in zip(sorted_times[:-1], sorted_times[1:]):
+        d = select_doc_for_tsec(sorted_docs, tbeg)
+        s += '\ntime stamp range: [%s - %s) sec: %d - %d  exp: %s run_orig: %4d'%\
+              (tstamp_from_sec(tbeg), tstamp_from_sec(tend), tbeg, tend, d['experiment'], d['run_orig'])
+    return s
+
+
+def print_calib_constants_for_ctype(expname, detlongname, ctype='pedestals', run=None, time_sec=None):
+    resp = wu.calib_constants_for_ctype(detlongname, exp=expname, ctype=ctype, run=run, time_sec=time_sec, vers=None, dbsuffix='')
+    print('selected constants in calib_constants_for_ctype:\n%s\n' % (str(resp[1] if resp is not None else None)))
+
+
+def _calib_validity_ranges(exp, shortname, ctype='pedestals'):
+    dbname = 'cdb_%s' % exp
+    colname = shortname
+    print(info_time_validity_ranges(shortname, ctype))
+    print(info_run_validity_ranges(dbname, colname, ctype))
+
+
+def calib_validity_ranges(**kwa):
+
+    print('parameters: %s' % str(kwa))
+
+    exp = None
+    shortname = None
+    ctype  = kwa.get('ctype', 'pedestals')
+    direct = kwa.get('direct', None)
+    if direct is None:
+        from psana import DataSource
+        import psana.detector.UtilsCalib as uc
+        import psana.detector.utils_psana as up
+        dskwargs = up.data_source_kwargs(**kwa)
+        print('dskwargs:%s' % str(dskwargs))
+        ds = DataSource(**dskwargs)
+        orun = next(ds.runs())
+        odet = orun.Detector(kwa['detname'])
+        longname = odet.raw._uniqueid
+        shortname = uc.detector_name_short(longname)
+        exp = dskwargs['exp']
+        print_calib_constants_for_ctype(exp, longname, ctype, run=orun.runnum, time_sec=time())
+    else:
+        exp, shortname = direct.split(',')
+
+    print('exp: %s shortname: %s ctype: %s' % (exp, shortname, ctype))
+    _calib_validity_ranges(exp, shortname, ctype=ctype)
+
+
+if __name__ == "__main__":
+
+    _calib_validity_ranges('mfx101332224', 'jungfrau_000002', ctype='pedestals')
+
+    sys.exit('\nuse calibvalidity CLI')
+# EOF
