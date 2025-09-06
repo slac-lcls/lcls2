12ff873c7383a4dbc8b86db256eef4bc3173d4a0
msdubrovin <msdubrovin@gmail.com>
2025-09-05 17:42:04 -0700

    use return ... arrf * gfac * mask

--- psana/psana/detector/UtilsEpix100.py
+++ psana/psana/detector/UtilsEpix100.py
@@ -18,8 +18,10 @@ Created on 2022-02-07 by Mikhail Dubrovin
 from time import time
 
 import psana.detector.UtilsCommonMode as ucm
-
+import psana.detector.Utils as ut
 logger = ucm.logging.getLogger(__name__)
+
+is_none = ut.is_none
 
 info_ndarr = ucm.info_ndarr
 GAIN_FACTOR_DEFAULT = 0.06 # keV/ADU on 2022/0/03 gain factor(Philip) = 60 eV/ADU, gain(Conny) = 16.4 ADU/keV
@@ -36,7 +38,7 @@ def calib_epix100(det_raw, evt, cmpars=None, **kwa): #cmpars=(0,7,100,10)):
 
     gfac = det_raw._gain_factor()# - 3d gain factors shape:(1, 704, 768)
     peds = det_raw._pedestals()
-    mask = det_raw._mask()
+    mask = det_raw._mask(**kwa)
     _cmpars  = cmpars #(0,7,100,10) #None #cmpars # det_raw._common_mode() if cmpars is None else cmpars
 
     if peds is None: return raw
@@ -46,15 +48,15 @@ def calib_epix100(det_raw, evt, cmpars=None, **kwa): #cmpars=(0,7,100,10)):
 
     if _cmpars is not None: ucm.common_mode_apply(arrf, mask, cmpars=_cmpars)
 
-    logger.debug('calib_epix100 consumed time (sec) = %.6f  \n  %s\n  %s\n  %s\n  %s\n  %s\n' %(\
-      (time()-t0_sec),
-      info_ndarr(raw,'raw'),
-      info_ndarr(gfac,'gfac'),
-      info_ndarr(peds,'peds'),
-      info_ndarr(mask,'mask'),
-      info_ndarr(_cmpars,'cmpars')))
+#    logger.debug('calib_epix100 consumed time (sec) = %.6f  \n  %s\n  %s\n  %s\n  %s\n  %s\n' %(\
+#      (time()-t0_sec),
+#      info_ndarr(raw,'raw'),
+#      info_ndarr(gfac,'gfac'),
+#      info_ndarr(peds,'peds'),
+#      info_ndarr(mask,'mask'),
+#      info_ndarr(_cmpars,'cmpars')))
 
-    return arrf * gfac
+    return arrf * gfac if mask is None else arrf * gfac * mask
 
 
 def common_mode_increment(det_raw, evt, cmpars=None, **kwa):
