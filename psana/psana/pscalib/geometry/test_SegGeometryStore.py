5d48f9fe91d48bb8ccfc8ad314884068a4bf091d
msdubrovin <msdubrovin@gmail.com>
2025-09-18 11:13:50 -0700

    add test 15 for JUNGFRAU:V3

--- psana/psana/pscalib/geometry/test_SegGeometryStore.py
+++ psana/psana/pscalib/geometry/test_SegGeometryStore.py
@@ -65,6 +65,7 @@ def selector():
     elif(tname=='12'):sg = test_segname('EPIXMASIC:V1')
     elif(tname=='13'):sg = test_segname('EPIXUHRASIC:V1')
     elif(tname=='14'):sg = test_segname('MTRXANY:V1')
+    elif(tname=='15'):sg = test_segname('JUNGFRAU:V3')
     elif(tname=='99'):sg = sgs.Create(segname='ABRACADABRA:V1');\
         logger.info('Return for non-existent segment name: %s' % sg)
     else: logger.warning('NON-EXPECTED TEST NAME: %s\n\n%s' % (tname, usage()))
