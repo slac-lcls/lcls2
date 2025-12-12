b1503f5302f83ac40457513e6e3f723494ee8be4
msdubrovin <msdubrovin@gmail.com>
2025-12-12 09:33:02 -0800

    update variable types, size_t > sizeb_t = uint32_t

--- psana/psana/pycalgos/UtilsDetector.cc
+++ psana/psana/pycalgos/UtilsDetector.cc
@@ -20,21 +20,20 @@ namespace utilsdetector {
 // out = (raw-peds)*gain if mask>0 else 0
 // returns (double) execution time, us
 
-  /*
-//time_t calib_std_2(rawd_t *raw, peds_t *peds, gain_t *gain, mask_t *mask, const size_t& size, const rawd_t databits, out_t *out)
-time_t calib_std_2(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, const rawd_t databits, out_t *out)
+/*
+//time_t calib_std_2(rawd_t *raw, peds_t *peds, gain_t *gain, mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out)
+time_t calib_std_2(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out)
 {
   time_point_t t0 = time_now();
-  for (size_t i=0; i<size; ++i) {
+  for (sizeb_t i=0; i<size; ++i) {
     //out[i] = mask[i]>0 ? ((raw[i] & databits) - peds[i])*gain[i] : 0;
     out[i] = ((raw[i] & databits) - peds[i])*gain[i]*mask[i];
   }
   return duration_us(time_now() - t0).count();
 }
 
-
-  //time_t calib_std_v2(rawd_t *raw, peds_t *peds, gain_t *gain, mask_t *mask, const size_t& size, const rawd_t databits, out_t *out)
-time_t calib_std_v3(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, const rawd_t databits, out_t *out)
+//time_t calib_std_v2(rawd_t *raw, peds_t *peds, gain_t *gain, mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out)
+time_t calib_std_v3(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out)
 {
 //RAWD_T* end = raw+size;
   time_point_t t0 = time_now();
@@ -44,16 +43,17 @@ time_t calib_std_v3(const rawd_t *raw, const peds_t *peds, const gain_t *gain, c
   }
   return duration_us(time_now() - t0).count();
 }
-  */
+*/
 
-time_t calib_std(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, const rawd_t databits, out_t *out)
+
+time_t calib_std(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out)
 {
+  time_point_t t0 = time_now();
   const rawd_t *r = raw;
   const peds_t *p = peds;
   const gain_t *g = gain;
   const mask_t *m = mask;
   out_t *o = out;
-  time_point_t t0 = time_now();
   while (r<raw+size) {
     *o++ = ((*r++ & databits) - *p++)*(*g++)*(*m++);
   }
@@ -61,17 +61,15 @@ time_t calib_std(const rawd_t *raw, const peds_t *peds, const gain_t *gain, cons
 }
 
 
-time_t calib_jungfrau_v0(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, out_t *out)
+time_t calib_jungfrau_v0(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, out_t *out)
 {
   //raw[i], out[i], mask[i] shape:   (<number-of-segs>, 512, 1024)
   //peds[icc], gain[icc]    shape:(3, <number-of-segs>, 512, 1024)
-
-  rawd_t rawi;  // copy of raw[i]
-  uint8_t igr; // index of the gain range: 0,1,2
-  uint16_t icc; // pixel index in the array of calibration constants
-
   time_point_t t0 = time_now();
-  for (size_t i=0; i<size; ++i) {
+  rawd_t rawi;  // copy of raw[i]
+  uint8_t igr;  // index of the gain range: 0,1,2
+  sizeb_t icc; // pixel index in the array of calibration constants
+  for (sizeb_t i=0; i<size; ++i) {
     rawi = raw[i];
     igr = rawi >> BSH; if (igr > 1) igr=2; // gain bits gr0/1/2 = 00/01/11. Combination 10 - bad pixel accounted in pixel_status
     icc = igr * size + i;
@@ -81,27 +79,28 @@ time_t calib_jungfrau_v0(const rawd_t *raw, const peds_t *peds, const gain_t *ga
 }
 
 
-void calib_jungrfau_blk_v1(const rawd_t *raw, const cc_t *cc, const size_t& size_blk, out_t *out)
+void calib_jungrfau_blk_v1(const rawd_t *raw, const cc_t *cc, const sizeb_t& size_blk, out_t *out)
 {
   //float cc[size_block][8], where 8 is 4-peds and 4 gains per pixel
   //uint8_t igr; // index of the gain range index gr0/1/2 = 00/01/11, combination 10 - bad pixel status
-  size_t icc;
-  for (size_t i=0; i<size_blk; ++i) {
+  sizeb_t icc;
+  for (sizeb_t i=0; i<size_blk; ++i) {
     icc = 8*i + (raw[i] >> BSH); // index of calibration constants 4*granges for peds and gains
     out[i] = ((raw[i] & MDA) - cc[icc]) * cc[icc+4];
   }
 }
 
-  //void calib_jungrfau_blk_v2(const rawd_t *raw, const ccstruct *cc, const size_t& size_blk, out_t *out)
+  //void calib_jungrfau_blk_v2(const rawd_t *raw, const ccstruct *cc, const sizeb_t& size_blk, out_t *out)
   //{
   //  ccstruct& ccgrpix;
-  //  for (size_t i=0; i<size_blk; ++i) {
+  //  for (sizeb_t i=0; i<size_blk; ++i) {
   //    ccgrpix = cc[i][rawi >> BSH]; // gain range index gr0/1/2 = 00/01/11, combination 10 - bad pixel status
   //    out[i] = (raw[i] & MDA - ccgrpix.pedestal) * ccgrpix.gain;
   //  }
   //}
 
-time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out)
+
+time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out)
 {
   // V1 - assuming that
   // * constants are defined as cc[<number-of-pixels>][8],
@@ -109,11 +108,9 @@ time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const size_t& size, 
   //   cc.shape = (<number-of-pixels-in detector>, <2-for-peds-and-gains>, <4-gain-ranges>) = (npix, 2, 4)
   // * raw and out have letgth of size, where
   //   size = size_blk * (int)number_of_blocks
-
   time_point_t t0 = time_now();
-  size_t ipx0; // the 0-th pixel index of the block in data and other arrays
-
-  for (size_t ib=0; ib<size/size_blk; ++ib) { // loop over blocks in entire data array of size
+  sizeb_t ipx0; // the 0-th pixel index of the block in data and other arrays
+  for (sizeb_t ib=0; ib<size/size_blk; ++ib) { // loop over blocks in entire data array of size
     ipx0 = ib*size_blk;
     calib_jungrfau_blk_v1(&raw[ipx0], &cc[ipx0*8], size_blk, &out[ipx0]);
   }
@@ -121,7 +118,7 @@ time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const size_t& size, 
 }
 
 
-time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out)
+time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out)
 {
   // V2 - assuming that
   // * constants are defined as cc[8][<number-of-pixels>],
@@ -129,11 +126,10 @@ time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const size_t& size, 
   //   cc.shape = (<2-for-peds-and-gains>, <4-gain-ranges>, <number-of-pixels-in detector>) = (2, 4, npix)
   // * raw and out have letgth of size, where
   // size_blk IS NOT USED
-
   time_point_t t0 = time_now();
-  size_t igap = 4*size;
-  size_t icc;
-  for (size_t i=0; i<size; ++i) {
+  sizeb_t igap = 4*size;
+  sizeb_t icc;
+  for (sizeb_t i=0; i<size; ++i) {
     icc = i + size*(raw[i] >> BSH); // index of calibration constants of V2
     out[i] = ((raw[i] & MDA) - cc[icc]) * cc[icc+igap];
   }
@@ -141,7 +137,6 @@ time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const size_t& size, 
 }
 
 
-//time_t calib_jungfrau_v3(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out)
 time_t calib_jungfrau_v3(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out)
 {
   // V3 - assuming that
@@ -152,9 +147,8 @@ time_t calib_jungfrau_v3(const rawd_t *raw, const cc_t *cc, const sizeb_t& size,
   // size_blk IS NOT USED
 
   time_point_t t0 = time_now();
-  //size_t icc;
-  uint32_t icc;
-  uint16_t rawt;
+  sizeb_t icc;
+  rawd_t rawt;
   for (sizeb_t i=0; i<size; ++i) {
     rawt = raw[i];
     icc = 2*(i + size*(rawt >> BSH)); // index of calibration constants of V3
@@ -166,7 +160,7 @@ time_t calib_jungfrau_v3(const rawd_t *raw, const cc_t *cc, const sizeb_t& size,
 }
 
 
-time_t calib_jungfrau_v3_struct(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out)
+time_t calib_jungfrau_v3_struct(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out)
 {
   // V3 - use struct,  assuming that
   // * constants are defined as cc[4][<number-of-pixels>][2] - Rick's shape,
@@ -181,11 +175,10 @@ time_t calib_jungfrau_v3_struct(const rawd_t *raw, const cc_t *cc, const size_t&
   // >  gain=data[i]>>14 & 0x3 # valid gain is 0,1,3.  gain 2 should have pg.gain 0
   // >  datum = data[i] & 0x3fff
   // >  result = (datam-pg[gain][i].pedestal)*pg[gain][i].gain
-
   time_point_t t0 = time_now();
   fill_CCSV3(cc);
   ccstruct *pcc;
-  for (size_t i=0; i<size; ++i) {
+  for (sizeb_t i=0; i<size; ++i) {
     pcc = &CCSV3[raw[i] >> BSH][i];
     out[i] = ((raw[i] & MDA) - pcc->pedestal) * pcc->gain;
   }
@@ -199,7 +192,7 @@ void fill_CCSV3(const cc_t *cc)
   //std::cout << "  counter:" << counter << std::endl;
   if (counter>0) return;
   std::cout << "fill_CCSV3 at 1st entrance only" << std::endl;
-  size_t icc;
+  sizeb_t icc;
   for (int n=0; n<NGRINDS; n++) {
     std::cout << "gain bits combination: " << n << std::endl;
     for (int i=0; i<NPIXELS; i++) {
@@ -212,6 +205,19 @@ void fill_CCSV3(const cc_t *cc)
     }
   }
   std::cout << "fill_CCSV3 initialization of CCSV3 is completed" << std::endl;
+}
+
+
+time_t calib_jungfrau_v4_empty()
+{
+  time_point_t t0 = time_now();
+  return duration_us(time_now() - t0).count();
+}
+
+
+time_t calib_jungfrau_v5_empty(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out)
+{
+  return calib_jungfrau_v4_empty();
 }
 
 
--- psana/psana/pycalgos/UtilsDetector.hh
+++ psana/psana/pycalgos/UtilsDetector.hh
@@ -21,7 +21,6 @@ using namespace std;
 
 namespace utilsdetector {
 
-
 //typedef long unsigned int size_t;
 typedef double   time_t;
 typedef uint16_t rawd_t;
@@ -44,33 +43,29 @@ typedef uint32_t sizeb_t;
   static ccstruct CCSV3 [NGRINDS][NPIXELS];
   void fill_CCSV3(const cc_t *cc);
 
-  void  calib_jungrfau_blk_v1(const rawd_t *raw, const cc_t *cc, const size_t& size_blk, out_t *out);
+  void  calib_jungrfau_blk_v1(const rawd_t *raw, const cc_t *cc, const sizeb_t& size_blk, out_t *out);
 
-  time_t calib_std        (const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, const rawd_t databits, out_t *out);
-  time_t calib_jungfrau_v0(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const size_t& size, out_t *out);
-  time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out);
-  time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out);
+  time_t calib_std(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, const rawd_t databits, out_t *out);
+  time_t calib_jungfrau_v0(const rawd_t *raw, const peds_t *peds, const gain_t *gain, const mask_t *mask, const sizeb_t& size, out_t *out);
+  time_t calib_jungfrau_v1(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out);
+  time_t calib_jungfrau_v2(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out);
   time_t calib_jungfrau_v3(const rawd_t *raw, const cc_t *cc, const sizeb_t& npix, const sizeb_t& size_blk, out_t *out);
-  time_t calib_jungfrau_v3_struct(const rawd_t *raw, const cc_t *cc, const size_t& size, const size_t& size_blk, out_t *out);
+  time_t calib_jungfrau_v3_struct(const rawd_t *raw, const cc_t *cc, const sizeb_t& size, const sizeb_t& size_blk, out_t *out);
+  time_t calib_jungfrau_v4_empty();
+  time_t calib_jungfrau_v5_empty(const rawd_t *raw, const cc_t *cc, const sizeb_t& npix, const sizeb_t& size_blk, out_t *out);
 
   class CalibConsSingleton{
   public:
     static CalibConsSingleton* instance();
     void print();
-
   private:
     CalibConsSingleton();                 // !!!!! Private so that it can not be called from outside
     virtual ~CalibConsSingleton(){};
-
     static CalibConsSingleton* m_pInstance; // !!!!! Singleton instance
-
     // Copy constructor and assignment are disabled by default
     CalibConsSingleton(const CalibConsSingleton&);
     CalibConsSingleton& operator = (const CalibConsSingleton&);
   };
-
-
-
 
 }; // namespace utilsdetector
 
