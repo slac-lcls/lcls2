59979f685b87391e9d3da3a554d15008f9fab8e6
Monarin Uervirojnangkoorn <monarin@gmail.com>
2026-01-29 17:33:38 -0800

    add raise if raw is not found or _calibc_ is None, reset env vars to make run init faster in the test

--- psana/psana/tests/test_calib_prefetch_cache_shmem.py
+++ psana/psana/tests/test_calib_prefetch_cache_shmem.py
@@ -9,8 +9,20 @@ client_count = 3
 calib_prefetch_count = 1
 dgram_count = 64
 
+# Disable shared Jungfrau derived cache and shared geometry cache for this test.
+TEST_ENV_OVERRIDES = {
+    "PS_JF_SHARE_DERIVED": "0",
+    "PS_GEO_SHARE": "0",
+}
+
 # Test-specific xtc2 file for jungfrau detector relative to the test file
-tmp_file_path = "/sdf/data/lcls/drpsrcf/ffb/users/monarin/jungfrau/mfx101332224-r9999-small.xtc2"
+#tmp_file_path = "/sdf/data/lcls/drpsrcf/ffb/users/monarin/jungfrau/mfx101332224-r9999-small.xtc2"
+# The test file above is no longer working (no calibration constants available),
+# so using another jungfrau xtc2 file with available calibration constants.
+# The new file is larger, so the test will go own for a long time...
+# Use pytest -s and when noticed there are several events processed, interrupt the test (Ctrl-C).
+tmp_file_path = "/sdf/data/lcls/ds/mfx/mfx101344525/xtc/mfx101344525-r0125-s009-c000.xtc2"
+
 
 @pytest.mark.skipif(
     sys.platform == 'darwin' or
@@ -26,41 +38,43 @@ class TestCalibPrefetchCacheShmem:
     """
 
     @staticmethod
-    def launch_server(pid):
+    def launch_server(pid, env):
         cmd_args = [
             'shmemServer', '-c', str(client_count + calib_prefetch_count),
             '-n', '10', '-f', tmp_file_path, '-p', f'shmem_test_{pid}',
             '-r', '1', '-L', '1'
         ]
-        return subprocess.Popen(cmd_args)
+        return subprocess.Popen(cmd_args, env=env)
 
-    def launch_supervisor(self, pid, supervisor_ip_addr):
+    def launch_supervisor(self, pid, supervisor_ip_addr, env):
         shmem_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'shmem_client.py')
         cmd_args = ['python', shmem_file, pid, '1', supervisor_ip_addr, '--test-detector-cache']
-        return subprocess.Popen(cmd_args)
+        return subprocess.Popen(cmd_args, env=env)
 
-    def launch_client(self, pid, supervisor_ip_addr):
+    def launch_client(self, pid, supervisor_ip_addr, env):
         shmem_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'shmem_client.py')
         cmd_args = ['python', shmem_file, pid, '0', supervisor_ip_addr, '--test-detector-cache']
-        return subprocess.Popen(cmd_args)
+        return subprocess.Popen(cmd_args, env=env)
 
-    def launch_calib_prefetch(self, pid):
+    def launch_calib_prefetch(self, pid, env):
         cmd_args = [
             'python', '-m', 'psana.pscalib.app.calib_prefetch',
             '--shmem', f'shmem_test_{pid}',
-            '--log-level', 'DEBUG',
+            '--log-level', 'INFO',
             '--detectors', 'jungfrau'
         ]
-        return subprocess.Popen(cmd_args)
+        return subprocess.Popen(cmd_args, env=env)
 
     @pytest.mark.slow
     def test_detector_cache_shmem(self):
         cli = []
         pid = str(os.getpid())
-        srv = self.launch_server(pid)
+        env = os.environ.copy()
+        env.update(TEST_ENV_OVERRIDES)
+        srv = self.launch_server(pid, env)
         assert srv is not None, "server launch failure"
 
-        self.launch_calib_prefetch(pid)
+        self.launch_calib_prefetch(pid, env)
 
         IPAddr = socket.gethostbyname(socket.gethostname())
         sock = socket.socket()
@@ -72,9 +86,9 @@ class TestCalibPrefetchCacheShmem:
         try:
             for i in range(client_count):
                 if i == 0:
-                    cli.append(self.launch_supervisor(pid, supervisor_ip_addr))
+                    cli.append(self.launch_supervisor(pid, supervisor_ip_addr, env))
                 else:
-                    cli.append(self.launch_client(pid, supervisor_ip_addr))
+                    cli.append(self.launch_client(pid, supervisor_ip_addr, env))
                 assert cli[i] is not None, f"client {i} launch failure"
         except:
             srv.kill()
