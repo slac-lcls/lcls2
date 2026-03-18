5175e95ff9fadba2f91e58f30a2a89bb01783bfb
msdubrovin <msdubrovin@gmail.com>
2026-03-18 09:32:08 -0700

    almost working wrapper. --stages=2 does not work, sbatch does not receive environment vars

--- psana/psana/app/jungfrau_dark_proc_wrapper.sh
+++ psana/psana/app/jungfrau_dark_proc_wrapper.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
 ####!/usr/bin/env zsh
 
-# lcls2/psana/psana/app/jungfrau_dark_proc_mpi.sh --skipcmds - SKIP/DO NOT execute commands of this script, just show structure of calls
-# lcls2/psana/psana/app/jungfrau_dark_proc_mpi.sh -S 1 - run stage 1 ONLY !
-# lcls2/psana/psana/app/jungfrau_dark_proc_mpi.sh -S 2 - run stage 2 ONLY !
+# LOCAL PARAMETERS:
+# --skipcmd ### for debugging, show what script is doing, but DO NOT EXECUTE COMMANDS
+# --stages ### ALL-for all, 1-for stage 1 ONLY, 2-...
+# --nranks ### number of mpirun runks, if 1-no mpi
+
+# lcls2/psana/psana/app/jungfrau_dark_proc_wrapper.sh --skipcmd ### SKIP/DO NOT execute commands of this script, just show structure of calls
+# lcls2/psana/psana/app/jungfrau_dark_proc_wrapper.sh -S 1 ### run stage 1 ONLY !
+# lcls2/psana/psana/app/jungfrau_dark_proc_wrapper.sh -S 2 ### run stage 2 ONLY !
+# jungfrau_dark_proc --wrapper --skipcmd -k exp=mfx100848724,run=49 -d jungfrau -o ./work1 --stages 7
 
 
 # Initialize variables with default values
@@ -46,7 +52,7 @@ segind="None"
 igmode="None"
 nranks="1"   #"19" 1-no mpi
 stages="ALL" # expected "1" or "2"
-skipcmds=false # for debudding of this script execute/skip commands
+skipcmd=false # for debudding of this script execute/skip commands
 
 # Loop through all arguments
 while [[ "$#" -gt 0 ]]; do
@@ -187,8 +193,8 @@ while [[ "$#" -gt 0 ]]; do
             stages="$2"
             shift
             ;;
-        --skipcmds)
-            skipcmds=true
+        --skipcmd)
+            skipcmd=true
             shift
             ;;
         *)
@@ -199,8 +205,22 @@ while [[ "$#" -gt 0 ]]; do
     shift # Skip the option argument
 done
 
+istages=$(($stages))
 
-echo "skipcmds=$skipcmds"
+((stage1 = istages & 1)); [[ stage1 -gt 0 ]] && stage1=true || stage1=false
+((stage2 = istages & 2)); [[ stage2 -gt 0 ]] && stage2=true || stage2=false
+((stage3 = istages & 4)); [[ stage3 -gt 0 ]] && stage3=true || stage3=false
+
+echo "--stages $stages"
+echo "stage1:  $stage1"
+echo "stage2:  $stage2"
+echo "stage3:  $stage3"
+echo "skipcmd: $skipcmd"
+
+#exit 0
+
+script_dir=$(dirname "$(realpath "$0")")
+echo "script_dir: $script_dir"
 
 
 c11="jungfrau_dark_proc -k $dskwargs -d $detname -n $nrecs --nrecs1 $nrecs1 -o $dirrepo -L $logmode -E $errskip --stepnum $stepnum --stepmax $stepmax --evskip $evskip"
@@ -210,112 +230,99 @@ c14="--fraclm $fraclm --fraclo $fraclo --frachi $frachi -v $version --datbits $d
 c1="$c11 $c12 $c13 $c14"
 
 #echo $c1
-#cmd00="jungfrau_dark_proc -k exp=mfx100848724,run=49 -d jungfrau -o ./work1"
-cmd00="jungfrau_dark_proc -k $dskwargs -d $detname -o $dirrepo -L $logmode"
-cmd10="$cmd00 --nrecs $nrecs1 --nrecs1 $nrecs1 --events 10000"     #--nrecs 50 --nrecs1 50 --events 50"
+cmnpars="-k $dskwargs -d $detname -o $dirrepo -L $logmode"
+cmd00="jungfrau_dark_proc $cmnpars" ### "jungfrau_dark_proc -k exp=mfx100848724,run=49 -d jungfrau -o ./work1"
+cmd10="$cmd00 --nrecs $nrecs1 --nrecs1 $nrecs1"
 cmd11="$cmd10 --stepnum 0"
 cmd12="$cmd10 --stepnum 1"
 cmd13="$cmd10 --stepnum 2"
 
 
-#cmdmpi="mpirun --mca osc ^ucx -n $nranks"
-#cmdmpi="mpirun -n $nranks"
-
-#cmdmpi=""
-#if [[ "$nranks" != "1" ]]; then
-#    cmdmpi="mpirun -n $nranks"
-#fi
-
-[[ "$nranks" != "1" ]] && cmdmpi="mpirun --mca osc ^ucx -n $nranks" || cmdmpi=""
-
 #nevts=$(($nrecs * 3))
 #echo "evaluate total number of events as $nevts"
 #exit 1
 
-cmd20="$cmdmpi $cmd00 --nrecs $nrecs --nrecs1 0 --events 10000"     #--nrecs 1000 --nrecs1 0 --events 1000"
-cmd21="$cmd20 --stepnum 0"
-cmd22="$cmd20 --stepnum 1"
-cmd23="$cmd20 --stepnum 2"
+#cmdmpi="mpirun --mca osc ^ucx -n $nranks"
+#[[ "$nranks" != "1" ]] && cmdmpi="mpirun --mca osc ^ucx -n $nranks " || cmdmpi=""
+
+#cmd20="$cmdmpi$cmd00 --nrecs $nrecs --nrecs1 0"
+#cmd20="$script_dir/jungfrau_dark_proc.py $cmnpars --nrecs $nrecs --nrecs1 0"
+cmd20="$cmd00 --nrecs $nrecs --nrecs1 0"
+#cmd21="$cmd20 --stepnum 0"
+#cmd22="$cmd20 --stepnum 1"
+#cmd23="$cmd20 --stepnum 2"
+
+cmd30="jungfrau_deploy_constants $cmnpars -F" # -S -D
 
 if [[ "$logmode" == "DEBUG" ]]; then
   echo "COMMAND LINES"
   echo "common : $cmd00"
   echo "stage 1: $cmd10"
   echo "stage 2: $cmd20"
+  echo "stage 3: $cmd30"
 fi
 
 t0_sec=$SECONDS
 
 #time {
 
-if [[ "$stages" == "1" || "$stages" == "ALL" ]]; then
 
-echo
-echo
-echo "BEGIN STAGE 1 - evaluate intensity gates for $fraclo and $frachi part of statistics on $nrecs1 events"
+if $stage1; then
+  echo
+  echo "=== STAGE 1 - evaluate intensity gates for $fraclo and $frachi part of statistics on $nrecs1 events"
 
-if [[ "$skipcmds" == false ]]; then nohup $cmd11 >/dev/null 2>&1 & fi
-pid11=$!
-echo "started PID $pid11 for command: $cmd11"
+  if [[ "$skipcmd" == false ]]; then nohup $cmd11 >/dev/null 2>&1 & fi
+  pid11=$!
+  echo "started PID $pid11 for command: $cmd11"
 
-if [[ "$skipcmds" == false ]]; then nohup $cmd12 >/dev/null 2>&1 & fi
-pid12=$!
-echo "started PID $pid12 for command: $cmd12"
+  if [[ "$skipcmd" == false ]]; then nohup $cmd12 >/dev/null 2>&1 & fi
+  pid12=$!
+  echo "started PID $pid12 for command: $cmd12"
 
-echo "run command: $cmd13"
-if [[ "$skipcmds" == false ]]; then $cmd13; fi
+  echo "run command: $cmd13"
+  if [[ "$skipcmd" == false ]]; then $cmd13; fi
 
-for p in {$pid11,$pid12}; do
-  if ps -p $p > /dev/null; then
-    echo "process $p is running"
-  else
-    echo "process $p is not running"
-  fi
-done
-
-dt_sec=$((SECONDS - t0_sec))
-echo "STAGE 1 IS COMPLETED time for three steps: $dt_sec sec"
-
-fi ### "$stages" == "1" || "$stages" == "ALL"
+  for p in {$pid11,$pid12}; do
+    if ps -p $p > /dev/null; then
+      echo "process $p is running"
+    else
+      echo "process $p is not running"
+    fi
+  done
+  dt_sec=$((SECONDS - t0_sec))
+  echo "STAGE 1 IS COMPLETED time for three steps: $dt_sec sec"
+fi ### $stage1
 
 
-echo
-echo "CHECK THAT ALL 6 ARRAYS WITH GATE LIMITS ARE AVAILABLE for -d $detname and -k $dskwargs"
-cmddir="ls -ltr $dirrepo/jungfrau/block_results/"
-echo $cmddir
-$cmddir
-echo
-
-if [[ "$stages" == "1" ]]; then
-  echo "EXIT AFTER STAGE 1    due to -S|--stages $stages"
-  exit 0
-elif [[ "$stages" == "2" ]]; then
-  echo "RUN STAGE 2 ONLY   due to -S|--stages $stages"
-fi
+if $stage1 || $stage2; then
+  echo
+  echo "CHECK THAT ALL 6 ARRAYS WITH GATE LIMITS ARE AVAILABLE for -d $detname and -k $dskwargs"
+  cmddir="ls -ltr $dirrepo/jungfrau/block_results/"
+  echo $cmddir
+  $cmddir
+  echo
+fi ### $stage1 || $stage2
 
 
-echo
-echo "BEGIN STAGE 2 - evaluate gated average, rms, min, max, pixel status on $nrecs events"
+if $stage2; then
+  #echo
+  echo "=== STAGE 2 - evaluate gated average, rms, min, max, pixel status on $nrecs events"
+  #### Test: sbatch --ntasks-per-node 19 jungfrau_dark_proc_sbatch.sh "jungfrau_dark_proc -k exp=mfx100848724,run=49 -d jungfrau -o ./work1 --nrecs 100 --nrecs1 0"
+  #### Ex: sbatch --wait --ntasks-per-node 19 jungfrau_dark_proc_sbatch.sh "jungfrau_dark_proc -k exp=mfx100848724,run=49 -d jungfrau -o ./work1 --nrecs 1000 --nrecs1 0"
+  #cmd_sbatch="sbatch --wait --ntasks-per-node $nranks $script_dir/jungfrau_dark_proc_sbatch.sh \"$cmd20\""
+  cmd_sbatch="sbatch --export=ALL --ntasks-per-node $nranks $script_dir/jungfrau_dark_proc_sbatch.sh \"$cmd20\""
+  echo "command for sbatch: $cmd20"
+  echo "cmd_sbatch: $cmd_sbatch"
+  if [[ "$skipcmd" == false ]]; then $cmd_sbatch; fi
+fi # $stage2
 
-# === DETACHED
-#nohup $cmd21 >/dev/null 2>&1 &
-#pid21=$!
-#echo
-#echo "started PID $pid21 for command: $cmd21"
 
-#nohup $cmd22 >/dev/null 2>&1 &
-#pid22=$!
-#echo "started PID $pid22 for command: $cmd22"
-# ===
+if $stage3; then
+  echo
+  echo "=== STAGE 3 - deploy calibration constants"
+  echo "run command: $cmd30"
+fi # $stage3
 
-echo "run command: $cmd21"
-if [[ "$skipcmds" == false ]]; then $cmd21; fi
-
-echo "run command: $cmd22"
-if [[ "$skipcmds" == false ]]; then $cmd22; fi
-
-echo "run command: $cmd23"
-if [[ "$skipcmds" == false ]]; then $cmd23; fi
 
 dt_sec=$((SECONDS - t0_sec))
 echo
