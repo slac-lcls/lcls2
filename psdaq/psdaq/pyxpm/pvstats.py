cd30555290b9967fd7b6641ae062b9c2fbc0723b
Matt Weaver <weaver@slac.stanford.edu>
2026-03-19 08:34:40 -0700

    Add xtpg resynch counts and 35.5 kHz rate marker

--- psdaq/psdaq/pyxpm/pvstats.py
+++ psdaq/psdaq/pyxpm/pvstats.py
@@ -319,6 +319,7 @@ class CuStatus(object):
                     pv.post(value)
 
         updatePv(self._pv_fiducialErr , self._device.cuFiducialIntvErr.get())
+        updatePv(self._pv_fiducialSyncs, self._device.cuFiducialResyncCount.get())
 
 class NoCuStatus(object):
     def __init__(self, name):
@@ -549,11 +550,10 @@ class PathTimer(object):
 
     def update(self, pv, val):
         timev = divmod(float(time.time_ns()), 1.0e9)
-        latch = self._pathTimer.latched.get()
+        updatePv(self._pv_latched, self._pathTimer.latched.get(), timev)
         pathtm = [0 for i in range(14)]
         for i in range(14):
             pathtm[i] = self._pathTimer.chan[i].get()
-        updatePv(self._pv_latched, latch, timev)
         updatePv(self._pv_array, pathtm, timev)
 
 class PVStats(object):
@@ -566,7 +566,6 @@ class PVStats(object):
         global fidPeriod
         fidPeriod  = fiducialPeriod
 
-        self._name = name
         self._xpm  = xpm
         self._app  = xpm.XpmApp
 
@@ -604,7 +603,7 @@ class PVStats(object):
         else:
             self._sfpStat  = QSFPStatus  (name+':QSFPSTATUS',self._xpm)
 
-        self._pathTimer = [PathTimer(self._name, self._xpm, i) for i in range(8)]
+        self._pathTimer = [PathTimer(name, xpm, i) for i in range(8)]
 
 #        self._mmcm = []
 #        for i,m in enumerate(xpm.mmcms):
