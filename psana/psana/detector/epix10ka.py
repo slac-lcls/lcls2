3a54de1dadc01db78fcb0305882353dfca7c2baf
msdubrovin <msdubrovin@gmail.com>
2026-04-21 08:38:53 -0700

    add raw and raw_v1 for tentative re-implementation

--- psana/psana/detector/epix10ka.py
+++ psana/psana/detector/epix10ka.py
@@ -5,6 +5,9 @@ import numpy as np
 from amitypes import Array2d, Array3d
 import psana.detector.epix_base as eb
 from psana.detector.detector_impl import DetectorImpl
+import psana.detector.Utils as ut # info_dict, is_true, is_none
+from psana.detector.NDArrUtils import info_ndarr
+
 import logging
 logger = logging.getLogger(__name__)
 
@@ -38,6 +41,54 @@ class epix10ka_raw_2_0_1(eb.epix_base):
     def _cbits_config_segment(self, cob):
         """cob=det.raw._seg_configs()[<seg-ind>].config - segment configuration object"""
         return eb.cbits_config_epix10ka(cob, shape=(352, 384))
+
+    
+    def raw(self, evt, copy=True) -> Array3d:
+        return eb.epix_base.raw(self, evt, copy=copy)
+        #return self.raw_v1(evt, copy=copy)
+
+
+    def raw_v1(self, evt, copy=True) -> Array3d:
+        """reshafle ASICs"""
+
+        print('XXX RE-IMPLEMENTED raw')
+
+        if evt is None: return None
+        if ut.is_true(evt is None, 'evt is None'): return None
+
+        segs = self._segments(evt)    # dict = {seg_index: seg_obj}
+        if ut.is_true(segs is None, 'self._segments(evt) is None'): return None
+
+        if len(segs) == 1:
+            ind = self._segment_numbers[0]
+            return segs[ind].raw
+
+        #print('XXX self._segment_numbers', self._segment_numbers)
+        first_seg = segs[self._segment_numbers[0]].raw
+        dtype = first_seg.dtype
+        shape = (len(self._segment_numbers),) + first_seg.shape
+        if self._raw_buf is None or self._raw_shape != shape or self._raw_dtype != dtype:
+            self._raw_buf = np.empty(shape, dtype=dtype)
+            self._raw_shape = shape
+            self._raw_dtype = dtype
+            self._shape_seg = first_seg.shape
+            self._shape_asic = [int(i/2) for i in self._shape_seg]
+            #print('XXX shape_asic:', self._shape_asic)
+
+        for idx, seg_id in enumerate(self._segment_numbers):
+            np.copyto(self._raw_buf[idx], segs[seg_id].raw, casting='no')
+            #print(info_ndarr(segs[seg_id].raw, f'segs[{seg_id}].raw'))
+            seg = segs[seg_id].raw
+            r,c = self._shape_asic
+            asics = seg[:r,:c], seg[r:,:c], seg[r:,c:], seg[:r,c:]
+            seg1 = np.vstack((np.hstack((np.flipud(np.fliplr(asics[2])),
+                                    np.flipud(np.fliplr(asics[1])))),
+                                    np.hstack((asics[3],asics[0]))))
+            print(info_ndarr(seg1, 'reshaffled seg1'))
+            np.copyto(self._raw_buf[idx], seg1, casting='no')
+
+        arr = eb.reshape_to_3d(self._raw_buf)
+        return arr.copy() if copy else arr
 
 
 # calib is the same as in epix_base
