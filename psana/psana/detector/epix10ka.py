c97ee89457190a4ee86f9808e0b87f2a1604dc33
msdubrovin <msdubrovin@gmail.com>
2026-05-02 05:13:07 -0700

    add class epix10ka_raw_3_0_1, class epix10ka_config_3_0_0, for test add raw and calib to class epix10ka_raw_2_0_1

--- psana/psana/detector/epix10ka.py
+++ psana/psana/detector/epix10ka.py
@@ -17,6 +17,12 @@ class epix10kaquad_config_2_0_0(DetectorImpl):
     def __init__(self, *args, **kwargs):
         super(epix10kaquad_config_2_0_0, self).__init__(*args)
 
+class epix10ka_config_3_0_0(DetectorImpl):
+    def __init__(self, *args, **kwargs):
+        logger.debug('XXX epix10ka_config_3_0_0')
+        super().__init__(*args)
+        #super(epix10kaquad_config_2_0_0, self).__init__(*args)
+
 
 class epix10k_raw_0_0_1(eb.epix_base):
     def __init__(self, *args, **kwargs):
@@ -43,15 +49,16 @@ class epix10ka_raw_2_0_1(eb.epix_base):
         return eb.cbits_config_epix10ka(cob, shape=(352, 384))
 
 
-#    def raw(self, evt, copy=True) -> Array3d:
-#        """TEST overrides lcls2/psana/psana/detector/areadetector.py AreaDetectorRaw.raw"""
-#        print('XXX epix10ka_raw_2_0_1.raw')
-#        return eb.epix_base.raw(self, evt, copy=copy)
+    def raw(self, evt, copy=True) -> Array3d:
+        """TEST overrides lcls2/psana/psana/detector/areadetector.py AreaDetectorRaw.raw"""
+        logger.debug('XXX epix10ka_raw_2_0_1.raw')
+        return eb.epix_base.raw(self, evt, copy=copy)
 
 
-#    def calib(self, evt, **kwa) -> Array3d:
-#        """TEST overrides lcls2/psana/psana/detector/epix_base.py epix_base.calib"""
-#        print('XXX epix10ka_raw_2_0_1.calib')
+    def calib(self, evt, **kwa) -> Array3d:
+        """TEST overrides lcls2/psana/psana/detector/epix_base.py epix_base.calib"""
+        logger.debug('XXX epix10ka_raw_2_0_1.calib')
+        return eb.epix_base.calib(self, evt, **kwa)(self, evt, **kwa)
 #        return eb.ue.calib_epix10ka_v02(self, evt, **kwa)
 #        #return eb.ue.calib_epix10ka_v02(self, evt, nda_raw=self.raw(evt), **kwa)
 
@@ -89,20 +96,30 @@ class epix10ka_raw_2_0_1(eb.epix_base):
                           np.hstack((segs[1].raw, segs[0].raw)))) & 0x3fff
 
 
-class epix10ka_raw_3_0_0(epix10ka_raw_2_0_1):
+class epix10ka_raw_3_0_1(epix10ka_raw_2_0_1):
     def __init__(self, *args, **kwargs):
-        logger.debug('epix10ka_raw_2_0_1.__init__')
+        logger.debug('epix10ka_raw_3_0_1.__init__')
         epix10ka_raw_2_0_1.__init__(self, *args, **kwargs)
+
+#    def _cbits_config_segment(self, cob):
+#        """cob=det.raw._seg_configs()[<seg-ind>].config - segment configuration object, where self=det.raw"""
+#        return eb.cbits_config_epix10ka(cob, shape=(352, 384))
 
     def _cbits_config_segment(self, cob):
         """cob=det.raw._seg_configs()[<seg-ind>].config - segment configuration object, where self=det.raw"""
-        print(info_ndarr(cob.asicPixelConfig, 'XXX epix10ka_raw_3_0_0._cbits_config_segment'))
+        print('XXX _cbits_config_segment dir(cob):', dir(cob))
+        print(info_ndarr(cob.asicPixelConfig, 'XXX epix10ka_raw_3_0_1._cbits_config_segment'))
         return cob.asicPixelConfig # expected panel shape:(352, 384) dtype:uint8
         #return eb.cbits_config_epix10ka_v02(cob) # shape=(352, 384)
 
+    def raw(self, evt, copy=True) -> Array3d:
+        """TEST overrides lcls2/psana/psana/detector/areadetector.py AreaDetectorRaw.raw"""
+        logger.debug('XXX epix10ka_raw_3_0_1.raw')
+        return eb.epix_base.raw(self, evt, copy=copy)
+
     def calib(self, evt, **kwa) -> Array3d:
         """TEST overrides lcls2/psana/psana/detector/epix_base.py epix_base.calib"""
-        print('XXX epix10ka_raw_2_0_1.calib')
+        print('XXX epix10ka_raw_3_0_1.calib')
         return eb.ue.calib_epix10ka_v02(self, evt, **kwa)
         #return eb.ue.calib_epix10ka_v02(self, evt, nda_raw=self.raw(evt), **kwa)
 
