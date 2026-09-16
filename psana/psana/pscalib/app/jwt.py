8154847dcbeb1b9f2c55767f157e806da69af501
msdubrovin <msdubrovin@gmail.com>
2026-09-16 15:42:48 -0700

    tune output for command jwt

--- psana/psana/pscalib/app/jwt.py
+++ psana/psana/pscalib/app/jwt.py
@@ -3,7 +3,7 @@ import sys
 import psana.pscalib.calib.MDBWebUtils as wu
 
 def do_main():
-    print('command jwt' + wu.info_ticket)
+    print(wu.info_ticket)
     sys.exit(0)
 
 if __name__ == "__main__":
